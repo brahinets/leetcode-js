673204d7b244b72f9546f348bb2663241b7f8e31
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-02-16 22:02:25 +0200

    #104 Add model definition

--- 104. Maximum Depth of Binary Tree/script.js
+++ 104. Maximum Depth of Binary Tree/script.js
@@ -33,3 +33,10 @@ var maxDepth = function (root) {
     return Math.max(rightDepth, leftDepth);
 };
 
+class TreeNode {
+    constructor(val, left, right) {
+        this.val = (val === undefined ? 0 : val)
+        this.left = (left === undefined ? null : left)
+        this.right = (right === undefined ? null : right)
+    }
+}
