759ab80a74710d09e1d0e1472e0aa2470edf4bf9
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-02-20 21:09:40 +0200

    #35 Optimize using bi-section for search

--- 35. Search Insert Position/script.js
+++ 35. Search Insert Position/script.js
@@ -3,11 +3,21 @@
  * @param {number} target
  * @return {number}
  */
-const searchInsert = function(nums, target) {
-    let i = 0;
-    while (i < nums.length && nums[i] < target) {
-        i++;
+const searchInsert = function (nums, target) {
+    let start = 0;
+    let end = nums.length - 1;
+
+    while (start <= end) {
+        let mid = Math.floor((end + start) / 2);
+
+        if (nums[mid] === target) {
+            return mid;
+        } else if (nums[mid] < target) {
+            start = mid + 1;
+        } else {
+            end = mid - 1;
+        }
     }
 
-    return i;
+    return start;
 };
