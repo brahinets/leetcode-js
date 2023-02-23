b1bce0ff7b71e4ae86a4ea80c92a9f8774e9a0a1
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-02-23 22:04:11 +0200

    #13 Use more math-like solution, replace mapping onto addition and subtraction

--- 13. Roman to Integer/script.js
+++ 13. Roman to Integer/script.js
@@ -1,13 +1,4 @@
-const SPECIAL_ROMAN = {
-    "IV": 4,
-    "IX": 9,
-    "XL": 40,
-    "XC": 90,
-    "CD": 400,
-    "CM": 900,
-}
-
-const REGULAR_ROMAN = {
+const ROMAN = {
     "I": 1,
     "V": 5,
     "X": 10,
@@ -21,20 +12,15 @@ const REGULAR_ROMAN = {
  * @param {string} s
  * @return {number}
  */
-var romanToInt = function (roman) {
+const romanToInt = function (roman) {
     let result = 0;
 
     for (let i = 0; i < roman.length; i++) {
-        let val = 0;
-
-        if (i < roman.length - 1 && SPECIAL_ROMAN[roman[i] + roman[i + 1]]) {
-            val += SPECIAL_ROMAN[roman[i] + roman[i + 1]];
-            i += 1;
+        if ((i < roman.length - 1) && ROMAN[roman[i]] < ROMAN[roman[i + 1]]) {
+            result -= ROMAN[roman[i]];
         } else {
-            val += REGULAR_ROMAN[roman[i]];
+            result += ROMAN[roman[i]];
         }
-
-        result += val;
     }
 
     return result;
