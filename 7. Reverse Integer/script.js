65723b2146a6419e2119f0ec3b2a294ec6a0702b
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-02-27 21:48:39 +0200

    #7 Remove useless trailing zeros cleanup, are skipped automatically during number conversion

--- 7. Reverse Integer/script.js
+++ 7. Reverse Integer/script.js
@@ -5,12 +5,7 @@
 var reverse = function (x) {
     const signMultiplier = x < 0 ? -1 : 1;
 
-    const val = String(Math.abs(x)).split("").reverse();
-    while (val && val[0] === '0') {
-        val.shift();
-    }
-
-    const reversedNumber = signMultiplier * Number(val.join(''));
+    const reversedNumber = Number(String(Math.abs(x)).split('').reverse().join('')) * signMultiplier;
 
     if (reversedNumber < (Math.pow(2, 31) * -1) || reversedNumber > Math.pow(2, 31) - 1) {
         return 0;
