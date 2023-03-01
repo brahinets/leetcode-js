7912d243a213088dd90ead0de9672246946342cc
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-03-01 21:54:55 +0200

    #912 Follow requirements and eliminate built-in functions

--- 912. Sort an Array/script.js
+++ 912. Sort an Array/script.js
@@ -2,6 +2,32 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-var sortArray = function(nums) {
-    return nums.sort((a, b) => a - b);
+var sortArray = function (nums) {
+    return mergeSort(nums);
 };
+
+function mergeSort(arr) {
+    if (arr.length <= 1) {
+        return arr;
+    }
+
+    const mid = Math.floor(arr.length / 2)
+    const left = mergeSort(arr.slice(0, mid))
+    const right = mergeSort(arr.slice(mid))
+
+    return merge(left, right)
+}
+
+function merge(left, right) {
+    const sortedArr = [];
+
+    while (left.length && right.length) {
+        if (left[0] < right[0]) {
+            sortedArr.push(left.shift())
+        } else {
+            sortedArr.push(right.shift())
+        }
+    }
+
+    return [...sortedArr, ...left, ...right]
+}
