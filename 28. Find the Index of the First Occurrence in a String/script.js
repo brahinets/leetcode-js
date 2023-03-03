7e18f853665d08b34e304a7f18377d0dba286892
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-03-03 20:03:17 +0200

    #28 Use less js magic, implement algorithm

--- 28. Find the Index of the First Occurrence in a String/script.js
+++ 28. Find the Index of the First Occurrence in a String/script.js
@@ -4,5 +4,15 @@
  * @return {number}
  */
 const strStr = function (haystack, needle) {
-    return haystack.indexOf(needle);
+    let i = 0;
+
+    while (i <= haystack.length - needle.length) {
+        if (haystack.substring(i, i + needle.length) === needle) {
+            return i;
+        }
+
+        i++;
+    }
+
+    return -1;
 };
