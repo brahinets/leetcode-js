16e451eaebabde1766a82dab9baf112024d0ffa4
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-03-07 23:06:07 +0200

    #2187 Optimize, use binary steps instead of sequential

--- 2187. Minimum Time to Complete Trips/script.ts
+++ 2187. Minimum Time to Complete Trips/script.ts
@@ -1,13 +1,19 @@
 const minimumTime = (time: number[], totalTrips: number): number => {
-    let enough: boolean = false;
-    let count: number = 0;
+    let left: number = 1;
+    let right: number = totalTrips * time
+        .reduce((prev: number, curr: number) => prev > curr ? prev : curr, time[0]);
 
-    while (!enough) {
-        count++;
-        let tripsForTime = time.map(e => Math.floor(count / e))
+    while (left < right) {
+        const mid: number = Math.floor((left + right) / 2);
+
+        let tripsForTime = time.map(e => Math.floor(mid / e))
             .reduce((s, e) => s + e, 0);
-        enough = tripsForTime >= totalTrips;
+        if (tripsForTime >= totalTrips) {
+            right = mid;
+        } else {
+            left = mid + 1;
+        }
     }
 
-    return count;
+    return left;
 };
