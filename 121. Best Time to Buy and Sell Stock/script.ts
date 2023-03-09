0c40e6153ad98f7ab662c564d3ce781bf33e5328
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-03-09 22:04:33 +0200

    121. Migrate to TypeScript

---
+++ 121. Best Time to Buy and Sell Stock/script.ts
@@ -0,0 +1,17 @@
+const maxProfit = function (prices: number[]): number {
+    let minPrice: number = prices[0];
+    let maxProfit: number = 0;
+
+    for (let i: number = 1; i < prices.length; i++) {
+        if (prices[i] < minPrice) {
+            minPrice = prices[i];
+        } else {
+            const profit: number = prices[i] - minPrice;
+            if (profit > maxProfit) {
+                maxProfit = profit;
+            }
+        }
+    }
+
+    return maxProfit;
+};
