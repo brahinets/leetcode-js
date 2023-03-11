beb3bea3bf1de2f50aeae4396c1eb6b32522ddba
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-03-11 22:03:51 +0200

    #6 Migrate to Type Script

---
+++ 6. Zigzag Conversion/script.ts
@@ -0,0 +1,34 @@
+const convert = function (s: string, numRows: number): string {
+    const matrix: string[][] = [];
+    for (let i: number = 0; i < numRows; i++) {
+        const row: string[] = [];
+        matrix.push(row);
+    }
+
+    const chars: string[] = s.split('');
+    let toBottom: boolean = true;
+
+    while (chars.length > 0) {
+        if (toBottom) {
+            for (let i: number = 0; i < numRows && chars.length > 0; i++) {
+                let char: string = chars.shift() || '';
+                matrix[i].push(char);
+            }
+        } else {
+            for (let i: number = numRows - 1; i >= 0 && chars.length > 0; i--) {
+                for (let j: number = 1; j < numRows - 1 && chars.length > 0; j++) {
+                    if (i + j + 1 === numRows) {
+                        let char: string = chars.shift() || '';
+                        matrix[i].push(char);
+                    } else {
+                        matrix[i].push("");
+                    }
+                }
+            }
+        }
+
+        toBottom = !toBottom;
+    }
+
+    return matrix.reduce((str: string, row: string[]) => str + row.join(""), "");
+};
