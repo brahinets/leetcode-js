2b917be392928d55162b712fc59b2421cf8d75be
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-03-13 21:40:33 +0200

    #101 Symmetric Tree

---
+++ 101. Symmetric Tree/script.ts
@@ -0,0 +1,21 @@
+import {TreeNode} from '../common/TreeNode'
+
+const isSymmetric = (root: TreeNode | null): boolean => {
+    if (!root) {
+        return true;
+    }
+
+    return isSymmetricNode(root.left, root.right);
+}
+
+const isSymmetricNode = (left: TreeNode | null, right: TreeNode | null): boolean => {
+    if (!left && !right) {
+        return true;
+    }
+
+    if (!left || !right || left.val != right.val) {
+        return false;
+    }
+
+    return isSymmetricNode(left.left, right.right) && isSymmetricNode(left.right, right.left);
+}
