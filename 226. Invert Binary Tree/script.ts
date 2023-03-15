8c70576f721d97b2bd633f04188ba2d88731418c
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-03-15 22:24:18 +0200

    #226 Improve type safety

---
+++ 226. Invert Binary Tree/script.ts
@@ -0,0 +1,22 @@
+import {TreeNode} from "../common/TreeNode";
+
+const invertTree = function (root: TreeNode | null): TreeNode | null {
+    if (!root) {
+        return null;
+    }
+
+    let newRight: TreeNode | null = null;
+    if (root.left) {
+        newRight = invertTree(root.left);
+    }
+
+    let newLeft: TreeNode | null = null;
+    if (root.right) {
+        newLeft = invertTree(root.right);
+    }
+
+    root.left = newLeft;
+    root.right = newRight;
+
+    return root;
+}
