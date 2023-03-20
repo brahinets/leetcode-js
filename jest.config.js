acfbe955f4f3364877796caa9d43814d408f4e84
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-03-20 21:15:25 +0200

    Add Jest unit tests support

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    roots: ['<rootDir>'],
+    testMatch: ['**/__tests__/**/*.+(ts)', '**/?(*.)+(test).+(ts)'],
+    transform: {
+        '^.+\\.(ts|tsx)$': 'ts-jest',
+    },
+};
