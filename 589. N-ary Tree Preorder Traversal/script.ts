823ab333880d0148d964e69d19be8563e7f76151
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-03-23 08:53:09 +0200

    #589 Improve null-safety

--- 589. N-ary Tree Preorder Traversal/script.ts
+++ 589. N-ary Tree Preorder Traversal/script.ts
@@ -2,7 +2,11 @@ import {Node} from '../common/Node'
 
 export {preorder, Node}
 
-function preorder(root: Node): number[] {
+function preorder(root: Node | null): number[] {
+    if (!root) {
+        return [];
+    }
+
     const result: number[] = [root.val];
 
     for (let child of root.children) {
