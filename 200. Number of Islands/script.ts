6f4de6ba7e8795f7f13851b5ac7fd2dd72a8f26e
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-03-26 23:21:09 +0300

    #200 Speed-up via mutating original data

--- 200. Number of Islands/script.ts
+++ 200. Number of Islands/script.ts
@@ -1,9 +1,12 @@
 export {numIslands}
 
-function findIslandCoordinates(grid: string[][]): number[] {
-    for (let i: number = 0; i < grid.length; i++) {
-        for (let j: number = 0; j < grid[0].length; j++) {
-            if (grid[i][j] === '1') {
+const ISLAND: string = '1';
+const VISITED: string = '2';
+
+function findIslandCoordinates(sea: string[][]): number[] {
+    for (let i: number = 0; i < sea.length; i++) {
+        for (let j: number = 0; j < sea[0].length; j++) {
+            if (sea[i][j] === ISLAND) {
                 return [i, j]
             }
         }
@@ -13,15 +16,17 @@ function findIslandCoordinates(grid: string[][]): number[] {
 }
 
 function numIslands(grid: string[][]): number {
+    const sea: string[][] = JSON.parse(JSON.stringify(grid));
+
     let islandsCount: number = 0;
     let hasIsland: boolean;
 
     do {
-        const [islandCoordinateX, islandCoordinateY]: number[] = findIslandCoordinates(grid);
+        const [islandCoordinateX, islandCoordinateY]: number[] = findIslandCoordinates(sea);
 
         hasIsland = islandCoordinateX !== -1 && islandCoordinateY !== -1;
         if (hasIsland) {
-            grid = visitIsland(grid, islandCoordinateX, islandCoordinateY);
+            visitIsland(sea, islandCoordinateX, islandCoordinateY);
             islandsCount++;
         }
     } while (hasIsland)
@@ -29,21 +34,18 @@ function numIslands(grid: string[][]): number {
     return islandsCount;
 }
 
-function visitIsland(grid: string[][], islandCoordinateX: number, islandCoordinateY: number): string[][] {
-    if (islandCoordinateX < 0 || islandCoordinateY < 0 || islandCoordinateX >= grid.length || islandCoordinateY >= grid[0].length) {
-        return grid;
+
+function visitIsland(sea: string[][], islandCoordinateX: number, islandCoordinateY: number): void {
+    if (islandCoordinateX < 0 || islandCoordinateY < 0 || islandCoordinateX >= sea.length || islandCoordinateY >= sea[0].length) {
+        return;
     }
 
-    let gridCopy: string[][] = JSON.parse(JSON.stringify(grid));
+    if (ISLAND === sea[islandCoordinateX][islandCoordinateY]) {
+        sea[islandCoordinateX][islandCoordinateY] = VISITED;
 
-    if ('1' === gridCopy[islandCoordinateX][islandCoordinateY]) {
-        gridCopy[islandCoordinateX][islandCoordinateY] = '0';
-
-        gridCopy = visitIsland(gridCopy, islandCoordinateX - 1, islandCoordinateY);
-        gridCopy = visitIsland(gridCopy, islandCoordinateX, islandCoordinateY - 1);
-        gridCopy = visitIsland(gridCopy, islandCoordinateX + 1, islandCoordinateY);
-        gridCopy = visitIsland(gridCopy, islandCoordinateX, islandCoordinateY + 1);
+        visitIsland(sea, islandCoordinateX - 1, islandCoordinateY);
+        visitIsland(sea, islandCoordinateX, islandCoordinateY - 1);
+        visitIsland(sea, islandCoordinateX + 1, islandCoordinateY);
+        visitIsland(sea, islandCoordinateX, islandCoordinateY + 1);
     }
-
-    return gridCopy;
 }
