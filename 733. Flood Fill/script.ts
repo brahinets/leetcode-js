6a5ce022b3d87c7ab2b2fa77e14a0d91cd49ade6
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-03-26 12:25:17 +0300

    #733 Simplify readability combine boundaries conditions

--- 733. Flood Fill/script.ts
+++ 733. Flood Fill/script.ts
@@ -11,26 +11,19 @@ function floodFill(image: number[][], sr: number, sc: number, color: number): nu
 }
 
 function floodFills(image: number[][], sr: number, sc: number, newColor: number, startColor: number): number[][] {
+    if (sr < 0 || sc < 0 || sr >= image.length || sc >= image[0].length) {
+        return image;
+    }
+
     let imageCopy: number[][] = JSON.parse(JSON.stringify(image));
 
     if (startColor === imageCopy[sr][sc]) {
         imageCopy[sr][sc] = newColor;
 
-        if (sr > 0) {
-            imageCopy = floodFills(imageCopy, sr - 1, sc, newColor, startColor);
-        }
-
-        if (sc > 0) {
-            imageCopy = floodFills(imageCopy, sr, sc - 1, newColor, startColor);
-        }
-
-        if (sr < imageCopy.length - 1) {
-            imageCopy = floodFills(imageCopy, sr + 1, sc, newColor, startColor);
-        }
-
-        if (sc < imageCopy[0].length - 1) {
-            imageCopy = floodFills(imageCopy, sr, sc + 1, newColor, startColor);
-        }
+        imageCopy = floodFills(imageCopy, sr - 1, sc, newColor, startColor);
+        imageCopy = floodFills(imageCopy, sr, sc - 1, newColor, startColor);
+        imageCopy = floodFills(imageCopy, sr + 1, sc, newColor, startColor);
+        imageCopy = floodFills(imageCopy, sr, sc + 1, newColor, startColor);
     }
 
     return imageCopy;
