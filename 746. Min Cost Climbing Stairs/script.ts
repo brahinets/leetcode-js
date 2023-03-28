b02549a99698a6ccdd5df6ccc1d51d9e8357da9b
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-03-28 23:15:34 +0300

    #746 Speed-up using memoization

--- 746. Min Cost Climbing Stairs/script.ts
+++ 746. Min Cost Climbing Stairs/script.ts
@@ -1,16 +1,23 @@
 export {minCostClimbingStairs}
 
 function minCostClimbingStairs(cost: number[]): number {
-    return minCostClimbingToStair(cost, cost.length);
+    return minCostClimbingToStair(cost, cost.length, new Map<number, number>());
 }
 
-function minCostClimbingToStair(cost: number[], stair: number): number {
+function minCostClimbingToStair(cost: number[], stair: number, memo: Map<number, number>): number {
     if (stair <= 1) {
         return Math.min(0, cost[0]);
     }
 
-    const shortStep: number = minCostClimbingToStair(cost, stair - 1) + cost[stair - 1];
-    const bigStep: number = minCostClimbingToStair(cost, stair - 2) + cost[stair - 2];
+    let costToStair: number | undefined = memo.get(stair);
+    if (costToStair !== undefined) {
+        return costToStair;
+    }
 
-    return Math.min(shortStep, bigStep);
+    const shortStep: number = minCostClimbingToStair(cost, stair - 1, memo) + cost[stair - 1];
+    const bigStep: number = minCostClimbingToStair(cost, stair - 2, memo) + cost[stair - 2];
+    costToStair = Math.min(shortStep, bigStep);
+    memo.set(stair, costToStair);
+
+    return costToStair;
 }
