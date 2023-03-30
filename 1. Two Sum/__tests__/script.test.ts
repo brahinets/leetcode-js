bfaa1022f6042279f76c9f3057c899a8522573f2
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-03-30 19:17:05 +0300

    #1 Fix test suite name

--- 1. Two Sum/__tests__/script.test.ts
+++ 1. Two Sum/__tests__/script.test.ts
@@ -1,17 +1,17 @@
 import {twoSum} from '../script'
 
-describe('21. Merge Two Sorted Lists', (): void => {
+describe('1. Two Sum', (): void => {
     it('One', (): void => {
         expect(twoSum([2, 7, 11, 15], 9))
             .toStrictEqual([0, 1]);
     });
 
-    it('One', (): void => {
+    it('Two', (): void => {
         expect(twoSum([3, 2, 4], 6))
             .toStrictEqual([1, 2]);
     });
 
-    it('One', (): void => {
+    it('Three', (): void => {
         expect(twoSum([3, 3], 6))
             .toStrictEqual([0, 1]);
     });
