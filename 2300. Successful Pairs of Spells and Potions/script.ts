ba13d36d59bb3f784a00c34ada0d85ae26a58049
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-04-02 12:13:36 +0300

    #2300 Speed up by using sorted dataset

--- 2300. Successful Pairs of Spells and Potions/script.ts
+++ 2300. Successful Pairs of Spells and Potions/script.ts
@@ -1,9 +1,33 @@
 export {successfulPairs}
 
 function successfulPairs(spells: number[], potions: number[], success: number): number[] {
-    return spells.map((spell: number): number => {
-        return potions
-            .filter((potion: number): boolean => potion * spell >= success)
-            .length;
-    });
+    potions = potions.sort((a: number, b: number) => a - b);
+
+    const successfulPairsCounts: number[] = [];
+
+    for (let spell of spells) {
+        const minPotion: number = success / spell;
+        const minPotionIndex: number = indexOfFirstBigger(potions, minPotion);
+
+        successfulPairsCounts.push(potions.length - minPotionIndex);
+    }
+
+    return successfulPairsCounts;
+}
+
+function indexOfFirstBigger(nums: number[], target: number): number {
+    let left: number = 0;
+    let right: number = nums.length;
+
+    while (left < right) {
+        const mid: number = Math.floor((left + right) / 2);
+
+        if (nums[mid] >= target) {
+            right = mid;
+        } else {
+            left = mid + 1;
+        }
+    }
+
+    return right;
 }
