f3b7fcb0f5f72257670a3c7e97ae0502a27bec54
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-04-04 21:46:37 +0300

    #2 Cover by tests

---
+++ solutions/2. Add Two Numbers/__tests__/script.test.ts
@@ -0,0 +1,26 @@
+import {addTwoNumbers, ListNode} from '../script'
+
+describe('2. Add Two Numbers', (): void => {
+    it('Zero', (): void => {
+        expect(addTwoNumbers(new ListNode(0), new ListNode(0)))
+            .toStrictEqual(new ListNode(0));
+    });
+
+    it('Same radix', (): void => {
+        expect(addTwoNumbers(
+            new ListNode(2, new ListNode(4, new ListNode(3))),
+            new ListNode(5, new ListNode(6, new ListNode(4)))
+        )).toStrictEqual(
+            new ListNode(7, new ListNode(0, new ListNode(8)))
+        );
+    });
+
+    it('Different radix', (): void => {
+        expect(addTwoNumbers(
+            new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9))))))),
+            new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9))))
+        )).toStrictEqual(
+            new ListNode(8, new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(0, new ListNode(0, new ListNode(0, new ListNode(1))))))))
+        );
+    });
+})
