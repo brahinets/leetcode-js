64afb0efee42f65670263704fc3e847838834f5e
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-04-07 23:01:25 +0300

    #12 Integer to Roman

---
+++ solutions/12. Integer to Roman/__tests__/script.test.ts
@@ -0,0 +1,15 @@
+import {intToRoman} from '../script'
+
+describe('12. Integer to Roman', (): void => {
+    it('Units', (): void => {
+        expect(intToRoman(3)).toBe("III");
+    });
+
+    it('Dozens', (): void => {
+        expect(intToRoman(58)).toBe("LVIII");
+    });
+
+    it('Grands', (): void => {
+        expect(intToRoman(1994)).toBe("MCMXCIV");
+    });
+})
---
+++ solutions/12. Integer to Roman/script.ts
@@ -0,0 +1,39 @@
+export {intToRoman}
+
+const INTEGERS: Map<number, string> = new Map<number, string>([
+    [1, "I"],
+    [4, "IV"],
+    [5, "V"],
+    [9, "IX"],
+    [10, "X"],
+    [40, "XL"],
+    [50, "L"],
+    [90, "XC"],
+    [100, "C"],
+    [400, "CD"],
+    [500, "D"],
+    [900, "CM"],
+    [1000, "M"]
+]);
+
+function intToRoman(num: number): string {
+    let result: string = "";
+
+    while (num > 0) {
+        let roman: string = "";
+        let max: number = 0;
+        for (let [i, r] of INTEGERS) {
+            if (i <= num) {
+                roman = r;
+                max = i;
+            }
+        }
+
+        while (num >= max) {
+            result += roman;
+            num -= max;
+        }
+    }
+
+    return result;
+}
