e416ff111cf5e6a92e9cb70572182d4334e79c93
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-04-16 12:51:45 +0300

    #516 Support palindrome sub-sequences

--- solutions/516. Longest Palindromic Subsequence/__tests__/script.test.ts
+++ solutions/516. Longest Palindromic Subsequence/__tests__/script.test.ts
@@ -4,4 +4,12 @@ describe('516. Longest Palindromic Subsequence', (): void => {
     it('Keep as is', (): void => {
         expect(longestPalindromeSubseq("cbbd")).toBe(2);
     });
+
+    it('Remove one char', (): void => {
+        expect(longestPalindromeSubseq("bbd")).toBe(2);
+    });
+
+    it('Remove multiple chars', (): void => {
+        expect(longestPalindromeSubseq("abcabcabcabc")).toBe(7);
+    });
 });
--- solutions/516. Longest Palindromic Subsequence/script.ts
+++ solutions/516. Longest Palindromic Subsequence/script.ts
@@ -3,16 +3,48 @@ export {longestPalindromeSubseq}
 function longestPalindromeSubseq(s: string): number {
     let maxPalindrome: string = "";
 
-    for (let i: number = 0; i < s.length && i < s.length - maxPalindrome.length; i++) {
-        for (let j: number = i; j < s.length; j++) {
-            const checked: string = s.substring(i, j + 1);
-            if (isPalindrome(checked) && checked.length > maxPalindrome.length) {
-                maxPalindrome = checked;
-            }
+    const words: string[] = generateAllSubsequences(s);
+    for (const word of words) {
+        if (word.length > maxPalindrome.length && isPalindrome(word)) {
+            maxPalindrome = word;
         }
     }
 
     return maxPalindrome.length;
+}
+
+function generateAllSubsequences(string: string): string[] {
+    const bitmasks: number[][] = generateBitmasks(string.length);
+    const chars: string[] = string.split("");
+
+    const result: string[] = [];
+    for (const bitmask of bitmasks) {
+        let str: string = "";
+
+        for (let i: number = 0; i < bitmask.length; i++) {
+            str += (bitmask[i] === 1 ? chars[i] : "");
+        }
+
+        result.push(str);
+    }
+
+    return result;
+}
+
+function generateBitmasks(bits: number): number[][] {
+    const bitmasks: number[][] = [];
+
+    for (let i: number = 0; i < Math.pow(2, bits); i++) {
+        const bitmask: number[] = [];
+
+        for (let j: number = 0; j < bits; j++) {
+            bitmask.push((i & (1 << j)) !== 0 ? 1 : 0);
+        }
+
+        bitmasks.push(bitmask);
+    }
+
+    return bitmasks;
 }
 
 const isPalindrome = (string: string): boolean => {
