fef63527f5ba3fc0316cc3efcd150d0feab970f8
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-04-21 21:19:26 +0300

    #876 Optimize complexity, use only one pass

--- solutions/876. Middle of the Linked List/script.ts
+++ solutions/876. Middle of the Linked List/script.ts
@@ -3,25 +3,17 @@ import {ListNode} from "../../common/ListNode";
 export {middleNode, ListNode};
 
 function middleNode(head: ListNode | null): ListNode | null {
-    const middle: number = Math.floor(countNodes(head) / 2);
-
-    let counter: number = 0;
+    let long: ListNode | null = head;
     let result: ListNode | null = head;
-    while (counter < middle && result) {
-        result = result.next || null;
-        counter++;
+
+    while (long) {
+        long = long?.next || null;
+        if (long) {
+            result = result?.next || null;
+        }
+
+        long = long?.next || null;
     }
 
     return result;
-}
-
-function countNodes(head: ListNode | null): number {
-    let counter: number = 0;
-
-    while (head) {
-        counter++;
-        head = head.next;
-    }
-
-    return counter;
 }
