d976de2b3f161c9e9f54b2b73c4d14e25766515f
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-04-24 10:51:47 +0300

    #1046 Improve formatting

--- solutions/1046. Last Stone Weight/__tests__/script.test.ts
+++ solutions/1046. Last Stone Weight/__tests__/script.test.ts
@@ -1,16 +1,19 @@
 import {lastStoneWeight} from '../script'
 
 describe('1046. Last Stone Weight', (): void => {
-    it('Last stone left', (): void => {
-        expect(lastStoneWeight([5, 2, 3, 1]))
-            .toBe(1);
-    });
-    it('One stone left', (): void => {
-        expect(lastStoneWeight([1]))
-            .toBe(1);
-    });
+
     it('No last stone', (): void => {
         expect(lastStoneWeight([5, 5, 3, 3]))
             .toBe(0);
+    });
+
+    it('One stone', (): void => {
+        expect(lastStoneWeight([1]))
+            .toBe(1);
+    });
+
+    it('Last stone left', (): void => {
+        expect(lastStoneWeight([5, 2, 3, 1]))
+            .toBe(1);
     });
 })
