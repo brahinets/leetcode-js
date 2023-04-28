efe4481243a10243046aed559bf651fa2e4460b9
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-04-28 10:33:50 +0300

    #319 Optimize time

--- solutions/319. Bulb Switcher/script.ts
+++ solutions/319. Bulb Switcher/script.ts
@@ -3,15 +3,15 @@ export {bulbSwitch}
 function bulbSwitch(n: number): number {
     let on: number = 0;
     for (let j: number = 0; j < n; j++) {
-        on += calculateState(j + 1, n);
+        on += calculateState(j + 1);
     }
 
     return on;
 }
 
-function calculateState(bulb: number, total: number): number {
+function calculateState(bulb: number): number {
     let state: boolean = false;
-    for (let step: number = 1; step <= total; step++) {
+    for (let step: number = 1; step <= bulb; step++) {
         if (bulb % step === 0) {
             state = !state;
         }
