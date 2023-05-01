1a49f3ad8c322720ef58c7311af942b55fe7a92a
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-05-01 09:45:34 +0300

    #1491 Improve type safety, avoid array mutation

--- solutions/1491. Average Salary Excluding the Minimum and Maximum Salary/script.ts
+++ solutions/1491. Average Salary Excluding the Minimum and Maximum Salary/script.ts
@@ -4,9 +4,7 @@ function average(salary: number[]): number {
     const min: number = Math.min(...salary);
     const max: number = Math.max(...salary);
 
-    const data: number[] = [...salary];
-    data.splice(data.indexOf(min), 1);
-    data.splice(data.indexOf(max), 1);
+    const sum: number = salary.reduce((sum: number, value: number): number => sum + value, 0);
 
-    return data.reduce((sum: number, value: number): number => sum + value, 0) / data.length;
+    return (sum - min - max) / (salary.length - 2);
 }
