5491e08ad105a233a82f53b84f8d0dcc45578419
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-05-03 22:17:08 +0300

    #1523 Improve type safety

--- solutions/1523. Count Odd Numbers in an Interval Range/script.js
+++
@@ -1,8 +0,0 @@
-/**
- * @param {number} low
- * @param {number} high
- * @return {number}
- */
-var countOdds = function (low, high) {
-    return Math.ceil(high / 2) - Math.floor(low / 2);
-};
---
+++ solutions/1523. Count Odd Numbers in an Interval Range/script.ts
@@ -0,0 +1,3 @@
+function countOdds(low: number, high: number): number {
+    return Math.ceil(high / 2) - Math.floor(low / 2);
+}
