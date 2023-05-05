50690e6a094cc19647ef0124d42efeaca0d19808
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-05-05 12:13:22 +0300

    #1456 Optimize

--- solutions/1456. Maximum Number of Vowels in a Substring of Given Length/script.ts
+++ solutions/1456. Maximum Number of Vowels in a Substring of Given Length/script.ts
@@ -10,10 +10,12 @@ function maxVowels(s: string, k: number): number {
     let max: number = 0;
 
     for (let i: number = 0; i <= s.length - k; i++) {
-        const substring: string = s.substring(i, i + k);
-        const count: number = substring.split("")
-            .map((letter: string): number => isVowel(letter) ? 1 : 0)
-            .reduce((count: number, elem: number): number => count + elem);
+        let count: number = 0;
+        for (let j: number = i; j < i + k; j++) {
+            if (isVowel(s[j])) {
+                count++;
+            }
+        }
 
         if (count > max) {
             max = count;
