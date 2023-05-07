01e4e3b6e468f1e0b8b79d4ba5a5a4e62995ba50
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-05-07 09:03:56 +0300

    #1498 Fix modulo overflow

--- solutions/1498. Number of Subsequences That Satisfy the Given Sum Condition/script.ts
+++ solutions/1498. Number of Subsequences That Satisfy the Given Sum Condition/script.ts
@@ -3,17 +3,28 @@ export {numSubseq}
 function numSubseq(nums: number[], target: number): number {
     nums.sort((a: number, b: number): number => a - b);
 
-    let answer: number = 0;
     const mod: number = 1e9 + 7;
+    const power: number[] = calculatePowers(nums.length, mod);
+
+    let answer: number = 0;
     for (let left: number = 0; left < nums.length; left++) {
         const right: number = binarySearch(nums, target - nums[left]) - 1;
         if (left <= right) {
-            const validCombinationsBetween: number = 2 ** (right - left) % mod;
+            const validCombinationsBetween: number = power[right - left];
             answer = (answer + validCombinationsBetween) % mod;
         }
     }
 
     return answer;
+}
+
+function calculatePowers(nums: number, mod: number): number[] {
+    const power: number[] = new Array<number>();
+    power.push(1);
+    for (let i: number = 1; i < nums; i++) {
+        power[i] = (power[i - 1] * 2) % mod;
+    }
+    return power;
 }
 
 function binarySearch(nums: number[], target: number): number {
