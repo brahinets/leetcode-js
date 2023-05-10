2c665022146f1d5f398e8529e90b59375d0c4e8e
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-05-10 22:21:14 +0300

    #54 Remove useless support of non-square matrices

--- solutions/59. Spiral Matrix II/script.ts
+++ solutions/59. Spiral Matrix II/script.ts
@@ -16,28 +16,20 @@ function generateMatrix(n: number): number[][] {
 }
 
 function fillMatrixBorder(matrix: number[][], layer: number, startNumber: number): number {
-    if (matrix.length > layer * 2) {
-        for (let col: number = layer; col + layer < matrix[0].length; col++) {
-            matrix[layer][col] = startNumber++;
-        }
+    for (let col: number = layer; col + layer < matrix[0].length; col++) {
+        matrix[layer][col] = startNumber++;
     }
 
-    if (matrix[0].length > 2 * layer) {
-        for (let row: number = 1 + layer; row + layer < matrix.length; row++) {
-            matrix[row][matrix[0].length - 1 - layer] = startNumber++;
-        }
+    for (let row: number = 1 + layer; row + layer < matrix.length; row++) {
+        matrix[row][matrix[0].length - 1 - layer] = startNumber++;
     }
 
-    if (matrix.length > layer * 2 + 1) {
-        for (let col: number = matrix[0].length - 2 - layer; col - layer >= 0; col--) {
-            matrix[matrix.length - 1 - layer][col] = startNumber++;
-        }
+    for (let col: number = matrix[0].length - 2 - layer; col - layer >= 0; col--) {
+        matrix[matrix.length - 1 - layer][col] = startNumber++;
     }
 
-    if (matrix[0].length > layer * 2 + 1) {
-        for (let row: number = matrix.length - 2 - layer; row - layer > 0; row--) {
-            matrix[row][layer] = startNumber++;
-        }
+    for (let row: number = matrix.length - 2 - layer; row - layer > 0; row--) {
+        matrix[row][layer] = startNumber++;
     }
 
     return startNumber;
