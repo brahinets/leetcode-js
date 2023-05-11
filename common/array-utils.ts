3119cbdb68b88d07ead0ca3d0a8bd42f25eca5ec
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-05-11 23:10:55 +0300

    #1035 Optimize via memoization

--- common/array-utils.ts
+++ common/array-utils.ts
@@ -13,6 +13,21 @@ export function matrixOfZeros(rows: number, cols: number): number[][] {
     return matrix;
 }
 
+export function matrixOf<Type>(val: Type, rows: number, cols: number): Type[][] {
+    const matrix: Type[][] = [];
+
+    for (let i: number = 0; i < rows; i++) {
+        const row: Type[] = [];
+        for (let j: number = 0; j < cols; j++) {
+            row.push(val);
+        }
+
+        matrix.push(row);
+    }
+
+    return matrix;
+}
+
 export function matrixOfEmptyStrings(rows: number, cols: number): string[][] {
     const matrix: string[][] = [];
 
--- solutions/1035. Uncrossed Lines/script.ts
+++ solutions/1035. Uncrossed Lines/script.ts
@@ -1,17 +1,30 @@
+import {matrixOf} from "../../common/array-utils";
+
 export {maxUncrossedLines}
 
 function maxUncrossedLines(up: number[], bottom: number[]): number {
-    return uncrossedLines(up, bottom, 0, 0);
+    return uncrossedLines(up, bottom, 0, 0, matrixOf(-1, up.length, bottom.length));
 }
 
-function uncrossedLines(up: number[], bottom: number[], start: number, end: number): number {
+function uncrossedLines(up: number[], bottom: number[], start: number, end: number, memo: number[][]): number {
     if (start === up.length || end === bottom.length) {
         return 0;
     }
 
-    if (up[start] === bottom[end]) {
-        return 1 + uncrossedLines(up, bottom, start + 1, end + 1);
-    } else {
-        return Math.max(uncrossedLines(up, bottom, start + 1, end), uncrossedLines(up, bottom, start, end + 1));
+    if (memo[start][end] !== -1) {
+        return memo[start][end];
     }
+
+    let number: number;
+    if (up[start] === bottom[end]) {
+        number = 1 + uncrossedLines(up, bottom, start + 1, end + 1, memo);
+    } else {
+        number = Math.max(
+            uncrossedLines(up, bottom, start + 1, end, memo),
+            uncrossedLines(up, bottom, start, end + 1, memo)
+        );
+    }
+
+    memo[start][end] = number;
+    return number;
 }
