6320543c498d162191d787536718b9317357c501
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-05-12 19:04:53 +0300

    #2140 Improve code readability

--- solutions/2140. Solving Questions With Brainpower/script.ts
+++ solutions/2140. Solving Questions With Brainpower/script.ts
@@ -6,16 +6,19 @@ function mostPoints(questions: number[][]): number {
     const maxPointStartingFrom: number[] = arrayOfZeros(questions.length);
 
     maxPointStartingFrom[questions.length - 1] = questions[questions.length - 1][0];
-    for (let i: number = questions.length - 2; i >= 0; i--) {
-        const points: number = questions[i][0];
-        const skip: number = questions[i][1];
+    for (let position: number = questions.length - 2; position >= 0; position--) {
+        const points: number = questions[position][0];
+        const jumpDistance: number = questions[position][1];
 
-        maxPointStartingFrom[i] = points;
-        if (skip + i + 1 < questions.length) {
-            maxPointStartingFrom[i] += maxPointStartingFrom[skip + i + 1];
+        const reachablePoint: number = jumpDistance + position + 1;
+        const canReachPoint: boolean = reachablePoint < questions.length;
+        if (canReachPoint) {
+            maxPointStartingFrom[position] = points + maxPointStartingFrom[reachablePoint];
+        } else {
+            maxPointStartingFrom[position] = points;
         }
 
-        maxPointStartingFrom[i] = Math.max(maxPointStartingFrom[i], maxPointStartingFrom[i + 1]);
+        maxPointStartingFrom[position] = Math.max(maxPointStartingFrom[position], maxPointStartingFrom[position + 1]);
     }
 
     return maxPointStartingFrom[0];
