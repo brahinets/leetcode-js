082093aab1e977570dffbc87f34dd37f439e50bc
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-05-12 18:18:07 +0300

    #2140 Solving Questions With Brainpower

--- solutions/2140. Solving Questions With Brainpower/script.ts
+++ solutions/2140. Solving Questions With Brainpower/script.ts
@@ -1,5 +1,20 @@
 export {mostPoints}
 
 function mostPoints(questions: number[][]): number {
-    throw new Error("Implement me"); // TODO
+    const maxPointStartingFrom: number[] = new Array<number>(questions.length).fill(0);
+
+    maxPointStartingFrom[questions.length - 1] = questions[questions.length - 1][0];
+    for (let i: number = questions.length - 2; i >= 0; i--) {
+        const points: number = questions[i][0];
+        const skip: number = questions[i][1];
+
+        maxPointStartingFrom[i] = points;
+        if (skip + i + 1 < questions.length) {
+            maxPointStartingFrom[i] += maxPointStartingFrom[skip + i + 1];
+        }
+
+        maxPointStartingFrom[i] = Math.max(maxPointStartingFrom[i], maxPointStartingFrom[i + 1]);
+    }
+
+    return maxPointStartingFrom[0];
 }
