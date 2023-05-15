2d433038cb09f9dff41da18a6e8730195dd4643c
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-05-15 10:17:03 +0300

    #1799 Add todo

--- solutions/1799. Maximize Score After N Operations/__tests__/script.test.ts
+++ solutions/1799. Maximize Score After N Operations/__tests__/script.test.ts
@@ -15,4 +15,16 @@ describe('1799. Maximize Score After N Operations', (): void => {
         expect(maxScore([1, 2, 3, 4, 5, 6]))
             .toBe(14);
     });
+
+    // TODO #1799 Fix business logic
+    it('A bit Many', (): void => {
+        expect(maxScore([415, 230, 471, 705, 902, 87]))
+            .toBe(23);
+    });
+
+    // TODO #1799 Fix business logic
+    it('Much many', (): void => {
+        expect(maxScore([109497, 983516, 698308, 409009, 310455, 528595, 524079, 18036, 341150, 641864, 913962, 421869, 943382, 295019]))
+            .toBe(527);
+    });
 })
