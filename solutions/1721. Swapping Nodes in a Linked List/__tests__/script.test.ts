2183976e280d11492ba02d4e865b564b29f691e2
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-05-15 15:15:01 +0300

    #1721 Improve list factory readability

--- solutions/1721. Swapping Nodes in a Linked List/__tests__/script.test.ts
+++ solutions/1721. Swapping Nodes in a Linked List/__tests__/script.test.ts
@@ -1,70 +1,31 @@
-import {ListNode, swapNodes} from '../script'
+import {swapNodes} from '../script'
+import {linkedListOf} from "../../../common/list-utils";
 
 describe('1721. Swapping Nodes in a Linked List', (): void => {
     it('Swap boundaries, first and last', (): void => {
-        expect(
-            swapNodes(
-                new ListNode(1,
-                    new ListNode(2,
-                        new ListNode(3,
-                            new ListNode(4,
-                                new ListNode(5))))),
-                1
-            )
-        ).toStrictEqual(
-            new ListNode(5,
-                new ListNode(2,
-                    new ListNode(3,
-                        new ListNode(4,
-                            new ListNode(1)))))
+        expect(swapNodes(
+            linkedListOf([1, 2, 3, 4, 5]),
+            1
+        )).toStrictEqual(
+            linkedListOf([5, 2, 3, 4, 1])
         );
     });
 
     it('Swap center', (): void => {
-        expect(
-            swapNodes(
-                new ListNode(1,
-                    new ListNode(2,
-                        new ListNode(3,
-                            new ListNode(4,
-                                new ListNode(5))))),
-                3
-            )
-        ).toStrictEqual(
-            new ListNode(1,
-                new ListNode(2,
-                    new ListNode(3,
-                        new ListNode(4,
-                            new ListNode(5)))))
+        expect(swapNodes(
+            linkedListOf([1, 2, 3, 4, 5]),
+            3
+        )).toStrictEqual(
+            linkedListOf([1, 2, 3, 4, 5])
         );
     });
 
     it('Swap in the middle', (): void => {
-        expect(
-            swapNodes(
-                new ListNode(7,
-                    new ListNode(9,
-                        new ListNode(6,
-                            new ListNode(6,
-                                new ListNode(7,
-                                    new ListNode(8,
-                                        new ListNode(3,
-                                            new ListNode(0,
-                                                new ListNode(9,
-                                                    new ListNode(5)))))))))),
-                5
-            )
-        ).toStrictEqual(
-            new ListNode(7,
-                new ListNode(9,
-                    new ListNode(6,
-                        new ListNode(6,
-                            new ListNode(8,
-                                new ListNode(7,
-                                    new ListNode(3,
-                                        new ListNode(0,
-                                            new ListNode(9,
-                                                new ListNode(5)))))))))),
+        expect(swapNodes(
+            linkedListOf([7, 9, 6, 6, 7, 8, 3, 0, 9, 5]),
+            5
+        )).toStrictEqual(
+            linkedListOf([7, 9, 6, 6, 8, 7, 3, 0, 9, 5])
         );
     });
 })
