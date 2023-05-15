1f24d8a28cacec7d027c4b36fda384d481df8793
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-05-15 17:42:29 +0300

    #1721 Speed-up using two shifted iterators

--- solutions/1721. Swapping Nodes in a Linked List/script.ts
+++ solutions/1721. Swapping Nodes in a Linked List/script.ts
@@ -3,50 +3,27 @@ import {ListNode} from "../../common/ListNode";
 export {swapNodes, ListNode};
 
 function swapNodes(head: ListNode | null, k: number): ListNode | null {
-    let newHead: ListNode | null = head;
-    let length: number = 0;
-
-    while (newHead) {
-        newHead = newHead.next;
-        length++;
+    let count: number = 1;
+    let firstNode: ListNode | null = head;
+    while (count < k) {
+        firstNode = firstNode?.next || null;
+        count++;
     }
 
-
-    let first: number | undefined;
-    let second: number | undefined;
-    newHead = head;
-    let i: number = 1;
-    while (newHead) {
-        if (i === k) {
-            first = newHead.val;
-        }
-
-        if (i === length + 1 - k) {
-            second = newHead.val;
-        }
-
-        newHead = newHead.next;
-        i++;
+    let secondNode: ListNode | null = head;
+    let secondIter: ListNode | null = firstNode;
+    while (secondIter?.next) {
+        secondNode = secondNode?.next || null;
+        secondIter = secondIter.next;
     }
 
-    if (first === undefined || second === undefined) {
+    if (!firstNode || !secondNode) {
         throw new Error("Element not found");
     }
 
-    newHead = head;
-    i = 1;
-    while (head) {
-        if (i === k) {
-            head.val = second;
-        }
+    const tmp: number = firstNode.val;
+    firstNode.val = secondNode.val;
+    secondNode.val = tmp;
 
-        if (i === length + 1 - k) {
-            head.val = first;
-        }
-
-        head = head.next;
-        i++;
-    }
-
-    return newHead;
+    return head;
 }
