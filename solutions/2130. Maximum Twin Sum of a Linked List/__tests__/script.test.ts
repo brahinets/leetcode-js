94cbbecc90ef2d515ff0d38b63d625caed0fca7b
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-05-17 10:13:43 +0300

    #2130 Maximum Twin Sum of a Linked List

---
+++ solutions/2130. Maximum Twin Sum of a Linked List/__tests__/script.test.ts
@@ -0,0 +1,19 @@
+import {linkedListOf} from '../../../common/list-utils'
+import {pairSum} from "../script";
+
+describe('2130. Maximum Twin Sum of a Linked List', (): void => {
+    it('Multiple max sum', (): void => {
+        expect(pairSum(linkedListOf([5, 4, 2, 1])))
+            .toBe(6);
+    });
+
+    it('Single max sum', (): void => {
+        expect(pairSum(linkedListOf([4, 2, 2, 3])))
+            .toBe(7);
+    });
+
+    it('The only max sum', (): void => {
+        expect(pairSum(linkedListOf([1, 100000])))
+            .toBe(100001);
+    });
+})
---
+++ solutions/2130. Maximum Twin Sum of a Linked List/script.ts
@@ -0,0 +1,26 @@
+import {ListNode} from "../../common/ListNode";
+import {linkedListToArray} from "../../common/list-utils";
+
+export {pairSum, ListNode};
+
+function pairSum(head: ListNode | null): number {
+    const data: number[] | null = linkedListToArray(head);
+
+    if (!data) {
+        return 0;
+    }
+
+    let max: number | undefined = undefined;
+    for (let i: number = 0; i < data.length / 2; i++) {
+        const sum: number = data[i] + data[data.length - 1 - i];
+        if (max === undefined || sum > max) {
+            max = sum;
+        }
+    }
+
+    if (max === undefined) {
+        throw new Error("Not enough data to define max")
+    }
+
+    return max;
+}
