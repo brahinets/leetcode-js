8ad21751e11a3485c7b47cfd90f0b5658cdf4f60
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-05-19 08:50:34 +0300

    #28 Cover by tests

---
+++ solutions/28. Find the Index of the First Occurrence in a String/__tests__/script.test.ts
@@ -0,0 +1,19 @@
+import {strStr} from '../script'
+
+describe('28. Find the Index of the First Occurrence in a String', (): void => {
+    it('Not found', (): void => {
+        expect(strStr("leetcode", "leeto")).toBe(-1);
+    });
+
+    it('Found at start', (): void => {
+        expect(strStr("sadbutsad", "sad")).toBe(0);
+    });
+
+    it('Found in the middle', (): void => {
+        expect(strStr("sadbutsad", "dbu")).toBe(2);
+    });
+
+    it('Found in the end', (): void => {
+        expect(strStr("sadbutsadc", "c")).toBe(9);
+    });
+})
--- solutions/28. Find the Index of the First Occurrence in a String/script.ts
+++ solutions/28. Find the Index of the First Occurrence in a String/script.ts
@@ -1,3 +1,5 @@
+export {strStr}
+
 function strStr(haystack: string, needle: string): number {
     for (let i: number = 0; i <= haystack.length - needle.length; i++) {
         if (haystack.substring(i, i + needle.length) === needle) {
