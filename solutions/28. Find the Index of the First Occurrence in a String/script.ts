773a0d4c142d716d719e9d5528a165c2be4d7015
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-05-19 08:47:21 +0300

    #28 Simplify code

--- solutions/28. Find the Index of the First Occurrence in a String/script.ts
+++ solutions/28. Find the Index of the First Occurrence in a String/script.ts
@@ -1,12 +1,8 @@
 function strStr(haystack: string, needle: string): number {
-    let i: number = 0;
-
-    while (i <= haystack.length - needle.length) {
+    for (let i: number = 0; i <= haystack.length - needle.length; i++) {
         if (haystack.substring(i, i + needle.length) === needle) {
             return i;
         }
-
-        i++;
     }
 
     return -1;
