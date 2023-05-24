3947f5d6d5dda506288b60e65883c11fc6fa1d34
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-05-24 22:42:22 +0300

    #9 Improve type-safety

---
+++ solutions/9. Palindrome Number/script.ts
@@ -0,0 +1,12 @@
+const isPalindrome = function (x: number): boolean {
+    const str: string = String(x);
+    const middle: number = str.length / 2;
+
+    for (let i: number = 0; i < middle; i++) {
+        if (str[i] !== str[str.length - i - 1]) {
+            return false;
+        }
+    }
+
+    return true;
+}
