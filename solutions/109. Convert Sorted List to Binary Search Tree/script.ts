8cbf268684167c31b5bf13e0846db2b54d5297ac
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-06-02 19:15:14 +0300

    #109 Reuse List and Tree model

--- solutions/109. Convert Sorted List to Binary Search Tree/script.ts
+++ solutions/109. Convert Sorted List to Binary Search Tree/script.ts
@@ -1,24 +1,5 @@
-class ListNode {
-    val: number;
-    next: ListNode | null;
-
-    constructor(val?: number, next?: ListNode | null) {
-        this.val = (val === undefined ? 0 : val);
-        this.next = (next === undefined ? null : next);
-    }
-}
-
-class TreeNode {
-    val: number;
-    left: TreeNode | null;
-    right: TreeNode | null;
-
-    constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
-        this.val = (val === undefined ? 0 : val);
-        this.left = (left === undefined ? null : left);
-        this.right = (right === undefined ? null : right);
-    }
-}
+import {ListNode} from "../../common/ListNode";
+import {TreeNode} from "../../common/TreeNode";
 
 function sortedListToBST(head: ListNode | null): TreeNode | null {
     let data: number[] = [];
