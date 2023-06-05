b99f006bc809b771e4713cab0e8faa5e2df5a275
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-06-05 09:35:32 +0300

    #1232 Improve readability

--- solutions/1232. Check If It Is a Straight Line/script.ts
+++ solutions/1232. Check If It Is a Straight Line/script.ts
@@ -4,7 +4,10 @@ function checkStraightLine(coordinates: number[][]): boolean {
     coordinates.sort((a: number[], b: number[]): number => a[0] - b[0]);
 
     for (let i: number = 1; i < coordinates.length; i++) {
-        if (coordinates[i][0] - coordinates[i - 1][0] !== coordinates[i][1] - coordinates[i - 1][1]) {
+        const [x0, y0] = coordinates[i - 1];
+        const [x1, y1] = coordinates[i];
+
+        if (x1 - x0 !== y1 - y0) {
             return false;
         }
     }
