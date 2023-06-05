47e4fddf95b53798be6facb0ed0aefdc904394c5
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-06-05 10:34:54 +0300

    #1232 Improve performance

--- solutions/1232. Check If It Is a Straight Line/script.ts
+++ solutions/1232. Check If It Is a Straight Line/script.ts
@@ -7,11 +7,12 @@ function checkStraightLine(coordinates: number[][]): boolean {
 
     const [x0, y0] = coordinates[0];
     const [x1, y1] = coordinates[1];
+    const angleY: number = y1 - y0;
+    const angleX: number = x1 - x0;
 
     for (let i: number = 2; i < coordinates.length; i++) {
         const [x, y] = coordinates[i];
-
-        if ((x - x0) * (y1 - y0) !== (y - y0) * (x1 - x0)) {
+        if ((x - x0) * angleY !== (y - y0) * angleX) {
             return false;
         }
     }
