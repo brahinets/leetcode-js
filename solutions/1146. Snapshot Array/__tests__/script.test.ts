567b15c0c04700c8758e06f77140a34f310c903b
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-06-11 17:53:18 +0300

    #1146 Snapshot Array

---
+++ solutions/1146. Snapshot Array/__tests__/script.test.ts
@@ -0,0 +1,38 @@
+import {SnapshotArray} from "../script";
+
+describe('1146. Snapshot Array', (): void => {
+    it('Test initial zeros', (): void => {
+        const snapshotArr: SnapshotArray = new SnapshotArray(3);
+
+        const snap: number = snapshotArr.get(0, 0);
+        expect(snap).toBe(0);
+    });
+
+     it('Test original data', (): void => {
+        const snapshotArr: SnapshotArray = new SnapshotArray(3);
+        snapshotArr.set(0, 5);
+
+        const snap0: number = snapshotArr.get(0, 0);
+        expect(snap0).toBe(5);
+    });
+
+    it('Test first snapshot', (): void => {
+        const snapshotArr: SnapshotArray = new SnapshotArray(3);
+        snapshotArr.set(0, 5);
+        snapshotArr.snap();
+
+        const snap0: number = snapshotArr.get(0, 1);
+        expect(snap0).toBe(5);
+    });
+
+    it('Test another snapshot', (): void => {
+        const snapshotArr: SnapshotArray = new SnapshotArray(3);
+        snapshotArr.set(0, 5);
+        snapshotArr.snap();
+        snapshotArr.set(0, 6);
+        snapshotArr.snap();
+
+        const snap0: number = snapshotArr.get(0, 2);
+        expect(snap0).toBe(6);
+    });
+})
---
+++ solutions/1146. Snapshot Array/script.ts
@@ -0,0 +1,40 @@
+export {SnapshotArray}
+
+class SnapshotArray {
+    private readonly snaps: Map<number, number[]>;
+    private readonly length: number;
+    private readonly data: number[];
+
+    constructor(length: number) {
+        this.length = length;
+        this.snaps = new Map<number, number[]>();
+        this.data = new Array(length).fill(0);
+    }
+
+    set(index: number, val: number): void {
+        this.data[index] = val;
+    }
+
+    snap(): number {
+        let snapId: number = -1;
+        for (const id of this.snaps.keys()) {
+            if (id > snapId) {
+                snapId = id;
+            }
+        }
+
+        this.snaps.set(snapId + 1, Array.of(...this.data));
+
+        return snapId + 1;
+    }
+
+    get(index: number, snapId: number): number {
+        let data: number[] | undefined = this.snaps.get(snapId);
+
+        if (data === undefined) {
+            data = this.data;
+        }
+
+        return data[index];
+    }
+}
