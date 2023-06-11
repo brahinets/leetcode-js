4f513cd081d40efc628d87e0d8720b805d4263f0
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-06-11 18:19:29 +0300

    #1146 Optimize memory

--- solutions/1146. Snapshot Array/script.ts
+++ solutions/1146. Snapshot Array/script.ts
@@ -1,38 +1,53 @@
 export {SnapshotArray}
 
 class SnapshotArray {
-    private readonly snaps: Map<number, number[]>;
-    private readonly data: number[];
+    private readonly deltas: Map<number, Map<number, number>>;
+    private readonly length: number;
+    private delta: Map<number, number>;
 
     constructor(length: number) {
-        this.snaps = new Map<number, number[]>();
-        this.data = new Array(length).fill(0);
+        this.deltas = new Map<number, Map<number, number>>();
+        this.delta = new Map<number, number>();
+        this.length = length;
     }
 
     set(index: number, val: number): void {
-        this.data[index] = val;
+        this.delta.set(index, val);
     }
 
     snap(): number {
         let snapId: number = -1;
-        for (const id of this.snaps.keys()) {
+        for (const id of this.deltas.keys()) {
             if (id > snapId) {
                 snapId = id;
             }
         }
 
-        this.snaps.set(snapId + 1, Array.of(...this.data));
+        this.deltas.set(snapId + 1, this.delta);
+        this.delta = new Map<number, number>();
 
         return snapId + 1;
     }
 
     get(index: number, snapId: number): number {
-        let data: number[] | undefined = this.snaps.get(snapId);
+        const data: number[] = new Array(this.length).fill(0);
 
-        if (data === undefined) {
-            data = this.data;
+        for (let i: number = 0; i <= snapId; i++) {
+            this.applyDelta(data, this.deltas.get(i) ?? new Map<number, number>())
+        }
+
+        if(this.deltas.size === snapId) {
+            this.applyDelta(data, this.delta)
         }
 
         return data[index];
+    }
+
+    private applyDelta(data: number[], map: Map<number, number>): void {
+        for (const kv of map) {
+            const index: number = kv[0];
+            const value: number = kv[1];
+            data[index] = value;
+        }
     }
 }
