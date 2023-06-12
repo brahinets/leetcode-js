9ce16da93d529269407ca94cb710c2da2d7bf0ff
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-06-12 10:37:39 +0300

    #228 Summary Ranges

---
+++ solutions/228. Summary Ranges/script.ts
@@ -0,0 +1,23 @@
+export {summaryRanges}
+
+function summaryRanges(nums: number[]): string[] {
+    const ranges: string[] = [];
+
+    let start: number = 0;
+    let end: number = 0;
+    while (start < nums.length && end < nums.length) {
+        if (end + 1 < nums.length && (nums[end] + 1 === nums[end + 1])) {
+            end++;
+        } else {
+            if(start !== end) {
+                ranges.push(nums[start] + "->" + nums[end]);
+            } else {
+                ranges.push(nums[start] + "");
+            }
+            start = end + 1;
+            end = start;
+        }
+    }
+
+    return ranges;
+}
