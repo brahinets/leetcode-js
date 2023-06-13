5529b9f578aab2e8cc627769bf9406f401a4dc1a
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-06-13 10:34:39 +0300

    #2352 Optimize, pre-calculate columns

--- solutions/2352. Equal Row and Column Pairs/script.ts
+++ solutions/2352. Equal Row and Column Pairs/script.ts
@@ -1,13 +1,18 @@
 export {equalPairs}
 
 function equalPairs(grid: number[][]): number {
+    if (grid.length === 0) {
+        return 0;
+    }
+
     let count: number = 0;
+    const cols: Array<number[]> = [];
+    for (let i: number = 0; i < grid[0].length; i++) {
+        cols.push(grid.reduce((prev: number[], curr: number[]): number[] => [...prev, curr[i]], []))
+    }
 
     for (const row of grid) {
-        for (let i: number = 0; i < row.length; i++) {
-            const col: number[] = grid
-                .reduce((prev: number[], curr: number[]): number[] => [...prev, curr[i]], [])
-
+        for (const col of cols) {
             if (JSON.stringify(row) === JSON.stringify(col)) {
                 count++;
             }
