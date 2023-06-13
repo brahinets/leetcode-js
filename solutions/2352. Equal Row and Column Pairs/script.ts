b106c2a433bd88f689d3312b209c1569068063e0
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-06-13 10:34:50 +0300

    #2352 Improve readability

--- solutions/2352. Equal Row and Column Pairs/script.ts
+++ solutions/2352. Equal Row and Column Pairs/script.ts
@@ -6,11 +6,7 @@ function equalPairs(grid: number[][]): number {
     }
 
     let count: number = 0;
-    const cols: Array<number[]> = [];
-    for (let i: number = 0; i < grid[0].length; i++) {
-        cols.push(grid.reduce((prev: number[], curr: number[]): number[] => [...prev, curr[i]], []))
-    }
-
+    const cols: Array<number[]> = collectColumns(grid);
     for (const row of grid) {
         for (const col of cols) {
             if (JSON.stringify(row) === JSON.stringify(col)) {
@@ -20,4 +16,14 @@ function equalPairs(grid: number[][]): number {
     }
 
     return count;
+}
+
+function collectColumns(grid: number[][]): Array<number[]> {
+    const cols: Array<number[]> = [];
+
+    for (let i: number = 0; i < grid[0].length; i++) {
+        cols.push(grid.reduce((prev: number[], curr: number[]): number[] => [...prev, curr[i]], []));
+    }
+
+    return cols;
 }
