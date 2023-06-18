3b8527b08584139f6f4eaf8fdb5133d6058534b4
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-06-18 14:00:14 +0300

    #399 Improve type-safety

--- solutions/399. Evaluate Division/script.ts
+++ solutions/399. Evaluate Division/script.ts
@@ -1,33 +1,31 @@
 export {calcEquation}
 
 function calcEquation(equations: string[][], values: number[], queries: string[][]): number[] {
-    const graph: any = mapGraph(equations, values);
+    const graph: Map<string, Map<string, number>> = mapGraph(equations, values);
 
     return queries.map((q: string[]): number => dfs(q[0], q[1], [], graph));
 }
 
-function mapGraph(equations: string[][], values: number[]): any {
-    const graph: any = {};
+function mapGraph(equations: string[][], values: number[]): Map<string, Map<string, number>> {
+    const graph: Map<string, Map<string, number>> = new Map<string, Map<string, number>>();
 
     for (let i: number = 0; i < equations.length; i++) {
         const [a, b]: string[] = equations[i];
 
-        if (!graph[a]) {
-            graph[a] = {};
-        }
-        if (!graph[b]) {
-            graph[b] = {};
-        }
+        const ba: Map<string, number> = graph.get(b) ?? new Map<string, number>();
+        ba.set(a, values[i])
+        graph.set(b, ba);
 
-        graph[b][a] = values[i];
-        graph[a][b] = 1 / values[i];
+        const ab: Map<string, number> = graph.get(a) ?? new Map<string, number>();
+        ab.set(b, 1 / values[i])
+        graph.set(a, ab);
     }
 
     return graph;
 }
 
-function dfs(c: string, d: string, visited: string[], graph: any): number {
-    if (!graph[c] || !graph[d]) {
+function dfs(c: string, d: string, visited: string[], graph: Map<string, Map<string, number>>): number {
+    if (!graph.has(c) || !graph.has(d)) {
         return -1;
     }
 
@@ -35,12 +33,13 @@ function dfs(c: string, d: string, visited: string[], graph: any): number {
         return 1;
     }
 
-    for (const n in graph[c]) {
-        if (visited.includes(n)) {
+    const map: Map<string, number> = graph.get(c) ?? new Map<string, number>();
+    for (const n of map) {
+        if (visited.includes(n[0])) {
             continue;
         }
 
-        const res: number = dfs(n, d, [...visited, n], graph) * graph[n][c];
+        const res: number = dfs(n[0], d, [...visited, n[0]], graph) * (graph.get(n[0])?.get(c) ?? 0);
         if (res > 0) {
             return res;
         }
