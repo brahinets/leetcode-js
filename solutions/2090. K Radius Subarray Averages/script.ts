905f32c86515a7cc67b1e32c063a768114e49126
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-06-20 19:16:55 +0300

    #2090 Simplify code

--- solutions/2090. K Radius Subarray Averages/script.ts
+++ solutions/2090. K Radius Subarray Averages/script.ts
@@ -7,15 +7,19 @@ function getAverages(nums: number[], radius: number): number[] {
         return avg;
     }
 
-    const data: number[] = nums.slice(0, radius * 2 + 1);
-    let sum: number = data.reduce((sum: number, n: number): number => sum + n, 0);
-    avg[radius] = average(sum, data.length);
+    let sum: number = initialWindowSum(nums, radius);
+    avg[radius] = average(sum, radius * 2 + 1);
     for (let i: number = radius + 1; i < nums.length - radius; i++) {
         sum = sum - nums[i - radius - 1] + nums[i + radius];
-        avg[i] = average(sum, data.length);
+        avg[i] = average(sum, radius * 2 + 1);
     }
 
     return avg;
+}
+
+function initialWindowSum(nums: number[], radius: number): number {
+    const data: number[] = nums.slice(0, radius * 2 + 1);
+    return data.reduce((sum: number, n: number): number => sum + n, 0);
 }
 
 function average(sum: number, length: number) {
