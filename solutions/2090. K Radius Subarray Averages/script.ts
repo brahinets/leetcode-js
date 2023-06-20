8cbddcbdf1419a44f207f33da4b832680a39a66a
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-06-20 18:54:31 +0300

    #2090 Optimize, allocate result collection in advance

--- solutions/2090. K Radius Subarray Averages/script.ts
+++ solutions/2090. K Radius Subarray Averages/script.ts
@@ -1,18 +1,16 @@
 export {getAverages}
 
 function getAverages(nums: number[], radius: number): number[] {
-    if (radius > nums.length) {
-        return new Array(nums.length).fill(-1);
-    }
+    const avg: number[] = new Array(nums.length).fill(-1);
 
-    const avg: number[] = [];
+    if (radius > nums.length) {
+        return avg;
+    }
 
     for (let i: number = 0; i < nums.length; i++) {
         if (radius <= i && i + radius < nums.length) {
             const data: number[] = nums.slice(i - radius, i + radius + 1);
-            avg.push(Math.floor(data.reduce((sum: number, n: number): number => sum + n, 0) / data.length));
-        } else {
-            avg.push(-1);
+            avg[i] = Math.floor(data.reduce((sum: number, n: number): number => sum + n, 0) / data.length);
         }
     }
 
