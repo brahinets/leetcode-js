2d8d7ea533bb92a2e862a80e30e57816cb3bdb92
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-06-20 19:33:04 +0300

    #2090 Optimize, reuse window size

--- solutions/2090. K Radius Subarray Averages/script.ts
+++ solutions/2090. K Radius Subarray Averages/script.ts
@@ -1,26 +1,27 @@
 export {getAverages}
 
 function getAverages(nums: number[], radius: number): number[] {
+    const windowSize:number = radius * 2 + 1;
     const avg: number[] = new Array(nums.length).fill(-1);
 
-    if (radius * 2 + 1 > nums.length) {
+    if (windowSize > nums.length) {
         return avg;
     }
 
-    let sum: number = initialWindowSum(nums, radius);
-    avg[radius] = average(sum, radius * 2 + 1);
+    let windowSum: number = initialWindowSum(nums, windowSize);
+    avg[radius] = average(windowSum, windowSize);
     for (let i: number = radius + 1; i < nums.length - radius; i++) {
-        sum = sum - nums[i - radius - 1] + nums[i + radius];
-        avg[i] = average(sum, radius * 2 + 1);
+        windowSum = windowSum - nums[i - radius - 1] + nums[i + radius];
+        avg[i] = average(windowSum, windowSize);
     }
 
     return avg;
 }
 
-function initialWindowSum(nums: number[], radius: number): number {
+function initialWindowSum(nums: number[], windowSize: number): number {
     let sum: number = 0;
 
-    for (let i: number = 0; i < radius * 2 + 1; i++) {
+    for (let i: number = 0; i < windowSize; i++) {
         sum += nums[i];
     }
 
