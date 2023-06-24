6cc372c7b09bcd1afd1cc2aea085974fe695550a
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-06-24 19:46:37 +0300

    #966 Use only positive diff

--- solutions/956. Tallest Billboard/script.ts
+++ solutions/956. Tallest Billboard/script.ts
@@ -4,7 +4,7 @@ export {tallestBillboard}
 
 function tallestBillboard(rods: number[]): number {
     const maxDiff: number = rods.reduce((sum: number, r: number): number => sum + r, 0);
-    return solve(0, rods, 0, matrixOf(-1, rods.length, maxDiff * 2 + 1));
+    return solve(0, rods, 0, matrixOf(-1, rods.length, maxDiff + 1)) / 2;
 }
 
 function solve(i: number, rods: number[], diff: number, memo: number[][]): number {
@@ -12,13 +12,13 @@ function solve(i: number, rods: number[], diff: number, memo: number[][]): numbe
         return diff === 0 ? 0 : Number.NEGATIVE_INFINITY;
     }
 
-    if (!isNaN(memo[i][diff]) && memo[i][diff] !== -1) {
+    if (memo[i][diff] !== -1) {
         return memo[i][diff];
     }
 
-    const left: number = rods[i] + solve(i + 1, rods, diff + rods[i], memo);
-    const right: number = solve(i + 1, rods, diff - rods[i], memo);
+    const left: number = solve(i + 1, rods, diff + rods[i], memo);
+    const right: number = solve(i + 1, rods, Math.abs(diff - rods[i]), memo);
     const same: number = solve(i + 1, rods, diff, memo);
 
-    return memo[i][diff] = Math.max(left, right, same);
+    return memo[i][diff] = Math.max(rods[i] + left, rods[i] + right, same);
 }
