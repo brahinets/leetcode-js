d835f7dec6a1f56b45c0343ffbdc963f1b62f950
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-06-24 19:30:54 +0300

    #966 Allocate memo in advance

--- solutions/956. Tallest Billboard/script.ts
+++ solutions/956. Tallest Billboard/script.ts
@@ -1,7 +1,9 @@
+import {matrixOf} from "../../common/array-utils";
+
 export {tallestBillboard}
 
 function tallestBillboard(rods: number[]): number {
-    return solve(0, rods, 0, []);
+    return solve(0, rods, 0, matrixOf(-1, rods.length, 5000 * 2 + 1));
 }
 
 function solve(i: number, rods: number[], diff: number, memo: number[][]): number {
@@ -9,16 +11,13 @@ function solve(i: number, rods: number[], diff: number, memo: number[][]): numbe
         return diff === 0 ? 0 : Number.NEGATIVE_INFINITY;
     }
 
-    if (memo[i]?.[diff] !== undefined) {
+    if (!isNaN(memo[i][diff]) && memo[i][diff] !== -1) {
         return memo[i][diff];
     }
 
     const left: number = rods[i] + solve(i + 1, rods, diff + rods[i], memo);
     const right: number = solve(i + 1, rods, diff - rods[i], memo);
     const same: number = solve(i + 1, rods, diff, memo);
-    const num: number = Math.max(left, right, same);
 
-    memo[i] = memo[i] || [];
-    memo[i][diff] = num;
-    return num;
+    return memo[i][diff] = Math.max(left, right, same);
 }
