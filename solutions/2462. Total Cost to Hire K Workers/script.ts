cd420ff8608c2e8745ef4f8e365089b34e5b54b2
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-06-26 19:08:57 +0300

    #2462 Decrease code duplication

--- solutions/2462. Total Cost to Hire K Workers/script.ts
+++ solutions/2462. Total Cost to Hire K Workers/script.ts
@@ -5,25 +5,29 @@ function totalCost(costs: number[], k: number, candidates: number): number {
     let hired: number = 0;
 
     while (hired < k) {
-        const hasPools: boolean = costs.length > candidates * 2;
-
-        if (hasPools) {
-            const leftPool: number[] = costs.slice(0, candidates);
-            const rightPool: number[] = costs.slice(costs.length - candidates)
-            const cheapestLeft: number = Math.min(...leftPool)
-            const cheapestRight: number = Math.min(...rightPool)
-
-            if (cheapestLeft <= cheapestRight) {
-                cost += costs.splice(costs.indexOf(cheapestLeft), 1)[0];
-            } else {
-                cost += costs.splice(costs.lastIndexOf(cheapestRight), 1)[0];
-            }
-        } else {
-            cost += costs.splice(costs.indexOf(Math.min(...costs)), 1)[0];
-        }
-
+        const cheapestWorker: number = findCheapestWorker(costs, candidates);
+        cost += costs.splice(cheapestWorker, 1)[0];
         hired++;
     }
 
     return cost;
+}
+
+function findCheapestWorker(costs: number[], candidates: number): number {
+    const hasPools: boolean = costs.length > candidates * 2;
+
+    if (hasPools) {
+        const leftPool: number[] = costs.slice(0, candidates);
+        const rightPool: number[] = costs.slice(costs.length - candidates)
+        const cheapestLeft: number = Math.min(...leftPool)
+        const cheapestRight: number = Math.min(...rightPool)
+
+        if (cheapestLeft <= cheapestRight) {
+            return costs.indexOf(cheapestLeft);
+        } else {
+            return costs.lastIndexOf(cheapestRight);
+        }
+    } else {
+        return costs.indexOf(Math.min(...costs));
+    }
 }
