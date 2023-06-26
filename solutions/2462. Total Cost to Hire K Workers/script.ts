3e14dc76107f20eb59747ae8f3f5d2ae83ccf106
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-06-26 19:06:22 +0300

    #2462 Improve readability

--- solutions/2462. Total Cost to Hire K Workers/script.ts
+++ solutions/2462. Total Cost to Hire K Workers/script.ts
@@ -5,13 +5,15 @@ function totalCost(costs: number[], k: number, candidates: number): number {
     let hired: number = 0;
 
     while (hired < k) {
-        if (costs.length > candidates * 2) {
+        const hasPools: boolean = costs.length > candidates * 2;
+
+        if (hasPools) {
             const leftPool: number[] = costs.slice(0, candidates);
             const rightPool: number[] = costs.slice(costs.length - candidates)
             const cheapestLeft: number = Math.min(...leftPool)
             const cheapestRight: number = Math.min(...rightPool)
 
-            if(cheapestLeft <= cheapestRight) {
+            if (cheapestLeft <= cheapestRight) {
                 cost += costs.splice(costs.indexOf(cheapestLeft), 1)[0];
             } else {
                 cost += costs.splice(costs.lastIndexOf(cheapestRight), 1)[0];
