f362a97f5b54b892216983787dc495427ab59bd7
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-06-26 20:05:06 +0300

    #2462 Optimize memory

--- solutions/2462. Total Cost to Hire K Workers/__tests__/script.test.ts
+++ solutions/2462. Total Cost to Hire K Workers/__tests__/script.test.ts
@@ -6,6 +6,11 @@ describe('2462. Total Cost to Hire K Workers', (): void => {
             .toBe(11);
     });
 
+    it('Hire few more workers', (): void => {
+        expect(totalCost([31, 25, 72, 79, 74, 65, 84, 91, 18, 59, 27, 9, 81, 33, 17, 58], 11, 2))
+            .toBe(423);
+    });
+
     it('Hire some many workers', (): void => {
         expect(totalCost([50, 80, 34, 9, 86, 20, 67, 94, 65, 82, 40, 79, 74, 92, 84, 37, 19, 16, 85, 20, 79, 25, 89, 55, 67, 84, 3, 79, 38, 16, 44, 2, 54, 58], 7, 12))
             .toBe(95);
--- solutions/2462. Total Cost to Hire K Workers/script.ts
+++ solutions/2462. Total Cost to Hire K Workers/script.ts
@@ -17,29 +17,27 @@ function findCheapestWorker(costs: number[], candidates: number): number {
     const hasPools: boolean = costs.length > candidates * 2;
 
     if (hasPools) {
-        const leftPool: number[] = costs.slice(0, candidates);
-        const rightPool: number[] = costs.slice(costs.length - candidates)
-        const cheapestLeftIndex: number = findIndexOfMin(leftPool)
-        const cheapestRightIndex: number = findIndexOfMin(rightPool)
+        const cheapestLeftIndex: number = findIndexOfMin(costs, 0, candidates)
+        const cheapestRightIndex: number = findIndexOfMin(costs, costs.length - candidates, costs.length)
 
-        if (costs[cheapestLeftIndex] <= costs[costs.length - candidates + cheapestRightIndex]) {
+        if (costs[cheapestLeftIndex] <= costs[cheapestRightIndex]) {
             return cheapestLeftIndex;
         } else {
-            return costs.length - candidates + cheapestRightIndex;
+            return cheapestRightIndex;
         }
     } else {
-        return findIndexOfMin(costs);
+        return findIndexOfMin(costs, 0, costs.length);
     }
 }
 
-function findIndexOfMin(nums: number[]): number {
+function findIndexOfMin(nums: number[], minIndex: number, maxIndex: number): number {
     if (nums.length === 0) {
         throw new Error("Not enough data");
     }
 
-    let min: number = nums[0];
-    let indexOfMin: number = 0;
-    for (let i: number = 0; i < nums.length; i++) {
+    let min: number = nums[minIndex];
+    let indexOfMin: number = minIndex;
+    for (let i: number = minIndex; i < nums.length && i < maxIndex; i++) {
         if (nums[i] < min) {
             min = nums[i];
             indexOfMin = i;
