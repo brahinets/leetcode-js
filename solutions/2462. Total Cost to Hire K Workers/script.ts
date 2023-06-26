e4139bd650c554b41750ef82ce30f22abe35668d
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-06-26 19:27:34 +0300

    #2462 Optimize min index calculation

--- solutions/2462. Total Cost to Hire K Workers/script.ts
+++ solutions/2462. Total Cost to Hire K Workers/script.ts
@@ -19,15 +19,32 @@ function findCheapestWorker(costs: number[], candidates: number): number {
     if (hasPools) {
         const leftPool: number[] = costs.slice(0, candidates);
         const rightPool: number[] = costs.slice(costs.length - candidates)
-        const cheapestLeft: number = Math.min(...leftPool)
-        const cheapestRight: number = Math.min(...rightPool)
+        const cheapestLeftIndex: number = findIndexOfMin(leftPool)
+        const cheapestRightIndex: number = findIndexOfMin(rightPool)
 
-        if (cheapestLeft <= cheapestRight) {
-            return costs.indexOf(cheapestLeft);
+        if (costs[cheapestLeftIndex] <= costs[costs.length - candidates + cheapestRightIndex]) {
+            return cheapestLeftIndex;
         } else {
-            return costs.lastIndexOf(cheapestRight);
+            return costs.length - candidates + cheapestRightIndex;
         }
     } else {
-        return costs.indexOf(Math.min(...costs));
+        return findIndexOfMin(costs);
     }
+}
+
+function findIndexOfMin(nums: number[]): number {
+    if (nums.length === 0) {
+        throw new Error("Not enough data");
+    }
+
+    let min: number = nums[0];
+    let indexOfMin: number = 0;
+    for (let i: number = 0; i < nums.length; i++) {
+        if (nums[i] < min) {
+            min = nums[i];
+            indexOfMin = i;
+        }
+    }
+
+    return indexOfMin;
 }
