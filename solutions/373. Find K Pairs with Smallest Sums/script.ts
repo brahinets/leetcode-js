04d8b736ba3558b39bf38ad25634d29a7d86c7cc
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-06-28 09:38:27 +0300

    #373 Speed up, by caching max value

--- solutions/373. Find K Pairs with Smallest Sums/script.ts
+++ solutions/373. Find K Pairs with Smallest Sums/script.ts
@@ -15,29 +15,42 @@ function kSmallestPairs(nums1: number[], nums2: number[], k: number): number[][]
 class PriorityHeap {
     data: number[][];
     limit: number;
+    max: number | undefined;
 
     constructor(limit: number) {
         this.data = [];
         this.limit = limit;
+        this.max = undefined;
     }
 
     add(p1: number, p2: number): void {
         if (this.data.length < this.limit) {
             this.data.push([p1, p2]);
-        } else {
-            let max: number = this.data[0][0] + this.data[0][1];
-            let maxIndex: number = 0;
-            for (let i: number = 1; i < this.data.length; i++) {
-                const pairSum: number = this.data[i][0] + this.data[i][1];
-                if (pairSum > max) {
-                    max = pairSum;
-                    maxIndex = i;
-                }
+
+            if(!this.max || p1 + p2 > this.max) {
+                this.max = p1 + p2;
             }
+        } else {
+            const [max, maxIndex]: number[] = findMaxIndex(this.data);
 
             if (p1 + p2 < max) {
                 this.data[maxIndex] = [p1, p2];
+                this.max = findMaxIndex(this.data)[0];
             }
         }
     }
+}
+
+function findMaxIndex(data: number[][]): number[] {
+    let max: number = data[0][0] + data[0][1];
+    let maxIndex: number = 0;
+    for (let i: number = 1; i < data.length; i++) {
+        const pairSum: number = data[i][0] + data[i][1];
+        if (pairSum > max) {
+            max = pairSum;
+            maxIndex = i;
+        }
+    }
+
+    return [max, maxIndex];
 }
