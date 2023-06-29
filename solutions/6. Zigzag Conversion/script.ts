c7a975744cf8ce9b97db6a54148814938a80c49c
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-06-29 16:32:09 +0300

    #6 Improve type-safety

--- solutions/6. Zigzag Conversion/script.ts
+++ solutions/6. Zigzag Conversion/script.ts
@@ -1,6 +1,6 @@
 export {convert}
 
-const convert = function (s: string, numRows: number): string {
+function convert(s: string, numRows: number): string {
     const matrix: string[][] = [];
     for (let i: number = 0; i < numRows; i++) {
         const row: string[] = [];
@@ -13,14 +13,14 @@ const convert = function (s: string, numRows: number): string {
     while (chars.length > 0) {
         if (toBottom) {
             for (let i: number = 0; i < numRows && chars.length > 0; i++) {
-                let char: string = chars.shift() || '';
+                const char: string = chars.shift() || '';
                 matrix[i].push(char);
             }
         } else {
             for (let i: number = numRows - 1; i >= 0 && chars.length > 0; i--) {
                 for (let j: number = 1; j < numRows - 1 && chars.length > 0; j++) {
                     if (i + j + 1 === numRows) {
-                        let char: string = chars.shift() || '';
+                        const char: string = chars.shift() || '';
                         matrix[i].push(char);
                     } else {
                         matrix[i].push("");
