998330f72396374e8ee8cdd25fed0ff9079db9ce
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-07-08 19:41:42 +0300

    #2551 Add few more test cases

--- solutions/2551. Put Marbles in Bags/__tests__/script.test.ts
+++ solutions/2551. Put Marbles in Bags/__tests__/script.test.ts
@@ -16,4 +16,14 @@ describe('2551. Put Marbles in Bags', (): void => {
         expect(putMarbles([1, 3, 5, 1], 2))
             .toBe(4);
     });
+
+    it('Min and Max are also different', (): void => {
+        expect(putMarbles([1, 4, 2, 5, 2], 3))
+            .toBe(3);
+    });
+
+    it.skip('Load test', (): void => {
+        expect(putMarbles([54, 6, 34, 66, 63, 52, 39, 62, 46, 75, 28, 65, 18, 37, 18, 13, 33, 69, 19, 40, 13, 10, 43, 61, 72], 4))
+            .toBe(4);
+    });
 })
