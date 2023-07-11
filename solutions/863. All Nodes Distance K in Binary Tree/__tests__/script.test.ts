247d78f735526b1908260552e9b7d863fb6171fc
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-07-11 09:05:53 +0300

    #863 Search target node for start

--- solutions/863. All Nodes Distance K in Binary Tree/__tests__/script.test.ts
+++ solutions/863. All Nodes Distance K in Binary Tree/__tests__/script.test.ts
@@ -1,8 +1,18 @@
-import {distanceK} from '../script'
+import {distanceK, TreeNode} from '../script'
 
 describe('863. All Nodes Distance K in Binary Tree', (): void => {
     it('Zero elements with distance for empty tree', (): void => {
         expect(distanceK(null, null, 3))
+            .toStrictEqual([]);
+    });
+
+    it('Zero elements with distance, when node not found', (): void => {
+        expect(distanceK(new TreeNode(1), new TreeNode(2), 3))
+            .toStrictEqual([]);
+    });
+
+    it('Zero elements with distance, when node found', (): void => {
+        expect(distanceK(new TreeNode(1), new TreeNode(1), 3))
             .toStrictEqual([]);
     });
 });
--- solutions/863. All Nodes Distance K in Binary Tree/script.ts
+++ solutions/863. All Nodes Distance K in Binary Tree/script.ts
@@ -3,11 +3,38 @@ import {TreeNode} from "../../common/TreeNode";
 export {distanceK, TreeNode};
 
 function distanceK(root: TreeNode | null, target: TreeNode | null, k: number): number[] {
-    const result:number[] = [];
+    const result: number[] = [];
 
-    if(!root) {
+    if (!root || !target) {
+        return result;
+    }
+
+    const startNode: TreeNode | null = findNode(root, target);
+    if (!startNode) {
         return result;
     }
 
     return result;
+}
+
+function findNode(root: TreeNode, target: TreeNode): TreeNode | null {
+    if (root.val === target.val) {
+        return root;
+    }
+
+    if (root.left) {
+        const lefty: TreeNode | null = findNode(root.left, target);
+        if (lefty) {
+            return lefty;
+        }
+    }
+
+    if (root.right) {
+        const righty: TreeNode | null = findNode(root.right, target);
+        if (righty) {
+            return righty;
+        }
+    }
+
+    return null;
 }
