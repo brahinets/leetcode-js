f4d2264e11182a9f770889274e77f59c86b592e3
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-07-13 01:10:28 +0300

    #802 Remove now useless structure

--- solutions/802. Find Eventual Safe States/script.ts
+++ solutions/802. Find Eventual Safe States/script.ts
@@ -3,18 +3,19 @@ export {eventualSafeNodes};
 function eventualSafeNodes(graphMatrix: number[][]): number[] {
     const result: number[] = [];
 
-    const graph: Map<number, Set<number>> = buildGraph(graphMatrix);
-    for (const node of graph) {
-        if (safeNode(node[1], graph, new Set<number>([node[0]]))) {
-            result.push(node[0]);
+    for (let nodeId: number = 0; nodeId < graphMatrix.length; nodeId++) {
+        if (safeNode(nodeId, graphMatrix, new Set<number>([nodeId]))) {
+            result.push(nodeId);
         }
     }
 
     return result.sort((a: number, b: number): number => a - b);
 }
 
-function safeNode(neighbours: Set<number>, graph: Map<number, Set<number>>, visited: Set<number>): boolean {
-    if (neighbours === undefined || neighbours.size === 0) {
+function safeNode(nodeId: number, graphMatrix: number[][], visited: Set<number>): boolean {
+    const neighbours: number[] | undefined = graphMatrix[nodeId];
+
+    if (neighbours === undefined || neighbours.length === 0) {
         return true;
     }
 
@@ -23,23 +24,10 @@ function safeNode(neighbours: Set<number>, graph: Map<number, Set<number>>, visi
             return false;
         }
 
-        if (!safeNode(graph.get(n) ?? new Set<number>(), graph, new Set<number>([...visited, n]))) {
+        if (!safeNode(n, graphMatrix, new Set<number>([...visited, n]))) {
             return false;
         }
     }
 
     return true;
-}
-
-function buildGraph(root: number[][]): Map<number, Set<number>> {
-    const graph: Map<number, Set<number>> = new Map<number, Set<number>>();
-
-    for (let from: number = 0; from < root.length; from++) {
-        graph.set(from, new Set<number>());
-        for (const to of root[from]) {
-            graph.get(from)?.add(to);
-        }
-    }
-
-    return graph;
 }
