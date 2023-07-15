3f201e3bb6d8e7664543eb217c01fac2d6a19ba8
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-07-15 09:37:07 +0300

    #1218 Unify naming

--- solutions/1218. Longest Arithmetic Subsequence of Given Difference/script.ts
+++ solutions/1218. Longest Arithmetic Subsequence of Given Difference/script.ts
@@ -1,17 +1,16 @@
 export {longestSubsequence}
 
-function longestSubsequence(nums: number[], difference: number): number {
+function longestSubsequence(arr: number[], difference: number): number {
     const subsequenceCounts: Map<number, number>[] = [];
     let maxCount: number = 1;
 
-    for (let end: number = 0; end < nums.length; end++) {
+    for (let end: number = 0; end < arr.length; end++) {
         subsequenceCounts[end] = new Map<number, number>();
 
         for (let start: number = 0; start < end; start++) {
-            const diff: number = nums[end] - nums[start];
+            const diff: number = arr[end] - arr[start];
             const countByStart: number = subsequenceCounts[start].get(diff) ?? 1;
             const countByEnd: number = countByStart + 1;
-
             subsequenceCounts[end].set(diff, countByEnd);
 
             if (diff === difference) {
