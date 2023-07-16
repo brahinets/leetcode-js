187d803bc0b89f43415aea5598e07bd9409354ac
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-07-16 17:24:59 +0300

    #1125 Add heavy test cases

--- solutions/1125. Smallest Sufficient Team/__tests__/script.test.ts
+++ solutions/1125. Smallest Sufficient Team/__tests__/script.test.ts
@@ -29,4 +29,27 @@ describe('1125. Smallest Sufficient Team', (): void => {
         )).toStrictEqual([6]);
     });
 
+    // TODO 1125 Speed Up to pass check on site
+    it('Very many people', (): void => {
+        expect(smallestSufficientTeam(
+            ["vgzp", "urfys", "nvkjkzxrfn", "pqznypwpcv", "bdfmsjlddbinqo", "xvgkyzqpmzwcxol", "twlau", "vybgxpyq", "edpuw", "w"],
+            [["xvgkyzqpmzwcxol"], ["w"], ["urfys", "w"], ["urfys"], ["bdfmsjlddbinqo"], ["xvgkyzqpmzwcxol"], [], [], ["w"], ["vgzp", "w"], ["nvkjkzxrfn", "bdfmsjlddbinqo", "edpuw"], ["urfys"], ["bdfmsjlddbinqo", "xvgkyzqpmzwcxol", "twlau"], ["nvkjkzxrfn", "bdfmsjlddbinqo", "vybgxpyq"], ["vgzp", "edpuw"], ["urfys", "xvgkyzqpmzwcxol"], ["bdfmsjlddbinqo", "w"], ["nvkjkzxrfn", "bdfmsjlddbinqo", "w"], ["bdfmsjlddbinqo", "vybgxpyq"], ["urfys", "bdfmsjlddbinqo"], ["vgzp"], ["nvkjkzxrfn", "xvgkyzqpmzwcxol"], ["nvkjkzxrfn"], ["vgzp", "edpuw"], [], ["bdfmsjlddbinqo"], ["vgzp", "nvkjkzxrfn", "xvgkyzqpmzwcxol"], ["vgzp", "bdfmsjlddbinqo", "edpuw", "w"], ["xvgkyzqpmzwcxol", "vybgxpyq", "w"], ["pqznypwpcv", "vybgxpyq"], ["vybgxpyq"], ["twlau"], ["vgzp"], ["vgzp", "twlau", "edpuw"], ["nvkjkzxrfn", "bdfmsjlddbinqo"], ["bdfmsjlddbinqo"], ["vgzp"], ["urfys"], ["nvkjkzxrfn"], ["nvkjkzxrfn", "edpuw"], ["vgzp", "nvkjkzxrfn"], [], ["vgzp", "nvkjkzxrfn"], ["xvgkyzqpmzwcxol"], [], ["nvkjkzxrfn", "xvgkyzqpmzwcxol", "vybgxpyq"], ["vybgxpyq"], ["nvkjkzxrfn"], ["nvkjkzxrfn", "edpuw"], ["vgzp"]]
+        )).toStrictEqual([33, 15, 17, 29,]);
+    });
+
+    // TODO 1125 Speed Up to pass check on site
+    it('Very many people as well', (): void => {
+        expect(smallestSufficientTeam(
+            ["ohmvnkxnokpnwxmr", "jdjdb", "azdwg", "xvn", "mljb", "rdaavykiextpr", "ztp"],
+            [["azdwg"], ["ohmvnkxnokpnwxmr", "mljb"], ["jdjdb"], [], [], [], ["xvn", "mljb"], [], ["xvn"], [], ["xvn"], ["jdjdb", "mljb"], [], ["ohmvnkxnokpnwxmr", "mljb", "ztp"], ["ohmvnkxnokpnwxmr"], [], [], ["ohmvnkxnokpnwxmr", "jdjdb"], ["jdjdb"], [], [], [], ["azdwg", "mljb"], ["ztp"], ["xvn"], ["jdjdb"], ["mljb"], ["xvn"], ["rdaavykiextpr"], [], [], ["ohmvnkxnokpnwxmr", "jdjdb", "mljb", "rdaavykiextpr"], [], ["jdjdb", "mljb", "rdaavykiextpr", "ztp"], ["ohmvnkxnokpnwxmr"], ["rdaavykiextpr", "ztp"], ["mljb"], ["rdaavykiextpr"], [], ["azdwg"], [], ["rdaavykiextpr"], ["jdjdb"], [], ["mljb", "ztp"], [], ["rdaavykiextpr", "ztp"], ["jdjdb", "azdwg"], ["xvn", "ztp"], []]
+        )).toStrictEqual([31, 47, 48]);
+    });
+
+    // TODO 1125 Speed Up to pass check on site
+    it('Very many people again', (): void => {
+        expect(smallestSufficientTeam(
+            ["yvstvgspxy", "ohoawazvdz", "yvzuqettmru", "finmcneglinx", "daspbvnxn", "bzbipeubqgvtzl", "ufbsjdq", "wbtugbdzqcqpj", "qmucetzldtng", "elhzszjmeffcgtin"],
+            [["yvstvgspxy", "yvzuqettmru", "daspbvnxn", "ufbsjdq"], ["finmcneglinx", "daspbvnxn", "bzbipeubqgvtzl", "elhzszjmeffcgtin"], ["elhzszjmeffcgtin"], [], ["bzbipeubqgvtzl"], ["bzbipeubqgvtzl", "qmucetzldtng"], ["elhzszjmeffcgtin"], ["ufbsjdq", "qmucetzldtng"], ["ufbsjdq"], ["bzbipeubqgvtzl"], ["ohoawazvdz", "finmcneglinx", "daspbvnxn", "bzbipeubqgvtzl"], ["daspbvnxn", "ufbsjdq", "elhzszjmeffcgtin"], ["yvstvgspxy", "daspbvnxn", "bzbipeubqgvtzl", "qmucetzldtng"], [], ["yvstvgspxy", "daspbvnxn"], ["bzbipeubqgvtzl", "ufbsjdq"], ["ufbsjdq"], ["finmcneglinx", "daspbvnxn", "wbtugbdzqcqpj", "qmucetzldtng", "elhzszjmeffcgtin"], ["daspbvnxn", "qmucetzldtng"], ["elhzszjmeffcgtin"], [], ["yvstvgspxy", "bzbipeubqgvtzl", "wbtugbdzqcqpj"], [], ["yvstvgspxy", "bzbipeubqgvtzl", "qmucetzldtng"], [], ["yvstvgspxy", "bzbipeubqgvtzl"], ["bzbipeubqgvtzl"], ["finmcneglinx", "daspbvnxn", "bzbipeubqgvtzl", "qmucetzldtng"], ["yvstvgspxy", "finmcneglinx", "daspbvnxn", "wbtugbdzqcqpj", "qmucetzldtng"], [], ["finmcneglinx"], ["daspbvnxn", "elhzszjmeffcgtin"], ["daspbvnxn", "elhzszjmeffcgtin"], ["ufbsjdq", "qmucetzldtng"], ["finmcneglinx", "daspbvnxn", "qmucetzldtng"], ["ufbsjdq"], ["yvzuqettmru", "qmucetzldtng"], ["qmucetzldtng"], ["qmucetzldtng"], ["bzbipeubqgvtzl", "ufbsjdq"]]
+        )).toStrictEqual([0, 10, 17]);
+    });
 });
