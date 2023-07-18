c44373b5a0cac913b6c90673f6bfbd2cda282ea9
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-07-18 11:37:06 +0300

    #146 Add missing test case for usage log refresh

--- solutions/146. LRU Cache/__tests__/script.test.ts
+++ solutions/146. LRU Cache/__tests__/script.test.ts
@@ -23,7 +23,7 @@ describe('146. LRU Cache', (): void => {
         expect(cache.get(4)).toBe(40);
     });
 
-    it('Test repeatable insertion', (): void => {
+    it('Set of the same should refresh value', (): void => {
         const cache: LRUCache = new LRUCache(2);
 
         expect(cache.get(1)).toBe(-1);
@@ -38,5 +38,20 @@ describe('146. LRU Cache', (): void => {
         expect(cache.get(1)).toBe(2);
         expect(cache.get(2)).toBe(6);
 
+    });
+
+    it('Get should refresh usage log', (): void => {
+        const cache: LRUCache = new LRUCache(2);
+
+        cache.put(1, 10);
+        expect(cache.get(1)).toBe(10);
+
+        cache.put(2, 20);
+        expect(cache.get(1)).toBe(10);
+
+        cache.put(3, 30);
+        expect(cache.get(1)).toBe(10);
+        expect(cache.get(2)).toBe(-1);
+        expect(cache.get(3)).toBe(30);
     });
 })
