3b90cc918b516678310c0d4b6d1b74d4757c5058
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-07-20 00:28:37 +0300

    #435 Use math theory to speed up solution

--- solutions/435. Non-overlapping Intervals/__tests__/script.test.ts
+++ solutions/435. Non-overlapping Intervals/__tests__/script.test.ts
@@ -1,7 +1,12 @@
 import {eraseOverlapIntervals} from '../script'
 
 describe('435. Non-overlapping Intervals', (): void => {
-    it('Remove nothing, already non overlapping', (): void => {
+    it('Remove nothing, already non overlapping empty', (): void => {
+        expect(eraseOverlapIntervals([]))
+            .toBe(0);
+    });
+
+    it('Remove nothing, already non overlapping non-empty', (): void => {
         expect(eraseOverlapIntervals([[1, 2], [2, 3]]))
             .toBe(0);
     });
@@ -14,5 +19,10 @@ describe('435. Non-overlapping Intervals', (): void => {
     it('Remove multiple, to non overlap', (): void => {
         expect(eraseOverlapIntervals([[1, 2], [1, 2], [1, 2]]))
             .toBe(2);
+    });
+
+    it('Remove multiple, to non overlap. Load Test', (): void => {
+        expect(eraseOverlapIntervals([[-52, 31], [-73, -26], [82, 97], [-65, -11], [-62, -49], [95, 99], [58, 95], [-31, 49], [66, 98], [-63, 2], [30, 47], [-40, -26]]))
+            .toBe(7);
     });
 })
--- solutions/435. Non-overlapping Intervals/script.ts
+++ solutions/435. Non-overlapping Intervals/script.ts
@@ -1,44 +1,24 @@
 export {eraseOverlapIntervals}
 
 function eraseOverlapIntervals(intervals: number[][]): number {
-    const nonOverlappedIntervals: number[][][] = unoverlapIntervals(intervals);
-    return intervals.length - Math.max(...nonOverlappedIntervals.map((interval: number[][]): number => interval.length))
-}
+    if (intervals.length === 0) {
+        return 0
+    }
 
-function unoverlapIntervals(intervals: number[][]): number[][][] {
-    const result: number[][][] = [];
+    intervals.sort((a: number[], b: number[]): number => a[1] - b[1]);
 
-    intervals.sort((a: number[], b: number[]): number => a[1] - b[1])
+    let latestValidEnd: number = intervals[0][1];
+    let intervalsToRemove: number = 0;
 
-    const overlapped: number[] = findOverlaps(intervals);
-    if (overlapped.length === 0) {
-        result.push(intervals);
-    } else {
-        for (const o of overlapped) {
-            result.push(...unoverlapIntervals([...intervals.slice(0, o), ...intervals.slice(o + 1)]));
+    for (let i: number = 1; i < intervals.length; i++) {
+        const [start, end] = intervals[i];
+
+        if (start >= latestValidEnd) {
+            latestValidEnd = end;
+        } else {
+            intervalsToRemove++;
         }
     }
 
-    return result;
-}
-
-function findOverlaps(intervals: number[][]): number[] {
-    const overlaps: number[] = [];
-
-    if (intervals.length === 1) {
-        return overlaps;
-    }
-
-    for (let i: number = 0; i < intervals.length - 1; i++) {
-        if (overlap(intervals[i], intervals[i + 1])) {
-            overlaps.push(i, i + 1)
-        }
-    }
-
-    return overlaps;
-
-}
-
-function overlap(a: number[], b: number[]) {
-    return (a[1] > b[0] && a[0] < b[1]) || (b[1] > a[0] && b[0] < a[1]);
+    return intervalsToRemove;
 }
