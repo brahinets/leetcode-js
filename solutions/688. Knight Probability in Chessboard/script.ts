f488c2615baaf9b12316bc2fcd8b7d6cf285e946
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-07-22 11:05:03 +0300

    #688 Optimize memory

--- solutions/688. Knight Probability in Chessboard/script.ts
+++ solutions/688. Knight Probability in Chessboard/script.ts
@@ -8,21 +8,31 @@ const turns: number[][] = [
 ]
 
 function knightProbability(n: number, k: number, row: number, column: number): number {
-    return countSafeNds(n, k, row, column) / 8 ** k;
+    const memo: Map<string, number> = new Map<string, number>();
+    return countSafeNds(n, k, row, column, memo) / (8 ** k);
 }
 
-function countSafeNds(n: number, k: number, row: number, column: number): number {
+function countSafeNds(n: number, k: number, row: number, column: number, memo: Map<string, number>): number {
     let count: number = 0;
 
     if (row >= 0 && row < n && column >= 0 && column < n) {
         if (k === 0) {
             count = 1;
         } else {
-            for (const turn of turns) {
-                const nextRow: number = row + turn[0];
-                const nextCol: number = column + turn[1];
-                count += countSafeNds(n, k - 1, nextRow, nextCol);
+            const memoKey: string = `${row}${column}`;
+            const safe: number | undefined = memo.get(memoKey);
+
+            if (safe !== undefined) {
+                count = safe;
+            } else {
+                for (const turn of turns) {
+                    const nextRow: number = row + turn[0];
+                    const nextCol: number = column + turn[1];
+                    count += countSafeNds(n, k - 1, nextRow, nextCol, memo);
+                }
             }
+
+            memo.set(memoKey, count);
         }
     }
 
