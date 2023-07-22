855f3de28712b6814b3af85950c9c39a11938d5d
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-07-22 10:28:43 +0300

    #688 Optimize memory

--- solutions/688. Knight Probability in Chessboard/script.ts
+++ solutions/688. Knight Probability in Chessboard/script.ts
@@ -8,34 +8,22 @@ const turns: number[][] = [
 ]
 
 function knightProbability(n: number, k: number, row: number, column: number): number {
-    const ends: number[][] = [];
-    collectPossibleEnds(n, k, row, column, ends);
-
-    let keep: number = 0;
-
-    for (const end of ends) {
-        const endRow: number = end[0];
-        const endCol: number = end[1];
-
-        if (endRow >= 0 && endRow < n && endCol >= 0 && endCol < n) {
-            keep++;
-        }
-    }
-
-    return keep / 8 ** k;
+    return countSafeNds(n, k, row, column) / 8 ** k;
 }
 
-function collectPossibleEnds(n: number, k: number, row: number, column: number, possibleEnds: number[][]): void {
-    if (k === 0) {
-        possibleEnds.push([row, column]);
-        return;
+function countSafeNds(n: number, k: number, row: number, column: number): number {
+    if (k === 0 && row >= 0 && row < n && column >= 0 && column < n) {
+        return 1;
     }
 
+    let count: number = 0;
     if (row >= 0 && row < n && column >= 0 && column < n) {
         for (const turn of turns) {
             const nextRow: number = row + turn[0];
             const nextCol: number = column + turn[1];
-            collectPossibleEnds(n, k - 1, nextRow, nextCol, possibleEnds);
+            count += countSafeNds(n, k - 1, nextRow, nextCol);
         }
     }
+
+    return count;
 }
