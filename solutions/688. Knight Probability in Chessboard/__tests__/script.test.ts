cf5bd7e32713170c37633be8eaa272fbfc410635
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-07-22 10:47:39 +0300

    #688 Fix precision assertion

--- solutions/688. Knight Probability in Chessboard/__tests__/script.test.ts
+++ solutions/688. Knight Probability in Chessboard/__tests__/script.test.ts
@@ -3,16 +3,21 @@ import {knightProbability} from '../script'
 describe('688. Knight Probability in Chessboard', (): void => {
     it('Will not leave the board with few steps', (): void => {
         expect(knightProbability(1, 0, 0, 0))
-            .toStrictEqual(1);
+            .toBeCloseTo(1, 5);
+    });
+
+    it('Will likely leave the board with few steps', (): void => {
+        expect(knightProbability(3, 3, 0, 0))
+            .toBeCloseTo(0.01562, 5);
     });
 
     it('Will leave the board. Already out of the board', (): void => {
         expect(knightProbability(1, 1, -1, -1))
-            .toStrictEqual(0);
+            .toBeCloseTo(0, 5);
     });
 
     it('Most likely will leave the board with few steps', (): void => {
         expect(knightProbability(3, 2, 0, 0))
-            .toStrictEqual(0.06250);
+            .toBeCloseTo(0.06250, 5);
     });
 })
