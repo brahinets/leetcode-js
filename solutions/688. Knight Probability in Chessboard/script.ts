34a44d7b22b68db6bd3f8108a0f35aaa7cfdc549
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-07-22 10:30:53 +0300

    #688 Clean up code

--- solutions/688. Knight Probability in Chessboard/script.ts
+++ solutions/688. Knight Probability in Chessboard/script.ts
@@ -12,16 +12,17 @@ function knightProbability(n: number, k: number, row: number, column: number): n
 }
 
 function countSafeNds(n: number, k: number, row: number, column: number): number {
-    if (k === 0 && row >= 0 && row < n && column >= 0 && column < n) {
-        return 1;
-    }
-
     let count: number = 0;
+
     if (row >= 0 && row < n && column >= 0 && column < n) {
-        for (const turn of turns) {
-            const nextRow: number = row + turn[0];
-            const nextCol: number = column + turn[1];
-            count += countSafeNds(n, k - 1, nextRow, nextCol);
+        if (k === 0) {
+            count = 1;
+        } else {
+            for (const turn of turns) {
+                const nextRow: number = row + turn[0];
+                const nextCol: number = column + turn[1];
+                count += countSafeNds(n, k - 1, nextRow, nextCol);
+            }
         }
     }
 
