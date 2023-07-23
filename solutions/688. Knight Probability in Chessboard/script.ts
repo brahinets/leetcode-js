e460a2cb6d0a44c92a616a8184f0a2230898b8a3
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-07-23 23:48:19 +0300

    #688 Improve readability

--- solutions/688. Knight Probability in Chessboard/script.ts
+++ solutions/688. Knight Probability in Chessboard/script.ts
@@ -15,26 +15,32 @@ function knightProbability(n: number, k: number, row: number, column: number): n
 function countSafeNds(n: number, k: number, row: number, column: number, memo: Map<string, number>): number {
     let count: number = 0;
 
-    if (row >= 0 && row < n && column >= 0 && column < n) {
-        if (k === 0) {
-            count = 1;
+    if (!isInsideBoard(n, row, column)) {
+        return count;
+    }
+
+    if (k === 0) {
+        count = 1;
+    } else {
+        const memoKey: string = `${row}${column}`;
+        const safe: number | undefined = memo.get(memoKey);
+
+        if (safe !== undefined) {
+            count = safe;
         } else {
-            const memoKey: string = `${row}${column}`;
-            const safe: number | undefined = memo.get(memoKey);
-
-            if (safe !== undefined) {
-                count = safe;
-            } else {
-                for (const turn of turns) {
-                    const nextRow: number = row + turn[0];
-                    const nextCol: number = column + turn[1];
-                    count += countSafeNds(n, k - 1, nextRow, nextCol, memo);
-                }
+            for (const turn of turns) {
+                const nextRow: number = row + turn[0];
+                const nextCol: number = column + turn[1];
+                count += countSafeNds(n, k - 1, nextRow, nextCol, memo);
             }
-
-            memo.set(memoKey, count);
         }
+
+        memo.set(memoKey, count);
     }
 
     return count;
+}
+
+function isInsideBoard(boardSize: number, row: number, column: number): boolean {
+    return row >= 0 && row < boardSize && column >= 0 && column < boardSize;
 }
