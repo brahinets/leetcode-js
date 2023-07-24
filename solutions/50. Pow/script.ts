cb0c7f57c13c35d0d33dc47774ba5773893f718d
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-07-24 10:53:34 +0300

    #50 Optimize zero power

--- solutions/50. Pow/script.ts
+++ solutions/50. Pow/script.ts
@@ -1,12 +1,15 @@
 export {myPow}
 
 function myPow(x: number, n: number): number {
-    let result: number = 1;
+    if (n === 0) {
+        return 1;
+    }
 
-    if(Math.abs(x) === 1) {
+    if (Math.abs(x) === 1) {
         return (Math.abs(n) % 2) * x;
     }
 
+    let result: number = 1;
     for (let i: number = 0; i < Math.abs(n); i++) {
         result *= x;
     }
