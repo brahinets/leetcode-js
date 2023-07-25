4f91d597e11ce98303a90cfc9340155fccf8f105
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-07-25 07:58:29 +0300

    #852 Add validation for not enough data

--- solutions/852. Peak Index in a Mountain Array/script.ts
+++ solutions/852. Peak Index in a Mountain Array/script.ts
@@ -1,6 +1,10 @@
 export {peakIndexInMountainArray}
 
 function peakIndexInMountainArray(arr: number[]): number {
+    if(arr.length < 3) {
+        throw new Error("Input must contain at least three elements");
+    }
+
     let max: number = arr[0];
     let maxIndex: number = 0;
 
