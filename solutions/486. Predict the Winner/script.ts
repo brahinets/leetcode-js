3bc2601096aa500945adc62f6dbcbc5ef146faf1
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-07-28 16:59:31 +0300

    #486 Use virtual boundaries for turns

--- solutions/486. Predict the Winner/script.ts
+++ solutions/486. Predict the Winner/script.ts
@@ -1,16 +1,16 @@
 export {PredictTheWinner}
 
 function PredictTheWinner(nums: number[]): boolean {
-    return maxDiff(nums) >= 0;
+    return maxDiff(nums, 0, nums.length - 1) >= 0
 }
 
-function maxDiff(nums: number[]): number {
-    if (nums.length === 1) {
-        return nums[0];
+function maxDiff(nums: number[], left: number, right: number): number {
+    if (left === right) {
+        return nums[left];
     }
 
-    const takeLeft: number = nums[0] - maxDiff(nums.slice(1))
-    const takeRight: number = nums[nums.length - 1] - maxDiff(nums.slice(0, nums.length - 1))
+    const takeLeft: number = nums[left] - maxDiff(nums, left + 1, right)
+    const takeRight: number = nums[right] - maxDiff(nums, left, right - 1)
 
     return Math.max(takeLeft, takeRight)
 }
