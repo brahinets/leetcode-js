37adf02a94d3ce8bf7d4b978476c9d6b5e8d7530
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-07-30 12:34:07 +0300

    #808 Improve naming

--- solutions/808. Soup Servings/__tests__/script.test.ts
+++ solutions/808. Soup Servings/__tests__/script.test.ts
@@ -1,22 +1,22 @@
 import {soupServings} from '../script'
 
 describe('808. Soup Servings', (): void => {
-    it('Mid precision', (): void => {
+    it('Small chance', (): void => {
         expect(soupServings(50))
             .toBeCloseTo(0.62500, 5);
     });
 
-    it('Low precision', (): void => {
+    it('Medium chance', (): void => {
         expect(soupServings(100))
             .toBeCloseTo(0.71875, 5);
     });
 
-    it('High precision', (): void => {
+    it('High chance', (): void => {
         expect(soupServings(850))
             .toBeCloseTo(0.96612, 5);
     });
 
-    it('Very high chance', (): void => {
+    it('100% chance', (): void => {
         expect(soupServings(660295675))
             .toBe(1);
     });
