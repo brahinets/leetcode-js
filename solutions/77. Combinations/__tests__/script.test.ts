60cf805896ff8cf0cd3e9df6709a7af991d674cf
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-08-01 20:43:21 +0300

    #77 Combinations

---
+++ solutions/77. Combinations/__tests__/script.test.ts
@@ -0,0 +1,13 @@
+import {combine} from '../script'
+
+describe('77. Combinations', (): void => {
+    it('Single combination', (): void => {
+        expect(combine(1, 1))
+            .toStrictEqual([[1]]);
+    });
+
+    it('Many combinations', (): void => {
+        expect(combine(4, 2))
+            .toStrictEqual([[1, 2], [1, 3], [1, 4], [2, 3], [2, 4], [3, 4]]);
+    });
+})
---
+++ solutions/77. Combinations/script.ts
@@ -0,0 +1,23 @@
+export {combine}
+
+function solve(n: number, k: number, start: number, out: number[], result: number[][]): void {
+    if (out.length === k) {
+        result.push([...out])
+        return
+    }
+
+    for (let i: number = start; i <= n; i++) {
+        out.push(i)
+        solve(n, k, i + 1, [...out], result)
+        out.pop()
+    }
+}
+
+function combine(n: number, k: number): number[][] {
+    const result: number[][] = [];
+    const out: number[] = [];
+
+    solve(n, k, 1, [...out], result)
+
+    return result;
+}
