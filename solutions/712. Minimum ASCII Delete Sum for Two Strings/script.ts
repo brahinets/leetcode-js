01ff5fda5cc53f2447adb96b2fc18d3e2f5ccb04
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-08-02 10:20:09 +0300

    #712 Speed up by using dynamic programming memoization

--- solutions/712. Minimum ASCII Delete Sum for Two Strings/script.ts
+++ solutions/712. Minimum ASCII Delete Sum for Two Strings/script.ts
@@ -1,33 +1,31 @@
+import {matrixOf} from "../../common/array-utils";
+
 export {minimumDeleteSum}
 
 function minimumDeleteSum(s1: string, s2: string): number {
-    let sum: number = 0;
+    const removalsForEquality: number[][] = matrixOf(0, s1.length + 1, s2.length + 1);
 
-    if (!s1.length) {
-        return cost(s2)
+    for (let i: number = 1; i <= s1.length; i++) {
+        removalsForEquality[i][0] = removalsForEquality[i - 1][0] + s1.charCodeAt(i - 1)
+    }
+    for (let j: number = 1; j <= s2.length; j++) {
+        removalsForEquality[0][j] = removalsForEquality[0][j - 1] + s2.charCodeAt(j - 1)
     }
 
-    if (!s2.length) {
-        return cost(s1)
+    for (let i: number = 1; i <= s1.length; i++) {
+        for (let j: number = 1; j <= s2.length; j++) {
+            if (s1.charCodeAt(i - 1) === s2.charCodeAt(j - 1)) {
+                removalsForEquality[i][j] = removalsForEquality[i - 1][j - 1]
+            } else {
+                removalsForEquality[i][j] = Math.min(
+                    s1.charCodeAt(i - 1) + removalsForEquality[i - 1][j],
+                    s2.charCodeAt(j - 1) + removalsForEquality[i][j - 1]
+                )
+            }
+        }
     }
 
-    const trimFirst: string = s1.slice(0, s1.length - 1);
-    const trimSecond: string = s2.slice(0, s2.length - 1);
-
-    if (s1[s1.length - 1] === s2[s2.length - 1]) {
-        sum += minimumDeleteSum(trimFirst, trimSecond)
-    } else {
-        const trimFirstCost: number = s1[s1.length - 1].charCodeAt(0);
-        const trimSecondCost: number = s2[s2.length - 1].charCodeAt(0);
-
-        sum += Math.min(
-            trimFirstCost + minimumDeleteSum(trimFirst, s2),
-            trimSecondCost + minimumDeleteSum(s1, trimSecond),
-            trimFirstCost + trimSecondCost + minimumDeleteSum(trimFirst, trimSecond)
-        );
-    }
-
-    return sum;
+    return removalsForEquality[s1.length][s2.length];
 }
 
 function cost(s2: string): number {
