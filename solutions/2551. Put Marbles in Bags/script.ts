09ce61f4872708333714f425d3c853a8594aacdd
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-08-06 15:51:13 +0300

    #2551 Remove useless semicolons

--- solutions/2551. Put Marbles in Bags/script.ts
+++ solutions/2551. Put Marbles in Bags/script.ts
@@ -2,76 +2,76 @@ export {putMarbles}
 
 function putMarbles(weights: number[], k: number): number {
     if (weights.length < k) {
-        throw new Error("Not enough data");
+        throw new Error("Not enough data")
     }
 
-    const costs: number[] = [];
+    const costs: number[] = []
 
-    const endsIncluding: number[][] = generateEndsIncluding(k, weights.length);
+    const endsIncluding: number[][] = generateEndsIncluding(k, weights.length)
     for (const ends of endsIncluding) {
-        const buckets: number[][] = breakOnBuckets(weights, ends);
+        const buckets: number[][] = breakOnBuckets(weights, ends)
 
         costs.push(buckets
             .map((bucket: number[]): number => bucket[0] + bucket[bucket.length - 1])
-            .reduce((distributionCost: number, bucketCost: number): number => distributionCost + bucketCost, 0));
+            .reduce((distributionCost: number, bucketCost: number): number => distributionCost + bucketCost, 0))
     }
 
-    return Math.max(...costs) - Math.min(...costs);
+    return Math.max(...costs) - Math.min(...costs)
 }
 
 function generateEndsIncluding(bucketsCount: number, elementsCount: number): number[][] {
-    const distributionBucketsEnds: number[][] = [];
+    const distributionBucketsEnds: number[][] = []
 
     for (const bitsRow of bitMatrix(elementsCount)) {
         if (bitsRow[bitsRow.length - 1] !== 1) {
-            continue;
+            continue
         }
 
-        const endsBucket: number[] = [];
+        const endsBucket: number[] = []
         for (let i: number = 0; i < bitsRow.length; i++) {
             if (bitsRow[i] === 1) {
-                endsBucket.push(i);
+                endsBucket.push(i)
             }
         }
 
         if (endsBucket.length === bucketsCount) {
-            distributionBucketsEnds.push(endsBucket);
+            distributionBucketsEnds.push(endsBucket)
         }
     }
 
-    return distributionBucketsEnds;
+    return distributionBucketsEnds
 }
 
 function breakOnBuckets(weights: number[], endsIncluding: number[]) {
-    const res: number[][] = [];
+    const res: number[][] = []
 
-    let start: number = 0;
+    let start: number = 0
     for (const end of endsIncluding) {
-        res.push(weights.slice(start, end + 1));
-        start = end + 1;
+        res.push(weights.slice(start, end + 1))
+        start = end + 1
     }
 
-    return res;
+    return res
 }
 
 function bitMatrix(n: number): number[][] {
-    const result: number[][] = [];
-    const bits: number[] = new Array(n);
+    const result: number[][] = []
+    const bits: number[] = new Array(n)
 
     function generate(index: number): void {
         if (index === n) {
-            result.push([...bits]);
-            return;
+            result.push([...bits])
+            return
         }
 
-        bits[index] = 0;
-        generate(index + 1);
+        bits[index] = 0
+        generate(index + 1)
 
-        bits[index] = 1;
-        generate(index + 1);
+        bits[index] = 1
+        generate(index + 1)
     }
 
-    generate(0);
+    generate(0)
 
-    return result;
+    return result
 }
