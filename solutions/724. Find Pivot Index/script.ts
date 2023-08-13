9b6f49aabaffb7c7aeada5e6bbac3e4d72262ab5
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-08-13 17:42:09 +0300

    #724 Clean up

--- solutions/724. Find Pivot Index/script.ts
+++ solutions/724. Find Pivot Index/script.ts
@@ -1,18 +1,18 @@
 export {pivotIndex}
 
 function sum(a: number, b: number): number {
-    return a + b;
+    return a + b
 }
 
 function pivotIndex(nums: number[]): number {
     for (let index: number = 0; index < nums.length; index++) {
-        const left: number = nums.slice(0, index).reduce(sum, 0);
-        const right: number = nums.slice(index + 1).reduce(sum, 0);
+        const left: number = nums.slice(0, index).reduce(sum, 0)
+        const right: number = nums.slice(index + 1).reduce(sum, 0)
 
         if (left === right) {
-            return index;
+            return index
         }
     }
 
-    return -1;
+    return -1
 }
