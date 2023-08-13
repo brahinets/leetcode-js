ca99327f5b828cfe864375380ea8834dcaf8eb3b
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-08-13 18:09:16 +0300

    #724 Speed up by using accumulative sum

--- solutions/724. Find Pivot Index/script.ts
+++ solutions/724. Find Pivot Index/script.ts
@@ -1,18 +1,27 @@
 export {pivotIndex}
 
-function sum(a: number, b: number): number {
-    return a + b
-}
-
 function pivotIndex(nums: number[]): number {
-    for (let index: number = 0; index < nums.length; index++) {
-        const left: number = nums.slice(0, index).reduce(sum, 0)
-        const right: number = nums.slice(index + 1).reduce(sum, 0)
+    const accumulativeSums: Map<number, number> = sums(nums)
+
+    for (let i: number = 0; i < nums.length; i++) {
+        const left: number = accumulativeSums.get(i - 1) ?? 0
+        const right: number = (accumulativeSums.get(nums.length - 1) ?? 0) - left - nums[i]
 
         if (left === right) {
-            return index
+            return i
         }
     }
 
     return -1
+}
+
+function sums(nums: number[]): Map<number, number> {
+    const sums: Map<number, number> = new Map<number, number>()
+
+    for (let index: number = 0; index < nums.length; index++) {
+        const sumUntilIndex: number = sums.get(index - 1) ?? 0
+        sums.set(index, sumUntilIndex + nums[index])
+    }
+
+    return sums
 }
