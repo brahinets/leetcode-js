f31273d84f08d6ce63af4998700e9218af67eba0
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-08-14 19:32:07 +0300

    #215 Add extra test case

--- solutions/215. Kth Largest Element in an Array/__tests__/script.test.ts
+++ solutions/215. Kth Largest Element in an Array/__tests__/script.test.ts
@@ -11,4 +11,9 @@ describe('215. Kth Largest Element in an Array', (): void => {
         expect(findKthLargest([3, 2, 3, 1, 2, 4, 5, 5, 6], 4))
             .toStrictEqual(4);
     });
+
+    it('Max by duplicates again', (): void => {
+        expect(findKthLargest([3, 2, 3, 1, 2, 4, 5, 5, 6, 7, 7, 8, 2, 3, 1, 1, 1, 10, 11, 5, 6, 2, 4, 7, 8, 5, 6], 2))
+            .toStrictEqual(10);
+    });
 })
