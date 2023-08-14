7a6c1b138204e93b669a54b2c294e247511988bb
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-08-14 19:14:54 +0300

    #518 Improve readability

--- solutions/518. Coin Change II/script.ts
+++ solutions/518. Coin Change II/script.ts
@@ -1,3 +1,5 @@
+import {count} from "../../common/array-utils"
+
 export {change}
 
 function change(amount: number, coins: number[]): number {
@@ -5,7 +7,7 @@ function change(amount: number, coins: number[]): number {
 
     collectCombinations(amount, coins, [], combinations)
 
-    return new Set(combinations.map(c => c.sort().join("_"))).size
+    return combinations.length
 }
 
 function collectCombinations(amount: number, coins: number[], currentCombination: number[], combinations: number[][]): void {
@@ -15,8 +17,49 @@ function collectCombinations(amount: number, coins: number[], currentCombination
     }
 
     for (const coin of coins) {
-        if (coin <= amount) {
-            collectCombinations(amount - coin, coins, [...currentCombination, coin], combinations)
+        const newCombination: number[] = [...currentCombination, coin]
+
+        if (amount === coin) {
+            if (!includes(combinations, newCombination)) {
+                combinations.push([...currentCombination, coin])
+            }
+        } else {
+            if (coin <= amount) {
+                if (!includes(combinations, newCombination)) {
+                    collectCombinations(amount - coin, coins, newCombination, combinations)
+                }
+            }
         }
     }
+}
+
+function includes(combinations: number[][], another: number[]): boolean {
+    for (const combination of combinations) {
+        const first: Map<number, number> = count(combination)
+        const second: Map<number, number> = count(another)
+
+        if (first.size === second.size) {
+            if (equal(first, second)) {
+                return true
+            }
+        }
+    }
+
+    return false
+}
+
+function equal(first: Map<number, number>, second: Map<number, number>): boolean {
+    for (const kv of first) {
+        const secondValue: number | undefined = second.get(kv[0])
+
+        if (secondValue === undefined) {
+            return false
+        }
+
+        if (secondValue !== kv[1]) {
+            return false
+        }
+    }
+
+    return true
 }
