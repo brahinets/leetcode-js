41418e5afc61c25551e5cc3e4cd2d2f09e0895c1
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-08-14 19:33:51 +0300

    #215 Add test case for higher coverage

--- solutions/215. Kth Largest Element in an Array/__tests__/script.test.ts
+++ solutions/215. Kth Largest Element in an Array/__tests__/script.test.ts
@@ -16,4 +16,9 @@ describe('215. Kth Largest Element in an Array', (): void => {
         expect(findKthLargest([3, 2, 3, 1, 2, 4, 5, 5, 6, 7, 7, 8, 2, 3, 1, 1, 1, 10, 11, 5, 6, 2, 4, 7, 8, 5, 6], 2))
             .toStrictEqual(10);
     });
+
+    it('Illegal state by requirements', (): void => {
+        expect(() => findKthLargest([], 1))
+            .toThrowError("Illegal state. Not enough data");
+    });
 })
