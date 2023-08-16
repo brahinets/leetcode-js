049a2c4686de41e4320039a371a5bc8a80300db4
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-08-16 13:51:57 +0300

    #239 Clean up

--- solutions/239. Sliding Window Maximum/__tests__/script.test.ts
+++ solutions/239. Sliding Window Maximum/__tests__/script.test.ts
@@ -4,16 +4,16 @@ describe('239. Sliding Window Maximum', (): void => {
 
     it('Multiple windows', (): void => {
         expect(maxSlidingWindow([1, 3, -1, -3, 5, 3, 6, 7], 3))
-            .toStrictEqual([3, 3, 5, 5, 6, 7]);
-    });
+            .toStrictEqual([3, 3, 5, 5, 6, 7])
+    })
 
     it('The only window', (): void => {
         expect(maxSlidingWindow([1], 1))
-            .toStrictEqual([1]);
-    });
+            .toStrictEqual([1])
+    })
 
     it('No windows', (): void => {
         expect(maxSlidingWindow([1], 2))
-            .toStrictEqual([]);
-    });
+            .toStrictEqual([])
+    })
 })
