01aa089524c639663617ccb61d8feb490da66af3
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-08-16 14:40:19 +0300

    #239 Optimize using virtual window bounds

--- solutions/239. Sliding Window Maximum/script.ts
+++ solutions/239. Sliding Window Maximum/script.ts
@@ -9,9 +9,24 @@ function maxSlidingWindow(nums: number[], k: number): number[] {
 
     const maxs: number[] = []
     for (let i: number = windowSize; i <= nums.length; i++) {
-        const window: number[] = nums.slice(i - windowSize, i);
-        maxs.push(Math.max(...window))
+        maxs.push(findMax(nums, i - windowSize, i))
     }
 
     return maxs
+}
+
+function findMax(nums: number[], from: number, to: number): number {
+    let max: number | undefined = undefined;
+
+    for (let i: number = from; i < to; i++) {
+        if (max === undefined || nums[i] > max) {
+            max = nums[i]
+        }
+    }
+
+    if (max === undefined) {
+        throw new Error("Illegal state")
+    }
+
+    return max
 }
