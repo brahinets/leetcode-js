108fc885318b38c6c10d240ef11cdf265b144296
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-08-18 18:52:25 +0300

    #2 Clean up

--- solutions/2. Add Two Numbers/__tests__/script.test.ts
+++ solutions/2. Add Two Numbers/__tests__/script.test.ts
@@ -3,8 +3,8 @@ import {addTwoNumbers, ListNode} from '../script'
 describe('2. Add Two Numbers', (): void => {
     it('Zero', (): void => {
         expect(addTwoNumbers(new ListNode(0), new ListNode(0)))
-            .toStrictEqual(new ListNode(0));
-    });
+            .toStrictEqual(new ListNode(0))
+    })
 
     it('Same radix', (): void => {
         expect(addTwoNumbers(
@@ -12,8 +12,8 @@ describe('2. Add Two Numbers', (): void => {
             new ListNode(5, new ListNode(6, new ListNode(4)))
         )).toStrictEqual(
             new ListNode(7, new ListNode(0, new ListNode(8)))
-        );
-    });
+        )
+    })
 
     it('Different radix', (): void => {
         expect(addTwoNumbers(
@@ -21,6 +21,6 @@ describe('2. Add Two Numbers', (): void => {
             new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9))))
         )).toStrictEqual(
             new ListNode(8, new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(0, new ListNode(0, new ListNode(0, new ListNode(1))))))))
-        );
-    });
+        )
+    })
 })
--- solutions/2. Add Two Numbers/script.ts
+++ solutions/2. Add Two Numbers/script.ts
@@ -1,25 +1,25 @@
-import {ListNode} from "../../common/ListNode";
+import {ListNode} from "../../common/ListNode"
 
 export {ListNode, addTwoNumbers}
 
 const addTwoNumbers = function (l1: ListNode | null, l2: ListNode | null): ListNode {
-    let head: ListNode = new ListNode();
-    let incrementNextRadix: boolean = false;
+    let head: ListNode = new ListNode()
+    let incrementNextRadix: boolean = false
 
     while (l1 || l2 || incrementNextRadix) {
-        const sum: number = (incrementNextRadix ? 1 : 0) + (l1 ? l1.val : 0) + (l2 ? l2.val : 0);
+        const sum: number = (incrementNextRadix ? 1 : 0) + (l1 ? l1.val : 0) + (l2 ? l2.val : 0)
 
-        let end: ListNode = head;
+        let end: ListNode = head
         while (end.next) {
-            end = end.next;
+            end = end.next
         }
-        end.next = new ListNode(sum % 10);
+        end.next = new ListNode(sum % 10)
 
-        l1 &&= l1.next;
-        l2 &&= l2.next;
+        l1 &&= l1.next
+        l2 &&= l2.next
 
-        incrementNextRadix = sum >= 10;
+        incrementNextRadix = sum >= 10
     }
 
-    return head.next || new ListNode(0);
+    return head.next || new ListNode(0)
 }
