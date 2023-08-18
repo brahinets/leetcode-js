e7d8f7f094b1f5706a1b8a74e1902407bb270b94
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-08-18 18:51:50 +0300

    #1 Clean up

--- solutions/1. Two Sum/__tests__/script.test.ts
+++ solutions/1. Two Sum/__tests__/script.test.ts
@@ -3,16 +3,16 @@ import {twoSum} from '../script'
 describe('1. Two Sum', (): void => {
     it('One', (): void => {
         expect(twoSum([2, 7, 11, 15], 9))
-            .toStrictEqual([0, 1]);
-    });
+            .toStrictEqual([0, 1])
+    })
 
     it('Two', (): void => {
         expect(twoSum([3, 2, 4], 6))
-            .toStrictEqual([1, 2]);
-    });
+            .toStrictEqual([1, 2])
+    })
 
     it('Three', (): void => {
         expect(twoSum([3, 3], 6))
-            .toStrictEqual([0, 1]);
-    });
+            .toStrictEqual([0, 1])
+    })
 })
--- solutions/1. Two Sum/script.ts
+++ solutions/1. Two Sum/script.ts
@@ -1,16 +1,17 @@
 export {twoSum}
+
 const twoSum = (nums: number[], target: number): number[] => {
     for (let i: number = 0; i < nums.length; i++) {
         for (let j: number = 0; j < nums.length; j++) {
             if (i === j) {
-                continue;
+                continue
             }
 
             if ((nums[i] + nums[j]) === target) {
-                return [i, j];
+                return [i, j]
             }
         }
     }
 
-    throw new Error("Match not found");
-};
+    throw new Error("Match not found")
+}
