26502a9b0ddff30d8a39dab46d2b95d2293c229a
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-08-19 09:58:14 +0300

    #67 Clean up

--- solutions/67. Add Binary/__tests__/script.test.ts
+++ solutions/67. Add Binary/__tests__/script.test.ts
@@ -2,10 +2,12 @@ import {addBinary} from '../script'
 
 describe('67. Add Binary', (): void => {
     it('Different length', (): void => {
-        expect(addBinary("11", "1")).toBe("100");
-    });
+        expect(addBinary("11", "1"))
+            .toBe("100")
+    })
 
     it('Same length', (): void => {
-        expect(addBinary("1010", "1011")).toBe("10101");
-    });
-});
+        expect(addBinary("1010", "1011"))
+            .toBe("10101")
+    })
+})
--- solutions/67. Add Binary/script.ts
+++ solutions/67. Add Binary/script.ts
@@ -2,22 +2,22 @@ export {addBinary}
 
 function addBinary(a: string, b: string): string {
     const maxBits: number = Math.max(a.length, b.length)
-    const res: number[] = [];
+    const res: number[] = []
 
-    let carry: number = 0;
+    let carry: number = 0
     for (let i: number = 1; i <= maxBits; i++) {
-        const aBit: number = Number(a[a.length - i] ?? 0);
-        const bBit: number = Number(b[b.length - i] ?? 0);
-        const sum: number = carry + aBit + bBit;
+        const aBit: number = Number(a[a.length - i] ?? 0)
+        const bBit: number = Number(b[b.length - i] ?? 0)
+        const sum: number = carry + aBit + bBit
 
-        res.push(sum % 2);
+        res.push(sum % 2)
 
-        carry = sum > 1 ? 1 : 0;
+        carry = sum > 1 ? 1 : 0
     }
 
     if (carry) {
-        res.push(1);
+        res.push(1)
     }
 
-    return res.reverse().join("");
+    return res.reverse().join("")
 }
