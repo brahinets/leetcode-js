43ff1d5a3d723111d9cce63d0bd3dbeb33a0dcba
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-08-21 10:36:35 +0300

    #459 Speed up using rotation theory, copied word is a rotated itself

--- solutions/459. Repeated Substring Pattern/script.ts
+++ solutions/459. Repeated Substring Pattern/script.ts
@@ -1,16 +1,7 @@
 export {repeatedSubstringPattern}
 
 function repeatedSubstringPattern(s: string): boolean {
-    for (let patternLength: number = 1; patternLength < s.length; patternLength++) {
-        if (s.length % patternLength !== 0) {
-            continue
-        }
+    const doubled: string = s + s
 
-        const duplicatesNumber: number = s.length / patternLength
-        if (s.substring(0, patternLength).repeat(duplicatesNumber) === s) {
-            return true
-        }
-    }
-
-    return false
+    return doubled.substring(1, doubled.length - 1).includes(s)
 }
