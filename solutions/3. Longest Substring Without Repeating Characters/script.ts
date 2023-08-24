4f543ec68b19a1b40c6a66e95617025ed070d23f
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-08-24 22:24:26 +0300

    #3 Clean up

--- solutions/3. Longest Substring Without Repeating Characters/script.ts
+++ solutions/3. Longest Substring Without Repeating Characters/script.ts
@@ -1,27 +1,27 @@
 export {lengthOfLongestSubstring}
 
 function lengthOfLongestSubstring(s: string): number {
-    const count: Map<string, number> = new Map<string, number>();
+    const count: Map<string, number> = new Map<string, number>()
 
-    let left: number = 0;
-    let right: number = 0;
-    let res: number = 0;
+    let left: number = 0
+    let right: number = 0
+    let res: number = 0
 
     while (right < s.length) {
-        const rightBoundChar:string = s.charAt(right);
+        const rightBoundChar: string = s.charAt(right)
 
-        count.set(rightBoundChar, (count.get(rightBoundChar) ?? 0) + 1);
+        count.set(rightBoundChar, (count.get(rightBoundChar) ?? 0) + 1)
 
         while ((count.get(rightBoundChar) ?? 0) > 1) {
-            const leftBoundChar:string = s.charAt(left);
-            count.set(leftBoundChar, (count.get(leftBoundChar) ?? 1) - 1);
-            left++;
+            const leftBoundChar: string = s.charAt(left)
+            count.set(leftBoundChar, (count.get(leftBoundChar) ?? 1) - 1)
+            left++
         }
 
         res = Math.max(res, right - left + 1)
 
-        right++;
+        right++
     }
 
-    return res;
+    return res
 }
