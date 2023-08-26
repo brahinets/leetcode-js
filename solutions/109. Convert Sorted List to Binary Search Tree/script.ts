3375b10609720a3eeb4418ead38a50f4f39fcf38
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-08-26 20:42:13 +0300

    #109 Clean up

--- solutions/109. Convert Sorted List to Binary Search Tree/script.ts
+++ solutions/109. Convert Sorted List to Binary Search Tree/script.ts
@@ -1,42 +1,42 @@
-import {ListNode} from "../../common/ListNode";
-import {TreeNode} from "../../common/TreeNode";
+import {ListNode} from "../../common/ListNode"
+import {TreeNode} from "../../common/TreeNode"
 
 export {sortedListToBST, ListNode, TreeNode}
 
 function sortedListToBST(head: ListNode | null): TreeNode | null {
-    const data: number[] = [];
+    const data: number[] = []
     while (head) {
-        data.push(head.val);
-        head = head.next;
+        data.push(head.val)
+        head = head.next
     }
 
-    return toNode(data);
+    return toNode(data)
 }
 
 function toNode(data: number[]): TreeNode | null {
     if (data.length === 0) {
-        return null;
+        return null
     } else {
-        const left: number[] = [];
-        const right: number[] = [];
-        let val: number | null = null;
+        const left: number[] = []
+        const right: number[] = []
+        let val: number | null = null
 
-        const middle: number = Math.floor(data.length / 2);
+        const middle: number = Math.floor(data.length / 2)
 
         for (let i: number = 0; i < data.length; i++) {
             if (i < middle) {
-                left.push(data[i]);
+                left.push(data[i])
             } else if (i === middle) {
-                val = data[i];
+                val = data[i]
             } else {
-                right.push(data[i]);
+                right.push(data[i])
             }
         }
 
         if (val === null) {
-            throw new Error("Val is not defined. Illegal state");
+            throw new Error("Val is not defined. Illegal state")
         }
 
-        return new TreeNode(val, toNode(left), toNode(right));
+        return new TreeNode(val, toNode(left), toNode(right))
     }
 }
