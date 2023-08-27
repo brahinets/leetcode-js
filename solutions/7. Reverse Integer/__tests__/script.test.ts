5dfe9aaaa603722d4181850d6a2c6e964ca105d0
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-08-27 12:30:39 +0300

    #7 Clean up

--- solutions/7. Reverse Integer/__tests__/script.test.ts
+++ solutions/7. Reverse Integer/__tests__/script.test.ts
@@ -2,14 +2,17 @@ import {reverse} from '../script'
 
 describe('7. Reverse Integer', (): void => {
     it('Reverse positive', (): void => {
-        expect(reverse(123)).toBe(321);
-    });
+        expect(reverse(123))
+            .toBe(321)
+    })
 
     it('Reverse zeros', (): void => {
-        expect(reverse(120)).toBe(21);
-    });
+        expect(reverse(120))
+            .toBe(21)
+    })
 
     it('Reverse negative', (): void => {
-        expect(reverse(-123)).toBe(-321);
-    });
+        expect(reverse(-123))
+            .toBe(-321)
+    })
 })
--- solutions/7. Reverse Integer/script.ts
+++ solutions/7. Reverse Integer/script.ts
@@ -1,18 +1,18 @@
 export {reverse}
 
-const MAX_INT32: number = 2 ** 31 - 1;
-const MIN_INT32: number = -1 * 2 ** 31;
+const MAX_INT32: number = 2 ** 31 - 1
+const MIN_INT32: number = -1 * 2 ** 31
 
 function reverse(x: number): number {
-    const sign: number = x < 0 ? -1 : 1;
+    const sign: number = x < 0 ? -1 : 1
 
     const reversedNumber: number = Number(String(Math.abs(x))
         .split('')
-        .reverse().join('')) * sign;
+        .reverse().join('')) * sign
 
     if (reversedNumber < MIN_INT32 || reversedNumber > MAX_INT32) {
-        return 0;
+        return 0
     } else {
-        return reversedNumber;
+        return reversedNumber
     }
 }
