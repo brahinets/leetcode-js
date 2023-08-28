f1fa1c305df20b074951a1ed6d5d42e6561d53ad
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-08-28 13:51:55 +0300

    #225 Use queue as it is required

--- solutions/225. Implement Stack using Queues/script.ts
+++ solutions/225. Implement Stack using Queues/script.ts
@@ -1,28 +1,48 @@
 export {MyStack}
 
 class MyStack {
-    private readonly data: number[]
+    private readonly queue: HomebrewQueue
 
     constructor() {
-        this.data = []
+        this.queue = new HomebrewQueue()
     }
 
     push(x: number): void {
-        this.data.push(x)
+        const rotation: number = this.queue.size()
+
+        this.queue.push(x)
+
+        for (let i: number = 0; i < rotation; i++) {
+            this.queue.push(this.queue.pop())
+        }
     }
 
     pop(): number {
-        const value: number | undefined = this.data.splice(this.data.length - 1)[0]
-
-        if (value === undefined) {
-            throw new Error("Illegal state")
-        }
-
-        return value
+        return this.queue.pop()
     }
 
     top(): number {
-        const value: number | undefined = this.data[this.data.length - 1]
+        return this.queue.peek()
+    }
+
+    empty(): boolean {
+        return this.queue.isEmpty()
+    }
+}
+
+class HomebrewQueue {
+    queue: number[]
+
+    constructor() {
+        this.queue = []
+    }
+
+    push(n: number): void {
+        this.queue.push(n)
+    }
+
+    peek(): number {
+        const value: number | undefined = this.queue[0]
 
         if (value === undefined) {
             throw new Error("Illegal state")
@@ -31,7 +51,17 @@ class MyStack {
         return value
     }
 
-    empty(): boolean {
-        return this.data.length === 0
+    pop(): number {
+        const top: number = this.peek()
+        this.queue.shift()
+        return top
+    }
+
+    size(): number {
+        return this.queue.length
+    }
+
+    isEmpty(): boolean {
+        return this.queue.length == 0
     }
 }
