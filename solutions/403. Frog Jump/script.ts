a9d10524db55338fdb71d217918b0161d9aaba7c
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-08-29 10:55:35 +0300

    #403 Remove code duplication

--- solutions/403. Frog Jump/script.ts
+++ solutions/403. Frog Jump/script.ts
@@ -4,6 +4,10 @@ export {canCross}
 
 const MAX_STONES_COUNT: number = 2000
 
+const SLOW_DOWN: number = -1
+const KEEP_PACE: number = 0
+const SPEED_UP: number = 1
+
 function canCross(stones: number[]): boolean {
     const memo: number[][] = matrixOf(-1, MAX_STONES_COUNT, MAX_STONES_COUNT)
 
@@ -23,22 +27,16 @@ function hasLandForNextStep(stones: number[], memo: number[][], currentStone: nu
         return memo[currentStone][prevJumpLength] === 1
     }
 
-    const nextStoneIfSpeedUp: number = stones.indexOf(stones[currentStone] + prevJumpLength + 1)
-    const speedUp: boolean = hasLandForNextStep(stones, memo, nextStoneIfSpeedUp, prevJumpLength + 1)
-    memo[currentStone][prevJumpLength] = speedUp ? 1 : 0
-    if (speedUp) {
-        return true
+    let can: boolean = false
+    for (const turn of [SLOW_DOWN, KEEP_PACE, SPEED_UP]) {
+        const jumpOnLength: number = prevJumpLength + turn
+
+        if (jumpOnLength > 0) {
+            const nextStone: number = stones.indexOf(stones[currentStone] + jumpOnLength)
+            can ||= hasLandForNextStep(stones, memo, nextStone, jumpOnLength)
+        }
     }
 
-    const nextStoneIfKeepPace: number = stones.indexOf(stones[currentStone] + prevJumpLength)
-    const keepPace: boolean = hasLandForNextStep(stones, memo, nextStoneIfKeepPace, prevJumpLength)
-    memo[currentStone][prevJumpLength] = keepPace ? 1 : 0
-    if (keepPace) {
-        return true
-    }
-
-    const nextStoneIfSlowDown: number = stones.indexOf(stones[currentStone] + prevJumpLength)
-    const slowDown: boolean = hasLandForNextStep(stones, memo, nextStoneIfSlowDown, prevJumpLength - 1)
-    memo[currentStone][prevJumpLength] = slowDown ? 1 : 0
-    return slowDown
+    memo[currentStone][prevJumpLength] = can ? 1 : 0
+    return can
 }
