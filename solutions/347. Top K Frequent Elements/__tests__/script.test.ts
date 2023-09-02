b33add903d5f123e72788306dca1cac8bb13a8d8
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-09-02 15:24:56 +0300

    #347 Clean up

--- solutions/347. Top K Frequent Elements/__tests__/script.test.ts
+++ solutions/347. Top K Frequent Elements/__tests__/script.test.ts
@@ -1,12 +1,14 @@
 import {topKFrequent} from '../script'
 
 describe('347. Top K Frequent Elements', (): void => {
+
     it('All', (): void => {
         expect(topKFrequent([1], 1))
-            .toStrictEqual([1]);
-    });
+            .toStrictEqual([1])
+    })
+
     it('Top sorted by counts', (): void => {
         expect(topKFrequent([1, 1, 1, 2, 2, 3], 2))
-            .toStrictEqual([1, 2]);
-    });
+            .toStrictEqual([1, 2])
+    })
 })
--- solutions/347. Top K Frequent Elements/script.ts
+++ solutions/347. Top K Frequent Elements/script.ts
@@ -3,35 +3,35 @@ import {count} from '../../common/array-utils'
 export {topKFrequent}
 
 function topKFrequent(values: number[], k: number): number[] {
-    const counting: Map<number, number> = count(values);
-    values = sort(values, counting);
+    const counting: Map<number, number> = count(values)
+    values = sort(values, counting)
 
-    const result: number[] = [];
+    const result: number[] = []
     for (let i: number = 0; i < values.length && result.length < k; i++) {
-        const num: number = values[values.length - 1 - i];
+        const num: number = values[values.length - 1 - i]
 
         if (!arrayContains(result, num)) {
-            result.push(num);
+            result.push(num)
         }
     }
 
-    return result;
+    return result
 }
 
 function arrayContains(values: number[], targetValue: number): boolean {
-    return values.some((value: number): boolean => value === targetValue);
+    return values.some((value: number): boolean => value === targetValue)
 }
 
 function sort(values: number[], counting: Map<number, number>): number[] {
-    const valuesCopy: number[] = [...values];
+    const valuesCopy: number[] = [...values]
 
     return valuesCopy.sort((v1: number, v2: number): number => {
-        let counter: number = (counting.get(v1) || 0) - (counting.get(v2) || 0);
+        let counter: number = (counting.get(v1) || 0) - (counting.get(v2) || 0)
 
         if (counter !== 0) {
-            return counter;
+            return counter
         }
 
-        return 1;
+        return 1
     })
 }
