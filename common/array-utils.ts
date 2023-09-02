9e0ea309529d82748efbeee9f46a943c4c80fb4a
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-09-02 15:32:55 +0300

    Improve code style by nullish coalescing

--- common/array-utils.ts
+++ common/array-utils.ts
@@ -41,7 +41,7 @@ function arrayOf<Type>(val: Type, size: number): Type[] {
 
 function count<Type>(nums: Type[]): Map<Type, number> {
     return nums.reduce((count: Map<Type, number>, num: Type) => {
-        count.set(num, (count.get(num) || 0) + 1);
+        count.set(num, (count.get(num) ?? 0) + 1);
         return count;
     }, new Map<Type, number>());
 }
