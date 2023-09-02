058d73f2db5e7ac43a18662031361ca7ac67ef05
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-09-02 15:30:58 +0300

    #347 Cover by test edge case with the same count

--- solutions/347. Top K Frequent Elements/__tests__/script.test.ts
+++ solutions/347. Top K Frequent Elements/__tests__/script.test.ts
@@ -11,4 +11,9 @@ describe('347. Top K Frequent Elements', (): void => {
         expect(topKFrequent([1, 1, 1, 2, 2, 3], 2))
             .toStrictEqual([1, 2])
     })
+
+    it('Top sorted by counts and similar count use latter', (): void => {
+        expect(topKFrequent([1, 1, 1, 2, 2, 2, 3, 3, 3], 2))
+            .toStrictEqual([3, 2])
+    })
 })
