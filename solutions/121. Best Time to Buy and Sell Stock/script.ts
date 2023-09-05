46e2ffb50af84fb06ee25877f701e600bfc74bf3
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-09-05 11:21:01 +0300

    #121 Clean up

--- solutions/121. Best Time to Buy and Sell Stock/script.ts
+++ solutions/121. Best Time to Buy and Sell Stock/script.ts
@@ -1,17 +1,19 @@
-export const maxProfit = function (prices: number[]): number {
-    let minPrice: number = prices[0];
-    let maxProfit: number = 0;
+export {maxProfit}
+
+function maxProfit(prices: number[]): number {
+    let minPrice: number = prices[0]
+    let maxProfit: number = 0
 
     for (let i: number = 1; i < prices.length; i++) {
         if (prices[i] < minPrice) {
-            minPrice = prices[i];
+            minPrice = prices[i]
         } else {
-            const profit: number = prices[i] - minPrice;
+            const profit: number = prices[i] - minPrice
             if (profit > maxProfit) {
-                maxProfit = profit;
+                maxProfit = profit
             }
         }
     }
 
-    return maxProfit;
-};
+    return maxProfit
+}
