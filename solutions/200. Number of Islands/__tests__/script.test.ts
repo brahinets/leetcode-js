b94a775cf6391fa5d5e23b3a88fa0118bdf4891e
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-09-07 21:21:27 +0300

    #200 Clean Up

--- solutions/200. Number of Islands/__tests__/script.test.ts
+++ solutions/200. Number of Islands/__tests__/script.test.ts
@@ -2,8 +2,9 @@ import {numIslands} from '../script'
 
 describe('200. Number of Islands', (): void => {
     it('No islands. Empty sea', (): void => {
-        expect(numIslands([])).toBe(0);
-    });
+        expect(numIslands([]))
+            .toBe(0)
+    })
 
     it('No islands', (): void => {
         expect(numIslands([
@@ -11,8 +12,8 @@ describe('200. Number of Islands', (): void => {
             ["0", "0", "0", "0", "0"],
             ["0", "0", "0", "0", "0"],
             ["0", "0", "0", "0", "0"]
-        ])).toBe(0);
-    });
+        ])).toBe(0)
+    })
 
     it('One island', (): void => {
         expect(numIslands([
@@ -20,8 +21,8 @@ describe('200. Number of Islands', (): void => {
             ["1", "1", "0", "1", "0"],
             ["1", "1", "0", "0", "0"],
             ["0", "0", "0", "0", "0"]
-        ])).toBe(1);
-    });
+        ])).toBe(1)
+    })
 
     it('Multiple islands', (): void => {
         expect(numIslands([
@@ -29,6 +30,6 @@ describe('200. Number of Islands', (): void => {
             ["1", "1", "0", "0", "0"],
             ["0", "0", "1", "0", "0"],
             ["0", "0", "0", "1", "1"]
-        ])).toBe(3);
-    });
-});
+        ])).toBe(3)
+    })
+})
--- solutions/200. Number of Islands/script.ts
+++ solutions/200. Number of Islands/script.ts
@@ -1,7 +1,7 @@
 export {numIslands}
 
-const ISLAND: string = '1';
-const VISITED: string = '2';
+const ISLAND: string = '1'
+const VISITED: string = '2'
 
 function findIslandCoordinates(sea: string[][]): number[] {
     for (let i: number = 0; i < sea.length; i++) {
@@ -12,40 +12,40 @@ function findIslandCoordinates(sea: string[][]): number[] {
         }
     }
 
-    return [-1, -1];
+    return [-1, -1]
 }
 
 function numIslands(grid: string[][]): number {
-    const sea: string[][] = JSON.parse(JSON.stringify(grid));
+    const sea: string[][] = JSON.parse(JSON.stringify(grid))
 
-    let islandsCount: number = 0;
-    let hasIsland: boolean;
+    let islandsCount: number = 0
+    let hasIsland: boolean
 
     do {
-        const [islandCoordinateX, islandCoordinateY]: number[] = findIslandCoordinates(sea);
+        const [islandCoordinateX, islandCoordinateY]: number[] = findIslandCoordinates(sea)
 
-        hasIsland = islandCoordinateX !== -1 && islandCoordinateY !== -1;
+        hasIsland = islandCoordinateX !== -1 && islandCoordinateY !== -1
         if (hasIsland) {
-            visitIsland(sea, islandCoordinateX, islandCoordinateY);
-            islandsCount++;
+            visitIsland(sea, islandCoordinateX, islandCoordinateY)
+            islandsCount++
         }
     } while (hasIsland)
 
-    return islandsCount;
+    return islandsCount
 }
 
 
 function visitIsland(sea: string[][], islandCoordinateX: number, islandCoordinateY: number): void {
     if (islandCoordinateX < 0 || islandCoordinateY < 0 || islandCoordinateX >= sea.length || islandCoordinateY >= sea[0].length) {
-        return;
+        return
     }
 
     if (ISLAND === sea[islandCoordinateX][islandCoordinateY]) {
-        sea[islandCoordinateX][islandCoordinateY] = VISITED;
+        sea[islandCoordinateX][islandCoordinateY] = VISITED
 
-        visitIsland(sea, islandCoordinateX - 1, islandCoordinateY);
-        visitIsland(sea, islandCoordinateX, islandCoordinateY - 1);
-        visitIsland(sea, islandCoordinateX + 1, islandCoordinateY);
-        visitIsland(sea, islandCoordinateX, islandCoordinateY + 1);
+        visitIsland(sea, islandCoordinateX - 1, islandCoordinateY)
+        visitIsland(sea, islandCoordinateX, islandCoordinateY - 1)
+        visitIsland(sea, islandCoordinateX + 1, islandCoordinateY)
+        visitIsland(sea, islandCoordinateX, islandCoordinateY + 1)
     }
 }
