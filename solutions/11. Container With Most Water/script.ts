97230540a8738637b23a1f9aba7adc18db0797ed
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-09-10 18:10:10 +0300

    #11 Optimize

--- solutions/11. Container With Most Water/script.ts
+++ solutions/11. Container With Most Water/script.ts
@@ -3,15 +3,22 @@ export {maxArea}
 function maxArea(height: number[]): number {
     let max: number = 0
 
-    for (let i: number = 0; i < height.length; i++) {
-        for (let j: number = i; j < height.length; j++) {
-            const width: number = j - i
-            const commonHeight: number = Math.min(height[i], height[j])
-            const area: number = commonHeight * width
+    let left: number = 0
+    let right: number = height.length - 1
 
-            if (area > max) {
-                max = area
-            }
+    while (left < right) {
+        const width: number = right - left
+        const commonHeight: number = Math.min(height[left], height[right])
+        const area: number = commonHeight * width
+
+        if (area > max) {
+            max = area
+        }
+
+        if (height[left] > height[right]) {
+            right--
+        } else {
+            left++
         }
     }
 
