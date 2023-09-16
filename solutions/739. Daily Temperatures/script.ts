9a48acdb49e6d617643e534b85b7cc1c1ba9d506
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-09-16 14:25:57 +0300

    #739 Use stack

--- solutions/739. Daily Temperatures/script.ts
+++ solutions/739. Daily Temperatures/script.ts
@@ -1,25 +1,17 @@
 export {dailyTemperatures}
 
 function dailyTemperatures(temperatures: number[]): number[] {
-    const result: number[] = []
+    const result: number[] = new Array(temperatures.length).fill(0)
+    const stack: number[] = []
 
     for (let i: number = 0; i < temperatures.length; i++) {
-        result.push(findNextGreaterThan(temperatures[i], i + 1, temperatures))
+        while (stack.length > 0 && temperatures[i] > temperatures[stack[stack.length - 1]]) {
+            const day: number = stack.pop() ?? 0
+            result[day] = i - day
+        }
+
+        stack.push(i)
     }
 
     return result
-}
-
-function findNextGreaterThan(value: number, startIndex: number, values: number[]): number {
-    let daysToWait: number = 0
-    let found: boolean = false
-
-    for (let i: number = startIndex; i < values.length && !found; i++) {
-        daysToWait++
-        if (values[i] > value) {
-            found = true
-        }
-    }
-
-    return found ? daysToWait : 0
 }
