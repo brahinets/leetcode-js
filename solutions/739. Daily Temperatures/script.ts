3c313a4761406c43575d8a2bc0987e326a2c5e5c
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-09-16 14:27:02 +0300

    #739 Use type-script like suppression of guaranteed non-nullish result

--- solutions/739. Daily Temperatures/script.ts
+++ solutions/739. Daily Temperatures/script.ts
@@ -6,7 +6,7 @@ function dailyTemperatures(temperatures: number[]): number[] {
 
     for (let i: number = 0; i < temperatures.length; i++) {
         while (stack.length > 0 && temperatures[i] > temperatures[stack[stack.length - 1]]) {
-            const day: number = stack.pop() ?? 0
+            const day: number = stack.pop()!
             result[day] = i - day
         }
 
