5ac7ad840f8624a2f3ead9c97163ffb07258f25a
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-09-17 20:01:17 +0300

    #912 Clean up

--- solutions/912. Sort an Array/script.ts
+++ solutions/912. Sort an Array/script.ts
@@ -1,33 +1,29 @@
 export {sortArray}
 
-const sortArray = (nums: number[]): number[] => {
-    return mergeSort(nums);
-};
-
-const mergeSort = (nums: number[]): number[] => {
+function sortArray(nums: number[]): number[] {
     if (nums.length <= 1) {
-        return nums;
+        return nums
     }
 
-    const mid: number = Math.floor(nums.length / 2);
-    const left: number[] = mergeSort(nums.slice(0, mid));
-    const right: number[] = mergeSort(nums.slice(mid));
+    const mid: number = Math.floor(nums.length / 2)
+    const left: number[] = sortArray(nums.slice(0, mid))
+    const right: number[] = sortArray(nums.slice(mid))
 
-    return merge(left, right);
+    return merge(left, right)
 }
 
 const merge = (left: number[], right: number[]): number[] => {
-    const sorted: number[] = [];
+    const sorted: number[] = []
 
     while (left.length && right.length) {
-        const val: number | undefined = left[0] < right[0] ? left.shift() : right.shift();
+        const val: number | undefined = left[0] < right[0] ? left.shift() : right.shift()
 
         if (val === undefined) {
             throw new Error("Illegal state")
         }
 
-        sorted.push(val);
+        sorted.push(val)
     }
 
-    return [...sorted, ...left, ...right];
+    return [...sorted, ...left, ...right]
 }
