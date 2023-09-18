0c4fe30a52936073a3c3ceb4ab65909f89fc3826
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-09-18 21:41:10 +0300

    #875 Clean up

--- solutions/875. Koko Eating Bananas/__tests__/script.test.ts
+++ solutions/875. Koko Eating Bananas/__tests__/script.test.ts
@@ -3,16 +3,16 @@ import {minEatingSpeed} from '../script'
 describe('875. Koko Eating Bananas', (): void => {
     it('Zero multi-bite piles', (): void => {
         expect(minEatingSpeed([30, 11, 23, 4, 20], 5))
-            .toBe(30);
-    });
+            .toBe(30)
+    })
 
     it('One multi-bite pile', (): void => {
         expect(minEatingSpeed([30, 11, 23, 4, 20], 6))
-            .toBe(23);
-    });
+            .toBe(23)
+    })
 
     it('Multiple multi-bite piles', (): void => {
         expect(minEatingSpeed([3, 6, 7, 11], 8))
-            .toBe(4);
-    });
-});
+            .toBe(4)
+    })
+})
--- solutions/875. Koko Eating Bananas/script.ts
+++ solutions/875. Koko Eating Bananas/script.ts
@@ -1,23 +1,23 @@
 export {minEatingSpeed}
 
 function minEatingSpeed(piles: number[], h: number): number {
-    let left: number = 1;
+    let left: number = 1
     let right: number = piles
-        .reduce((prev: number, curr: number): number => prev > curr ? prev : curr, piles[0]);
+        .reduce((prev: number, curr: number): number => prev > curr ? prev : curr, piles[0])
 
     while (left < right) {
-        const mid: number = Math.floor((left + right) / 2);
+        const mid: number = Math.floor((left + right) / 2)
 
         const time: number = piles
             .map((pile: number) => Math.ceil(pile / mid))
-            .reduce((s: number, e: number) => s + e, 0);
+            .reduce((s: number, e: number) => s + e, 0)
 
         if (time <= h) {
-            right = mid;
+            right = mid
         } else {
-            left = mid + 1;
+            left = mid + 1
         }
     }
 
-    return left;
+    return left
 }
