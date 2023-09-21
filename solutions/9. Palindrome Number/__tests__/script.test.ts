13de421e7f8e144097bdb5cdff4e5a045b189492
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-09-21 23:06:22 +0300

    #9 Clean up

--- solutions/9. Palindrome Number/__tests__/script.test.ts
+++ solutions/9. Palindrome Number/__tests__/script.test.ts
@@ -2,14 +2,17 @@ import {isPalindrome} from '../script'
 
 describe('9. Palindrome Number', (): void => {
     it('Symmetric positive is palindrome', (): void => {
-        expect(isPalindrome(121)).toBe(true);
-    });
+        expect(isPalindrome(121))
+            .toBe(true)
+    })
 
     it('Negative is not palindrome', (): void => {
-        expect(isPalindrome(-121)).toBe(false);
-    });
+        expect(isPalindrome(-121))
+            .toBe(false)
+    })
 
     it('Asymmetric positive is not palindrome', (): void => {
-        expect(isPalindrome(10)).toBe(false);
-    });
+        expect(isPalindrome(10))
+            .toBe(false)
+    })
 })
--- solutions/9. Palindrome Number/script.ts
+++ solutions/9. Palindrome Number/script.ts
@@ -1,14 +1,14 @@
 export {isPalindrome}
 
-const isPalindrome = function (x: number): boolean {
-    const str: string = String(x);
-    const middle: number = str.length / 2;
+function isPalindrome(x: number): boolean {
+    const str: string = String(x)
+    const middle: number = str.length / 2
 
     for (let i: number = 0; i < middle; i++) {
         if (str[i] !== str[str.length - i - 1]) {
-            return false;
+            return false
         }
     }
 
-    return true;
+    return true
 }
