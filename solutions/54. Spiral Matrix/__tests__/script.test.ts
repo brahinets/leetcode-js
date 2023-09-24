c3f7061ece20136d32442f72fe81285d50c00cff
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-09-24 21:33:03 +0300

    #54 Clean up

--- solutions/54. Spiral Matrix/__tests__/script.test.ts
+++ solutions/54. Spiral Matrix/__tests__/script.test.ts
@@ -3,63 +3,63 @@ import {spiralOrder} from '../script'
 describe('54. Spiral Matrix', (): void => {
     it('Single element', (): void => {
         expect(spiralOrder([[5]]))
-            .toStrictEqual([5]);
-    });
+            .toStrictEqual([5])
+    })
 
     it('Single column', (): void => {
         expect(spiralOrder([
             [3],
             [2]
-        ])).toStrictEqual([3, 2]);
-    });
+        ])).toStrictEqual([3, 2])
+    })
 
     it('Single higher column', (): void => {
         expect(spiralOrder([
             [7],
             [9],
             [6]
-        ])).toStrictEqual([7, 9, 6]);
-    });
+        ])).toStrictEqual([7, 9, 6])
+    })
 
     it('Single row', (): void => {
         expect(spiralOrder([
             [3, 2]
-        ])).toStrictEqual([3, 2]);
-    });
+        ])).toStrictEqual([3, 2])
+    })
 
     it('Single longer row', (): void => {
         expect(spiralOrder([
             [7, 9, 6]
-        ])).toStrictEqual([7, 9, 6]);
-    });
+        ])).toStrictEqual([7, 9, 6])
+    })
 
     it('Square small', (): void => {
         expect(spiralOrder([
             [1, 2],
             [3, 4]
-        ])).toStrictEqual([1, 2, 4, 3]);
-    });
+        ])).toStrictEqual([1, 2, 4, 3])
+    })
 
     it('Non square small', (): void => {
         expect(spiralOrder([
             [2, 5, 8],
             [4, 0, -1]
-        ])).toStrictEqual([2, 5, 8, -1, 0, 4]);
-    });
+        ])).toStrictEqual([2, 5, 8, -1, 0, 4])
+    })
 
     it('Square', (): void => {
         expect(spiralOrder([
             [1, 2, 3],
             [4, 5, 6],
             [7, 8, 9]
-        ])).toStrictEqual([1, 2, 3, 6, 9, 8, 7, 4, 5]);
-    });
+        ])).toStrictEqual([1, 2, 3, 6, 9, 8, 7, 4, 5])
+    })
 
     it('Non square', (): void => {
         expect(spiralOrder([
             [1, 2, 3, 4],
             [5, 6, 7, 8],
             [9, 10, 11, 12]
-        ])).toStrictEqual([1, 2, 3, 4, 8, 12, 11, 10, 9, 5, 6, 7]);
-    });
+        ])).toStrictEqual([1, 2, 3, 4, 8, 12, 11, 10, 9, 5, 6, 7])
+    })
 })
--- solutions/54. Spiral Matrix/script.ts
+++ solutions/54. Spiral Matrix/script.ts
@@ -1,43 +1,43 @@
 export {spiralOrder}
 
 function spiralOrder(matrix: number[][]): number[] {
-    const result: number[] = [];
-    let layer: number = 0;
+    const result: number[] = []
+    let layer: number = 0
 
     while (layer < Math.min(matrix.length, matrix[0].length)) {
-        result.push(...matrixBorder(matrix, layer));
-        layer++;
+        result.push(...matrixBorder(matrix, layer))
+        layer++
     }
 
-    return result;
+    return result
 }
 
 function matrixBorder(matrix: number[][], layer: number): number[] {
-    const result: number[] = [];
+    const result: number[] = []
 
     if (matrix.length > layer * 2) {
         for (let col: number = layer; col + layer < matrix[0].length; col++) {
-            result.push(matrix[layer][col]);
+            result.push(matrix[layer][col])
         }
     }
 
     if (matrix[0].length > 2 * layer) {
         for (let row: number = 1 + layer; row + layer < matrix.length; row++) {
-            result.push(matrix[row][matrix[0].length - 1 - layer]);
+            result.push(matrix[row][matrix[0].length - 1 - layer])
         }
     }
 
     if (matrix.length > layer * 2 + 1) {
         for (let col: number = matrix[0].length - 2 - layer; col - layer >= 0; col--) {
-            result.push(matrix[matrix.length - 1 - layer][col]);
+            result.push(matrix[matrix.length - 1 - layer][col])
         }
     }
 
     if (matrix[0].length > layer * 2 + 1) {
         for (let row: number = matrix.length - 2 - layer; row - layer > 0; row--) {
-            result.push(matrix[row][layer]);
+            result.push(matrix[row][layer])
         }
     }
 
-    return result;
+    return result
 }
