f08370ac901fa56d1dd243f198b298c215d6ffd6
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-09-25 14:26:35 +0300

    #21 Improve codestyle

--- solutions/21. Merge Two Sorted Lists/script.ts
+++ solutions/21. Merge Two Sorted Lists/script.ts
@@ -1,7 +1,7 @@
-import {mergeKLists, ListNode} from "../23. Merge k Sorted Lists/script"
+import {ListNode, mergeKLists} from "../23. Merge k Sorted Lists/script"
 
 export {mergeTwoLists, ListNode}
 
-const mergeTwoLists = function (list1: ListNode | null, list2: ListNode | null): ListNode | null {
+function mergeTwoLists(list1: ListNode | null, list2: ListNode | null): ListNode | null {
     return mergeKLists([list1, list2])
 }
