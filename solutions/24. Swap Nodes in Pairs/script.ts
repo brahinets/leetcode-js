169ab82e98a85049aaeef576fbe9a0cd66ca195c
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-09-26 20:24:59 +0300

    #24 Use more safe nullish coalesce

--- solutions/24. Swap Nodes in Pairs/script.ts
+++ solutions/24. Swap Nodes in Pairs/script.ts
@@ -4,15 +4,15 @@ export {swapPairs}
 
 function swapPairs(head: ListNode | null): ListNode | null {
     let firstNode: ListNode | null = head
-    let secondNode: ListNode | null = head?.next || null
+    let secondNode: ListNode | null = head?.next ?? null
 
     while (firstNode && secondNode) {
         const tmp: number = firstNode.val
         firstNode.val = secondNode.val
         secondNode.val = tmp
 
-        firstNode = firstNode?.next?.next || null
-        secondNode = secondNode?.next?.next || null
+        firstNode = firstNode?.next?.next ?? null
+        secondNode = secondNode?.next?.next ?? null
     }
 
     return head
