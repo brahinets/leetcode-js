12c6b70c1c5344d29775289714d5a108e035603e
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-09-26 20:23:58 +0300

    #24 Clean up

--- solutions/24. Swap Nodes in Pairs/__tests__/script.test.ts
+++ solutions/24. Swap Nodes in Pairs/__tests__/script.test.ts
@@ -1,28 +1,19 @@
 import {swapPairs} from '../script'
-import {linkedListOf} from "../../../common/list-utils";
+import {linkedListOf} from "../../../common/list-utils"
 
 describe('24. Swap Nodes in Pairs', (): void => {
     it('Swap nothing', (): void => {
-        expect(swapPairs(
-            linkedListOf([])
-        )).toStrictEqual(
-            linkedListOf([])
-        );
-    });
+        expect(swapPairs(linkedListOf([])))
+            .toStrictEqual(linkedListOf([]))
+    })
 
     it('Swap one', (): void => {
-        expect(swapPairs(
-            linkedListOf([1])
-        )).toStrictEqual(
-            linkedListOf([1])
-        );
-    });
+        expect(swapPairs(linkedListOf([1])))
+            .toStrictEqual(linkedListOf([1]))
+    })
 
     it('Swap all pairs', (): void => {
-        expect(swapPairs(
-            linkedListOf([1, 2, 3, 4])
-        )).toStrictEqual(
-            linkedListOf([2, 1, 4, 3])
-        );
-    });
+        expect(swapPairs(linkedListOf([1, 2, 3, 4])))
+            .toStrictEqual(linkedListOf([2, 1, 4, 3]))
+    })
 })
--- solutions/24. Swap Nodes in Pairs/script.ts
+++ solutions/24. Swap Nodes in Pairs/script.ts
@@ -1,19 +1,19 @@
-import {ListNode} from "../../common/ListNode";
+import {ListNode} from "../../common/ListNode"
 
-export {swapPairs};
+export {swapPairs}
 
 function swapPairs(head: ListNode | null): ListNode | null {
-    let firstNode: ListNode | null = head;
-    let secondNode: ListNode | null = head?.next || null;
+    let firstNode: ListNode | null = head
+    let secondNode: ListNode | null = head?.next || null
 
     while (firstNode && secondNode) {
-        const tmp: number = firstNode.val;
-        firstNode.val = secondNode.val;
-        secondNode.val = tmp;
+        const tmp: number = firstNode.val
+        firstNode.val = secondNode.val
+        secondNode.val = tmp
 
-        firstNode = firstNode?.next?.next || null;
-        secondNode = secondNode?.next?.next || null;
+        firstNode = firstNode?.next?.next || null
+        secondNode = secondNode?.next?.next || null
     }
 
-    return head;
+    return head
 }
