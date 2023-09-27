f2db57acd79eb3480fcdfa3fe0158f84c37bf11e
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-09-27 16:43:25 +0300

    #206 Improve readability

--- solutions/206. Reverse Linked List/__tests__/script.test.ts
+++ solutions/206. Reverse Linked List/__tests__/script.test.ts
@@ -1,30 +1,23 @@
-import {reverseList, ListNode} from '../script'
+import {reverseList} from '../script'
+import {linkedListOf} from "../../../common/list-utils";
 
 describe('206. Reverse Linked List', (): void => {
     it('Reverse long list', (): void => {
-        expect(
-            reverseList(
-                new ListNode(1,
-                    new ListNode(2,
-                        new ListNode(3,
-                            new ListNode(4,
-                                new ListNode(5))))),
-            )
-        ).toStrictEqual(
-            new ListNode(5,
-                new ListNode(4,
-                    new ListNode(3,
-                        new ListNode(2,
-                            new ListNode(1)))))
-        )
+        expect(reverseList(linkedListOf(1, 2, 3, 4, 5)))
+            .toStrictEqual(linkedListOf(5, 4, 3, 2, 1))
     })
 
     it('Reverse short list', (): void => {
-        expect(reverseList(new ListNode(1, new ListNode(2))))
-            .toStrictEqual(new ListNode(2, new ListNode(1)))
+        expect(reverseList(linkedListOf(1, 2)))
+            .toStrictEqual(linkedListOf(2, 1))
     })
 
     it('Reverse empty list', (): void => {
+        expect(reverseList(linkedListOf()))
+            .toStrictEqual(linkedListOf())
+    })
+
+    it('Reverse null list', (): void => {
         expect(reverseList(null))
             .toStrictEqual(null)
     })
