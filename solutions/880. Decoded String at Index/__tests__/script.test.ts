e2cd937298d4fd76dd497cabcad92585e20670be
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-09-27 18:52:19 +0300

    #880 Fix indexing start

--- solutions/880. Decoded String at Index/__tests__/script.test.ts
+++ solutions/880. Decoded String at Index/__tests__/script.test.ts
@@ -4,7 +4,7 @@ describe('880. Decoded String at Index', (): void => {
 
     it('No duplication', (): void => {
         expect(decodeAtIndex("leet", 3))
-            .toBe("t")
+            .toBe("e")
     })
 
 })
--- solutions/880. Decoded String at Index/script.ts
+++ solutions/880. Decoded String at Index/script.ts
@@ -3,5 +3,5 @@ export {decodeAtIndex}
 function decodeAtIndex(s: string, k: number): string {
     const result: string = s
 
-    return result[k]
+    return result[k - 1]
 }
