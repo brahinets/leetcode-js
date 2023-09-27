249cdb4dac5337e04c0f9a9a9522fa8fc396cf66
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-09-27 18:55:09 +0300

    #880 Clean up

--- solutions/880. Decoded String at Index/script.ts
+++ solutions/880. Decoded String at Index/script.ts
@@ -4,7 +4,8 @@ function decodeAtIndex(s: string, k: number): string {
     let result: string = ""
 
     for (const char of s) {
-        const count: number = Number(char);
+        const count: number = Number(char)
+
         if (count >= 0 && count <= 9) {
             result = result.repeat(count)
         } else {
