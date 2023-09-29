2cc676f8900598f0c92592d940ae5bef0d3b18e2
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-09-29 18:36:20 +0300

    #23 Improve readability

--- solutions/23. Merge k Sorted Lists/__tests__/script.test.ts
+++ solutions/23. Merge k Sorted Lists/__tests__/script.test.ts
@@ -1,28 +1,18 @@
-import {ListNode, mergeKLists} from '../script'
+import {mergeKLists} from '../script'
+import {linkedListOf} from "../../../common/list-utils";
 
 describe('23. Merge k Sorted Lists', (): void => {
     it('Two lists full', (): void => {
-        expect(
-            mergeKLists([
-                new ListNode(1,
-                    new ListNode(4,
-                        new ListNode(5))),
-                new ListNode(1,
-                    new ListNode(3,
-                        new ListNode(4))),
-                new ListNode(2,
-                    new ListNode(6))
-            ])
-        ).toStrictEqual(
-            new ListNode(1,
-                new ListNode(1,
-                    new ListNode(2,
-                        new ListNode(3,
-                            new ListNode(4,
-                                new ListNode(4,
-                                    new ListNode(5,
-                                        new ListNode(6))))))))
-        )
+        expect(mergeKLists([
+            linkedListOf(1, 4, 5),
+            linkedListOf(1, 3, 4),
+            linkedListOf(2, 6)
+        ])).toStrictEqual(linkedListOf(1, 1, 2, 3, 4, 4, 5, 6))
+    })
+
+    it('Two empty lists', (): void => {
+        expect(mergeKLists([linkedListOf(), linkedListOf()]))
+            .toStrictEqual(linkedListOf())
     })
 
     it('Both lists are empty', (): void => {
