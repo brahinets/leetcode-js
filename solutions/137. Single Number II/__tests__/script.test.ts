e501411ad11dacda081da04324bf0e1ea4e5689e
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-09-30 21:01:49 +0300

    #137 Clean up

--- solutions/137. Single Number II/__tests__/script.test.ts
+++ solutions/137. Single Number II/__tests__/script.test.ts
@@ -4,21 +4,21 @@ describe('137. Single Number II', (): void => {
 
     it('Element not found', (): void => {
         expect(() => singleNumber([]))
-            .toThrowError("Element not found");
-    });
+            .toThrowError("Element not found")
+    })
 
     it('Has none three times', (): void => {
         expect(singleNumber([1]))
-            .toBe(1);
-    });
+            .toBe(1)
+    })
 
     it('Has one three times', (): void => {
         expect(singleNumber([2, 2, 3, 2]))
-            .toBe(3);
-    });
+            .toBe(3)
+    })
 
     it('Has several three times', (): void => {
         expect(singleNumber([0, 1, 0, 1, 0, 1, 99]))
-            .toBe(99);
-    });
+            .toBe(99)
+    })
 })
--- solutions/137. Single Number II/script.ts
+++ solutions/137. Single Number II/script.ts
@@ -1,15 +1,15 @@
-import {count} from "../../common/array-utils";
+import {count} from "../../common/array-utils"
 
 export {singleNumber}
 
 function singleNumber(nums: number[]): number {
-    const counts: Map<number, number> = count(nums);
+    const counts: Map<number, number> = count(nums)
 
     for (const c of counts) {
         if (c[1] === 1) {
-            return c[0];
+            return c[0]
         }
     }
 
-    throw new Error("Element not found");
+    throw new Error("Element not found")
 }
