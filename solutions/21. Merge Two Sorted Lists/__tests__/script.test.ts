01734d514fc8d5f9d9eeb61903bb414a4af5a331
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-10-01 21:56:54 +0300

    #21 Improve readability

--- solutions/21. Merge Two Sorted Lists/__tests__/script.test.ts
+++ solutions/21. Merge Two Sorted Lists/__tests__/script.test.ts
@@ -1,33 +1,19 @@
-import {mergeTwoLists, ListNode} from '../script'
+import {mergeTwoLists} from '../script'
+import {linkedListOf} from "../../../common/list-utils";
 
 describe('21. Merge Two Sorted Lists', (): void => {
     it('Two lists full', (): void => {
-        expect(
-            mergeTwoLists(
-                new ListNode(1,
-                    new ListNode(2,
-                        new ListNode(4))),
-                new ListNode(1,
-                    new ListNode(3,
-                        new ListNode(4)))
-            )
-        ).toStrictEqual(
-            new ListNode(1,
-                new ListNode(1,
-                    new ListNode(2,
-                        new ListNode(3,
-                            new ListNode(4,
-                                new ListNode(4))))))
-        )
+        expect(mergeTwoLists(linkedListOf(1, 2, 4), linkedListOf(1, 3, 4)))
+            .toStrictEqual(linkedListOf(1, 1, 2, 3, 4, 4))
     })
 
     it('Only one lists full', (): void => {
-        expect(mergeTwoLists(null, new ListNode(1)))
-            .toStrictEqual(new ListNode(1))
+        expect(mergeTwoLists(linkedListOf(), linkedListOf(1)))
+            .toStrictEqual(linkedListOf(1))
     })
 
     it('Both lists are empty', (): void => {
-        expect(mergeTwoLists(null, null))
-            .toStrictEqual(null)
+        expect(mergeTwoLists(linkedListOf(), linkedListOf()))
+            .toStrictEqual(linkedListOf())
     })
 })
