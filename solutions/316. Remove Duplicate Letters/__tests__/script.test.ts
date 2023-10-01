c59e63adb066ccb67e9ad563ec26598a7d538fe9
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-10-01 21:53:54 +0300

    #316 Return smallest in lexicographical order

--- solutions/316. Remove Duplicate Letters/__tests__/script.test.ts
+++ solutions/316. Remove Duplicate Letters/__tests__/script.test.ts
@@ -10,4 +10,9 @@ describe('316. Remove Duplicate Letters', (): void => {
         expect(removeDuplicateLetters("bcabc"))
             .toBe("abc")
     })
+
+    it('Should return smallest in lexicographical order', (): void => {
+        expect(removeDuplicateLetters("cbacdcbc"))
+            .toBe("acdb")
+    })
 })
--- solutions/316. Remove Duplicate Letters/script.ts
+++ solutions/316. Remove Duplicate Letters/script.ts
@@ -1,5 +1,25 @@
 export {removeDuplicateLetters}
 
 function removeDuplicateLetters(s: string): string {
-    return [...new Set<string>(s.split(""))].sort().join("")
+    const result: string[] = []
+
+    for (let i: number = 0; i < s.length; i++) {
+        const char: string = s[i]
+
+        if (result.includes(char)) {
+            continue
+        }
+
+        while (last(result) > char && s.indexOf(last(result), i) > i) {
+            result.pop()
+        }
+
+        result.push(char)
+    }
+
+    return result.join("")
+}
+
+function last(string: string[]): string {
+    return string.length > 0 ? string[string.length - 1] : ""
 }
