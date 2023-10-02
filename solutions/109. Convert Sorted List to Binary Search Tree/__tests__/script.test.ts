74ec3d25f32ce05c3d66ae9d808896c342c2ca87
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-10-02 17:54:07 +0300

    #109 Improve readability

--- solutions/109. Convert Sorted List to Binary Search Tree/__tests__/script.test.ts
+++ solutions/109. Convert Sorted List to Binary Search Tree/__tests__/script.test.ts
@@ -1,21 +1,17 @@
 import {ListNode, sortedListToBST, TreeNode} from '../script'
+import {linkedListOf} from "../../../common/list-utils"
 
 describe('109. Convert Sorted List to Binary Search Tree', (): void => {
     it('Empty Tree', (): void => {
-        expect(sortedListToBST(
-            new ListNode(-10,
-                new ListNode(-3,
-                    new ListNode(0,
-                        new ListNode(5,
-                            new ListNode(9))))))
-        ).toStrictEqual(
-            new TreeNode(0,
-                new TreeNode(-3,
-                    new TreeNode(-10),
-                    null),
-                new TreeNode(9,
-                    new TreeNode(5))
-            ))
+        expect(sortedListToBST(linkedListOf(-10, -3, 0, 5, 9)))
+            .toStrictEqual(
+                new TreeNode(0,
+                    new TreeNode(-3,
+                        new TreeNode(-10),
+                        null),
+                    new TreeNode(9,
+                        new TreeNode(5))
+                ))
     })
 
     it('Empty Tree', (): void => {
