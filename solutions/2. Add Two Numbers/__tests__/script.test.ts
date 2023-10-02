f7e423619ab993d6490848d4d2c0d7961be4a831
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-10-02 17:55:40 +0300

    #2 Improve readability

--- solutions/2. Add Two Numbers/__tests__/script.test.ts
+++ solutions/2. Add Two Numbers/__tests__/script.test.ts
@@ -1,26 +1,19 @@
-import {addTwoNumbers, ListNode} from '../script'
+import {addTwoNumbers} from '../script'
+import {linkedListOf} from "../../../common/list-utils";
 
 describe('2. Add Two Numbers', (): void => {
     it('Zero', (): void => {
-        expect(addTwoNumbers(new ListNode(0), new ListNode(0)))
-            .toStrictEqual(new ListNode(0))
+        expect(addTwoNumbers(linkedListOf(0), linkedListOf(0)))
+            .toStrictEqual(linkedListOf(0))
     })
 
     it('Same radix', (): void => {
-        expect(addTwoNumbers(
-            new ListNode(2, new ListNode(4, new ListNode(3))),
-            new ListNode(5, new ListNode(6, new ListNode(4)))
-        )).toStrictEqual(
-            new ListNode(7, new ListNode(0, new ListNode(8)))
-        )
+        expect(addTwoNumbers(linkedListOf(2, 4, 3), linkedListOf(5, 6, 4)))
+            .toStrictEqual(linkedListOf(7, 0, 8))
     })
 
     it('Different radix', (): void => {
-        expect(addTwoNumbers(
-            new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9))))))),
-            new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9))))
-        )).toStrictEqual(
-            new ListNode(8, new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(0, new ListNode(0, new ListNode(0, new ListNode(1))))))))
-        )
+        expect(addTwoNumbers(linkedListOf(9, 9, 9, 9, 9, 9, 9), linkedListOf(9, 9, 9, 9)))
+            .toStrictEqual(linkedListOf(8, 9, 9, 9, 0, 0, 0, 1))
     })
 })
