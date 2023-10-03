7c171c2db00e3c7bd4092f0c16b407ea3a5e314c
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-10-03 21:10:09 +0300

    #445 Clean up

--- solutions/445. Add Two Numbers II/__tests__/script.test.ts
+++ solutions/445. Add Two Numbers II/__tests__/script.test.ts
@@ -1,19 +1,19 @@
 import {addTwoNumbers} from '../script'
-import {linkedListOf} from "../../../common/list-utils";
+import {linkedListOf} from "../../../common/list-utils"
 
 describe('445. Add Two Numbers II', (): void => {
     it('Zero', (): void => {
         expect(addTwoNumbers(linkedListOf(0), linkedListOf(0)))
-            .toStrictEqual(linkedListOf(0));
-    });
+            .toStrictEqual(linkedListOf(0))
+    })
 
     it('Same radix', (): void => {
         expect(addTwoNumbers(linkedListOf(2, 4, 3), linkedListOf(5, 6, 4)))
-            .toStrictEqual(linkedListOf(8, 0, 7));
-    });
+            .toStrictEqual(linkedListOf(8, 0, 7))
+    })
 
     it('Different radix', (): void => {
         expect(addTwoNumbers(linkedListOf(9, 9, 9, 9, 9, 9, 9), linkedListOf(9, 9, 9, 9)))
-            .toStrictEqual(linkedListOf(1, 0, 0, 0, 9, 9, 9, 8));
-    });
+            .toStrictEqual(linkedListOf(1, 0, 0, 0, 9, 9, 9, 8))
+    })
 })
--- solutions/445. Add Two Numbers II/script.ts
+++ solutions/445. Add Two Numbers II/script.ts
@@ -1,40 +1,40 @@
-import {ListNode} from "../../common/ListNode";
+import {ListNode} from "../../common/ListNode"
 
 export {ListNode, addTwoNumbers}
 
 function addTwoNumbers(l1: ListNode | null, l2: ListNode | null): ListNode {
-    l1 = reverseList(l1);
-    l2 = reverseList(l2);
+    l1 = reverseList(l1)
+    l2 = reverseList(l2)
 
-    const head: ListNode = new ListNode();
-    let incrementNextRadix: boolean = false;
+    const head: ListNode = new ListNode()
+    let incrementNextRadix: boolean = false
 
     while (l1 || l2 || incrementNextRadix) {
-        const sum: number = (incrementNextRadix ? 1 : 0) + (l1 ? l1.val : 0) + (l2 ? l2.val : 0);
+        const sum: number = (incrementNextRadix ? 1 : 0) + (l1 ? l1.val : 0) + (l2 ? l2.val : 0)
 
-        let end: ListNode = head;
+        let end: ListNode = head
         while (end.next) {
-            end = end.next;
+            end = end.next
         }
-        end.next = new ListNode(sum % 10);
+        end.next = new ListNode(sum % 10)
 
-        l1 &&= l1.next;
-        l2 &&= l2.next;
+        l1 &&= l1.next
+        l2 &&= l2.next
 
-        incrementNextRadix = sum >= 10;
+        incrementNextRadix = sum >= 10
     }
 
-    return reverseList(head.next) || new ListNode(0);
+    return reverseList(head.next) || new ListNode(0)
 }
 
 function reverseList(head: ListNode | null): ListNode | null {
-    let newHead: ListNode | null = null;
+    let newHead: ListNode | null = null
 
     while (head) {
-        newHead = new ListNode(head.val, newHead);
+        newHead = new ListNode(head.val, newHead)
 
-        head = head.next;
+        head = head.next
     }
 
-    return newHead;
+    return newHead
 }
