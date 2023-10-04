0422ffd9f44cc093bcae6863806a6a5ff2532d44
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-10-04 15:11:31 +0300

    #605 Clean up

--- solutions/605. Can Place Flowers/__tests__/script.test.ts
+++ solutions/605. Can Place Flowers/__tests__/script.test.ts
@@ -1,20 +1,22 @@
 import {canPlaceFlowers} from '../script'
-
 describe('605. Can Place Flowers', (): void => {
     it('Can place zero flower', (): void => {
-        expect(canPlaceFlowers([1, 0, 1, 0, 1], 0)).toBe(true);
-    });
+        expect(canPlaceFlowers([1, 0, 1, 0, 1], 0))
+            .toBe(true)
+    })
 
     it('Can place one flower', (): void => {
-        expect(canPlaceFlowers([1, 0, 0, 0, 1], 1)).toBe(true);
-    });
+        expect(canPlaceFlowers([1, 0, 0, 0, 1], 1))
+            .toBe(true)
+    })
 
     it('Can place one flower', (): void => {
-        expect(canPlaceFlowers([0], 1)).toBe(true);
-    });
+        expect(canPlaceFlowers([0], 1))
+            .toBe(true)
+    })
 
     it('Cannot place two flowers', (): void => {
-        expect(canPlaceFlowers([1, 0, 0, 0, 1], 2)).toBe(false);
-    });
-});
-
+        expect(canPlaceFlowers([1, 0, 0, 0, 1], 2))
+            .toBe(false)
+    })
+})
--- solutions/605. Can Place Flowers/script.ts
+++ solutions/605. Can Place Flowers/script.ts
@@ -1,26 +1,26 @@
 export function canPlaceFlowers(flowerbed: number[], n: number): boolean {
-    let canMore: number = 0;
-    let i: number = 0;
+    let canMore: number = 0
+    let i: number = 0
 
     while (i < flowerbed.length) {
         if (flowerbed[i] === 1) {
-            i += 2;
-            continue;
+            i += 2
+            continue
         }
 
-        const prev: number = i - 1;
-        const next: number = i + 1;
-        const noLeftBlockers: boolean = prev < 0 || flowerbed[prev] === 0;
-        const norRightBlockers: boolean = next >= flowerbed.length || flowerbed[next] === 0;
+        const prev: number = i - 1
+        const next: number = i + 1
+        const noLeftBlockers: boolean = prev < 0 || flowerbed[prev] === 0
+        const norRightBlockers: boolean = next >= flowerbed.length || flowerbed[next] === 0
 
         if (noLeftBlockers && norRightBlockers) {
-            i += 2;
-            canMore++;
-            continue;
+            i += 2
+            canMore++
+            continue
         }
 
-        i++;
+        i++
     }
 
-    return canMore >= n;
+    return canMore >= n
 }
