a5aab01dc68f8798168a33989fcdca9b9fc32ded
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-10-06 17:37:07 +0300

    #443 Clean up

--- solutions/443. String Compression/script.ts
+++ solutions/443. String Compression/script.ts
@@ -1,25 +1,25 @@
 export {compress}
 
 function compress(chars: string[]): number {
-    const result: string[] = [];
+    const result: string[] = []
 
     for (let i: number = 0; i < chars.length;) {
-        const char: string = chars[i];
+        const char: string = chars[i]
 
-        let count: number = 0;
+        let count: number = 0
         while (chars[i] === char) {
-            count++;
-            i++;
+            count++
+            i++
         }
 
-        result.push(char);
+        result.push(char)
         if (count > 1) {
-            result.push(...String(count));
+            result.push(...String(count))
         }
     }
 
-    chars.length = 0;
-    chars.push(...result);
+    chars.length = 0
+    chars.push(...result)
 
-    return result.length;
+    return result.length
 }
