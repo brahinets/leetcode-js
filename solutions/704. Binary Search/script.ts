282f4b7013f941ded4190a0399419bc2c2157074
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-10-08 23:23:04 +0300

    #704 Clean up

--- solutions/704. Binary Search/script.ts
+++ solutions/704. Binary Search/script.ts
@@ -1,20 +1,20 @@
 export {search}
 
 function search(nums: number[], target: number): number {
-    let left: number = 0;
-    let right: number = nums.length;
+    let left: number = 0
+    let right: number = nums.length
 
     while (left < right) {
-        const mid: number = Math.floor((left + right) / 2);
+        const mid: number = Math.floor((left + right) / 2)
 
         if (nums[mid] === target) {
             return mid
         } else if(nums[mid] > target) {
-            right = mid;
+            right = mid
         } else {
-            left = mid + 1;
+            left = mid + 1
         }
     }
 
-    return -1;
+    return -1
 }
