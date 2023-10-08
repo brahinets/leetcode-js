554f8d094979980535796a65a035f2772aaa25f1
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-10-08 23:21:25 +0300

    #228 Clean up

--- solutions/228. Summary Ranges/__tests__/script.test.ts
+++ solutions/228. Summary Ranges/__tests__/script.test.ts
@@ -3,21 +3,21 @@ import {summaryRanges} from '../script'
 describe('228. Summary Ranges', (): void => {
     it('One range', (): void => {
         expect(summaryRanges([0, 1, 2]))
-            .toStrictEqual(["0->2"]);
-    });
+            .toStrictEqual(["0->2"])
+    })
 
     it('Multiple ranges', (): void => {
         expect(summaryRanges([0, 1, 2, 5, 6, 7]))
-            .toStrictEqual(["0->2", "5->7"]);
-    });
+            .toStrictEqual(["0->2", "5->7"])
+    })
 
     it('Ends with singular range', (): void => {
         expect(summaryRanges([0, 1, 2, 4, 5, 7]))
-            .toStrictEqual(["0->2", "4->5", "7"]);
-    });
+            .toStrictEqual(["0->2", "4->5", "7"])
+    })
 
     it('Starts with singular range', (): void => {
         expect(summaryRanges([0, 2, 3, 4, 6, 8, 9]))
-            .toStrictEqual(["0", "2->4", "6", "8->9"]);
-    });
+            .toStrictEqual(["0", "2->4", "6", "8->9"])
+    })
 })
--- solutions/228. Summary Ranges/script.ts
+++ solutions/228. Summary Ranges/script.ts
@@ -1,24 +1,24 @@
 export {summaryRanges}
 
 function summaryRanges(nums: number[]): string[] {
-    const ranges: string[] = [];
+    const ranges: string[] = []
 
-    let start: number = 0;
-    let end: number = 0;
+    let start: number = 0
+    let end: number = 0
     while (start < nums.length && end < nums.length) {
         if (end + 1 < nums.length && nums[end] + 1 === nums[end + 1]) {
-            end++;
+            end++
         } else {
-            if(start !== end) {
-                ranges.push(nums[start] + "->" + nums[end]);
+            if (start !== end) {
+                ranges.push(nums[start] + "->" + nums[end])
             } else {
-                ranges.push(String(nums[start]));
+                ranges.push(String(nums[start]))
             }
 
-            start = end + 1;
-            end = start;
+            start = end + 1
+            end = start
         }
     }
 
-    return ranges;
+    return ranges
 }
