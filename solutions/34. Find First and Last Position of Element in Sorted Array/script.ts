82e17c561c0360ee6ebf059fc77bc23a32fd226c
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-10-09 12:55:14 +0300

    #34 Add todo for speed up to meet requirements

--- solutions/34. Find First and Last Position of Element in Sorted Array/script.ts
+++ solutions/34. Find First and Last Position of Element in Sorted Array/script.ts
@@ -1,6 +1,7 @@
 export {searchRange}
 
 function searchRange(nums: number[], target: number): number[] {
+    // TODO Speed up
     const position: number = nums.indexOf(target)
 
     if (position === -1) {
