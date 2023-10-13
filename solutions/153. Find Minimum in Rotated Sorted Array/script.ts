7d5a41366f65d9557392d8e38cb4d3725dac76f9
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-10-13 14:11:53 +0300

    #153 Optimise, use binary search to find the start of initial array

--- solutions/153. Find Minimum in Rotated Sorted Array/script.ts
+++ solutions/153. Find Minimum in Rotated Sorted Array/script.ts
@@ -1,5 +1,18 @@
 export {findMin}
 
 function findMin(nums: number[]): number {
-    return Math.min(...nums)
+    let left: number = 0
+    let right: number = nums.length - 1
+
+    while (left < right) {
+        const mid: number = left + Math.floor((right - left) / 2)
+
+        if (nums[mid] > nums[right]) {
+            left = mid + 1
+        } else {
+            right = mid
+        }
+    }
+
+    return nums[left]
 }
