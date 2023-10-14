1c87640a1b60a77005d2f45bc09eb3d422ab30f3
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-10-14 22:07:37 +0300

    #35 Clean up

--- solutions/35. Search Insert Position/script.ts
+++ solutions/35. Search Insert Position/script.ts
@@ -1,20 +1,20 @@
 export {searchInsert}
 
-const searchInsert = function (nums: number[], target: number): number {
-    let start: number = 0;
-    let end: number = nums.length - 1;
+function searchInsert(nums: number[], target: number): number {
+    let start: number = 0
+    let end: number = nums.length - 1
 
     while (start <= end) {
-        const mid: number = Math.floor((end + start) / 2);
+        const mid: number = Math.floor((end + start) / 2)
 
         if (nums[mid] === target) {
-            return mid;
+            return mid
         } else if (nums[mid] < target) {
-            start = mid + 1;
+            start = mid + 1
         } else {
-            end = mid - 1;
+            end = mid - 1
         }
     }
 
-    return start;
+    return start
 }
