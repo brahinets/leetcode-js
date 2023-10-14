48617918f5a0eb8eacc8c51d2b77bef8ef919546
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-10-14 22:06:40 +0300

    #33 Optimise, use binary search to find the element in array

--- solutions/33. Search in Rotated Sorted Array/script.ts
+++ solutions/33. Search in Rotated Sorted Array/script.ts
@@ -1,5 +1,30 @@
 export {search}
 
 function search(nums: number[], target: number): number {
-    return nums.indexOf(target)
+    let left: number = 0
+    let right: number = nums.length - 1
+
+    while (left <= right) {
+        const mid: number = Math.floor((right + left) / 2)
+
+        if (nums[mid] === target) {
+            return mid
+        }
+
+        if (nums[mid] >= nums[left]) {
+            if (target > nums[mid] || target < nums[left]) {
+                left = mid + 1
+            } else {
+                right = mid - 1
+            }
+        } else if (nums[mid] <= nums[right]) {
+            if (target > nums[right] || target < nums[mid]) {
+                right = mid - 1
+            } else {
+                left = mid + 1
+            }
+        }
+    }
+
+    return -1
 }
