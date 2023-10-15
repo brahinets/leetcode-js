f108e49b0a2f73022c51aeb3eafb646a013932da
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-10-15 19:20:26 +0300

    #4 Clean up

--- solutions/4. Median of Two Sorted Arrays/__tests__/script.test.ts
+++ solutions/4. Median of Two Sorted Arrays/__tests__/script.test.ts
@@ -2,10 +2,12 @@ import {findMedianSortedArrays} from '../script'
 
 describe('4. Median of Two Sorted Arrays', (): void => {
     it('Odd size', (): void => {
-        expect(findMedianSortedArrays([1, 3], [2])).toBe(2);
+        expect(findMedianSortedArrays([1, 3], [2]))
+            .toBe(2)
     });
 
     it('Even size', (): void => {
-        expect(findMedianSortedArrays([1, 2], [3, 4])).toBe(2.5);
+        expect(findMedianSortedArrays([1, 2], [3, 4]))
+            .toBe(2.5)
     });
 })
--- solutions/4. Median of Two Sorted Arrays/script.ts
+++ solutions/4. Median of Two Sorted Arrays/script.ts
@@ -1,12 +1,12 @@
 export {findMedianSortedArrays}
 
-const findMedianSortedArrays = function (nums1: number[], nums2: number[]):number {
+function findMedianSortedArrays(nums1: number[], nums2: number[]): number {
     const numbers: number[] = [...nums1, ...nums2]
-        .sort((a: number, b: number) => a - b);
+        .sort((a: number, b: number) => a - b)
 
     if (numbers.length % 2 === 0) {
-        return (numbers[numbers.length / 2 - 1] + numbers[numbers.length / 2]) / 2;
+        return (numbers[numbers.length / 2 - 1] + numbers[numbers.length / 2]) / 2
     } else {
-        return numbers[Math.floor(numbers.length / 2)];
+        return numbers[Math.floor(numbers.length / 2)]
     }
 }
