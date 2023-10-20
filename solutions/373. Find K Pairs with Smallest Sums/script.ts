6515e79f25289e61e97f21cf8d958c9a78267c01
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-10-20 21:56:21 +0300

    #373 Clean up

--- solutions/373. Find K Pairs with Smallest Sums/script.ts
+++ solutions/373. Find K Pairs with Smallest Sums/script.ts
@@ -1,7 +1,7 @@
 export {kSmallestPairs}
 
 function kSmallestPairs(nums1: number[], nums2: number[], k: number): number[][] {
-    const topPairs: PriorityHeap = new PriorityHeap(k);
+    const topPairs: PriorityHeap = new PriorityHeap(k)
 
     for (let i: number = 0; i < nums1.length; i++) {
         for (let j: number = 0; j < nums2.length; j++) {
@@ -9,48 +9,48 @@ function kSmallestPairs(nums1: number[], nums2: number[], k: number): number[][]
         }
     }
 
-    return topPairs.data;
+    return topPairs.data
 }
 
 class PriorityHeap {
-    data: number[][];
-    limit: number;
-    max: number | undefined;
+    data: number[][]
+    limit: number
+    max: number | undefined
 
     constructor(limit: number) {
-        this.data = [];
-        this.limit = limit;
-        this.max = undefined;
+        this.data = []
+        this.limit = limit
+        this.max = undefined
     }
 
     add(p1: number, p2: number): void {
         if (this.data.length < this.limit) {
-            this.data.push([p1, p2]);
+            this.data.push([p1, p2])
 
             if(!this.max || p1 + p2 > this.max) {
-                this.max = p1 + p2;
+                this.max = p1 + p2
             }
         } else {
-            const [max, maxIndex]: number[] = findMaxIndex(this.data);
+            const [max, maxIndex]: number[] = findMaxIndex(this.data)
 
             if (p1 + p2 < max) {
-                this.data[maxIndex] = [p1, p2];
-                this.max = findMaxIndex(this.data)[0];
+                this.data[maxIndex] = [p1, p2]
+                this.max = findMaxIndex(this.data)[0]
             }
         }
     }
 }
 
 function findMaxIndex(data: number[][]): number[] {
-    let max: number = data[0][0] + data[0][1];
-    let maxIndex: number = 0;
+    let max: number = data[0][0] + data[0][1]
+    let maxIndex: number = 0
     for (let i: number = 1; i < data.length; i++) {
-        const pairSum: number = data[i][0] + data[i][1];
+        const pairSum: number = data[i][0] + data[i][1]
         if (pairSum > max) {
-            max = pairSum;
-            maxIndex = i;
+            max = pairSum
+            maxIndex = i
         }
     }
 
-    return [max, maxIndex];
+    return [max, maxIndex]
 }
