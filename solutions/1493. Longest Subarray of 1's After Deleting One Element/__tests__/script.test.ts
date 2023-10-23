02b18f669f28a9388146f5aa8faef298934237ea
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-10-23 11:41:39 +0300

    #1493 Clean up

--- solutions/1493. Longest Subarray of 1's After Deleting One Element/__tests__/script.test.ts
+++ solutions/1493. Longest Subarray of 1's After Deleting One Element/__tests__/script.test.ts
@@ -4,21 +4,21 @@ describe('1493. Longest Subarray of 1\'s After Deleting One Element', (): void =
 
     it('Zero', (): void => {
         expect(longestSubarray([0, 0, 0, 0]))
-            .toBe(0);
-    });
+            .toBe(0)
+    })
 
     it('All already but must delete one element', (): void => {
         expect(longestSubarray([1, 1, 1]))
-            .toBe(2);
-    });
+            .toBe(2)
+    })
 
     it('Keep all after remove one', (): void => {
         expect(longestSubarray([1, 1, 0, 1]))
-            .toBe(3);
-    });
+            .toBe(3)
+    })
 
     it('Keep subset after remove one', (): void => {
         expect(longestSubarray([0, 1, 1, 1, 0, 1, 1, 0, 1]))
-            .toBe(5);
-    });
+            .toBe(5)
+    })
 })
--- solutions/1493. Longest Subarray of 1's After Deleting One Element/script.ts
+++ solutions/1493. Longest Subarray of 1's After Deleting One Element/script.ts
@@ -1,28 +1,28 @@
 export {longestSubarray}
 
 function longestSubarray(nums: number[]): number {
-    let maxCount: number = 0;
-    let start: number = 0;
-    let end: number = 0;
-    const barrierSize:number = 1;
-    let brokenSize: number = 0;
+    let maxCount: number = 0
+    let start: number = 0
+    let end: number = 0
+    const barrierSize: number = 1
+    let brokenSize: number = 0
 
     for (let i: number = 0; i < nums.length; i++) {
-        end++;
+        end++
 
         if (nums[i] !== 1) {
             if (brokenSize >= barrierSize) {
                 while (nums[start] === 1) {
-                    start++;
+                    start++
                 }
-                start++;
+                start++
             }
 
-            brokenSize++;
+            brokenSize++
         }
 
-        maxCount = Math.max(brokenSize >= barrierSize ? end - start - 1 : end - start, maxCount);
+        maxCount = Math.max(brokenSize >= barrierSize ? end - start - 1 : end - start, maxCount)
     }
 
-    return brokenSize >= barrierSize ? maxCount : maxCount - 1;
+    return brokenSize >= barrierSize ? maxCount : maxCount - 1
 }
