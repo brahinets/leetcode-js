f5ee58af857f76dda7cb009f93f79cdef205d64f
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-10-23 11:40:24 +0300

    #1498 Clean up

--- solutions/1498. Number of Subsequences That Satisfy the Given Sum Condition/script.ts
+++ solutions/1498. Number of Subsequences That Satisfy the Given Sum Condition/script.ts
@@ -1,45 +1,45 @@
 export {numSubseq}
 
 function numSubseq(nums: number[], target: number): number {
-    nums.sort((a: number, b: number): number => a - b);
+    nums.sort((a: number, b: number): number => a - b)
 
-    const mod: number = 1e9 + 7;
-    const power: number[] = calculatePowers(nums.length, mod);
+    const mod: number = 1e9 + 7
+    const power: number[] = calculatePowers(nums.length, mod)
 
-    let answer: number = 0;
+    let answer: number = 0
     for (let left: number = 0; left < nums.length; left++) {
-        const right: number = binarySearch(nums, target - nums[left]) - 1;
+        const right: number = binarySearch(nums, target - nums[left]) - 1
         if (left <= right) {
-            const validCombinationsBetween: number = power[right - left];
-            answer = (answer + validCombinationsBetween) % mod;
+            const validCombinationsBetween: number = power[right - left]
+            answer = (answer + validCombinationsBetween) % mod
         }
     }
 
-    return answer;
+    return answer
 }
 
 function calculatePowers(nums: number, mod: number): number[] {
-    const power: number[] = new Array<number>();
-    power.push(1);
+    const power: number[] = new Array<number>()
+    power.push(1)
     for (let i: number = 1; i < nums; i++) {
-        power[i] = (power[i - 1] * 2) % mod;
+        power[i] = (power[i - 1] * 2) % mod
     }
-    return power;
+    return power
 }
 
 function binarySearch(nums: number[], target: number): number {
-    let left: number = 0;
-    let right: number = nums.length - 1;
+    let left: number = 0
+    let right: number = nums.length - 1
 
     while (left <= right) {
-        const mid: number = Math.floor((left + right) / 2);
+        const mid: number = Math.floor((left + right) / 2)
 
         if (nums[mid] <= target) {
-            left = mid + 1;
+            left = mid + 1
         } else {
-            right = mid - 1;
+            right = mid - 1
         }
     }
 
-    return left;
+    return left
 }
