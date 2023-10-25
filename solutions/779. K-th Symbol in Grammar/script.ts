12d84339f20366c63ee83c3761fb0e8c82dfda4e
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-10-25 13:26:55 +0300

    #779 Optimize regexp re-calculation

--- solutions/779. K-th Symbol in Grammar/script.ts
+++ solutions/779. K-th Symbol in Grammar/script.ts
@@ -5,11 +5,15 @@ export {TreeNode, kthGrammar}
 function kthGrammar(n: number, k: number): number {
     let str: string = "0"
 
+    const first: RegExp = new RegExp("0", "g");
+    const second: RegExp = new RegExp("1", "g");
+    const third: RegExp = new RegExp("x", "g")
+
     let i: number = 1
     while (i++ < n) {
-        str = str.replace(new RegExp("0", "g"), "0x")
-        str = str.replace(new RegExp("1", "g"), "x0")
-        str = str.replace(new RegExp("x", "g"), "1")
+        str = str.replace(first, "0x")
+        str = str.replace(second, "x0")
+        str = str.replace(third, "1")
     }
 
     return parseInt(str[k - 1])
