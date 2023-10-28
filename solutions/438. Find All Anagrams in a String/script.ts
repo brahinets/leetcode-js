d1417d89deb7ad58deb40a1f2397f30c5196d45a
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-10-28 09:39:12 +0300

    #438 Reuse code

--- solutions/438. Find All Anagrams in a String/script.ts
+++ solutions/438. Find All Anagrams in a String/script.ts
@@ -1,3 +1,5 @@
+import {mapsAreEqual} from "../../common/map-utils"
+
 export {findAnagrams}
 
 function countChars(str: string): Map<string, number> {
@@ -16,21 +18,14 @@ function countChars(str: string): Map<string, number> {
     return charsCounts
 }
 
-const mapsAreEqual = (m1: Map<string, number>, m2: Map<string, number>): boolean => {
-    return m1.size === m2.size
-        && Array.from(m1.keys()).every((key: string): boolean => m1.get(key) === m2.get(key))
-}
-
-function isAnagram(charsCount1: Map<string, number>, charsCount2: Map<string, number>): boolean {
-    return mapsAreEqual(charsCount1, charsCount2)
-}
-
 function findAnagrams(s: string, p: string): number[] {
     const anagramsBeginnings: number[] = []
     const charsP: Map<string, number> = countChars(p)
 
     for (let i: number = 0; i < s.length - p.length + 1; i++) {
-        if (isAnagram(countChars(s.substring(i, i + p.length)), charsP)) {
+        const anagram: boolean = mapsAreEqual(countChars(s.substring(i, i + p.length)), charsP)
+
+        if (anagram) {
             anagramsBeginnings.push(i)
         }
     }
