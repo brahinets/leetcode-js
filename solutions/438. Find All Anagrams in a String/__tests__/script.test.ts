3e01bcf7cef4cdd9b0bcbb4d006dc3cc4741b0fe
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-10-28 09:37:26 +0300

    #438 Clean up

--- solutions/438. Find All Anagrams in a String/__tests__/script.test.ts
+++ solutions/438. Find All Anagrams in a String/__tests__/script.test.ts
@@ -2,10 +2,12 @@ import {findAnagrams} from '../script'
 
 describe('438. Find All Anagrams in a String', (): void => {
     it('One anagram', (): void => {
-        expect(findAnagrams("cbaebabacd", "abc")).toStrictEqual([0, 6]);
-    });
+        expect(findAnagrams("cbaebabacd", "abc"))
+            .toStrictEqual([0, 6])
+    })
 
     it('Multiple anagrams', (): void => {
-        expect(findAnagrams("abab", "ab")).toStrictEqual([0, 1, 2]);
-    });
-});
+        expect(findAnagrams("abab", "ab"))
+            .toStrictEqual([0, 1, 2])
+    })
+})
--- solutions/438. Find All Anagrams in a String/script.ts
+++ solutions/438. Find All Anagrams in a String/script.ts
@@ -1,39 +1,39 @@
 export {findAnagrams}
 
 function countChars(str: string): Map<string, number> {
-    const charsCounts: Map<string, number> = new Map<string, number>();
+    const charsCounts: Map<string, number> = new Map<string, number>()
 
     for (const char of str) {
-        let count: number | undefined = charsCounts.get(char);
+        let count: number | undefined = charsCounts.get(char)
 
         if (count === undefined) {
-            count = 0;
+            count = 0
         }
 
-        charsCounts.set(char, count + 1);
+        charsCounts.set(char, count + 1)
     }
 
-    return charsCounts;
+    return charsCounts
 }
 
 const mapsAreEqual = (m1: Map<string, number>, m2: Map<string, number>): boolean => {
     return m1.size === m2.size
-        && Array.from(m1.keys()).every((key: string): boolean => m1.get(key) === m2.get(key));
+        && Array.from(m1.keys()).every((key: string): boolean => m1.get(key) === m2.get(key))
 }
 
 function isAnagram(charsCount1: Map<string, number>, charsCount2: Map<string, number>): boolean {
-    return mapsAreEqual(charsCount1, charsCount2);
+    return mapsAreEqual(charsCount1, charsCount2)
 }
 
 function findAnagrams(s: string, p: string): number[] {
-    const anagramsBeginnings: number[] = [];
-    const charsP: Map<string, number> = countChars(p);
+    const anagramsBeginnings: number[] = []
+    const charsP: Map<string, number> = countChars(p)
 
     for (let i: number = 0; i < s.length - p.length + 1; i++) {
         if (isAnagram(countChars(s.substring(i, i + p.length)), charsP)) {
-            anagramsBeginnings.push(i);
+            anagramsBeginnings.push(i)
         }
     }
 
-    return anagramsBeginnings;
+    return anagramsBeginnings
 }
