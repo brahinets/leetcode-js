cf180b338e8511230026b2ab42056cbd95a81000
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-11-09 13:12:11 +0200

    #1603 Clean up

--- solutions/1603. Design Parking System/__tests__/script.test.ts
+++ solutions/1603. Design Parking System/__tests__/script.test.ts
@@ -1,27 +1,27 @@
-import {ParkingSystem} from "../script";
+import {ParkingSystem} from "../script"
 
 describe('1603. Design Parking System', (): void => {
     it('Test Valid', (): void => {
         const system: ParkingSystem = new ParkingSystem(1, 1, 0)
-        let added: boolean;
+        let added: boolean
 
-        added = system.addCar(1);
-        expect(added).toBe(true);
+        added = system.addCar(1)
+        expect(added).toBe(true)
 
-        added = system.addCar(2);
-        expect(added).toBe(true);
+        added = system.addCar(2)
+        expect(added).toBe(true)
 
-        added = system.addCar(3);
-        expect(added).toBe(false);
+        added = system.addCar(3)
+        expect(added).toBe(false)
 
-        added = system.addCar(1);
-        expect(added).toBe(false);
-    });
+        added = system.addCar(1)
+        expect(added).toBe(false)
+    })
 
     it('Test illegal car type', (): void => {
         const system: ParkingSystem = new ParkingSystem(1, 1, 0)
 
         expect(() => system.addCar(0))
-            .toThrowError("Unsupported car type provided");
-    });
+            .toThrowError("Unsupported car type provided")
+    })
 })
--- solutions/1603. Design Parking System/script.ts
+++ solutions/1603. Design Parking System/script.ts
@@ -9,27 +9,27 @@ enum CarType {
 
 class ParkingSystem {
 
-    private capacity: Map<CarType, number>;
+    private capacity: Map<CarType, number>
 
     constructor(small: number, medium: number, big: number) {
-        this.capacity = new Map<CarType, number>();
+        this.capacity = new Map<CarType, number>()
         this.capacity.set(CarType.SmallCar, small)
         this.capacity.set(CarType.MediumCar, medium)
         this.capacity.set(CarType.BigCar, big)
     }
 
     addCar(carType: number): boolean {
-        const capacity: number | undefined = this.capacity.get(carType);
+        const capacity: number | undefined = this.capacity.get(carType)
 
         if (capacity !== undefined) {
             if (capacity === 0) {
-                return false;
+                return false
             }
 
-            this.capacity.set(carType, capacity - 1);
-            return true;
+            this.capacity.set(carType, capacity - 1)
+            return true
         }
 
-        throw new Error("Unsupported car type provided");
+        throw new Error("Unsupported car type provided")
     }
 }
