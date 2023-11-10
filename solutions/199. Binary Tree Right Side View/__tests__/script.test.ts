be1b21036f7c54f802869916f75bff07e89a4ed8
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-11-10 12:01:07 +0200

    #199 Support mixed tree

--- solutions/199. Binary Tree Right Side View/__tests__/script.test.ts
+++ solutions/199. Binary Tree Right Side View/__tests__/script.test.ts
@@ -36,4 +36,13 @@ describe('199. Binary Tree Right Side View', (): void => {
             )
         )).toEqual([1, 3, 4])
     })
+
+    it('Long mixed tree', (): void => {
+        expect(rightSideView(
+            new TreeNode(1,
+                new TreeNode(2,
+                    new TreeNode(4)),
+                new TreeNode(3))
+        )).toEqual([1, 3, 4])
+    })
 })
--- solutions/199. Binary Tree Right Side View/script.ts
+++ solutions/199. Binary Tree Right Side View/script.ts
@@ -2,11 +2,28 @@ export {rightSideView, TreeNode}
 import {TreeNode} from '../../common/TreeNode'
 
 function rightSideView(root: TreeNode | null): number[] {
-    const result: number[] = []
+    return [...collect(root, 0).values()]
+        .map((values: number[]) => values[values.length - 1])
+}
 
-    if (!root) {
+function collect(node: TreeNode | null, layer: number): Map<number, number[]> {
+    const result: Map<number, number[]> = new Map<number, number[]>()
+
+    if (!node) {
         return result
     }
 
-    return [root.val, ...rightSideView(root.right ?? root.left)]
+    result.set(layer, [node.val])
+
+    const left: Map<number, number[]> = collect(node.left, layer + 1)
+    for (const [level, values] of left.entries()) {
+        result.set(level, [...(result.get(level) ?? []), ...values])
+    }
+
+    const right: Map<number, number[]> = collect(node.right, layer + 1)
+    for (const [level, values] of right.entries()) {
+        result.set(level, [...(result.get(level) ?? []), ...values])
+    }
+
+    return result
 }
