2ff9af5bf49c4a2783aefc9afcf4206ef109ae70
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-11-10 11:58:02 +0200

    #199 Support lefty tree

--- solutions/199. Binary Tree Right Side View/__tests__/script.test.ts
+++ solutions/199. Binary Tree Right Side View/__tests__/script.test.ts
@@ -7,10 +7,18 @@ describe('199. Binary Tree Right Side View', (): void => {
             .toEqual([])
     })
 
-    it('Short tree', (): void => {
+    it('Short lefty tree', (): void => {
         expect(rightSideView(
             new TreeNode(1,
                 null,
+                new TreeNode(3)
+            )
+        )).toEqual([1, 3])
+    })
+
+    it('Short righty tree', (): void => {
+        expect(rightSideView(
+            new TreeNode(1,
                 new TreeNode(3)
             )
         )).toEqual([1, 3])
--- solutions/199. Binary Tree Right Side View/script.ts
+++ solutions/199. Binary Tree Right Side View/script.ts
@@ -2,11 +2,11 @@ export {rightSideView, TreeNode}
 import {TreeNode} from '../../common/TreeNode'
 
 function rightSideView(root: TreeNode | null): number[] {
-    const result:number[] = []
+    const result: number[] = []
 
-    if(!root) {
+    if (!root) {
         return result
     }
 
-    return [root.val, ...rightSideView(root.right)]
+    return [root.val, ...rightSideView(root.right ?? root.left)]
 }
