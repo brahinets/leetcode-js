24ec7307140889ac53fc769aa3696dbac231e0e7
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-11-11 17:43:05 +0200

    #2448 Clean up

--- solutions/2448. Minimum Cost to Make Array Equal/__tests__/script.test.ts
+++ solutions/2448. Minimum Cost to Make Array Equal/__tests__/script.test.ts
@@ -3,18 +3,18 @@ import {minCost} from '../script'
 describe('2448. Minimum Cost to Make Array Equal', (): void => {
     it('Some cost, require balancing', (): void => {
         expect(minCost([1, 3, 5, 2], [2, 3, 1, 14]))
-            .toBe(8);
-    });
+            .toBe(8)
+    })
 
     it('Big cost, require balancing', (): void => {
         expect(minCost(
             [735103, 366367, 132236, 133334, 808160, 113001, 49051, 735598, 686615, 665317, 999793, 426087, 587000, 649989, 509946, 743518],
             [724182, 447415, 723725, 902336, 600863, 287644, 13836, 665183, 448859, 917248, 397790, 898215, 790754, 320604, 468575, 825614]
-        )).toBe(1907611126748);
-    });
+        )).toBe(1907611126748)
+    })
 
     it('Zero cost, data already equal', (): void => {
         expect(minCost([2, 2, 2, 2, 2], [4, 2, 8, 1, 3]))
-            .toBe(0);
-    });
+            .toBe(0)
+    })
 })
--- solutions/2448. Minimum Cost to Make Array Equal/script.ts
+++ solutions/2448. Minimum Cost to Make Array Equal/script.ts
@@ -1,29 +1,29 @@
 export {minCost}
 
 function minCost(nums: number[], cost: number[]): number {
-    let minCost: number = Number.MAX_SAFE_INTEGER;
-    let left: number = Math.min(...nums);
-    let right: number = Math.max(...nums);
+    let minCost: number = Number.MAX_SAFE_INTEGER
+    let left: number = Math.min(...nums)
+    let right: number = Math.max(...nums)
 
     while (left <= right) {
-        const target: number = Math.floor((left + right) / 2);
-        const costOfMid: number = calculateCost(nums, cost, target);
-        const costOfMidNext: number = calculateCost(nums, cost, target + 1);
+        const target: number = Math.floor((left + right) / 2)
+        const costOfMid: number = calculateCost(nums, cost, target)
+        const costOfMidNext: number = calculateCost(nums, cost, target + 1)
 
-        minCost = Math.min(costOfMid, costOfMidNext);
+        minCost = Math.min(costOfMid, costOfMidNext)
         if (costOfMid > costOfMidNext) {
-            left = target + 1;
+            left = target + 1
         } else {
-            right = target - 1;
+            right = target - 1
         }
     }
 
-    return minCost;
+    return minCost
 }
 
 function calculateCost(nums: number[], cost: number[], target: number) {
     return nums.reduce((result: number, e: number, index: number) =>
             result + Math.abs(target - e) * cost[index],
         0
-    );
+    )
 }
