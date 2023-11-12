b335f5d4c7c581aa9698c25fd16e4c8c52bcdca9
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-11-12 19:46:10 +0200

    #1491 Clean up

--- solutions/1491. Average Salary Excluding the Minimum and Maximum Salary/script.ts
+++ solutions/1491. Average Salary Excluding the Minimum and Maximum Salary/script.ts
@@ -1,10 +1,10 @@
 export {average}
 
 function average(salary: number[]): number {
-    const min: number = Math.min(...salary);
-    const max: number = Math.max(...salary);
+    const min: number = Math.min(...salary)
+    const max: number = Math.max(...salary)
 
-    const sum: number = salary.reduce((sum: number, value: number): number => sum + value, 0);
+    const sum: number = salary.reduce((sum: number, value: number): number => sum + value, 0)
 
-    return (sum - min - max) / (salary.length - 2);
+    return (sum - min - max) / (salary.length - 2)
 }
