6b6bb8122e8aced6744e4684ba9a2bf2d1a80b55
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-11-19 19:43:51 +0200

    #1838 Fix window shrinking

--- solutions/1838. Frequency of the Most Frequent Element/script.ts
+++ solutions/1838. Frequency of the Most Frequent Element/script.ts
@@ -10,14 +10,13 @@ function maxFrequency(nums: number[], k: number): number {
     for (let windowEnd: number = 0; windowEnd < nums.length; windowEnd++) {
         const targetNum: number = nums[windowEnd]
         allocated += targetNum
-        const windowSize: number = 1 + (windowEnd - windowStart)
 
-        while (targetNum * windowSize - allocated > k) {
+        while (allocated + k < targetNum * (windowEnd - windowStart + 1)) {
             allocated -= nums[windowStart]
             windowStart++
         }
 
-        maxFrequency = Math.max(maxFrequency, windowSize)
+        maxFrequency = Math.max(maxFrequency, windowEnd - windowStart + 1)
     }
 
     return maxFrequency
