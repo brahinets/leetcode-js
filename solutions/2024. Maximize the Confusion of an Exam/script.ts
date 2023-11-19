11cb061d35d0154b0b4473a9b948fb56e50ae5ee
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-11-19 19:45:05 +0200

    #2024 Clean up

--- solutions/2024. Maximize the Confusion of an Exam/script.ts
+++ solutions/2024. Maximize the Confusion of an Exam/script.ts
@@ -4,32 +4,32 @@ function maxConsecutiveAnswers(answerKey: string, k: number): number {
     return Math.max(
         maxConsecutiveAnswersOf("T", answerKey, k),
         maxConsecutiveAnswersOf("F", answerKey, k)
-    );
+    )
 }
 
 function maxConsecutiveAnswersOf(key: string, answerKey: string, limit: number): number {
-    let maxCount: number = 0;
-    let start: number = 0;
-    let end: number = 0;
+    let maxCount: number = 0
+    let start: number = 0
+    let end: number = 0
 
-    let change: number = 0;
+    let change: number = 0
 
     for (let i: number = 0; i < answerKey.length; i++) {
-        end++;
+        end++
 
         if (answerKey[i] !== key) {
-            change++;
+            change++
             while (change > limit) {
                 while (answerKey[start] === key) {
-                    start++;
+                    start++
                 }
-                start++;
-                change--;
+                start++
+                change--
             }
         }
 
-        maxCount = Math.max(end - start, maxCount);
+        maxCount = Math.max(end - start, maxCount)
     }
 
-    return maxCount;
+    return maxCount
 }
