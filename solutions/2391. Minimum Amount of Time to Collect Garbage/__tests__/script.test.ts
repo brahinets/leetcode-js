59f083a83f6d13ed966423b7a9322b9b49fcd698
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-11-20 17:47:20 +0200

    #2391 Clean up

--- solutions/2391. Minimum Amount of Time to Collect Garbage/__tests__/script.test.ts
+++ solutions/2391. Minimum Amount of Time to Collect Garbage/__tests__/script.test.ts
@@ -1,13 +1,14 @@
 import {garbageCollection} from '../script'
 
 describe('2391. Minimum Amount of Time to Collect Garbage', (): void => {
+
     it('Less garbage', (): void => {
         expect(garbageCollection(["G", "P", "GP", "GG"], [2, 4, 3]))
-            .toBe(21);
-    });
+            .toBe(21)
+    })
 
     it('More garbage', (): void => {
         expect(garbageCollection(["MMM", "PGM", "GP"], [3, 10]))
-            .toBe(37);
-    });
+            .toBe(37)
+    })
 })
--- solutions/2391. Minimum Amount of Time to Collect Garbage/script.ts
+++ solutions/2391. Minimum Amount of Time to Collect Garbage/script.ts
@@ -1,46 +1,46 @@
 export {garbageCollection}
 
-const HOUSE_CLEANING_COTS: number = 1;
-const GARBAGE_TYPES: string[] = ["G", "P", "M"];
+const HOUSE_CLEANING_COTS: number = 1
+const GARBAGE_TYPES: string[] = ["G", "P", "M"]
 
 function calculateTravelCost(travelCosts: number[], houseNumber: number): number {
     if (houseNumber > travelCosts.length + 1) {
-        throw new Error("Travel cost is not specified for this house");
+        throw new Error("Travel cost is not specified for this house")
     }
 
-    let cost: number = 0;
+    let cost: number = 0
 
     for (let i: number = 1; i <= houseNumber; i++) {
-        cost += travelCosts[i - 1];
+        cost += travelCosts[i - 1]
     }
 
-    return cost;
+    return cost
 }
 
 function removeChar(string: string, garbageType: string): string {
-    const index: number = string.indexOf(garbageType);
+    const index: number = string.indexOf(garbageType)
     if (index === -1) {
-        return string;
+        return string
     }
 
-    return string.substring(0, index) + string.substring(index + 1);
+    return string.substring(0, index) + string.substring(index + 1)
 }
 
 function garbageCollection(garbage: string[], travel: number[]): number {
-    let totalCost: number = 0;
+    let totalCost: number = 0
 
     for (const garbageType of GARBAGE_TYPES) {
-        let maxVisitedHouse: number = 0;
+        let maxVisitedHouse: number = 0
         for (let i: number = 0; i < garbage.length; i++) {
             while (garbage[i].includes(garbageType)) {
-                garbage[i] = removeChar(garbage[i], garbageType);
-                totalCost += HOUSE_CLEANING_COTS;
-                maxVisitedHouse = i;
+                garbage[i] = removeChar(garbage[i], garbageType)
+                totalCost += HOUSE_CLEANING_COTS
+                maxVisitedHouse = i
             }
         }
 
-        totalCost += calculateTravelCost(travel, maxVisitedHouse);
+        totalCost += calculateTravelCost(travel, maxVisitedHouse)
     }
 
-    return totalCost;
+    return totalCost
 }
