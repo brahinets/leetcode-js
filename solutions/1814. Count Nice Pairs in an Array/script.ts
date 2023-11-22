2e48bf3c2679017af22d28cd027ef5a307ffb460
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-11-22 13:07:55 +0200

    #1814 Optimize

--- solutions/1814. Count Nice Pairs in an Array/script.ts
+++ solutions/1814. Count Nice Pairs in an Array/script.ts
@@ -1,23 +1,21 @@
 import {reverse} from "../7. Reverse Integer/script";
+import {count} from "../../common/array-utils";
 
 export {countNicePairs}
 
 const MOD: number = 10 ** 9 + 7
 
 function countNicePairs(nums: number[]): number {
-    let nicePairs: number = 0
-
+    const data: number[] = []
     for (let i: number = 0; i < nums.length; i++) {
-        for (let j: number = 0; j < nums.length; j++) {
-            if (isNicePair(nums, i, j)) {
-                nicePairs = (nicePairs + 1 % MOD)
-            }
-        }
+        data.push(nums[i] - reverse(nums[i]))
+    }
+
+    let nicePairs: number = 0
+    const counts: Map<number, number> = count(data);
+    for (const [key, value] of counts) {
+        nicePairs = (nicePairs + value * (value - 1) / 2) % MOD
     }
 
     return nicePairs
-}
-
-function isNicePair(nums: number[], i: number, j: number): boolean {
-    return i < j && nums[i] + reverse(nums[j]) === nums[j] + reverse(nums[i])
 }
