634d8e65c4c6ede72360dee0835ea34c5d5b2f63
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-11-25 19:54:17 +0200

    #2352 Clean up

--- solutions/2352. Equal Row and Column Pairs/__tests__/script.test.ts
+++ solutions/2352. Equal Row and Column Pairs/__tests__/script.test.ts
@@ -3,21 +3,21 @@ import {equalPairs} from '../script'
 describe('2352. Equal Row and Column Pairs', (): void => {
     it('No pairs for empty matrix', (): void => {
         expect(equalPairs([]))
-            .toBe(0);
-    });
+            .toBe(0)
+    })
 
     it('No pairs for non-square matrix', (): void => {
         expect(equalPairs([[1, 2]]))
-            .toBe(0);
-    });
+            .toBe(0)
+    })
 
     it('One pair for small matrix', (): void => {
         expect(equalPairs([
             [3, 2, 1],
             [1, 7, 6],
             [2, 7, 7]
-        ])).toBe(1);
-    });
+        ])).toBe(1)
+    })
 
     it('Multiple pairs for big matrix', (): void => {
         expect(equalPairs([
@@ -25,6 +25,6 @@ describe('2352. Equal Row and Column Pairs', (): void => {
             [1, 4, 4, 5],
             [2, 4, 2, 2],
             [2, 4, 2, 2]
-        ])).toBe(3);
-    });
-});
+        ])).toBe(3)
+    })
+})
--- solutions/2352. Equal Row and Column Pairs/script.ts
+++ solutions/2352. Equal Row and Column Pairs/script.ts
@@ -2,43 +2,43 @@ export {equalPairs}
 
 function equalPairs(grid: number[][]): number {
     if (grid.length === 0 || grid.length !== grid[0].length) {
-        return 0;
+        return 0
     }
 
-    let count: number = 0;
-    const cols: Array<number[]> = collectColumns(grid);
+    let count: number = 0
+    const cols: Array<number[]> = collectColumns(grid)
     for (const row of grid) {
         for (const col of cols) {
-            count += equal(row, col) ? 1 : 0;
+            count += equal(row, col) ? 1 : 0
         }
     }
 
-    return count;
+    return count
 }
 
 function collectColumns(grid: number[][]): Array<number[]> {
-    const cols: Array<number[]> = [];
+    const cols: Array<number[]> = []
 
     for (let i: number = 0; i < grid[0].length; i++) {
         cols.push(grid.reduce((agg: number[], curr: number[]): number[] => {
-            agg.push(curr[i]);
-            return agg;
-        }, []));
+            agg.push(curr[i])
+            return agg
+        }, []))
     }
 
-    return cols;
+    return cols
 }
 
 function equal(row: number[], col: number[]): boolean {
     if (row.length !== col.length) {
-        return false;
+        return false
     }
 
     for (let i: number = 0; i < row.length && i < col.length; i++) {
         if (row[i] !== col[i]) {
-            return false;
+            return false
         }
     }
 
-    return true;
+    return true
 }
