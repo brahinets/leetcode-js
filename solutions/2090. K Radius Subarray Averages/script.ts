9188c9547dd7e15546a2a9f28edc00ffd6d7e20c
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-11-26 19:28:59 +0200

    #2090 Clean up

--- solutions/2090. K Radius Subarray Averages/script.ts
+++ solutions/2090. K Radius Subarray Averages/script.ts
@@ -1,33 +1,33 @@
 export {getAverages}
 
 function getAverages(nums: number[], radius: number): number[] {
-    const windowSize:number = radius * 2 + 1;
-    const avg: number[] = new Array(nums.length).fill(-1);
+    const windowSize: number = radius * 2 + 1
+    const avg: number[] = new Array(nums.length).fill(-1)
 
     if (windowSize > nums.length) {
-        return avg;
+        return avg
     }
 
-    let windowSum: number = initialWindowSum(nums, windowSize);
-    avg[radius] = average(windowSum, windowSize);
+    let windowSum: number = initialWindowSum(nums, windowSize)
+    avg[radius] = average(windowSum, windowSize)
     for (let i: number = radius + 1; i < nums.length - radius; i++) {
-        windowSum = windowSum - nums[i - radius - 1] + nums[i + radius];
-        avg[i] = average(windowSum, windowSize);
+        windowSum = windowSum - nums[i - radius - 1] + nums[i + radius]
+        avg[i] = average(windowSum, windowSize)
     }
 
-    return avg;
+    return avg
 }
 
 function initialWindowSum(nums: number[], windowSize: number): number {
-    let sum: number = 0;
+    let sum: number = 0
 
     for (let i: number = 0; i < windowSize; i++) {
-        sum += nums[i];
+        sum += nums[i]
     }
 
-    return sum;
+    return sum
 }
 
 function average(sum: number, length: number) {
-    return Math.floor(sum / length);
+    return Math.floor(sum / length)
 }
