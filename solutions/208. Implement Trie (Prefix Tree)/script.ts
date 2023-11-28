9ed27bb6045c912d39eea8e4518887cfef806495
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-11-28 21:45:08 +0200

    #208 Clean up

--- solutions/208. Implement Trie (Prefix Tree)/script.ts
+++ solutions/208. Implement Trie (Prefix Tree)/script.ts
@@ -1,29 +1,29 @@
 class Trie {
-    private readonly data: Set<string>;
+    private readonly data: Set<string>
 
     constructor() {
-        this.data = new Set<string>();
+        this.data = new Set<string>()
     }
 
     insert(word: string): void {
         if (!word) {
-            throw new Error("Illegal word: " + word);
+            throw new Error("Illegal word: " + word)
         }
 
-        this.data.add(word);
+        this.data.add(word)
     }
 
     search(word: string): boolean {
-        return this.data.has(word);
+        return this.data.has(word)
     }
 
     startsWith(prefix: string): boolean {
         for (const str of this.data) {
             if (str.startsWith(prefix)) {
-                return true;
+                return true
             }
         }
 
-        return false;
+        return false
     }
 }
