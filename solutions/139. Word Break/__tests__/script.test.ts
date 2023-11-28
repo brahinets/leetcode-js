493b74a0caaed461c17e3408f708221c3950c73a
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-11-28 21:03:10 +0200

    #139 Clean up

--- solutions/139. Word Break/__tests__/script.test.ts
+++ solutions/139. Word Break/__tests__/script.test.ts
@@ -3,43 +3,43 @@ import {wordBreak} from '../script'
 describe('139. Word Break', (): void => {
     it('No segments needed', (): void => {
         expect(wordBreak("", ["leetcode"]))
-            .toBe(true);
-    });
+            .toBe(true)
+    })
 
     it('Already segments', (): void => {
         expect(wordBreak("leetcode", ["leetcode"]))
-            .toBe(true);
-    });
+            .toBe(true)
+    })
 
     it('Combine all segments', (): void => {
         expect(wordBreak("leetcode", ["leet", "code"]))
-            .toBe(true);
-    });
+            .toBe(true)
+    })
 
     it('Combine all segments #2', (): void => {
         expect(wordBreak("aaaaaaa", ["aaaa", "aaa"]))
-            .toBe(true);
-    });
+            .toBe(true)
+    })
 
     it('Reuse segment', (): void => {
         expect(wordBreak("applepenapple", ["apple", "pen"]))
-            .toBe(true);
-    });
+            .toBe(true)
+    })
 
     it('Cannot build', (): void => {
         expect(wordBreak("catsandog", ["cats", "dog", "sand", "and", "cat"]))
-            .toBe(false);
-    });
+            .toBe(false)
+    })
 
     it('Cannot build #2', (): void => {
         expect(wordBreak("ccbb", ["bc", "cb"]))
-            .toBe(false);
-    });
+            .toBe(false)
+    })
 
 
 
     it('Heavy case', (): void => {
         expect(wordBreak("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaab", ["a", "aa", "aaa", "aaaa", "aaaaa", "aaaaaa", "aaaaaaa", "aaaaaaaa", "aaaaaaaaa", "aaaaaaaaaa"]))
-            .toBe(false);
-    });
+            .toBe(false)
+    })
 })
