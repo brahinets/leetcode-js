80cbc66d815e1224b4c3ad1130b6343bd6a8bf34
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-11-29 21:58:34 +0200

    #382 Clean up

--- solutions/382. Linked List Random Node/script.ts
+++ solutions/382. Linked List Random Node/script.ts
@@ -3,36 +3,36 @@ import {ListNode} from '../../common/ListNode'
 export {ListNode, Solution}
 
 class Solution {
-    private readonly list: ListNode;
-    private readonly size: number;
+    private readonly list: ListNode
+    private readonly size: number
 
     constructor(head: ListNode | null) {
         if (!head) {
-            throw new Error("List cannot be empty");
+            throw new Error("List cannot be empty")
         }
 
-        this.list = head;
-        this.size = 0;
+        this.list = head
+        this.size = 0
         while (head) {
-            this.size++;
-            head = head.next;
+            this.size++
+            head = head.next
         }
     }
 
     private getRandomInt(maxExclusive: number): number {
-        return Math.floor(Math.random() * maxExclusive);
+        return Math.floor(Math.random() * maxExclusive)
     }
 
     getRandom(): number {
-        const randomIndex: number = this.getRandomInt(this.size);
-        let index: number = 0;
+        const randomIndex: number = this.getRandomInt(this.size)
+        let index: number = 0
 
-        let node: ListNode = this.list;
+        let node: ListNode = this.list
         while (index < randomIndex && node.next) {
-            node = node.next;
-            index++;
+            node = node.next
+            index++
         }
 
-        return node.val;
+        return node.val
     }
 }
