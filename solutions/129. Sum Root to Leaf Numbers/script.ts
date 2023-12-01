9bfbb7d9f0aa10e72ae426f4f57b927424626fce
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-12-01 21:32:09 +0200

    #129 Clean up

--- solutions/129. Sum Root to Leaf Numbers/script.ts
+++ solutions/129. Sum Root to Leaf Numbers/script.ts
@@ -5,23 +5,23 @@ export {sumNumbers, TreeNode}
 function sumNumbers(root: TreeNode): number {
     return collectPaths(root, "")
         .map((num: string) => Number(num))
-        .reduce((num: number, sum: number) => num + sum, 0);
+        .reduce((num: number, sum: number) => num + sum, 0)
 }
 
 function collectPaths(node: TreeNode, parentPath: string): string [] {
-    const paths: string[] = [];
+    const paths: string[] = []
 
     if (node.left) {
-        paths.push(...collectPaths(node.left, parentPath + node.val));
+        paths.push(...collectPaths(node.left, parentPath + node.val))
     }
 
     if (node.right) {
-        paths.push(...collectPaths(node.right, parentPath + node.val));
+        paths.push(...collectPaths(node.right, parentPath + node.val))
     }
 
     if (!node.left && !node.right) {
         paths.push(parentPath + node.val)
     }
 
-    return paths;
+    return paths
 }
