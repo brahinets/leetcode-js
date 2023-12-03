ca8ffe32dcf4f27a28649563ba7e525360cc0b59
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-12-03 20:21:21 +0200

    #1502 Clean up

--- solutions/1502. Can Make Arithmetic Progression From Sequence/__tests__/script.test.ts
+++ solutions/1502. Can Make Arithmetic Progression From Sequence/__tests__/script.test.ts
@@ -3,26 +3,26 @@ import {canMakeArithmeticProgression} from '../script'
 describe('1502. Can Make Arithmetic Progression From Sequence', (): void => {
     it('Yes, singleton set', (): void => {
         expect(canMakeArithmeticProgression([0]))
-            .toBe(true);
-    });
+            .toBe(true)
+    })
 
     it('Yes, small set', (): void => {
         expect(canMakeArithmeticProgression([0, 1]))
-            .toBe(true);
-    });
+            .toBe(true)
+    })
 
     it('Yes, immediately', (): void => {
         expect(canMakeArithmeticProgression([0, 1, 2, 3, 4]))
-            .toBe(true);
-    });
+            .toBe(true)
+    })
 
     it('Yes, after sorting', (): void => {
         expect(canMakeArithmeticProgression([3, 5, 1]))
-            .toBe(true);
-    });
+            .toBe(true)
+    })
 
     it('No', (): void => {
         expect(canMakeArithmeticProgression([1, 2, 4]))
-            .toBe(false);
-    });
-});
+            .toBe(false)
+    })
+})
--- solutions/1502. Can Make Arithmetic Progression From Sequence/script.ts
+++ solutions/1502. Can Make Arithmetic Progression From Sequence/script.ts
@@ -2,18 +2,18 @@ export {canMakeArithmeticProgression}
 
 function canMakeArithmeticProgression(arr: number[]): boolean {
     if (arr.length <= 2) {
-        return true;
+        return true
     }
 
-    arr.sort((a: number, b: number): number => a - b);
+    arr.sort((a: number, b: number): number => a - b)
 
-    const step: number = arr[1] - arr[0];
+    const step: number = arr[1] - arr[0]
 
     for (let i: number = 1; i < arr.length; i++) {
         if (arr[i] - arr[i - 1] !== step) {
-            return false;
+            return false
         }
     }
 
-    return true;
+    return true
 }
