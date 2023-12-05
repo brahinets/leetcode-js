4a682b91a79e00f05cb38c899a4fa63b4d508a7c
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-12-05 14:38:17 +0200

    #2336 Clean up

--- solutions/2336. Smallest Number in Infinite Set/__tests__/script.test.ts
+++ solutions/2336. Smallest Number in Infinite Set/__tests__/script.test.ts
@@ -3,29 +3,29 @@ import {SmallestInfiniteSet} from '../script'
 describe('2336. Smallest Number in Infinite Set', (): void => {
     it('Test', (): void => {
         const set: SmallestInfiniteSet = new SmallestInfiniteSet()
-        let number;
+        let number
 
-        set.addBack(2);
+        set.addBack(2)
 
-        number = set.popSmallest();
-        expect(number).toBe(1);
+        number = set.popSmallest()
+        expect(number).toBe(1)
 
-        number = set.popSmallest();
-        expect(number).toBe(2);
+        number = set.popSmallest()
+        expect(number).toBe(2)
 
-        number = set.popSmallest();
-        expect(number).toBe(3);
+        number = set.popSmallest()
+        expect(number).toBe(3)
 
-        set.addBack(1);
+        set.addBack(1)
 
-        number = set.popSmallest();
-        expect(number).toBe(1);
+        number = set.popSmallest()
+        expect(number).toBe(1)
 
-        number = set.popSmallest();
-        expect(number).toBe(4);
+        number = set.popSmallest()
+        expect(number).toBe(4)
 
-        number = set.popSmallest();
-        expect(number).toBe(5);
+        number = set.popSmallest()
+        expect(number).toBe(5)
 
-    });
+    })
 })
--- solutions/2336. Smallest Number in Infinite Set/script.ts
+++ solutions/2336. Smallest Number in Infinite Set/script.ts
@@ -2,82 +2,82 @@ export {SmallestInfiniteSet}
 
 class SmallestInfiniteSet {
 
-    private ranges: Range[];
+    private ranges: Range[]
 
     constructor() {
-        this.ranges = [new Range(1, undefined)];
+        this.ranges = [new Range(1, undefined)]
     }
 
     findRangeBiggerThan(number: number): number {
-        let min: number | undefined;
+        let min: number | undefined
         for (let i: number = 0; i < this.ranges.length; i++) {
             if (min === undefined || this.ranges[i].start > number && this.ranges[i].start < this.ranges[min].start) {
-                min = i;
+                min = i
             }
         }
 
         if (min === undefined) {
-            throw new Error("Min not found");
+            throw new Error("Min not found")
         }
 
-        return min;
+        return min
     }
 
     findRangeIncluding(number: number): number | undefined {
-        let index: number | undefined;
+        let index: number | undefined
 
         for (let i: number = 0; i < this.ranges.length; i++) {
             if (this.ranges[i].start <= number) {
-                const end: number | undefined = this.ranges[i].end;
+                const end: number | undefined = this.ranges[i].end
                 if (end === undefined || end >= number) {
-                    index = i;
+                    index = i
                 }
             }
         }
 
-        return index;
+        return index
     }
 
     popSmallest(): number {
-        const index: number = this.findRangeBiggerThan(0);
-        const min: Range = this.ranges[index];
+        const index: number = this.findRangeBiggerThan(0)
+        const min: Range = this.ranges[index]
         const before: Range[] = this.ranges.slice(0, index)
-        const after: Range[] = this.ranges.slice(index + 1);
+        const after: Range[] = this.ranges.slice(index + 1)
 
-        this.ranges = [...before, ...after];
+        this.ranges = [...before, ...after]
 
         if (min.start !== min.end) {
             this.ranges.push(new Range(min.start + 1, min.end))
         }
 
-        return min.start;
+        return min.start
     }
 
     addBack(num: number): void {
-        const index: number | undefined = this.findRangeIncluding(num);
+        const index: number | undefined = this.findRangeIncluding(num)
         if (index !== undefined) {
-            return;
+            return
         }
 
-        const bigger: number = this.findRangeBiggerThan(num);
+        const bigger: number = this.findRangeBiggerThan(num)
         if (this.ranges[bigger].start === num + 1) {
             const before: Range[] = this.ranges.slice(0, bigger)
-            const after: Range[] = this.ranges.slice(bigger + 1);
+            const after: Range[] = this.ranges.slice(bigger + 1)
 
-            this.ranges = [...before, new Range(num, this.ranges[bigger].end), ...after];
+            this.ranges = [...before, new Range(num, this.ranges[bigger].end), ...after]
         } else {
-            this.ranges.push(new Range(num, num));
+            this.ranges.push(new Range(num, num))
         }
     }
 }
 
 
 class Range {
-    start: number;
-    end: number | undefined;
+    start: number
+    end: number | undefined
 
     constructor(start: number, set: number | undefined) {
-        this.start = start;
-        this.end = set;
+        this.start = start
+        this.end = set
     }
 }
