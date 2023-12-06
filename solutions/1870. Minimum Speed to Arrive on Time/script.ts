330d674a25f0989fd2c2059adb72eda063848d75
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-12-06 20:54:06 +0200

    #1870 Clean up

--- solutions/1870. Minimum Speed to Arrive on Time/script.ts
+++ solutions/1870. Minimum Speed to Arrive on Time/script.ts
@@ -2,32 +2,32 @@ export {minSpeedOnTime}
 
 function minSpeedOnTime(dist: number[], hour: number): number {
     if (dist.length > Math.ceil(hour)) {
-        return -1;
+        return -1
     }
 
-    let min: number = -1;
-    let left: number = 1;
-    let right: number = 1e7;
+    let min: number = -1
+    let left: number = 1
+    let right: number = 1e7
     while (left <= right) {
-        const mid: number = Math.round((right + left) / 2);
+        const mid: number = Math.round((right + left) / 2)
 
         if ((sumTime(dist, mid)) <= hour) {
-            min = mid;
-            right = mid - 1;
+            min = mid
+            right = mid - 1
         } else {
-            left = mid + 1;
+            left = mid + 1
         }
     }
 
-    return min;
+    return min
 }
 
 function sumTime(dist: number[], speed: number): number {
     return dist.reduce((totalTime: number, trainDistance: number, trainNumber: number): number => {
-        const lastTrain: boolean = trainNumber === dist.length - 1;
-        const time: number = trainDistance / speed;
+        const lastTrain: boolean = trainNumber === dist.length - 1
+        const time: number = trainDistance / speed
 
-        return totalTime + (lastTrain ? time : Math.ceil(time));
-    }, 0);
+        return totalTime + (lastTrain ? time : Math.ceil(time))
+    }, 0)
 }
 
