17414ae323adf1f6eea764f2050de5b2f194ab08
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-12-07 18:18:09 +0200

    #2300 Use easier assertion

--- solutions/2300. Successful Pairs of Spells and Potions/__tests__/script.test.ts
+++ solutions/2300. Successful Pairs of Spells and Potions/__tests__/script.test.ts
@@ -1,13 +1,14 @@
 import {successfulPairs} from '../script'
 
 describe('2300. Successful Pairs of Spells and Potions', (): void => {
+
     it('Several successes', (): void => {
         expect(successfulPairs([5, 1, 3], [1, 2, 3, 4, 5], 7))
-            .toStrictEqual([4, 0, 3])
+            .toEqual([4, 0, 3])
     })
 
     it('Many successes', (): void => {
         expect(successfulPairs([3, 1, 2], [8, 5, 8], 16))
-            .toStrictEqual([2, 0, 2])
+            .toEqual([2, 0, 2])
     })
 })
