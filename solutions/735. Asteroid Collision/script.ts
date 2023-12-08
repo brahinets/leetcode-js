3ed7d230d7e477672cd024fd34fa348bbb33a40e
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-12-08 21:38:57 +0200

    #735 Clean up

--- solutions/735. Asteroid Collision/script.ts
+++ solutions/735. Asteroid Collision/script.ts
@@ -1,31 +1,31 @@
 export {asteroidCollision}
 
 function asteroidCollision(asteroids: number[]): number[] {
-    let canExplodeMore: boolean;
+    let canExplodeMore: boolean
 
     do {
-        canExplodeMore = false;
+        canExplodeMore = false
 
         for (let i: number = 0; i < asteroids.length - 1; i++) {
             if (asteroids[i] > 0 && asteroids[i + 1] < 0) {
                 if (Math.abs(asteroids[i]) > Math.abs(asteroids[i + 1])) {
-                    asteroids[i + 1] = Number.NaN;
+                    asteroids[i + 1] = Number.NaN
                 } else if (Math.abs(asteroids[i]) < Math.abs(asteroids[i + 1])) {
-                    asteroids[i] = Number.NaN;
+                    asteroids[i] = Number.NaN
                 } else {
-                    asteroids[i] = Number.NaN;
-                    asteroids[i + 1] = Number.NaN;
+                    asteroids[i] = Number.NaN
+                    asteroids[i + 1] = Number.NaN
                 }
             }
         }
 
-        canExplodeMore ||= asteroids.includes(Number.NaN);
+        canExplodeMore ||= asteroids.includes(Number.NaN)
         asteroids = filterNonCrashedAsteroids(asteroids)
     } while (canExplodeMore)
 
-    return filterNonCrashedAsteroids(asteroids);
+    return filterNonCrashedAsteroids(asteroids)
 }
 
 function filterNonCrashedAsteroids(asteroids: number[]): number[] {
-    return asteroids.filter((asteroidPower: number): boolean => !Number.isNaN(asteroidPower));
+    return asteroids.filter((asteroidPower: number): boolean => !Number.isNaN(asteroidPower))
 }
