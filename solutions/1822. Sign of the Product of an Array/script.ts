e1b5959706b96e745e984146c7bc3083df975704
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-12-09 19:15:49 +0200

    #1822 Clean up

--- solutions/1822. Sign of the Product of an Array/script.ts
+++ solutions/1822. Sign of the Product of an Array/script.ts
@@ -1,17 +1,17 @@
 export {arraySign}
 
 function arraySign(nums: number[]): number {
-    let positive: boolean = true;
+    let positive: boolean = true
 
     for (const n of nums) {
         if (n === 0) {
-            return 0;
+            return 0
         }
 
         if (n < 0) {
-            positive = !positive;
+            positive = !positive
         }
     }
 
-    return positive ? 1 : -1;
+    return positive ? 1 : -1
 }
