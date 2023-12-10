344266e004d7d5815bec713cf51bea9f163b408c
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-12-10 17:36:44 +0200

    #1572 Clean up

--- solutions/1572. Matrix Diagonal Sum/__tests__/script.test.ts
+++ solutions/1572. Matrix Diagonal Sum/__tests__/script.test.ts
@@ -3,16 +3,16 @@ import {diagonalSum} from '../script'
 describe('1572. Matrix Diagonal Sum', (): void => {
     it('Single', (): void => {
         expect(diagonalSum([[5]]))
-            .toBe(5);
-    });
+            .toBe(5)
+    })
 
     it('Odd size. Doubled center case', (): void => {
         expect(diagonalSum([
             [1, 2, 3],
             [4, 5, 6],
             [7, 8, 9]
-        ])).toBe(25);
-    });
+        ])).toBe(25)
+    })
 
     it('Even size', (): void => {
         expect(diagonalSum([
@@ -20,6 +20,6 @@ describe('1572. Matrix Diagonal Sum', (): void => {
             [1, 1, 1, 1],
             [1, 1, 1, 1],
             [1, 1, 1, 1]
-        ])).toBe(8);
-    });
+        ])).toBe(8)
+    })
 })
--- solutions/1572. Matrix Diagonal Sum/script.ts
+++ solutions/1572. Matrix Diagonal Sum/script.ts
@@ -1,18 +1,18 @@
 export {diagonalSum}
 
 function diagonalSum(mat: number[][]): number {
-    let sum: number = 0;
+    let sum: number = 0
 
     for (let i: number = 0; i < mat.length; i++) {
         for (let j: number = 0; j < mat.length; j++) {
-            const mainDiagonal:boolean = i === j;
-            const secondaryDiagonal:boolean = i + j === mat.length - 1;
+            const mainDiagonal: boolean = i === j
+            const secondaryDiagonal: boolean = i + j === mat.length - 1
 
             if (mainDiagonal || secondaryDiagonal) {
-                sum += mat[i][j];
+                sum += mat[i][j]
             }
         }
     }
 
-    return sum;
+    return sum
 }
