936b9d4383dec3cad4e37af6635f163f98c5ba44
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-12-14 15:22:29 +0200

    #2482 Optimize calculation

--- solutions/2482. Difference Between Ones and Zeros in Row and Column/script.ts
+++ solutions/2482. Difference Between Ones and Zeros in Row and Column/script.ts
@@ -1,17 +1,23 @@
 export {onesMinusZeros}
 
 function onesMinusZeros(grid: number[][]): number[][] {
+    const rows: number = grid.length
+    const cols: number = grid[0].length
+
+    const onesInRow: number[] = new Array(rows).fill(0)
+        .map((_: number, i: number): number => valuesInRow(grid, i, 1))
+    const onesInColumn: number[] = new Array(cols).fill(0)
+        .map((_: number, i: number): number => valuesInColumn(grid, i, 1))
+
     const result: number[][] = []
-
-    for (let i: number = 0; i < grid.length; i++) {
+    for (let i: number = 0; i < rows; i++) {
         result.push([])
-        for (let j: number = 0; j < grid[i].length; j++) {
-            const onesInRow: number = valuesInRow(grid, i, 1)
-            const onesInColumn: number = valuesInColumn(grid, j, 1)
-            const zerosInRow: number = grid[i].length - onesInRow
-            const zerosInColumn: number = grid.length - onesInColumn
 
-            const diff: number = onesInRow + onesInColumn - zerosInRow - zerosInColumn
+        for (let j: number = 0; j < grid[i].length; j++) {
+            const zerosInRow: number = cols - onesInRow[i]
+            const zerosInColumn: number = rows - onesInColumn[j]
+
+            const diff: number = onesInRow[i] + onesInColumn[j] - zerosInRow - zerosInColumn
             result[i].push(diff)
         }
     }
