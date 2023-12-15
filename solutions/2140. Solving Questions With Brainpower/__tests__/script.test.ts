ac8d74382ef44e3fdf982b3315b5a8beea85545a
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-12-15 10:28:27 +0200

    #2140 Clean up

--- solutions/2140. Solving Questions With Brainpower/__tests__/script.test.ts
+++ solutions/2140. Solving Questions With Brainpower/__tests__/script.test.ts
@@ -3,11 +3,11 @@ import {mostPoints} from '../script'
 describe('2140. Solving Questions With Brainpower', (): void => {
     it('Skip first', (): void => {
         expect(mostPoints([[1, 1], [2, 2], [3, 3], [4, 4], [5, 5]]))
-            .toBe(7);
-    });
+            .toBe(7)
+    })
 
     it('Skip non first', (): void => {
         expect(mostPoints([[3, 2], [4, 3], [4, 4], [2, 5]]))
-            .toBe(5);
-    });
+            .toBe(5)
+    })
 })
--- solutions/2140. Solving Questions With Brainpower/script.ts
+++ solutions/2140. Solving Questions With Brainpower/script.ts
@@ -3,23 +3,23 @@ import {arrayOfZeros} from '../../common/array-factories'
 export {mostPoints}
 
 function mostPoints(questions: number[][]): number {
-    const maxPointStartingFrom: number[] = arrayOfZeros(questions.length);
+    const maxPointStartingFrom: number[] = arrayOfZeros(questions.length)
 
-    maxPointStartingFrom[questions.length - 1] = questions[questions.length - 1][0];
+    maxPointStartingFrom[questions.length - 1] = questions[questions.length - 1][0]
     for (let position: number = questions.length - 2; position >= 0; position--) {
-        const points: number = questions[position][0];
-        const jumpDistance: number = questions[position][1];
+        const points: number = questions[position][0]
+        const jumpDistance: number = questions[position][1]
 
-        const reachablePoint: number = jumpDistance + position + 1;
-        const canReachPoint: boolean = reachablePoint < questions.length;
+        const reachablePoint: number = jumpDistance + position + 1
+        const canReachPoint: boolean = reachablePoint < questions.length
         if (canReachPoint) {
-            maxPointStartingFrom[position] = points + maxPointStartingFrom[reachablePoint];
+            maxPointStartingFrom[position] = points + maxPointStartingFrom[reachablePoint]
         } else {
-            maxPointStartingFrom[position] = points;
+            maxPointStartingFrom[position] = points
         }
 
-        maxPointStartingFrom[position] = Math.max(maxPointStartingFrom[position], maxPointStartingFrom[position + 1]);
+        maxPointStartingFrom[position] = Math.max(maxPointStartingFrom[position], maxPointStartingFrom[position + 1])
     }
 
-    return maxPointStartingFrom[0];
+    return maxPointStartingFrom[0]
 }
