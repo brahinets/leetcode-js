4b3f0ea98cf0767dec824458c5209841289f5a53
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-12-18 00:00:44 +0200

    #2353 Speed up using original data

--- solutions/2353. Design a Food Rating System/script.ts
+++ solutions/2353. Design a Food Rating System/script.ts
@@ -1,55 +1,45 @@
 export {FoodRatings}
 
 class FoodRatings {
-    private readonly foodRating: Map<string, number> = new Map<string, number>()
-    private readonly foodCuisine: Map<string, string> = new Map<string, string>()
+    private readonly foods: string[]
+    private readonly cuisines: string[]
+    private readonly ratings: number[]
 
     constructor(foods: string[], cuisines: string[], ratings: number[]) {
-        this.foodRating = new Map<string, number>()
-        this.foodCuisine = new Map<string, string>()
-
-        for (let i: number = 0; i < foods.length; i++) {
-            this.foodRating.set(foods[i], ratings[i])
-            this.foodCuisine.set(foods[i], cuisines[i])
-        }
+        this.foods = foods
+        this.cuisines = cuisines
+        this.ratings = ratings
     }
 
     changeRating(food: string, newRating: number): void {
-        this.foodRating.set(food, newRating)
+        this.ratings[this.foods.indexOf(food)] = newRating
     }
 
     highestRated(cuisine: string): string {
-        return this.getHighestRatedFood(this.getFoodsByCuisine(cuisine))
-    }
-
-    private getFoodsByCuisine(cuisine: string): string[] {
-        const foods: string[] = []
-
-        this.foodCuisine.forEach((c: string, food: string): void => {
-            if (c === cuisine) {
-                foods.push(food)
-            }
-        })
-
-        return foods
-    }
-
-    private getHighestRatedFood(foods: string[]): string {
         let maxRating: number = -1
-        let maxRatingIndex: number = -1
+        let maxRatingFoodIndex: number = -1
 
-        for (let i: number = 0; i < foods.length; i++) {
-            const rating: number = this.foodRating.get(foods[i])!
-            if (rating === maxRating) {
-                if (foods[i].localeCompare(foods[maxRatingIndex]) < 0) {
-                    maxRatingIndex = i
+        for (let index: number = 0; index < this.cuisines.length; index++) {
+            const food: string = this.foods[index]
+
+            if (this.cuisines[index] !== cuisine) {
+                continue
+            }
+
+            if (this.ratings[index] > maxRating) {
+                maxRating = this.ratings[index]
+                maxRatingFoodIndex = index
+            } else if (this.ratings[index] === maxRating) {
+                if (food < this.foods[maxRatingFoodIndex]) {
+                    maxRatingFoodIndex = index
                 }
-            } else if (rating > maxRating) {
-                maxRating = rating
-                maxRatingIndex = i
             }
         }
 
-        return foods[maxRatingIndex]
+        if (maxRatingFoodIndex === -1) {
+            throw new Error('There is no food with this cuisine')
+        }
+
+        return this.foods[maxRatingFoodIndex]
     }
 }
