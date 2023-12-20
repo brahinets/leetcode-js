6dd53bdc380eebd6728f36c5a407b68547a6f2e9
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-12-20 21:44:06 +0200

    #2130 Clean up

--- solutions/2130. Maximum Twin Sum of a Linked List/__tests__/script.test.ts
+++ solutions/2130. Maximum Twin Sum of a Linked List/__tests__/script.test.ts
@@ -1,19 +1,19 @@
-import {pairSum} from "../script";
+import {pairSum} from "../script"
 import {linkedListOf} from "../../../common/list-factories"
 
 describe('2130. Maximum Twin Sum of a Linked List', (): void => {
     it('Multiple max sum', (): void => {
         expect(pairSum(linkedListOf(5, 4, 2, 1)))
-            .toBe(6);
-    });
+            .toBe(6)
+    })
 
     it('Single max sum', (): void => {
         expect(pairSum(linkedListOf(4, 2, 2, 3)))
-            .toBe(7);
-    });
+            .toBe(7)
+    })
 
     it('The only max sum', (): void => {
         expect(pairSum(linkedListOf(1, 100000)))
-            .toBe(100001);
-    });
+            .toBe(100001)
+    })
 })
--- solutions/2130. Maximum Twin Sum of a Linked List/script.ts
+++ solutions/2130. Maximum Twin Sum of a Linked List/script.ts
@@ -1,20 +1,20 @@
-import {ListNode} from "../../common/ListNode";
-import {linkedListToArray} from "../../common/list-utils";
+import {ListNode} from "../../common/ListNode"
+import {linkedListToArray} from "../../common/list-utils"
 
-export {pairSum, ListNode};
+export {pairSum, ListNode}
 
 function pairSum(head: ListNode | null): number {
-    const data: number[] | null = linkedListToArray(head);
+    const data: number[] | null = linkedListToArray(head)
 
     if (!data) {
-        return 0;
+        return 0
     }
 
-    let max: number | undefined = undefined;
+    let max: number | undefined = undefined
     for (let i: number = 0; i < data.length / 2; i++) {
-        const sum: number = data[i] + data[data.length - 1 - i];
+        const sum: number = data[i] + data[data.length - 1 - i]
         if (max === undefined || sum > max) {
-            max = sum;
+            max = sum
         }
     }
 
@@ -22,5 +22,5 @@ function pairSum(head: ListNode | null): number {
         throw new Error("Not enough data to define max")
     }
 
-    return max;
+    return max
 }
