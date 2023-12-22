544319b8f1669ccfdf81098b9608635f9cff0990
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-12-22 12:45:27 +0200

    #1146 Improve naming

--- solutions/1146. Snapshot Array/__tests__/script.test.ts
+++ solutions/1146. Snapshot Array/__tests__/script.test.ts
@@ -2,67 +2,67 @@ import {SnapshotArray} from "../script"
 
 describe('1146. Snapshot Array', (): void => {
     it('Test first snapshot', (): void => {
-        const snapshotArr: SnapshotArray = new SnapshotArray(3)
-        snapshotArr.set(0, 5)
-        snapshotArr.snap()
+        const array: SnapshotArray = new SnapshotArray(3)
+        array.set(0, 5)
+        array.snap()
 
-        const snap0: number = snapshotArr.get(0, 0)
-        expect(snap0).toBe(5)
+        const snap: number = array.get(0, 0)
+        expect(snap).toBe(5)
     })
 
     it('Test first index after modification', (): void => {
-        const snapshotArr: SnapshotArray = new SnapshotArray(3)
-        snapshotArr.set(0, 5)
-        snapshotArr.snap()
-        snapshotArr.set(0, 6)
+        const array: SnapshotArray = new SnapshotArray(3)
+        array.set(0, 5)
+        array.snap()
+        array.set(0, 6)
 
-        const snap: number = snapshotArr.get(0, 0)
+        const snap: number = array.get(0, 0)
         expect(snap).toBe(5)
     })
 
     it('Test empty snapshots', (): void => {
-        const snapshotArr: SnapshotArray = new SnapshotArray(3)
-        snapshotArr.set(0, 5)
-        snapshotArr.snap()
-        snapshotArr.snap()
-        snapshotArr.snap()
-        snapshotArr.snap()
-        snapshotArr.snap()
-        snapshotArr.snap()
+        const array: SnapshotArray = new SnapshotArray(3)
+        array.set(0, 5)
+        array.snap()
+        array.snap()
+        array.snap()
+        array.snap()
+        array.snap()
+        array.snap()
 
-        const snap: number = snapshotArr.get(0, 5)
+        const snap: number = array.get(0, 5)
         expect(snap).toBe(5)
     })
 
     it('Test another snapshot', (): void => {
-        const snapshotArr: SnapshotArray = new SnapshotArray(3)
-        snapshotArr.set(0, 5)
-        snapshotArr.snap()
-        snapshotArr.set(0, 6)
-        snapshotArr.snap()
+        const array: SnapshotArray = new SnapshotArray(3)
+        array.set(0, 5)
+        array.snap()
+        array.set(0, 6)
+        array.snap()
 
-        const snap0: number = snapshotArr.get(0, 1)
-        expect(snap0).toBe(6)
+        const snap: number = array.get(0, 1)
+        expect(snap).toBe(6)
     })
 
     it('Error for non existing snap', (): void => {
-        const snapshotArr: SnapshotArray = new SnapshotArray(3)
+        const array: SnapshotArray = new SnapshotArray(3)
 
-        expect(() => snapshotArr.get(0, 1))
+        expect(() => array.get(0, 1))
             .toThrowError("Snap does not exist")
     })
 
     it('Error for non existing index set', (): void => {
-        const snapshotArr: SnapshotArray = new SnapshotArray(3)
+        const array: SnapshotArray = new SnapshotArray(3)
 
-        expect(() => snapshotArr.set(4, 1))
+        expect(() => array.set(4, 1))
             .toThrowError("Element index is out of bounds")
     })
 
     it('Error for non existing index get', (): void => {
-        const snapshotArr: SnapshotArray = new SnapshotArray(3)
+        const array: SnapshotArray = new SnapshotArray(3)
 
-        expect(() => snapshotArr.get(4, 1))
+        expect(() => array.get(4, 1))
             .toThrowError("Element index is out of bounds")
     })
 })
