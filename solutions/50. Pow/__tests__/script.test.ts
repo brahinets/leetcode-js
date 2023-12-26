57b7e0cce78dfdba233022ab78128497a2becd85
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-12-26 17:09:51 +0200

    #50 Clean up

--- solutions/50. Pow/__tests__/script.test.ts
+++ solutions/50. Pow/__tests__/script.test.ts
@@ -1,43 +1,44 @@
 import {myPow} from '../script'
 
 describe('50. Pow(x, n)', (): void => {
+
     it('Integer in positive power', (): void => {
         expect(myPow(2.00000, 10))
-            .toBe(1024);
-    });
+            .toBe(1024)
+    })
 
     it('Float in positive power', (): void => {
         expect(myPow(2.10000, 3))
-            .toBeCloseTo(9.26100, 5);
-    });
+            .toBeCloseTo(9.26100, 5)
+    })
 
     it('Integer in negative power', (): void => {
         expect(myPow(2.00000, -2))
-            .toBeCloseTo(0.25000, 5);
-    });
+            .toBeCloseTo(0.25000, 5)
+    })
 
     it('Float in negative power', (): void => {
         expect(myPow(2.10000, -3))
-            .toBeCloseTo(0.10798, 5);
-    });
+            .toBeCloseTo(0.10798, 5)
+    })
 
     it('Powering one in positive power', (): void => {
         expect(myPow(-1.00000, 2147483647))
-            .toBe(-1);
-    });
+            .toBe(-1)
+    })
 
     it('Powering one in negative power', (): void => {
         expect(myPow(1.00000, -2147483647))
-            .toBe(1);
-    });
+            .toBe(1)
+    })
 
     it('Powering one in negative power', (): void => {
         expect(myPow(1.00000, -2147483648))
-            .toBe(1);
-    });
+            .toBe(1)
+    })
 
     it('Zero power', (): void => {
         expect(myPow(2.00000, 0))
-            .toBe(1);
-    });
+            .toBe(1)
+    })
 })
--- solutions/50. Pow/script.ts
+++ solutions/50. Pow/script.ts
@@ -2,7 +2,7 @@ export {myPow}
 
 function myPow(x: number, n: number): number {
     if (n === 0) {
-        return 1;
+        return 1
     }
 
     if (n < 0) {
@@ -10,8 +10,8 @@ function myPow(x: number, n: number): number {
     }
 
     if (n % 2 === 0) {
-        return myPow(x * x, n / 2);
+        return myPow(x * x, n / 2)
     } else {
-        return myPow(x * x, (n - 1) / 2) * x;
+        return myPow(x * x, (n - 1) / 2) * x
     }
 }
