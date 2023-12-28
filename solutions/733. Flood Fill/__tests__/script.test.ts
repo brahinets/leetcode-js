d47e300c395d27d71583279be51e41e087f8da2e
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-12-29 00:35:56 +0200

    #733 Clean up

--- solutions/733. Flood Fill/__tests__/script.test.ts
+++ solutions/733. Flood Fill/__tests__/script.test.ts
@@ -1,13 +1,14 @@
 import {floodFill} from '../script'
 
 describe('733. Flood Fill', (): void => {
+
     it('Fill neighbours', (): void => {
         expect(floodFill([[1, 1, 1], [1, 1, 0], [1, 0, 1]], 1, 1, 2))
-            .toStrictEqual([[2, 2, 2], [2, 2, 0], [2, 0, 1]]);
-    });
+            .toStrictEqual([[2, 2, 2], [2, 2, 0], [2, 0, 1]])
+    })
 
     it('No neighbours for fill', (): void => {
         expect(floodFill([[0, 0, 0], [0, 0, 0]], 0, 0, 0))
-            .toStrictEqual([[0, 0, 0], [0, 0, 0]]);
-    });
-});
+            .toStrictEqual([[0, 0, 0], [0, 0, 0]])
+    })
+})
--- solutions/733. Flood Fill/script.ts
+++ solutions/733. Flood Fill/script.ts
@@ -1,30 +1,30 @@
 export {floodFill}
 
 function floodFill(image: number[][], sr: number, sc: number, color: number): number[][] {
-    const startColor: number = image[sr][sc];
+    const startColor: number = image[sr][sc]
 
     if (startColor === color) {
-        return image;
+        return image
     }
 
-    return floodFills(image, sr, sc, color, startColor);
+    return floodFills(image, sr, sc, color, startColor)
 }
 
 function floodFills(image: number[][], sr: number, sc: number, newColor: number, startColor: number): number[][] {
     if (sr < 0 || sc < 0 || sr >= image.length || sc >= image[0].length) {
-        return image;
+        return image
     }
 
-    let imageCopy: number[][] = JSON.parse(JSON.stringify(image));
+    let imageCopy: number[][] = JSON.parse(JSON.stringify(image))
 
     if (startColor === imageCopy[sr][sc]) {
-        imageCopy[sr][sc] = newColor;
+        imageCopy[sr][sc] = newColor
 
-        imageCopy = floodFills(imageCopy, sr - 1, sc, newColor, startColor);
-        imageCopy = floodFills(imageCopy, sr, sc - 1, newColor, startColor);
-        imageCopy = floodFills(imageCopy, sr + 1, sc, newColor, startColor);
-        imageCopy = floodFills(imageCopy, sr, sc + 1, newColor, startColor);
+        imageCopy = floodFills(imageCopy, sr - 1, sc, newColor, startColor)
+        imageCopy = floodFills(imageCopy, sr, sc - 1, newColor, startColor)
+        imageCopy = floodFills(imageCopy, sr + 1, sc, newColor, startColor)
+        imageCopy = floodFills(imageCopy, sr, sc + 1, newColor, startColor)
     }
 
-    return imageCopy;
+    return imageCopy
 }
