d6670457bbe6dd07ed02b3a93d218ac5b97f0615
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-12-29 20:00:45 +0200

    #6 Clean up

--- solutions/6. Zigzag Conversion/__tests__/script.test.ts
+++ solutions/6. Zigzag Conversion/__tests__/script.test.ts
@@ -1,18 +1,19 @@
 import {convert} from '../script'
 
 describe('6. Zigzag Conversion', (): void => {
+
     it('Single step', (): void => {
         expect(convert("A", 1))
-            .toBe("A");
-    });
+            .toBe("A")
+    })
 
     it('Short Zig Zag', (): void => {
         expect(convert("PAYPALISHIRING", 3))
-            .toBe("PAHNAPLSIIGYIR");
-    });
+            .toBe("PAHNAPLSIIGYIR")
+    })
 
     it('Long Zig Zag', (): void => {
         expect(convert("PAYPALISHIRING", 4))
-            .toBe("PINALSIGYAHRPI");
-    });
+            .toBe("PINALSIGYAHRPI")
+    })
 })
--- solutions/6. Zigzag Conversion/script.ts
+++ solutions/6. Zigzag Conversion/script.ts
@@ -1,36 +1,36 @@
 export {convert}
 
 function convert(s: string, numRows: number): string {
-    const matrix: string[][] = [];
+    const matrix: string[][] = []
     for (let i: number = 0; i < numRows; i++) {
-        const row: string[] = [];
-        matrix.push(row);
+        const row: string[] = []
+        matrix.push(row)
     }
 
-    const chars: string[] = s.split('');
-    let toBottom: boolean = true;
+    const chars: string[] = s.split('')
+    let toBottom: boolean = true
 
     while (chars.length > 0) {
         if (toBottom) {
             for (let i: number = 0; i < numRows && chars.length > 0; i++) {
-                const char: string = chars.shift() || '';
-                matrix[i].push(char);
+                const char: string = chars.shift() || ''
+                matrix[i].push(char)
             }
         } else {
             for (let i: number = numRows - 1; i >= 0 && chars.length > 0; i--) {
                 for (let j: number = 1; j < numRows - 1 && chars.length > 0; j++) {
                     if (i + j + 1 === numRows) {
-                        const char: string = chars.shift() || '';
-                        matrix[i].push(char);
+                        const char: string = chars.shift() || ''
+                        matrix[i].push(char)
                     } else {
-                        matrix[i].push("");
+                        matrix[i].push("")
                     }
                 }
             }
         }
 
-        toBottom = !toBottom;
+        toBottom = !toBottom
     }
 
-    return matrix.reduce((str: string, row: string[]) => str + row.join(""), "");
+    return matrix.reduce((str: string, row: string[]) => str + row.join(""), "")
 }
