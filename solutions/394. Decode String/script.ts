8822a12fcd2b1f61bfacb4b2493330c23bc8e633
Yaroslav Brahinets <y.brahinets@gmail.com>
2024-01-03 17:31:47 +0200

    #394 Clean up

--- solutions/394. Decode String/script.ts
+++ solutions/394. Decode String/script.ts
@@ -1,46 +1,46 @@
 export {decodeString}
 
 function decodeString(s: string): string {
-    let result: string = "";
+    let result: string = ""
 
-    const chars: string[] = s.split("");
-    let char;
-    let multiplier: string = "";
+    const chars: string[] = s.split("")
+    let char
+    let multiplier: string = ""
 
     while (char = chars.shift()) {
         if (isDigit(char)) {
-            multiplier += char;
+            multiplier += char
         } else if (char === "[") {
-            let block: string = "";
-            const blocks: string[] = [char];
+            let block: string = ""
+            const blocks: string[] = [char]
             while (char = chars.shift()) {
                 if (char === "[") {
-                    block += char;
-                    blocks.push(char);
+                    block += char
+                    blocks.push(char)
                 } else if (char === "]") {
-                    block += char;
-                    blocks.pop();
+                    block += char
+                    blocks.pop()
                 } else {
-                    block += char;
+                    block += char
                 }
 
                 if (blocks.length === 0) {
-                    block = block.slice(0, block.length - 1);
-                    break;
+                    block = block.slice(0, block.length - 1)
+                    break
                 }
             }
 
-            result += decodeString(block).repeat(Number(multiplier));
-            multiplier = "";
+            result += decodeString(block).repeat(Number(multiplier))
+            multiplier = ""
         } else {
-            result += char;
-            multiplier = "";
+            result += char
+            multiplier = ""
         }
     }
 
-    return result;
+    return result
 }
 
 function isDigit(str: string): boolean {
-    return /^\d+$/.test(str);
+    return /^\d+$/.test(str)
 }
