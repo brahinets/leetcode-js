37bf9608147d7a112b85bda1ed9e3564933e3ef8
Yaroslav Brahinets <y.brahinets@gmail.com>
2024-01-04 15:29:18 +0200

    #1035 Clean up

--- solutions/1035. Uncrossed Lines/script.ts
+++ solutions/1035. Uncrossed Lines/script.ts
@@ -3,28 +3,28 @@ import {matrixOf} from "../../common/array-factories"
 export {maxUncrossedLines}
 
 function maxUncrossedLines(up: number[], bottom: number[]): number {
-    return uncrossedLines(up, bottom, 0, 0, matrixOf(-1, up.length, bottom.length));
+    return uncrossedLines(up, bottom, 0, 0, matrixOf(-1, up.length, bottom.length))
 }
 
 function uncrossedLines(up: number[], bottom: number[], start: number, end: number, memo: number[][]): number {
     if (start === up.length || end === bottom.length) {
-        return 0;
+        return 0
     }
 
     if (memo[start][end] !== -1) {
-        return memo[start][end];
+        return memo[start][end]
     }
 
-    let number: number;
+    let number: number
     if (up[start] === bottom[end]) {
-        number = 1 + uncrossedLines(up, bottom, start + 1, end + 1, memo);
+        number = 1 + uncrossedLines(up, bottom, start + 1, end + 1, memo)
     } else {
         number = Math.max(
             uncrossedLines(up, bottom, start + 1, end, memo),
             uncrossedLines(up, bottom, start, end + 1, memo)
-        );
+        )
     }
 
-    memo[start][end] = number;
-    return number;
+    memo[start][end] = number
+    return number
 }
