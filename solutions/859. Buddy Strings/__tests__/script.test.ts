06ec388b8cc9aa683d805558196f7fed76616cc7
Yaroslav Brahinets <y.brahinets@gmail.com>
2024-01-05 20:09:37 +0200

    #859 Clean up

--- solutions/859. Buddy Strings/__tests__/script.test.ts
+++ solutions/859. Buddy Strings/__tests__/script.test.ts
@@ -1,23 +1,24 @@
 import {buddyStrings} from '../script'
 
 describe('859. Buddy Strings', (): void => {
+
     it('Can swap', (): void => {
         expect(buddyStrings("ab", "ba"))
-            .toBe(true);
-    });
+            .toBe(true)
+    })
 
     it('Cannot swap, will break equality', (): void => {
         expect(buddyStrings("ab", "ab"))
-            .toBe(false);
-    });
+            .toBe(false)
+    })
 
     it('Cannot swap, different length', (): void => {
         expect(buddyStrings("aa", "aaa"))
-            .toBe(false);
-    });
+            .toBe(false)
+    })
 
     it('Cannot swap, will not break equality', (): void => {
         expect(buddyStrings("aa", "aa"))
-            .toBe(true);
-    });
+            .toBe(true)
+    })
 })
