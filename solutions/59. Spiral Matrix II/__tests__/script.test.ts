5f729bfca61e8e42c9cf0b156d14a4fcbd904b6f
Yaroslav Brahinets <y.brahinets@gmail.com>
2024-01-08 08:07:41 +0200

    #59 Simplify assertion

--- solutions/59. Spiral Matrix II/__tests__/script.test.ts
+++ solutions/59. Spiral Matrix II/__tests__/script.test.ts
@@ -3,22 +3,23 @@ import {generateMatrix} from '../script'
 describe('59. Spiral Matrix II', (): void => {
     it('Single element', (): void => {
         expect(generateMatrix(1))
-            .toStrictEqual([[1]])
+            .toEqual([[1]])
     })
 
     it('Square small', (): void => {
         expect(generateMatrix(2))
-            .toStrictEqual([
+            .toEqual([
                 [1, 2],
                 [4, 3]
             ])
     })
 
     it('Square big', (): void => {
-        expect(generateMatrix(3)).toStrictEqual([
-            [1, 2, 3],
-            [8, 9, 4],
-            [7, 6, 5]
-        ])
+        expect(generateMatrix(3))
+            .toEqual([
+                [1, 2, 3],
+                [8, 9, 4],
+                [7, 6, 5]
+            ])
     })
 })
