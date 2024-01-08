ae940a0c822b01837bfc24124c1ce597771a1508
Yaroslav Brahinets <y.brahinets@gmail.com>
2024-01-08 08:07:18 +0200

    #59 Clean up

--- solutions/59. Spiral Matrix II/__tests__/script.test.ts
+++ solutions/59. Spiral Matrix II/__tests__/script.test.ts
@@ -3,22 +3,22 @@ import {generateMatrix} from '../script'
 describe('59. Spiral Matrix II', (): void => {
     it('Single element', (): void => {
         expect(generateMatrix(1))
-            .toStrictEqual([[1]]);
-    });
+            .toStrictEqual([[1]])
+    })
 
     it('Square small', (): void => {
         expect(generateMatrix(2))
             .toStrictEqual([
                 [1, 2],
                 [4, 3]
-            ]);
-    });
+            ])
+    })
 
     it('Square big', (): void => {
         expect(generateMatrix(3)).toStrictEqual([
             [1, 2, 3],
             [8, 9, 4],
             [7, 6, 5]
-        ]);
-    });
+        ])
+    })
 })
--- solutions/59. Spiral Matrix II/script.ts
+++ solutions/59. Spiral Matrix II/script.ts
@@ -3,34 +3,34 @@ import {matrixOfZeros} from '../../common/array-factories'
 export {generateMatrix}
 
 function generateMatrix(n: number): number[][] {
-    const result: number[][] = matrixOfZeros(n, n);
+    const result: number[][] = matrixOfZeros(n, n)
 
-    let layer: number = 0;
-    let startNumber: number = 1;
+    let layer: number = 0
+    let startNumber: number = 1
     while (startNumber <= n * n) {
-        startNumber = fillMatrixBorder(result, layer, startNumber);
-        layer++;
+        startNumber = fillMatrixBorder(result, layer, startNumber)
+        layer++
     }
 
-    return result;
+    return result
 }
 
 function fillMatrixBorder(matrix: number[][], layer: number, startNumber: number): number {
     for (let col: number = layer; col + layer < matrix[0].length; col++) {
-        matrix[layer][col] = startNumber++;
+        matrix[layer][col] = startNumber++
     }
 
     for (let row: number = 1 + layer; row + layer < matrix.length; row++) {
-        matrix[row][matrix[0].length - 1 - layer] = startNumber++;
+        matrix[row][matrix[0].length - 1 - layer] = startNumber++
     }
 
     for (let col: number = matrix[0].length - 2 - layer; col - layer >= 0; col--) {
-        matrix[matrix.length - 1 - layer][col] = startNumber++;
+        matrix[matrix.length - 1 - layer][col] = startNumber++
     }
 
     for (let row: number = matrix.length - 2 - layer; row - layer > 0; row--) {
-        matrix[row][layer] = startNumber++;
+        matrix[row][layer] = startNumber++
     }
 
-    return startNumber;
+    return startNumber
 }
