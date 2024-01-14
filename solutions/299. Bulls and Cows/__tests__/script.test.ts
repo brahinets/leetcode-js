b6737d7285cdd5ea25e3fdb8f9e02e64637586b6
Yaroslav Brahinets <y.brahinets@gmail.com>
2024-01-14 15:15:25 +0200

    #299 Clean up

--- solutions/299. Bulls and Cows/__tests__/script.test.ts
+++ solutions/299. Bulls and Cows/__tests__/script.test.ts
@@ -3,16 +3,16 @@ import {getHint} from '../script'
 describe('299. Bulls and Cows', (): void => {
     it('Unique', (): void => {
         expect(getHint("1807", "7810"))
-            .toBe("1A3B");
-    });
+            .toBe("1A3B")
+    })
 
     it('Duplicates', (): void => {
         expect(getHint("1123", "0111"))
-            .toBe("1A1B");
-    });
+            .toBe("1A1B")
+    })
 
     it('Multiple duplicates', (): void => {
         expect(getHint("1122", "2211"))
-            .toBe("0A4B");
-    });
+            .toBe("0A4B")
+    })
 })
