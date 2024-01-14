814bca338d051761739e5585db020976f2f17e56
Yaroslav Brahinets <y.brahinets@gmail.com>
2024-01-14 15:18:08 +0200

    #934 Clean up

--- solutions/934. Shortest Bridge/__tests__/script.test.ts
+++ solutions/934. Shortest Bridge/__tests__/script.test.ts
@@ -1,25 +1,26 @@
 import {shortestBridge} from '../script'
 
 describe('934. Shortest Bridge', (): void => {
+
     it('No islands. Empty sea', (): void => {
         expect(shortestBridge([]))
-            .toBe(0);
-    });
+            .toBe(0)
+    })
 
     it('Small close island', (): void => {
         expect(shortestBridge([
             [0, 1],
             [1, 0]
-        ])).toBe(1);
-    });
+        ])).toBe(1)
+    })
 
     it('Small far island', (): void => {
         expect(shortestBridge([
             [0, 1, 0],
             [0, 0, 0],
             [0, 0, 1]
-        ])).toBe(2);
-    });
+        ])).toBe(2)
+    })
 
     it('Big close islands', (): void => {
         expect(shortestBridge([
@@ -28,8 +29,9 @@ describe('934. Shortest Bridge', (): void => {
             [1, 0, 1, 0, 1],
             [1, 0, 0, 0, 1],
             [1, 1, 1, 1, 1]
-        ])).toBe(1);
-    });
+        ])).toBe(1)
+    })
+
     it('Big far islands', (): void => {
         expect(shortestBridge([
             [0, 0, 0, 1, 1, 0, 0],
@@ -39,6 +41,6 @@ describe('934. Shortest Bridge', (): void => {
             [0, 0, 0, 0, 0, 0, 0],
             [0, 1, 0, 0, 0, 0, 0],
             [0, 1, 1, 0, 0, 0, 0]
-        ])).toBe(4);
-    });
-});
+        ])).toBe(4)
+    })
+})
--- solutions/934. Shortest Bridge/script.ts
+++ solutions/934. Shortest Bridge/script.ts
@@ -1,34 +1,34 @@
 export {shortestBridge}
 
-const ISLAND: number = 1;
-const VISITED: number = 2;
+const ISLAND: number = 1
+const VISITED: number = 2
 
 function shortestBridge(grid: number[][]): number {
-    const firstIsland: number[] = findIslandCoordinates(grid);
+    const firstIsland: number[] = findIslandCoordinates(grid)
     if (firstIsland[0] === -1 || firstIsland[1] === -1) {
-        return 0;
+        return 0
     }
-    visitIsland(grid, firstIsland[0], firstIsland[1]);
+    visitIsland(grid, firstIsland[0], firstIsland[1])
 
-    const firstIslandCoordinates: Array<number[]> = collectIslandCoordinates(grid, 1);
-    const secondIslandCoordinates: Array<number[]> = collectIslandCoordinates(grid, 2);
-    let minBridge: number = grid.length + grid.length - 1;
+    const firstIslandCoordinates: Array<number[]> = collectIslandCoordinates(grid, 1)
+    const secondIslandCoordinates: Array<number[]> = collectIslandCoordinates(grid, 2)
+    let minBridge: number = grid.length + grid.length - 1
 
     for (const first of firstIslandCoordinates) {
         for (const second of secondIslandCoordinates) {
-            const bridgeSize: number = distance(first[0], first[1], second[0], second[1]) - 1;
+            const bridgeSize: number = distance(first[0], first[1], second[0], second[1]) - 1
 
             if (bridgeSize < minBridge) {
-                minBridge = bridgeSize;
+                minBridge = bridgeSize
             }
         }
     }
 
-    return minBridge;
+    return minBridge
 }
 
 function distance(firstX: number, firstY: number, secondX: number, secondY: number) {
-    return Math.abs(firstX - secondX) + Math.abs(firstY - secondY);
+    return Math.abs(firstX - secondX) + Math.abs(firstY - secondY)
 }
 
 function findIslandCoordinates(sea: number[][]): number[] {
@@ -40,34 +40,34 @@ function findIslandCoordinates(sea: number[][]): number[] {
         }
     }
 
-    return [-1, -1];
+    return [-1, -1]
 }
 
 function collectIslandCoordinates(grid: number[][], islandCode: number) {
-    const coordinates: Array<number[]> = [];
+    const coordinates: Array<number[]> = []
 
     for (let i: number = 0; i < grid.length; i++) {
         for (let j: number = 0; j < grid[0].length; j++) {
             if (grid[i][j] === islandCode) {
-                coordinates.push([i, j]);
+                coordinates.push([i, j])
             }
         }
     }
 
-    return coordinates;
+    return coordinates
 }
 
 function visitIsland(sea: number[][], islandCoordinateX: number, islandCoordinateY: number): void {
     if (islandCoordinateX < 0 || islandCoordinateY < 0 || islandCoordinateX >= sea.length || islandCoordinateY >= sea[0].length) {
-        return;
+        return
     }
 
     if (ISLAND === sea[islandCoordinateX][islandCoordinateY]) {
-        sea[islandCoordinateX][islandCoordinateY] = VISITED;
+        sea[islandCoordinateX][islandCoordinateY] = VISITED
 
-        visitIsland(sea, islandCoordinateX - 1, islandCoordinateY);
-        visitIsland(sea, islandCoordinateX, islandCoordinateY - 1);
-        visitIsland(sea, islandCoordinateX + 1, islandCoordinateY);
-        visitIsland(sea, islandCoordinateX, islandCoordinateY + 1);
+        visitIsland(sea, islandCoordinateX - 1, islandCoordinateY)
+        visitIsland(sea, islandCoordinateX, islandCoordinateY - 1)
+        visitIsland(sea, islandCoordinateX + 1, islandCoordinateY)
+        visitIsland(sea, islandCoordinateX, islandCoordinateY + 1)
     }
 }
