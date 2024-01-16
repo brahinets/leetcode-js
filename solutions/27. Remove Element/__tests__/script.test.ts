6a29bb3fb6c56c89dd720456513e1ab3f65ebbb4
Yaroslav Brahinets <y.brahinets@gmail.com>
2024-01-16 17:46:55 +0200

    #27 Clean up

--- solutions/27. Remove Element/__tests__/script.test.ts
+++ solutions/27. Remove Element/__tests__/script.test.ts
@@ -1,19 +1,20 @@
 import {removeElement} from '../script'
 
 describe('27. Remove Element', (): void => {
-    it('Several', (): void => {
-        const input: number[] = [3, 2, 2, 3];
-        const left: number = removeElement(input, 2);
 
-        expect(left).toBe(2);
-        expect(input).toStrictEqual([3, 3]);
-    });
+    it('Several', (): void => {
+        const input: number[] = [3, 2, 2, 3]
+        const left: number = removeElement(input, 2)
+
+        expect(left).toBe(2)
+        expect(input).toStrictEqual([3, 3])
+    })
 
     it('Many', (): void => {
-        const input: number[] = [0, 1, 2, 2, 3, 0, 4, 2];
-        const left: number = removeElement(input, 2);
+        const input: number[] = [0, 1, 2, 2, 3, 0, 4, 2]
+        const left: number = removeElement(input, 2)
 
-        expect(left).toBe(5);
-        expect(input).toStrictEqual([0, 1, 3, 0, 4]);
-    });
+        expect(left).toBe(5)
+        expect(input).toStrictEqual([0, 1, 3, 0, 4])
+    })
 })
--- solutions/27. Remove Element/script.ts
+++ solutions/27. Remove Element/script.ts
@@ -2,8 +2,8 @@ export {removeElement}
 
 function removeElement(nums: number[], val: number): number {
     while (nums.includes(val)) {
-        nums.splice(nums.indexOf(val), 1);
+        nums.splice(nums.indexOf(val), 1)
     }
 
-    return nums.length;
+    return nums.length
 }
