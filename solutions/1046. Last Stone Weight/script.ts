2f14905e183bd09e9afc626321d6bdab0b2e78dc
Yaroslav Brahinets <y.brahinets@gmail.com>
2024-01-17 11:40:27 +0200

    #1046 Use more type-safe coalesce

--- solutions/1046. Last Stone Weight/script.ts
+++ solutions/1046. Last Stone Weight/script.ts
@@ -4,13 +4,13 @@ function lastStoneWeight(stones: number[]): number {
     while (stones.length > 1) {
         stones = stones.toSorted((a: number, b: number) => a - b)
 
-        const s1: number = stones.pop() || 0
-        const s2: number = stones.pop() || 0
+        const s1: number = stones.pop() ?? 0
+        const s2: number = stones.pop() ?? 0
 
         if (s1 !== s2) {
             stones.push(s1 - s2)
         }
     }
 
-    return stones[0] || 0
+    return stones[0] ?? 0
 }
