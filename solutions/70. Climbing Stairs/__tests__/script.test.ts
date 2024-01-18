5dd086a71d388c48e1c55b6946c8732482f85e71
Yaroslav Brahinets <y.brahinets@gmail.com>
2024-01-18 11:40:09 +0200

    #70 Clean up

--- solutions/70. Climbing Stairs/__tests__/script.test.ts
+++ solutions/70. Climbing Stairs/__tests__/script.test.ts
@@ -1,15 +1,19 @@
 import {climbStairs} from '../script'
 
 describe('70. Climbing Stairs', (): void => {
+
     it('Two ways ', (): void => {
-        expect(climbStairs(2)).toBe(2);
-    });
+        expect(climbStairs(2))
+            .toBe(2)
+    })
 
     it('Three ways', (): void => {
-        expect(climbStairs(3)).toBe(3);
-    });
+        expect(climbStairs(3))
+            .toBe(3)
+    })
 
     it('Many ways', (): void => {
-        expect(climbStairs(45)).toBe(1836311903);
-    });
-});
+        expect(climbStairs(45))
+            .toBe(1836311903)
+    })
+})
--- solutions/70. Climbing Stairs/script.ts
+++ solutions/70. Climbing Stairs/script.ts
@@ -2,18 +2,18 @@ export {climbStairs}
 
 function climbStairs(n: number, memo: Map<number, number> = new Map<number, number>()): number {
     if (n === 0 || n === 1) {
-        return 1;
+        return 1
     }
 
     if (n === 2) {
-        return 2;
+        return 2
     }
 
-    let value: number | undefined = memo.get(n);
+    let value: number | undefined = memo.get(n)
     if (!value) {
-        value = climbStairs(n - 1, memo) + climbStairs(n - 2, memo);
-        memo.set(n, value);
+        value = climbStairs(n - 1, memo) + climbStairs(n - 2, memo)
+        memo.set(n, value)
     }
 
-    return value;
+    return value
 }
