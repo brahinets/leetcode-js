a354f00ca48f57369275708357491d349befbbdd
Yaroslav Brahinets <y.brahinets@gmail.com>
2024-01-19 20:41:12 +0200

    #547 Clean up

--- solutions/547. Number of Provinces/__tests__/script.test.ts
+++ solutions/547. Number of Provinces/__tests__/script.test.ts
@@ -1,16 +1,18 @@
 import {findCircleNum} from '../script'
 
 describe('547. Number of Provinces', (): void => {
+
     it('No provinces', (): void => {
-        expect(findCircleNum([])).toBe(0);
-    });
+        expect(findCircleNum([]))
+            .toBe(0)
+    })
 
     it('One small province', (): void => {
         expect(findCircleNum([
             [1, 1],
             [1, 1]
-        ])).toBe(1);
-    });
+        ])).toBe(1)
+    })
 
     it('One big province', (): void => {
         expect(findCircleNum([
@@ -18,22 +20,22 @@ describe('547. Number of Provinces', (): void => {
             [0, 1, 1, 0],
             [0, 1, 1, 1],
             [1, 0, 1, 1]
-        ])).toBe(1);
-    });
+        ])).toBe(1)
+    })
 
     it('Two provinces', (): void => {
         expect(findCircleNum([
             [1, 1, 0],
             [1, 1, 0],
             [0, 0, 1]
-        ])).toBe(2);
-    });
+        ])).toBe(2)
+    })
 
     it('Three provinces', (): void => {
         expect(findCircleNum([
             [1, 0, 0],
             [0, 1, 0],
             [0, 0, 1]
-        ])).toBe(3);
-    });
-});
+        ])).toBe(3)
+    })
+})
--- solutions/547. Number of Provinces/script.ts
+++ solutions/547. Number of Provinces/script.ts
@@ -1,52 +1,52 @@
 export {findCircleNum}
 
-const PROVINCE: number = 1;
-const VISITED: number = 2;
+const PROVINCE: number = 1
+const VISITED: number = 2
 
 function findCircleNum(isConnected: number[][]): number {
-    const map: number[][] = JSON.parse(JSON.stringify(isConnected));
+    const map: number[][] = JSON.parse(JSON.stringify(isConnected))
 
-    let provincesCount: number = 0;
-    let hasProvince: boolean;
+    let provincesCount: number = 0
+    let hasProvince: boolean
 
     do {
-        const id: number = findProvinceCoordinates(map);
+        const id: number = findProvinceCoordinates(map)
 
-        hasProvince = id !== -1;
+        hasProvince = id !== -1
         if (hasProvince) {
-            gatherProvince(map, id);
-            provincesCount++;
+            gatherProvince(map, id)
+            provincesCount++
         }
     } while (hasProvince)
 
-    return provincesCount;
+    return provincesCount
 }
 
 function findProvinceCoordinates(map: number[][]): number {
     for (let i: number = 0; i < map.length; i++) {
         if (map[i][i] === PROVINCE) {
-            return i;
+            return i
         }
     }
 
-    return -1;
+    return -1
 }
 
 function gatherProvince(map: number[][], cityId: number): number[] {
-    const province: number[] = [cityId];
-    map[cityId][cityId] = VISITED;
+    const province: number[] = [cityId]
+    map[cityId][cityId] = VISITED
 
-    const neighbours: number[] = map[cityId];
-    for (let neighbourId:number = 0; neighbourId < neighbours.length; neighbourId++) {
-        if(map[cityId][neighbourId] === PROVINCE) {
-            map[cityId][neighbourId] = VISITED;
-            province.push(neighbourId);
+    const neighbours: number[] = map[cityId]
+    for (let neighbourId: number = 0; neighbourId < neighbours.length; neighbourId++) {
+        if (map[cityId][neighbourId] === PROVINCE) {
+            map[cityId][neighbourId] = VISITED
+            province.push(neighbourId)
 
             if (neighbourId !== cityId) {
-                province.push(...gatherProvince(map, neighbourId));
+                province.push(...gatherProvince(map, neighbourId))
             }
         }
     }
 
-    return province;
+    return province
 }
