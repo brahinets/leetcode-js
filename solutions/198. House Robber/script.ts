fb920456608c4ee6b58ca3777219c6a9a752dbd2
Yaroslav Brahinets <y.brahinets@gmail.com>
2024-01-21 19:24:39 +0200

    #198 Simplify

--- solutions/198. House Robber/script.ts
+++ solutions/198. House Robber/script.ts
@@ -1,15 +1,8 @@
 export {rob}
 
 function rob(house: number[]): number {
-    if (house.length === 0) {
-        return 0
-    }
-
-    if (house.length === 1) {
-        return house[0]
-    }
-
     const maxForHouse: number[] = []
+
     for (let i: number = 0; i < house.length; i++) {
         maxForHouse[i] = Math.max(
             (maxForHouse[i - 1] ?? 0),
@@ -17,5 +10,5 @@ function rob(house: number[]): number {
         )
     }
 
-    return maxForHouse[house.length - 1]
+    return maxForHouse[house.length - 1] ?? 0
 }
