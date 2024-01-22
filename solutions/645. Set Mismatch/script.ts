14aa054c1bc77ee74e41ddcc6c5a60429bb89b09
Yaroslav Brahinets <y.brahinets@gmail.com>
2024-01-22 13:51:43 +0200

    #645 Eliminate unreachable code using assertive operator

--- solutions/645. Set Mismatch/script.ts
+++ solutions/645. Set Mismatch/script.ts
@@ -9,21 +9,11 @@ function findErrorNums(nums: number[]): number[] {
 }
 
 function findDuplicated(counts: Map<number, number>): number {
-    for (const [num, count] of counts) {
-        if (count > 1) {
-            return num
-        }
-    }
-
-    throw new Error("No duplicated number found")
+    return [...counts.entries()]
+        .find(([, count]): boolean => count > 1)![0]
 }
 
-function findMissing(totalCount:number, counts: Map<number, number>): number {
-    for (let i: number = 1; i <= totalCount; i++) {
-        if (!counts.has(i)) {
-            return i
-        }
-    }
-
-    throw new Error("No missing number found")
+function findMissing(totalCount: number, counts: Map<number, number>): number {
+    return Array.from({length: totalCount}, (_, i) => i + 1)
+        .find((num: number) => !counts.has(num))!
 }
