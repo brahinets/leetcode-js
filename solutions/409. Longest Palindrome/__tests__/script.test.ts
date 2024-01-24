011494a7e9bbbca744ba8c3bf918ccaddde5affc
Yaroslav Brahinets <y.brahinets@gmail.com>
2024-01-24 17:27:50 +0200

    #409 Clean up

--- solutions/409. Longest Palindrome/__tests__/script.test.ts
+++ solutions/409. Longest Palindrome/__tests__/script.test.ts
@@ -1,11 +1,14 @@
 import {longestPalindrome} from '../script'
 
 describe('409. Longest Palindrome', (): void => {
+
     it('One letter palindrome', (): void => {
-        expect(longestPalindrome("a")).toBe(1);
-    });
+        expect(longestPalindrome("a"))
+            .toBe(1)
+    })
 
     it('Multiple letters palindrome', (): void => {
-        expect(longestPalindrome("abccccdd", )).toBe(7);
-    });
-});
+        expect(longestPalindrome("abccccdd",))
+            .toBe(7)
+    })
+})
