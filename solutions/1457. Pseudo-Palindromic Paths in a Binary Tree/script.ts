24438aac4e92fa044950c6ed3ddfda188a5731ca
Yaroslav Brahinets <y.brahinets@gmail.com>
2024-01-24 17:10:57 +0200

    #1457 Optimize memory for parent path hold

--- solutions/1457. Pseudo-Palindromic Paths in a Binary Tree/script.ts
+++ solutions/1457. Pseudo-Palindromic Paths in a Binary Tree/script.ts
@@ -4,31 +4,32 @@ import {count} from "../../common/array-utils"
 export {TreeNode, pseudoPalindromicPaths}
 
 function pseudoPalindromicPaths(root: TreeNode): number {
-    return collectPaths(root, "")
-        .filter((path: string) => isPseudoPalindromic(path))
+    return collectPaths(root, [])
+        .filter((path: number[]) => isPseudoPalindromic(path))
         .length
 }
 
-function collectPaths(node: TreeNode, parentPath: string): string [] {
-    const paths: string[] = []
+function collectPaths(node: TreeNode, parentPath: number[]): number [][] {
+    const paths: number[][] = []
 
     if (node.left) {
-        paths.push(...collectPaths(node.left, parentPath + node.val))
+        paths.push(...collectPaths(node.left, [...parentPath, node.val]))
     }
 
     if (node.right) {
-        paths.push(...collectPaths(node.right, parentPath + node.val))
+        paths.push(...collectPaths(node.right, [...parentPath, node.val]))
     }
 
     if (!node.left && !node.right) {
-        paths.push(parentPath + node.val)
+        paths.push([...parentPath, node.val])
     }
 
     return paths
 }
 
-function isPseudoPalindromic(path: string): boolean {
-    const counts: Map<string, number> = count(path.split(''))
+function isPseudoPalindromic(path: number[]): boolean {
+    const counts: Map<number, number> = count(path)
+
     const oddCounts: number = Array.from(counts.values())
         .filter((count: number): boolean => count % 2 === 1)
         .length
