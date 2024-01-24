dd6c60ab66cff99a1ce3731b3be1cdf0ac610f50
Yaroslav Brahinets <y.brahinets@gmail.com>
2024-01-24 17:24:37 +0200

    #1457 Optimize memory to store only non-paired elements (enough for palindromic check)

--- solutions/1457. Pseudo-Palindromic Paths in a Binary Tree/script.ts
+++ solutions/1457. Pseudo-Palindromic Paths in a Binary Tree/script.ts
@@ -1,5 +1,4 @@
 import {TreeNode} from "../../common/TreeNode"
-import {count} from "../../common/array-utils"
 
 export {TreeNode, pseudoPalindromicPaths}
 
@@ -7,27 +6,23 @@ function pseudoPalindromicPaths(root: TreeNode): number {
     return countPseudoPalindromicPaths(root, [])
 }
 
-function countPseudoPalindromicPaths(node: TreeNode | null, parentPath: number[]): number {
+function countPseudoPalindromicPaths(node: TreeNode | null, pathElementsWithoutPair: number[]): number {
     if (!node) {
         return 0
     }
 
-    const currentPath: number[] = [...parentPath, node.val]
+    const currentPath: number[] = [...pathElementsWithoutPair]
+    if (!currentPath.includes(node.val)) {
+        currentPath.push(node.val)
+    } else {
+        const index: number = currentPath.indexOf(node.val)
+        currentPath.splice(index, 1)
+    }
 
     const leaveNode: boolean = !node.left && !node.right
     if (leaveNode) {
-        return isPseudoPalindromic(currentPath) ? 1 : 0
+        return currentPath.length <= 1 ? 1 : 0
     }
 
     return countPseudoPalindromicPaths(node.left, currentPath) + countPseudoPalindromicPaths(node.right, currentPath)
-}
-
-function isPseudoPalindromic(path: number[]): boolean {
-    const counts: Map<number, number> = count(path)
-
-    const oddCounts: number = Array.from(counts.values())
-        .filter((count: number): boolean => count % 2 === 1)
-        .length
-
-    return oddCounts <= 1
 }
