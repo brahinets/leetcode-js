fdf59a7a414d15a5bb2c45dda654d62343caa6e2
Yaroslav Brahinets <y.brahinets@gmail.com>
2024-01-25 20:39:39 +0200

    #2385 Speed up by using more search-optimal data structure

--- solutions/2385. Amount of Time for Binary Tree to Be Infected/script.ts
+++ solutions/2385. Amount of Time for Binary Tree to Be Infected/script.ts
@@ -7,16 +7,17 @@ function amountOfTime(root: TreeNode, from: number): number {
     const graph: Map<number, Set<number>> = treeToGraph(root)
 
     let maxDistance: number = 0
-    const visited: number[] = [from]
+    const visited: Set<number> = new Set<number>([from])
     const queue: [number, number][] = [[from, 0]]
+
     while (queue.length > 0) {
         const [node, distance]: [number, number] = queue.shift()!
         maxDistance = Math.max(maxDistance, distance)
 
         const neighbours: Set<number> = graph.get(node) ?? new Set<number>()
         for (const neighbour of neighbours) {
-            if (!visited.includes(neighbour)) {
-                visited.push(neighbour)
+            if (!visited.has(neighbour)) {
+                visited.add(neighbour)
                 queue.push([neighbour, distance + 1])
             }
         }
