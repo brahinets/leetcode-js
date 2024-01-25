bf8154edfdf3144661fb3b8bbb8ea5a5b3d42ba0
Yaroslav Brahinets <y.brahinets@gmail.com>
2024-01-25 20:10:54 +0200

    #2385 Speed up by reusing parent node path

--- solutions/2385. Amount of Time for Binary Tree to Be Infected/script.ts
+++ solutions/2385. Amount of Time for Binary Tree to Be Infected/script.ts
@@ -7,40 +7,20 @@ function amountOfTime(root: TreeNode, from: number): number {
     const graph: Map<number, Set<number>> = treeToGraph(root)
 
     let maxDistance: number = 0
-    for (const node of graph.keys()) {
-        if (node === from) {
-            continue
-        }
+    const visited: number[] = [from]
+    const queue: [number, number][] = [[from, 0]]
+    while (queue.length > 0) {
+        const [node, distance]: [number, number] = queue.shift()!
+        maxDistance = Math.max(maxDistance, distance)
 
-        const distance: number = dfs(graph, from, node, [from])
-        if (distance > maxDistance) {
-            maxDistance = distance
-        }
-    }
-
-    return maxDistance
-}
-
-function dfs(graph: Map<number, Set<number>>, from: number, to: number, visited: number[]): number {
-    const neighbours: Set<number> | undefined = graph.get(from)
-
-    if (!neighbours) {
-        return -1
-    }
-
-    if (neighbours.has(to)) {
-        return 1
-    }
-
-    for (const neighbour of neighbours) {
-        if (!visited.includes(neighbour)) {
-            visited.push(neighbour)
-            const res: number = dfs(graph, neighbour, to, visited)
-            if (res > 0) {
-                return res + 1
+        const neighbours: Set<number> = graph.get(node) ?? new Set<number>()
+        for (const neighbour of neighbours) {
+            if (!visited.includes(neighbour)) {
+                visited.push(neighbour)
+                queue.push([neighbour, distance + 1])
             }
         }
     }
 
-    return -1
+    return maxDistance
 }
