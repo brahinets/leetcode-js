bf271355f755cd468b5b85fc5bbc206de3772af7
Yaroslav Brahinets <y.brahinets@gmail.com>
2024-01-25 20:28:29 +0200

    Optimize memory allocation for mapping tree to map

--- common/tree-utils.ts
+++ common/tree-utils.ts
@@ -1,4 +1,4 @@
-import {TreeNode} from "./TreeNode";
+import {TreeNode} from "./TreeNode"
 
 export {treeToGraph, TreeNode}
 
@@ -6,23 +6,33 @@ function treeToGraph(root: TreeNode): Map<number, Set<number>> {
     return buildGraph(root, new Map<number, Set<number>>())
 }
 
-function buildGraph(root: TreeNode | null, graph: Map<number, Set<number>>): Map<number, Set<number>> {
-    if (!root) {
+function buildGraph(node: TreeNode | null, graph: Map<number, Set<number>>): Map<number, Set<number>> {
+    if (!node) {
         return graph
     }
 
-    if (root.left) {
-        graph.set(root.val, new Set<number>([...graph.get(root.val) ?? [], root.left.val]))
-        graph.set(root.left.val, new Set<number>([...graph.get(root.left.val) ?? [], root.val]))
+    if (node.left) {
+        addNeighbour(graph, node, node.left)
     }
 
-    if (root.right) {
-        graph.set(root.val, new Set<number>([...graph.get(root.val) ?? [], root.right.val]))
-        graph.set(root.right.val, new Set<number>([...graph.get(root.right.val) ?? [], root.val]))
+    if (node.right) {
+        addNeighbour(graph, node, node.right)
     }
 
-    buildGraph(root.left, graph)
-    buildGraph(root.right, graph)
+    buildGraph(node.left, graph)
+    buildGraph(node.right, graph)
 
     return graph
+}
+
+function addNeighbour(graph: Map<number, Set<number>>, first: TreeNode, second: TreeNode): void {
+    if (!graph.has(first.val)) {
+        graph.set(first.val, new Set<number>())
+    }
+    graph.get(first.val)!.add(second.val)
+
+    if (!graph.has(second.val)) {
+        graph.set(second.val, new Set<number>())
+    }
+    graph.get(second.val)!.add(first.val)
 }
