88a58a4e772554ac6e4519c0169aef73a17834f8
Yaroslav Brahinets <y.brahinets@gmail.com>
2024-01-28 10:24:58 +0200

    #1436 Clean up

--- solutions/1436. Destination City/script.ts
+++ solutions/1436. Destination City/script.ts
@@ -4,7 +4,7 @@ function destCity(paths: string[][]): string {
     const start: string[] = paths.map((path: string[]): string => path[0])
     const destination: string[] = paths.map((path: string[]): string => path[1])
 
-    const last: string[] = destination.filter((city: string): boolean => !start.includes(city));
+    const last: string[] = destination.filter((city: string): boolean => !start.includes(city))
 
     if (last.length === 1) {
         return last[0]
