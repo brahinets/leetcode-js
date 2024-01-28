c81a46acf34c195b283403a8f69469f303e53afc
Yaroslav Brahinets <y.brahinets@gmail.com>
2024-01-28 10:25:28 +0200

    #1125 Clean up

--- solutions/1125. Smallest Sufficient Team/__tests__/script.test.ts
+++ solutions/1125. Smallest Sufficient Team/__tests__/script.test.ts
@@ -5,48 +5,48 @@ describe('1125. Smallest Sufficient Team', (): void => {
         expect(new Set<number>(smallestSufficientTeam(
             ["java", "nodejs", "reactjs"],
             [["java"], ["nodejs", "reactjs"]]
-        ))).toStrictEqual(new Set<number>([0, 1]));
-    });
+        ))).toStrictEqual(new Set<number>([0, 1]))
+    })
 
     it('Many of people', (): void => {
         expect(new Set<number>(smallestSufficientTeam(
             ["java", "nodejs", "reactjs"],
             [["java"], ["nodejs"], ["nodejs", "reactjs"]]
-        ))).toStrictEqual(new Set<number>([0, 2]));
-    });
+        ))).toStrictEqual(new Set<number>([0, 2]))
+    })
 
     it('Few of people', (): void => {
         expect(new Set<number>(smallestSufficientTeam(
             ["algorithms", "math", "java", "reactjs", "csharp", "aws"],
             [["algorithms", "math", "java"], ["algorithms", "math", "reactjs"], ["java", "csharp", "aws"], ["reactjs", "csharp"], ["csharp", "math"], ["aws", "java"]]
-        ))).toStrictEqual(new Set<number>([1, 2]));
-    });
+        ))).toStrictEqual(new Set<number>([1, 2]))
+    })
 
     it('Very many skills', (): void => {
         expect(smallestSufficientTeam(
             ["cpp", "python", "javascript", "kotlin", "ruby", "r", "c", "rust", "vb"],
             [["cpp", "c", "rust"], ["cpp", "python", "javascript", "c", "r", "rust"], ["cpp", "python", "javascript", "c", "r", "vb"], ["cpp", "python", "javascript", "ruby", "kotlin", "r", "c", "rust", "vb"], ["cpp", "python", "javascript", "kotlin", "ruby", "r", "c", "rust", "vb"], ["python", "r"], ["cpp", "python", "javascript", "ruby", "kotlin", "r", "c", "rust", "vb"], ["cpp", "python", "javascript", "r", "c", "rust"], ["cpp", "javascript", "rust", "vb"], ["kotlin", "ruby", "c", "vb"], ["cpp", "python", "kotlin", "ruby", "rust", "vb"], ["ruby", "c", "r", "rust"], ["python", "javascript"], ["javascript", "ruby", "rust"], ["python", "javascript", "ruby", "c", "r", "rust", "vb"], ["cpp", "javascript", "kotlin", "r", "c", "vb"], ["c", "rust"], ["cpp", "kotlin", "ruby", "c", "r", "rust", "vb"], ["cpp", "python", "javascript", "ruby", "kotlin", "rust"], ["cpp", "javascript", "ruby"], ["vb"]]
-        )).toStrictEqual([6]);
-    });
+        )).toStrictEqual([6])
+    })
 
     it('Very many people', (): void => {
         expect(new Set<number>(smallestSufficientTeam(
             ["vgzp", "urfys", "nvkjkzxrfn", "pqznypwpcv", "bdfmsjlddbinqo", "xvgkyzqpmzwcxol", "twlau", "vybgxpyq", "edpuw", "w"],
             [["xvgkyzqpmzwcxol"], ["w"], ["urfys", "w"], ["urfys"], ["bdfmsjlddbinqo"], ["xvgkyzqpmzwcxol"], [], [], ["w"], ["vgzp", "w"], ["nvkjkzxrfn", "bdfmsjlddbinqo", "edpuw"], ["urfys"], ["bdfmsjlddbinqo", "xvgkyzqpmzwcxol", "twlau"], ["nvkjkzxrfn", "bdfmsjlddbinqo", "vybgxpyq"], ["vgzp", "edpuw"], ["urfys", "xvgkyzqpmzwcxol"], ["bdfmsjlddbinqo", "w"], ["nvkjkzxrfn", "bdfmsjlddbinqo", "w"], ["bdfmsjlddbinqo", "vybgxpyq"], ["urfys", "bdfmsjlddbinqo"], ["vgzp"], ["nvkjkzxrfn", "xvgkyzqpmzwcxol"], ["nvkjkzxrfn"], ["vgzp", "edpuw"], [], ["bdfmsjlddbinqo"], ["vgzp", "nvkjkzxrfn", "xvgkyzqpmzwcxol"], ["vgzp", "bdfmsjlddbinqo", "edpuw", "w"], ["xvgkyzqpmzwcxol", "vybgxpyq", "w"], ["pqznypwpcv", "vybgxpyq"], ["vybgxpyq"], ["twlau"], ["vgzp"], ["vgzp", "twlau", "edpuw"], ["nvkjkzxrfn", "bdfmsjlddbinqo"], ["bdfmsjlddbinqo"], ["vgzp"], ["urfys"], ["nvkjkzxrfn"], ["nvkjkzxrfn", "edpuw"], ["vgzp", "nvkjkzxrfn"], [], ["vgzp", "nvkjkzxrfn"], ["xvgkyzqpmzwcxol"], [], ["nvkjkzxrfn", "xvgkyzqpmzwcxol", "vybgxpyq"], ["vybgxpyq"], ["nvkjkzxrfn"], ["nvkjkzxrfn", "edpuw"], ["vgzp"]]
-        ))).toStrictEqual(new Set<number>([33, 15, 17, 29]));
-    });
+        ))).toStrictEqual(new Set<number>([33, 15, 17, 29]))
+    })
 
     it('Very many people as well', (): void => {
         expect(new Set<number>(smallestSufficientTeam(
             ["ohmvnkxnokpnwxmr", "jdjdb", "azdwg", "xvn", "mljb", "rdaavykiextpr", "ztp"],
             [["azdwg"], ["ohmvnkxnokpnwxmr", "mljb"], ["jdjdb"], [], [], [], ["xvn", "mljb"], [], ["xvn"], [], ["xvn"], ["jdjdb", "mljb"], [], ["ohmvnkxnokpnwxmr", "mljb", "ztp"], ["ohmvnkxnokpnwxmr"], [], [], ["ohmvnkxnokpnwxmr", "jdjdb"], ["jdjdb"], [], [], [], ["azdwg", "mljb"], ["ztp"], ["xvn"], ["jdjdb"], ["mljb"], ["xvn"], ["rdaavykiextpr"], [], [], ["ohmvnkxnokpnwxmr", "jdjdb", "mljb", "rdaavykiextpr"], [], ["jdjdb", "mljb", "rdaavykiextpr", "ztp"], ["ohmvnkxnokpnwxmr"], ["rdaavykiextpr", "ztp"], ["mljb"], ["rdaavykiextpr"], [], ["azdwg"], [], ["rdaavykiextpr"], ["jdjdb"], [], ["mljb", "ztp"], [], ["rdaavykiextpr", "ztp"], ["jdjdb", "azdwg"], ["xvn", "ztp"], []]
-        ))).toStrictEqual(new Set<number>([31, 47, 48]));
-    });
+        ))).toStrictEqual(new Set<number>([31, 47, 48]))
+    })
 
     it('Very many people again', (): void => {
         expect(new Set<number>(smallestSufficientTeam(
             ["yvstvgspxy", "ohoawazvdz", "yvzuqettmru", "finmcneglinx", "daspbvnxn", "bzbipeubqgvtzl", "ufbsjdq", "wbtugbdzqcqpj", "qmucetzldtng", "elhzszjmeffcgtin"],
             [["yvstvgspxy", "yvzuqettmru", "daspbvnxn", "ufbsjdq"], ["finmcneglinx", "daspbvnxn", "bzbipeubqgvtzl", "elhzszjmeffcgtin"], ["elhzszjmeffcgtin"], [], ["bzbipeubqgvtzl"], ["bzbipeubqgvtzl", "qmucetzldtng"], ["elhzszjmeffcgtin"], ["ufbsjdq", "qmucetzldtng"], ["ufbsjdq"], ["bzbipeubqgvtzl"], ["ohoawazvdz", "finmcneglinx", "daspbvnxn", "bzbipeubqgvtzl"], ["daspbvnxn", "ufbsjdq", "elhzszjmeffcgtin"], ["yvstvgspxy", "daspbvnxn", "bzbipeubqgvtzl", "qmucetzldtng"], [], ["yvstvgspxy", "daspbvnxn"], ["bzbipeubqgvtzl", "ufbsjdq"], ["ufbsjdq"], ["finmcneglinx", "daspbvnxn", "wbtugbdzqcqpj", "qmucetzldtng", "elhzszjmeffcgtin"], ["daspbvnxn", "qmucetzldtng"], ["elhzszjmeffcgtin"], [], ["yvstvgspxy", "bzbipeubqgvtzl", "wbtugbdzqcqpj"], [], ["yvstvgspxy", "bzbipeubqgvtzl", "qmucetzldtng"], [], ["yvstvgspxy", "bzbipeubqgvtzl"], ["bzbipeubqgvtzl"], ["finmcneglinx", "daspbvnxn", "bzbipeubqgvtzl", "qmucetzldtng"], ["yvstvgspxy", "finmcneglinx", "daspbvnxn", "wbtugbdzqcqpj", "qmucetzldtng"], [], ["finmcneglinx"], ["daspbvnxn", "elhzszjmeffcgtin"], ["daspbvnxn", "elhzszjmeffcgtin"], ["ufbsjdq", "qmucetzldtng"], ["finmcneglinx", "daspbvnxn", "qmucetzldtng"], ["ufbsjdq"], ["yvzuqettmru", "qmucetzldtng"], ["qmucetzldtng"], ["qmucetzldtng"], ["bzbipeubqgvtzl", "ufbsjdq"]]
-        ))).toStrictEqual(new Set<number>([0, 10, 17]));
-    });
-});
+        ))).toStrictEqual(new Set<number>([0, 10, 17]))
+    })
+})
--- solutions/1125. Smallest Sufficient Team/script.ts
+++ solutions/1125. Smallest Sufficient Team/script.ts
@@ -1,93 +1,93 @@
 export {smallestSufficientTeam}
 
 function smallestSufficientTeam(req_skills: string[], people: string[][]): number[] {
-    const teams: Array<Set<number>> = formTeams(people, skillsByPopularity(people, req_skills));
+    const teams: Array<Set<number>> = formTeams(people, skillsByPopularity(people, req_skills))
 
     let minTeam: Set<number> = new Set<number>(new Array(people.length)
         .fill(0)
-        .map((_: number, index: number): number => index));
+        .map((_: number, index: number): number => index))
 
     for (const team of teams) {
         if (team.size <= minTeam.size) {
-            minTeam = team;
+            minTeam = team
         }
     }
 
-    return [...minTeam];
+    return [...minTeam]
 }
 
 function formTeams(teammatesPool: string[][], requiredSkills: string[]): Set<number>[] {
     if (requiredSkills.length === 0) {
-        return [new Set<number>()];
+        return [new Set<number>()]
     }
 
-    const teams: Set<number>[] = [];
-    const requiredSkill: string = requiredSkills[0];
-    const otherRequiredSkills: string[] = requiredSkills.slice(1);
-    const possibleTeammatesIds: number[] = findTeammatesWithSkill(teammatesPool, requiredSkill);
+    const teams: Set<number>[] = []
+    const requiredSkill: string = requiredSkills[0]
+    const otherRequiredSkills: string[] = requiredSkills.slice(1)
+    const possibleTeammatesIds: number[] = findTeammatesWithSkill(teammatesPool, requiredSkill)
     for (const teammateId of possibleTeammatesIds) {
-        teams.push(new Set<number>([teammateId]));
+        teams.push(new Set<number>([teammateId]))
     }
 
-    return fillTeams(teammatesPool, teams, otherRequiredSkills);
+    return fillTeams(teammatesPool, teams, otherRequiredSkills)
 }
 
 function fillTeams(teammatesPool: string[][], teams: Set<number>[], requiredSkills: string[]): Set<number>[] {
     if (requiredSkills.length === 0) {
-        return teams;
+        return teams
     }
 
-    const newTeams: Set<number>[] = [];
-    const requiredSkill: string = requiredSkills[0];
-    const otherRequiredSkills: string[] = requiredSkills.slice(1);
-    const possibleTeammatesIds: number[] = findTeammatesWithSkill(teammatesPool, requiredSkill);
+    const newTeams: Set<number>[] = []
+    const requiredSkill: string = requiredSkills[0]
+    const otherRequiredSkills: string[] = requiredSkills.slice(1)
+    const possibleTeammatesIds: number[] = findTeammatesWithSkill(teammatesPool, requiredSkill)
     for (const team of teams) {
-        let teamNeed: boolean = true;
+        let teamNeed: boolean = true
         for (const t of team) {
             if (teammatesPool[t].includes(requiredSkill)) {
-                teamNeed = false;
-                break;
+                teamNeed = false
+                break
             }
         }
 
         if (teamNeed) {
             for (const teammateId of possibleTeammatesIds) {
-                newTeams.push(new Set<number>([...team, teammateId]));
+                newTeams.push(new Set<number>([...team, teammateId]))
             }
         } else {
-            newTeams.push(team);
+            newTeams.push(team)
         }
     }
 
-    return fillTeams(teammatesPool, newTeams, otherRequiredSkills);
+    return fillTeams(teammatesPool, newTeams, otherRequiredSkills)
 }
 
 function findTeammatesWithSkill(teammatesPool: string[][], requiredSkill: string): number[] {
-    const team: number[] = [];
+    const team: number[] = []
 
     for (let i: number = 0; i < teammatesPool.length; i++) {
         if (teammatesPool[i].includes(requiredSkill)) {
-            team.push(i);
+            team.push(i)
         }
     }
 
-    return team;
+    return team
 }
 
 function skillsByPopularity(people: string[][], skills: string[]): string[] {
-    const counts: Map<string, number> = peopleHavingSkill(people);
+    const counts: Map<string, number> = peopleHavingSkill(people)
     return [...skills]
-        .toSorted((s1: string, s2: string): number => (counts.get(s1) ?? 0) - (counts.get(s2) ?? 0));
+        .toSorted((s1: string, s2: string): number => (counts.get(s1) ?? 0) - (counts.get(s2) ?? 0))
 }
 
 function peopleHavingSkill(people: string[][]): Map<string, number> {
-    const counts: Map<string, number> = new Map<string, number>();
+    const counts: Map<string, number> = new Map<string, number>()
 
     for (const skills of people) {
         for (const skill of skills) {
-            counts.set(skill, (counts.get(skill) ?? 0) + 1);
+            counts.set(skill, (counts.get(skill) ?? 0) + 1)
         }
     }
 
-    return counts;
+    return counts
 }
