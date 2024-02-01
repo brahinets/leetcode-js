3841f3bbe52d59185049cc6f7ed2822f62fc46ec
Yaroslav Brahinets <y.brahinets@gmail.com>
2024-02-01 10:54:27 +0200

    #1396 Clean up

--- solutions/1396. Design Underground System/script.ts
+++ solutions/1396. Design Underground System/script.ts
@@ -1,72 +1,72 @@
 export {UndergroundSystem}
 
 class UndergroundSystem {
-    private trips: Map<number, Trip[]>;
+    private trips: Map<number, Trip[]>
 
     constructor() {
-        this.trips = new Map<number, Trip[]>();
+        this.trips = new Map<number, Trip[]>()
     }
 
     checkIn(id: number, stationName: string, t: number): void {
-        const trips: Trip[] = this.trips.get(id) ?? [];
+        const trips: Trip[] = this.trips.get(id) ?? []
 
         for (const trip of trips) {
-            const tripIncomplete: boolean = !trip.end && !trip.endAt;
+            const tripIncomplete: boolean = !trip.end && !trip.endAt
 
             if (tripIncomplete) {
-                throw new Error("Client is already commuting");
+                throw new Error("Client is already commuting")
             }
         }
 
         trips.push(new Trip(stationName, t))
-        this.trips.set(id, trips);
+        this.trips.set(id, trips)
     }
 
     checkOut(id: number, stationName: string, t: number): void {
-        const trips: Trip[] = this.trips.get(id) ?? [];
+        const trips: Trip[] = this.trips.get(id) ?? []
 
         for (const trip of trips) {
-            const tripIncomplete: boolean = !trip.end && !trip.endAt;
+            const tripIncomplete: boolean = !trip.end && !trip.endAt
 
             if (tripIncomplete) {
-                trip.end = stationName;
-                trip.endAt = t;
-                return;
+                trip.end = stationName
+                trip.endAt = t
+                return
             }
         }
 
-        throw new Error("Client is not commuting");
+        throw new Error("Client is not commuting")
     }
 
     getAverageTime(startStation: string, endStation: string): number {
-        let totalDuration: number = 0;
-        let totalCount: number = 0;
+        let totalDuration: number = 0
+        let totalCount: number = 0
 
         for (const clientTrips of this.trips.values()) {
             for (const trip of clientTrips) {
                 if (trip.end && trip.endAt && trip.start === startStation && trip.end === endStation) {
-                    totalDuration += (trip.endAt - trip.startAt);
-                    totalCount++;
+                    totalDuration += (trip.endAt - trip.startAt)
+                    totalCount++
                 }
             }
         }
 
         if (totalCount === 0) {
-            return 0;
+            return 0
         }
 
-        return totalDuration / totalCount;
+        return totalDuration / totalCount
     }
 }
 
 class Trip {
-    readonly start: string;
-    readonly startAt: number;
-    end?: string;
-    endAt?: number;
+    readonly start: string
+    readonly startAt: number
+    end?: string
+    endAt?: number
 
     constructor(start: string, startAt: number) {
-        this.start = start;
-        this.startAt = startAt;
+        this.start = start
+        this.startAt = startAt
     }
 }
