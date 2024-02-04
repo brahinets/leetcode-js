071a9065c618c9e5104e6596667fce65624e279a
Yaroslav Brahinets <y.brahinets@gmail.com>
2024-02-04 10:59:41 +0200

    #76 Pull counts for memoization

--- solutions/76. Minimum Window Substring/script.ts
+++ solutions/76. Minimum Window Substring/script.ts
@@ -3,14 +3,15 @@ import {count} from "../../common/array-utils"
 export {minWindow}
 
 function minWindow(s: string, t: string): string {
-
     let min: string = ""
+    const required: Map<string, number> = count(t.split(""))
 
     for (let start: number = 0; start <= s.length - t.length; start++) {
         for (let end: number = start + t.length; end <= s.length + 1; end++) {
             const check: string = s.slice(start, end)
+            const present: Map<string, number> = count(check.split(""))
 
-            if ((min.length == 0 || check.length < min.length) && all(check, t)) {
+            if ((min.length == 0 || check.length < min.length) && all(required, present)) {
                 min = check
             }
         }
@@ -19,10 +20,7 @@ function minWindow(s: string, t: string): string {
     return min
 }
 
-function all(check: string, t: string): boolean {
-    const required: Map<string, number> = count(t.split(""))
-    const present: Map<string, number> = count(check.split(""))
-
+function all(required: Map<string, number>, present: Map<string, number>): boolean {
     for (const [letter, count] of required) {
         if (!present.has(letter)) {
             return false
