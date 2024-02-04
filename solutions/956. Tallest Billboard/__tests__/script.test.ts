78bb3372f19a7a8726e093bdeaf87a187c19d55e
Yaroslav Brahinets <y.brahinets@gmail.com>
2024-02-04 13:01:10 +0200

    #956 Clean up

--- solutions/956. Tallest Billboard/__tests__/script.test.ts
+++ solutions/956. Tallest Billboard/__tests__/script.test.ts
@@ -4,16 +4,16 @@ describe('956. Tallest Billboard', (): void => {
 
     it('Cannot build', (): void => {
         expect(tallestBillboard([1, 2]))
-            .toBe(0);
-    });
+            .toBe(0)
+    })
 
     it('Use some elements', (): void => {
         expect(tallestBillboard([1, 2, 3, 4, 5, 6]))
-            .toBe(10);
-    });
+            .toBe(10)
+    })
 
     it('Use all elements', (): void => {
         expect(tallestBillboard([1, 2, 3, 6]))
-            .toBe(6);
-    });
+            .toBe(6)
+    })
 })
