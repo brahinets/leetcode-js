5c3ddc09f1dec47739c618b1f8c2fdb65b244fe8
Yaroslav Brahinets <y.brahinets@gmail.com>
2024-02-09 09:55:54 +0200

    #368 Improve naming

--- solutions/368. Largest Divisible Subset/script.ts
+++ solutions/368. Largest Divisible Subset/script.ts
@@ -4,21 +4,21 @@ function largestDivisibleSubset(nums: number[]): number[] {
     nums.sort((a: number, b: number): number => a - b)
 
     let maxSubset: number[] = []
-    const memo: number[][] = []
+    const subsetsDivisibleByIndex: number[][] = []
 
     for (let i: number = nums.length - 1; i >= 0; i--) {
         let currentSubset: number[] = []
 
         for (let j: number = i + 1; j < nums.length; j++) {
             const goodPair: boolean = nums[i] % nums[j] === 0 || nums[j] % nums[i] === 0
-            if (goodPair && memo[j].length > currentSubset.length) {
-                currentSubset = memo[j]
+            if (goodPair && subsetsDivisibleByIndex[j].length > currentSubset.length) {
+                currentSubset = subsetsDivisibleByIndex[j]
             }
         }
 
-        memo[i] = [nums[i], ...currentSubset]
-        if (memo[i].length > maxSubset.length) {
-            maxSubset = memo[i]
+        subsetsDivisibleByIndex[i] = [nums[i], ...currentSubset]
+        if (subsetsDivisibleByIndex[i].length > maxSubset.length) {
+            maxSubset = subsetsDivisibleByIndex[i]
         }
     }
 
