df3641d40f9c35ee49ca352bf44035afe5a66515
Yaroslav Brahinets <y.brahinets@gmail.com>
2024-02-10 22:13:24 +0200

    #1802 Maximum Value at a Given Index in a Bounded Array

--- solutions/1802. Maximum Value at a Given Index in a Bounded Array/__tests__/script.test.ts
+++ solutions/1802. Maximum Value at a Given Index in a Bounded Array/__tests__/script.test.ts
@@ -1,7 +1,8 @@
 import {maxValue} from '../script'
 
 describe('1802. Maximum Value at a Given Index in a Bounded Array', (): void => {
-    it('Mirrored array', (): void => {
+
+    it('Few', (): void => {
         expect(maxValue(4, 2, 6))
             .toBe(2)
     })
--- solutions/1802. Maximum Value at a Given Index in a Bounded Array/script.ts
+++ solutions/1802. Maximum Value at a Given Index in a Bounded Array/script.ts
@@ -1,5 +1,15 @@
+import {arrayOf} from "../../common/array-factories"
+
 export {maxValue}
 
 function maxValue(n: number, index: number, maxSum: number): number {
+    let nums: number[] = arrayOf(1, n)
+    let sum: number = n
 
+    while (sum < maxSum && nums[index] < maxSum - sum) {
+        nums[index]++
+        sum++
+    }
+
+    return nums[index]
 }
