1456bcd8efc4a4a5dc5991c5be089e664710149a
Yaroslav Brahinets <y.brahinets@gmail.com>
2024-02-25 16:39:37 +0200

    #2709 Speed up, simplify traversal

--- solutions/2709. Greatest Common Divisor Traversal/script.ts
+++ solutions/2709. Greatest Common Divisor Traversal/script.ts
@@ -2,18 +2,9 @@ export {canTraverseAllPairs}
 
 function canTraverseAllPairs(nums: number[]): boolean {
     const graph: Graph = buildGraph(nums)
+    const visited: Set<number> = traverse(graph, 0, new Set<number>())
 
-    for (let i: number = 0; i < nums.length; i++) {
-        const reachable: Set<number> = graph.getReachableNodes(i)
-
-        for (let j: number = i + 1; j < nums.length; j++) {
-            if (!reachable.has(j)) {
-                return false
-            }
-        }
-    }
-
-    return true
+    return visited.size === nums.length
 }
 
 function buildGraph(nums: number[]): Graph {
@@ -41,6 +32,18 @@ function gcd(a: number, b: number): number {
     return gcd(b, a % b)
 }
 
+function traverse(graph: Graph, node: number, visited: Set<number>): Set<number> {
+    visited.add(node)
+
+    for (const neighbor of graph.getNeighbours(node)) {
+        if (!visited.has(neighbor)) {
+            traverse(graph, neighbor, visited)
+        }
+    }
+
+    return visited
+}
+
 class Graph {
     private readonly nodes: Map<number, Set<number>>
 
@@ -56,27 +59,7 @@ class Graph {
         this.nodes.get(from)!.add(to)
     }
 
-    getReachableNodes(start: number): Set<number> {
-        const visited: boolean[] = []
-        visited[start] = true
-
-        const queue: number[] = [start]
-
-        let front: number = 0
-        while (front < queue.length) {
-            const node: number = queue[front++]
-            const neighbours: Set<number> | undefined = this.nodes.get(node)
-
-            if (neighbours) {
-                for (const neighbour of neighbours) {
-                    if (!visited[neighbour]) {
-                        visited[neighbour] = true
-                        queue.push(neighbour)
-                    }
-                }
-            }
-        }
-
-        return new Set<number>(queue)
+    getNeighbours(node: number): Set<number> {
+        return this.nodes.get(node) ?? new Set<number>()
     }
 }
