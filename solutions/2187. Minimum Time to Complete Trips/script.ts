4ee53fa878989b893a799465d6baddba7c688891
Yaroslav Brahinets <y.brahinets@gmail.com>
2024-02-27 07:47:48 +0200

    #2187 Improve type-safety

--- solutions/2187. Minimum Time to Complete Trips/script.ts
+++ solutions/2187. Minimum Time to Complete Trips/script.ts
@@ -3,13 +3,15 @@ export {minimumTime}
 function minimumTime(time: number[], totalTrips: number): number {
     let left: number = 1
     let right: number = totalTrips * time
-        .reduce((prev: number, curr: number) => prev > curr ? prev : curr, time[0])
+        .reduce((prev: number, curr: number): number => prev > curr ? prev : curr, time[0])
 
     while (left < right) {
         const mid: number = Math.floor((left + right) / 2)
 
-        const tripsForTime = time.map(e => Math.floor(mid / e))
-            .reduce((s, e) => s + e, 0)
+        const tripsForTime: number = time
+            .map((t: number): number => Math.floor(mid / t))
+            .reduce((total: number, t: number) => total + t, 0)
+
         if (tripsForTime >= totalTrips) {
             right = mid
         } else {
