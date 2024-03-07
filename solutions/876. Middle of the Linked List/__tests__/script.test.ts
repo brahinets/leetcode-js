191f8d5eb260aeeae3ca20a9abd3d318d71fe1b3
Yaroslav Brahinets <y.brahinets@gmail.com>
2024-03-07 08:04:39 +0200

    #876 Simplify assertion

--- solutions/876. Middle of the Linked List/__tests__/script.test.ts
+++ solutions/876. Middle of the Linked List/__tests__/script.test.ts
@@ -5,11 +5,11 @@ import {linkedListOf} from "../../../common/list-factories"
 describe('876. Middle of the Linked List', (): void => {
     it('Middle of odd array length', (): void => {
         expect(middleNode(linkedListOf(1, 2, 3, 4, 5)))
-            .toStrictEqual(linkedListOf(3, 4, 5))
+            .toEqual(linkedListOf(3, 4, 5))
     })
 
     it('Middle of even array length', (): void => {
         expect(middleNode(linkedListOf(1, 2, 3, 4, 5, 6)))
-            .toStrictEqual(linkedListOf(4, 5, 6))
+            .toEqual(linkedListOf(4, 5, 6))
     })
 })
