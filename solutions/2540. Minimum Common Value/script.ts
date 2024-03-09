dd5f192ec1e8d71be943c533b5df73f9566250c1
Yaroslav Brahinets <y.brahinets@gmail.com>
2024-03-09 12:43:58 +0200

    #2540 Speed up using more optimal contains check

--- solutions/2540. Minimum Common Value/script.ts
+++ solutions/2540. Minimum Common Value/script.ts
@@ -1,8 +1,10 @@
 export {getCommon}
 
 function getCommon(nums1: number[], nums2: number[]): number {
+    const set2: Set<number> = new Set<number>(nums2)
+
     for (const num of nums1) {
-        if (nums2.includes(num)) {
+        if (set2.has(num)) {
             return num
         }
     }
