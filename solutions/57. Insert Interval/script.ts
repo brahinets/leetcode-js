e997e4fe56b07d007d7f0c78f983f87332b4915f
Yaroslav Brahinets <y.brahinets@gmail.com>
2024-03-17 10:18:50 +0200

    #57 Improve naming

--- solutions/57. Insert Interval/script.ts
+++ solutions/57. Insert Interval/script.ts
@@ -1,22 +1,22 @@
 export {insert}
 
 function insert(intervals: number[][], newInterval: number[]): number[][] {
-    let [start, end]: number[] = newInterval
+    let [newStart, newEnd]: number[] = newInterval
     const result: number[][] = []
     const total: number = intervals.length
 
     let i: number = 0
-    while (i < total && start > intervals[i][1]) {
+    while (i < total && newStart > intervals[i][1]) {
         result.push(intervals[i])
         i++
     }
 
-    while (i < total && end >= intervals[i][0]) {
-        start = Math.min(start, intervals[i][0])
-        end = Math.max(end, intervals[i][1])
+    while (i < total && newEnd >= intervals[i][0]) {
+        newStart = Math.min(newStart, intervals[i][0])
+        newEnd = Math.max(newEnd, intervals[i][1])
         i++
     }
-    result.push([start, end])
+    result.push([newStart, newEnd])
 
     while (i < total) {
         result.push(intervals[i])
