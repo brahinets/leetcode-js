571170eca39103cd28a0629f6e489831c5b75721
Yaroslav Brahinets <y.brahinets@gmail.com>
2024-03-23 08:17:35 +0200

    Cover by test list halving

--- common/__tests__/list-utils.test.ts
+++ common/__tests__/list-utils.test.ts
@@ -1,4 +1,4 @@
-import {linkedListToArray, ListNode, reverseLinkedList} from '../list-utils'
+import {linkedListMiddleNode, linkedListToArray, ListNode, reverseLinkedList} from '../list-utils'
 import {linkedListOf} from "../list-factories"
 
 describe('Linked List Factory', (): void => {
@@ -31,6 +31,28 @@ describe('Linked List To Array', (): void => {
     it('Non-empty list to array', (): void => {
         expect(linkedListToArray(linkedListOf(1, 2, 3)))
             .toStrictEqual([1, 2, 3])
+    })
+})
+
+describe('Linked List Middle Node', (): void => {
+    it('Middle node for null', (): void => {
+        expect(linkedListMiddleNode(null))
+            .toStrictEqual(null)
+    })
+
+    it('Middle node for singleton', (): void => {
+        expect(linkedListMiddleNode(linkedListOf(1)))
+            .toStrictEqual(new ListNode(1))
+    })
+
+    it('Middle node for odd size', (): void => {
+        expect(linkedListMiddleNode(linkedListOf(1, 2, 3)))
+            .toStrictEqual(new ListNode(2, new ListNode(3)))
+    })
+
+    it('Middle node for even size', (): void => {
+        expect(linkedListMiddleNode(linkedListOf(1, 2, 3, 4)))
+            .toStrictEqual(new ListNode(3, new ListNode(4)))
     })
 })
 
