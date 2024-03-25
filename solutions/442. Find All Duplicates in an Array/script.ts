940993799ae2cf612f6fff04bc0ddb62f187dfe8
Yaroslav Brahinets <y.brahinets@gmail.com>
2024-03-25 10:26:25 +0200

    #442 Speed up relying on number od duplicates and values range

--- solutions/442. Find All Duplicates in an Array/script.ts
+++ solutions/442. Find All Duplicates in an Array/script.ts
@@ -1,10 +1,17 @@
-import {count} from "../../common/array-utils"
-
 export {findDuplicates}
 
 function findDuplicates(nums: number[]): number[] {
-    return [...count(nums).entries()]
-        .filter(([, count]: number[]): boolean => count > 1)
-        .map(([value,]: number[]): number => value)
-        .sort((a: number, b: number): number => a - b)
+    const duplicates: number[] = []
+
+    for (let i: number = 0; i < nums.length; i++) {
+        const index: number = Math.abs(nums[i]) - 1
+
+        if (nums[index] < 0) {
+            duplicates.push(index + 1)
+        }
+
+        nums[index] = -nums[index]
+    }
+
+    return duplicates
 }
