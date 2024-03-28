991d62531ab35f0e687ae6dbf8ab50c72302360b
Yaroslav Brahinets <y.brahinets@gmail.com>
2024-03-28 21:51:09 +0200

    #2958 Add more test

--- solutions/2958. Length of Longest Subarray With at Most K Frequency/script.ts
+++ solutions/2958. Length of Longest Subarray With at Most K Frequency/script.ts
@@ -1,26 +1,28 @@
-import {count} from "../../common/array-utils"
-
 export {maxSubarrayLength}
 
 function maxSubarrayLength(nums: number[], k: number): number {
+    const frequencies: Map<number, number> = new Map<number, number>()
     let max: number = 0
     let left: number = 0
     let right: number = 0
-    while (right <= nums.length) {
-        let freq: number = maxFrequency(count(nums.slice(left, right)))
+    while (right < nums.length) {
+        frequencies.set(nums[right], (frequencies.get(nums[right]) ?? 0) + 1)
+
+        let freq: number = maxFrequency(frequencies)
         while (freq > k) {
+            frequencies.set(nums[left], frequencies.get(nums[left])! - 1)
+            freq = maxFrequency(frequencies)
             left++
-            freq = maxFrequency(count(nums.slice(left, right)))
         }
 
-        max = Math.max(max, right - left)
+        max = Math.max(max, right - left + 1)
         right++
     }
 
     return max
 }
 
-function maxFrequency(frequencies: Map<number, number>):number {
+function maxFrequency(frequencies: Map<number, number>): number {
     let maxFrequency: number = 0
 
     for (const frequency of frequencies.values()) {
