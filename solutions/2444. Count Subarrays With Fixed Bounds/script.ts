3950c76f54c4189cfc0372dd9d9313a61a0d12ef
Yaroslav Brahinets <y.brahinets@gmail.com>
2024-03-31 10:15:18 +0300

    #2444 Improve readability

--- solutions/2444. Count Subarrays With Fixed Bounds/script.ts
+++ solutions/2444. Count Subarrays With Fixed Bounds/script.ts
@@ -7,21 +7,23 @@ function countSubarrays(nums: number[], minK: number, maxK: number): number {
 
     let left: number = 0
     for (let right: number = 0; right < nums.length; right++) {
-        if (nums[right] < minK || nums[right] > maxK) {
+        let windowEnd: number = nums[right]
+
+        if (windowEnd < minK || windowEnd > maxK) {
             min = undefined
             max = undefined
             left = right + 1
         }
 
-        if (nums[right] === minK) {
+        if (windowEnd === minK) {
             min = right
         }
-        if (nums[right] === maxK) {
+        if (windowEnd === maxK) {
             max = right
         }
 
         if (min !== undefined && max !== undefined) {
-            count += (Math.min(min, max) + 1 - left)
+            count += Math.min(min, max) + 1 - left
         }
     }
 
