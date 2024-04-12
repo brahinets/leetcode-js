b324fea41e488c1d3fb3a245d3bc4c4c43f77ed2
Yaroslav Brahinets <y.brahinets@gmail.com>
2024-04-12 09:28:48 +0300

    #42 Speed up by reusing extremum

--- solutions/42. Trapping Rain Water/script.ts
+++ solutions/42. Trapping Rain Water/script.ts
@@ -1,16 +1,43 @@
+import {arrayOfZeros} from "../../common/array-factories"
+
 export {trap}
 
 function trap(height: number[]): number {
+    const leftBorderByPosition: number[] = getLeftBorderByPosition(height)
+    const rightBorderByPosition: number[] = getRightBorderByPosition(height)
+
     let water: number = 0
+    for (let i: number = 0; i < height.length; i++) {
+        const minHeight: number = Math.min(leftBorderByPosition[i], rightBorderByPosition[i])
 
-    for (let i: number = 1; i < height.length - 1; i++) {
-        const leftBorderHeight: number = Math.max(...height.slice(0, i))
-        const rightBorderHeight: number = Math.max(...height.slice(i + 1))
-
-        if (leftBorderHeight > height[i] && rightBorderHeight > height[i]) {
-            water += Math.min(leftBorderHeight, rightBorderHeight) - height[i]
+        if (minHeight > height[i]) {
+            water += minHeight - height[i]
         }
     }
 
     return water
+}
+
+function getRightBorderByPosition(height: number[]): number[] {
+    let borders: number[] = arrayOfZeros(height.length)
+
+    let maxLeft: number = 0
+    for (let i: number = 0; i < height.length; i++) {
+        borders[i] = maxLeft
+        maxLeft = Math.max(maxLeft, height[i])
+    }
+
+    return borders
+}
+
+function getLeftBorderByPosition(height: number[]): number[] {
+    let borders: number[] = arrayOfZeros(height.length)
+
+    let maxRight: number = 0
+    for (let i: number = height.length - 1; i >= 0; i--) {
+        borders[i] = maxRight
+        maxRight = Math.max(maxRight, height[i])
+    }
+
+    return borders
 }
