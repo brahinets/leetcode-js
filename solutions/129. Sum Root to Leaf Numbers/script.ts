d2dbf968f165c658ed0c4892340198f21674704e
Yaroslav Brahinets <y.brahinets@gmail.com>
2024-04-15 09:15:24 +0300

    #129 Optimize calculation on the fly

--- solutions/129. Sum Root to Leaf Numbers/script.ts
+++ solutions/129. Sum Root to Leaf Numbers/script.ts
@@ -2,26 +2,29 @@ import {TreeNode} from '../../common/TreeNode'
 
 export {sumNumbers, TreeNode}
 
-function sumNumbers(root: TreeNode): number {
-    return collectPaths(root, "")
-        .map((num: string) => Number(num))
-        .reduce((num: number, sum: number) => num + sum, 0)
+function sumNumbers(root: TreeNode | null): number {
+    if (!root) {
+        return 0
+    }
+
+    return collectPath(root, 0)
 }
 
-function collectPaths(node: TreeNode, parentPath: string): string [] {
-    const paths: string[] = []
+function collectPath(node: TreeNode, parentSum: number): number {
+    const currentSum: number = parentSum * 10 + node.val
 
+    if (!node.left && !node.right) {
+        return currentSum
+    }
+
+    let totalSum: number = 0
     if (node.left) {
-        paths.push(...collectPaths(node.left, parentPath + node.val))
+        totalSum += collectPath(node.left, currentSum)
     }
 
     if (node.right) {
-        paths.push(...collectPaths(node.right, parentPath + node.val))
+        totalSum += collectPath(node.right, currentSum)
     }
 
-    if (!node.left && !node.right) {
-        paths.push(parentPath + node.val)
-    }
-
-    return paths
+    return totalSum
 }
