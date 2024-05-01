26986a95acbb6145bdf73c0012a8f92ff50389b5
Yaroslav Brahinets <y.brahinets@gmail.com>
2024-05-01 12:33:21 +0300

    #2000 Optimize solution

--- solutions/2000. Reverse Prefix of Word/script.ts
+++ solutions/2000. Reverse Prefix of Word/script.ts
@@ -1,15 +1,19 @@
 export {reversePrefix}
 
 function reversePrefix(word: string, ch: string): string {
-    const index: number = word.indexOf(ch)
+    let result: string = ""
+    let found: boolean = false
 
-    if (index === -1) {
-        return word
-    } else {
-        return reverse(word.slice(0, index + 1)) + word.slice(index + 1)
+    for (const char of word) {
+        if (!found) {
+            if(ch === char) {
+                found = true
+            }
+            result = char + result
+        } else {
+            result = result + char
+        }
     }
-}
 
-function reverse(string: string): string {
-    return string.split('').reverse().join('')
+    return found ? result : word
 }
