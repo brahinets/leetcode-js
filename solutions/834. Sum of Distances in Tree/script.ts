c4864ead84c6e1b23a3a2bab0f58fc4f0f1c4c6f
Yaroslav Brahinets <y.brahinets@gmail.com>
2024-05-01 18:22:15 +0300

    #834 Reuse code

--- solutions/834. Sum of Distances in Tree/script.ts
+++ solutions/834. Sum of Distances in Tree/script.ts
@@ -1,15 +1,17 @@
 import {arrayOfZeros} from "../../common/array-factories"
+import {UndirectedUnweightedGraph} from "../../common/Graph"
+import {undirectedUnweightedGraphFromEdges} from "../../common/graph-factories"
 
 export {sumOfDistancesInTree}
 
 function sumOfDistancesInTree(n: number, edges: number[][]): number[] {
-    const graph: UndirectedUnweightedGraph = new UndirectedUnweightedGraph(n, edges)
+    const graph: UndirectedUnweightedGraph = undirectedUnweightedGraphFromEdges(edges)
     const result: number[] = arrayOfZeros(n)
     const subtreeSizes: number[] = arrayOfZeros(n)
 
     const dfs1 = (node: number, parent: number): void => {
         subtreeSizes[node] = 1
-        for (const neighbor of graph.getNeighbors(node)) {
+        for (const neighbor of graph.getNeighbours(node).keys()) {
             if (neighbor !== parent) {
                 dfs1(neighbor, node)
                 subtreeSizes[node] += subtreeSizes[neighbor]
@@ -19,7 +21,7 @@ function sumOfDistancesInTree(n: number, edges: number[][]): number[] {
     }
 
     const dfs2 = (node: number, parent: number): void => {
-        for (const neighbor of graph.getNeighbors(node)) {
+        for (const neighbor of graph.getNeighbours(node).keys()) {
             if (neighbor !== parent) {
                 result[neighbor] = result[node] + (n - subtreeSizes[neighbor]) - subtreeSizes[neighbor]
                 dfs2(neighbor, node)
@@ -31,31 +33,4 @@ function sumOfDistancesInTree(n: number, edges: number[][]): number[] {
     dfs2(0, -1)
 
     return result
-}
-
-class UndirectedUnweightedGraph {
-    readonly nodes: Map<number, Set<number>>
-
-    constructor(n: number, edges: number[][]) {
-        this.nodes = new Map<number, Set<number>>()
-        for (const [a, b] of edges) {
-            this.addEdge(a, b)
-        }
-    }
-
-    addEdge(from: number, to: number): void {
-        if (!this.nodes.has(from)) {
-            this.nodes.set(from, new Set<number>())
-        }
-        this.nodes.get(from)!.add(to)
-
-        if (!this.nodes.has(to)) {
-            this.nodes.set(to, new Set<number>())
-        }
-        this.nodes.get(to)!.add(from)
-    }
-
-    getNeighbors(node: number): Set<number> {
-        return this.nodes.get(node) ?? new Set<number>()
-    }
 }
