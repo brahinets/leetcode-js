e59383f2772a5d6f6a885e7d8a3494c2df6ad5a1
Yaroslav Brahinets <y.brahinets@gmail.com>
2024-05-02 09:13:13 +0300

    #2441 Decrease code duplication

--- solutions/2441. Largest Positive Integer That Exists With Its Negative/script.ts
+++ solutions/2441. Largest Positive Integer That Exists With Its Negative/script.ts
@@ -4,18 +4,14 @@ function findMaxK(nums: number[]): number {
     let k: number = -1
     const count: Set<number> = new Set<number>()
 
-    for (const n of nums) {
-        if (n > 0) {
-            if (count.has(-n)) {
-                k = Math.max(n, k)
-            }
-        } else {
-            if (count.has(Math.abs(n))) {
-                k = Math.max(Math.abs(n), k)
-            }
+    for (const num of nums) {
+        const abs: number = Math.abs(num);
+
+        if (abs > k && count.has(-num)) {
+            k = abs
         }
 
-        count.add(n)
+        count.add(num)
     }
 
     return k
