ac07657427ffcb2d7311548841699b8dcbe7ac8c
Yaroslav Brahinets <y.brahinets@gmail.com>
2024-05-12 13:04:33 +0300

    #2373 Optimize memory consumption

--- solutions/2373. Largest Local Values in a Matrix/script.ts
+++ solutions/2373. Largest Local Values in a Matrix/script.ts
@@ -7,15 +7,25 @@ function largestLocal(grid: number[][]): number[][] {
         const row: number[] = []
 
         for (let j: number = 1; j < grid[i].length - 1; j++) {
-            row.push(Math.max(
-                ...grid[i - 1].slice(j - 1, j + 1 + 1),
-                ...grid[i].slice(j - 1, j + 1 + 1),
-                ...grid[i + 1].slice(j - 1, j + 1 + 1)
-            ))
+            row.push(max(grid, i - 1, i + 1, j - 1, j + 1))
         }
 
         result.push(row)
     }
 
     return result
+}
+
+function max(grid: number[][], rowStart: number, rowEnd: number, colStart: number, colEnd: number): number {
+    let max: number = Number.MIN_SAFE_INTEGER
+
+    for (let i: number = rowStart; i <= rowEnd; i++) {
+        for (let j: number = colStart; j <= colEnd; j++) {
+            if (grid[i][j] > max) {
+                max = grid[i][j]
+            }
+        }
+    }
+
+    return max
 }
