886270cf145f5b36832874c6905c7106ec8fa72d
Yaroslav Brahinets <y.brahinets@gmail.com>
2024-05-21 09:45:58 +0300

    #1863 Reuse subsets code

--- solutions/1863. Sum of All Subset XOR Totals/script.ts
+++ solutions/1863. Sum of All Subset XOR Totals/script.ts
@@ -1,27 +1,13 @@
+import {subsets} from "../78. Subsets/script"
+
 export {subsetXORSum}
 
 function subsetXORSum(nums: number[]): number {
     let result: number = 0
 
-    for (const subset of getAllSubsets(nums)) {
+    for (const subset of subsets(nums)) {
         result += subset.reduce((xor: number, val: number): number => xor ^ val, 0)
     }
 
     return result
-}
-
-function getAllSubsets(nums: number[]): number[][] {
-    const subsets: number[][] = [[]]
-
-    for (const num of nums) {
-        const newSubsets: number[][] = []
-
-        for (const subset of subsets) {
-            newSubsets.push([...subset, num])
-        }
-
-        subsets.push(...newSubsets)
-    }
-
-    return subsets
 }
