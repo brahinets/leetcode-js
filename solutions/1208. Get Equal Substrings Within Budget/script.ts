132aab80222fd241626fef7533795e8b9574beca
Yaroslav Brahinets <y.brahinets@gmail.com>
2024-05-28 09:25:46 +0300

    #1208 Improve readability

--- solutions/1208. Get Equal Substrings Within Budget/script.ts
+++ solutions/1208. Get Equal Substrings Within Budget/script.ts
@@ -1,12 +1,9 @@
 export {equalSubstring}
 
 function equalSubstring(s: string, t: string, maxCost: number): number {
-    const costs: number[] = []
-    for (let i = 0; i < s.length; i++) {
-        costs.push(Math.abs(s[i].charCodeAt(0) - t[i].charCodeAt(0)))
-    }
+    const costs: number[] = calculateCosts(s, t)
 
-    let max: number = 0
+    let maxLength: number = 0
     for (let start = 0; start < s.length; start++) {
         let availableCosts: number = maxCost
 
@@ -16,8 +13,18 @@ function equalSubstring(s: string, t: string, maxCost: number): number {
             end++
         }
 
-        max = Math.max(end - start, max)
+        maxLength = Math.max(end - start, maxLength)
     }
 
-    return max
+    return maxLength
+}
+
+function calculateCosts(s: string, t: string): number[] {
+    const costs: number[] = []
+
+    for (let i = 0; i < s.length; i++) {
+        costs.push(Math.abs(s[i].charCodeAt(0) - t[i].charCodeAt(0)))
+    }
+
+    return costs
 }
