1c1fb1ed47b4976b358e02fb23119dd551b21249
Yaroslav Brahinets <y.brahinets@gmail.com>
2024-05-30 10:04:42 +0300

    #1442 Optimize by going in one iteration

--- solutions/1442. Count Triplets That Can Form Two Arrays of Equal XOR/script.ts
+++ solutions/1442. Count Triplets That Can Form Two Arrays of Equal XOR/script.ts
@@ -4,28 +4,20 @@ function countTriplets(arr: number[]): number {
     let count: number = 0
 
     for (let i = 0; i < arr.length; i++) {
+        let xorStart: number = 0
+
         for (let j = i + 1; j < arr.length; j++) {
+            xorStart ^= arr[j - 1]
+
+            let xorEnd: number = 0
             for (let k = j; k < arr.length; k++) {
-                if (valid(arr, i, j, k + 1)) {
+                xorEnd ^= arr[k]
+
+                if (xorStart === xorEnd)
                     count++
-                }
             }
         }
     }
 
     return count
-}
-
-function valid(array: number[], start: number, mid: number, end: number): boolean {
-    return xor(array, start, mid) === xor(array, mid, end)
-}
-
-function xor(nums: number[], start: number, end: number): number {
-    let result: number = 0
-
-    for (let i = start; i < end; i++) {
-        result ^= nums[i]
-    }
-
-    return result
 }
