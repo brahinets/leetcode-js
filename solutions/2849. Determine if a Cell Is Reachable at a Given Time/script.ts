adb8ff642eb5edba8ff2c297155f0a29469b6abf
Yaroslav Brahinets <y.brahinets@gmail.com>
2024-06-02 10:07:35 +0300

    #2849 Cannot stay should turn

--- solutions/2849. Determine if a Cell Is Reachable at a Given Time/script.ts
+++ solutions/2849. Determine if a Cell Is Reachable at a Given Time/script.ts
@@ -3,7 +3,11 @@ export {isReachableAtTime}
 function isReachableAtTime(sx: number, sy: number, fx: number, fy: number, t: number): boolean {
     const x: number = Math.abs(sx - fx)
     const y: number = Math.abs(sy - fy)
-    const shortestPath: number = Math.max(x, y)
 
+    if (t === 1 && x === 0 && y === 0) {
+        return false
+    }
+
+    const shortestPath: number = Math.max(x, y)
     return t >= shortestPath
 }
