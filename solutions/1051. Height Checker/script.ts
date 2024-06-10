d757304c8a859de9ee4c7ee478a01fa4b35dad79
Yaroslav Brahinets <y.brahinets@gmail.com>
2024-06-10 09:27:19 +0300

    #1051 Support earlier ECMAScript runtime on site

--- solutions/1051. Height Checker/script.ts
+++ solutions/1051. Height Checker/script.ts
@@ -1,7 +1,7 @@
 export {heightChecker}
 
 function heightChecker(heights: number[]): number {
-    const validHeights: number[] = heights.toSorted((a: number, b: number) => a - b)
+    const validHeights: number[] = [...heights].sort((a: number, b: number) => a - b)
 
     let count: number = 0
     for (let i = 0; i < validHeights.length; i++) {
