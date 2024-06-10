48ede8ab86dd9b8b517144045e36d3a586ccb998
Yaroslav Brahinets <y.brahinets@gmail.com>
2024-06-10 09:24:19 +0300

    #1051 Height Checker

---
+++ solutions/1051. Height Checker/script.ts
@@ -0,0 +1,14 @@
+export {heightChecker}
+
+function heightChecker(heights: number[]): number {
+    const validHeights: number[] = heights.toSorted((a: number, b: number) => a - b)
+
+    let count: number = 0
+    for (let i = 0; i < validHeights.length; i++) {
+        if (validHeights[i] !== heights[i]) {
+            count++
+        }
+    }
+
+    return count
+}
