b22af482432b5c3c6ce6831b122560aa555f0c9d
Yaroslav Brahinets <y.brahinets@gmail.com>
2024-06-11 21:08:36 +0300

    #1122 Speed up using index map

--- solutions/1122. Relative Sort Array/script.ts
+++ solutions/1122. Relative Sort Array/script.ts
@@ -1,9 +1,11 @@
 export {relativeSortArray}
 
 function relativeSortArray(arr1: number[], arr2: number[]): number[] {
+    const relativity: Map<number, number> = buildRelativity(arr2)
+
     return arr1.sort((first: number, second: number): number => {
-        const indexFirst: number = arr2.indexOf(first)
-        const indexSecond: number = arr2.indexOf(second)
+        const indexFirst: number = relativity.get(first) ?? -1
+        const indexSecond: number = relativity.get(second) ?? -1
 
         if (indexFirst !== -1 && indexSecond !== -1) {
             return indexFirst - indexSecond
@@ -15,4 +17,14 @@ function relativeSortArray(arr1: number[], arr2: number[]): number[] {
             return first - second
         }
     })
+}
+
+function buildRelativity(numbers: number[]): Map<number, number> {
+    const relativity: Map<number, number> = new Map<number, number>()
+
+    for (let i = 0; i < numbers.length; i++) {
+        relativity.set(numbers[i], i)
+    }
+
+    return relativity
 }
