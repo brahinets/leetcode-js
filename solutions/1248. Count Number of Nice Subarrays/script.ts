e1b7b0ce46d1aedd9290ff4c32ab9c93e3a3c7fa
Yaroslav Brahinets <y.brahinets@gmail.com>
2024-06-22 14:36:15 +0300

    #1248 Improve readability

--- solutions/1248. Count Number of Nice Subarrays/script.ts
+++ solutions/1248. Count Number of Nice Subarrays/script.ts
@@ -4,11 +4,11 @@ function numberOfSubarrays(nums: number[], k: number): number {
     let odds: number = 0
     let subArrays: number = 0
 
-    const prefixSum: Map<number,number> = new Map<number,number>([[0,1]])
+    const prefixSum: Map<number, number> = new Map<number, number>([[0, 1]])
     for (let i = 0; i < nums.length; i++) {
-        odds += (nums[i] % 2 === 1 ? 1 : 0)
+        odds += isOdd(nums, i) ? 1 : 0
 
-        if(prefixSum.has(odds - k)) {
+        if (prefixSum.has(odds - k)) {
             subArrays += prefixSum.get(odds - k)!
         }
 
@@ -16,4 +16,8 @@ function numberOfSubarrays(nums: number[], k: number): number {
     }
 
     return subArrays
+}
+
+function isOdd(nums: number[], i: number): boolean {
+    return nums[i] % 2 === 1
 }
