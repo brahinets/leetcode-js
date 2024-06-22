7b06d12a060c8f891e6e5f989c283a6df7062cd1
Yaroslav Brahinets <y.brahinets@gmail.com>
2024-06-22 14:47:04 +0300

    #560 Speed up

--- solutions/560. Subarray Sum Equals K/script.ts
+++ solutions/560. Subarray Sum Equals K/script.ts
@@ -1,18 +1,18 @@
 export {subarraySum}
 
 function subarraySum(nums: number[], k: number): number {
+    let sum: number = 0
     let subArrays: number = 0
 
-    for (let start = 0; start < nums.length; start++) {
-        let sum: number = 0
+    const prefixSum: Map<number, number> = new Map<number, number>([[0, 1]])
+    for (let i = 0; i < nums.length; i++) {
+        sum += nums[i]
 
-        for (let end = start; end < nums.length; end++) {
-            sum += nums[end]
-
-            if (sum === k) {
-                subArrays++
-            }
+        if (prefixSum.has(sum - k)) {
+            subArrays += prefixSum.get(sum - k)!
         }
+
+        prefixSum.set(sum, (prefixSum.get(sum) ?? 0) + 1)
     }
 
     return subArrays
