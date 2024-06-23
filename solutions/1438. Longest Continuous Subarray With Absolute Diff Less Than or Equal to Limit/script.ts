d3466ad59df76def8849565e8119e38265ff95ea
Yaroslav Brahinets <y.brahinets@gmail.com>
2024-06-23 18:28:31 +0300

    #1438 Speed up

--- solutions/1438. Longest Continuous Subarray With Absolute Diff Less Than or Equal to Limit/script.ts
+++ solutions/1438. Longest Continuous Subarray With Absolute Diff Less Than or Equal to Limit/script.ts
@@ -2,21 +2,32 @@ export {longestSubarray}
 
 function longestSubarray(nums: number[], limit: number): number {
     let maxLength: number = 0
+    let start: number = 0
 
-    for (let start = 0; start < nums.length; start++) {
-        let max: number = nums[start]
-        let min: number = nums[start]
-
-        for (let end = start; end <= nums.length; end++) {
-            min = Math.min(min, nums[end])
-            max = Math.max(max, nums[end])
-
-            if (max - min <= limit) {
-                maxLength = Math.max(maxLength, end - start + 1)
-            } else {
-                break
-            }
+    const max: number[] = []
+    const min: number[] = []
+    for (let end = 0; end < nums.length; end++) {
+        while (max.length > 0 && nums[end] > nums[max[max.length - 1]]) {
+            max.pop()
         }
+        max.push(end)
+
+        while (min.length > 0 && nums[end] < nums[min[min.length - 1]]) {
+            min.pop()
+        }
+        min.push(end)
+
+        while (nums[max[0]] - nums[min[0]] > limit) {
+            if (max[0] === start) {
+                max.shift()
+            }
+            if (min[0] === start) {
+                min.shift()
+            }
+            start++
+        }
+
+        maxLength = Math.max(maxLength, end - start + 1)
     }
 
     return maxLength
