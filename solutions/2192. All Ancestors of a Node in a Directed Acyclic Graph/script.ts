1ccc3045ac68cb44b611483b8afa1432b5811530
Yaroslav Brahinets <y.brahinets@gmail.com>
2024-06-29 22:25:39 +0300

    #2192 Speed up using reversed graph

--- solutions/2192. All Ancestors of a Node in a Directed Acyclic Graph/script.ts
+++ solutions/2192. All Ancestors of a Node in a Directed Acyclic Graph/script.ts
@@ -4,27 +4,44 @@ import {arrayOf} from "../../common/array-factories"
 export {getAncestors}
 
 function getAncestors(n: number, edges: number[][]): number[][] {
-    const graph: DirectedUnweightedGraph = buildGraph(edges)
+    const transposedGraph: DirectedUnweightedGraph = buildTransposedGraph(edges)
 
     const answer: number[][] = arrayOf([], n)
-    for (let source: number = 0; source < n; source++) {
-        for (let target: number = 0; target < n; target++) {
-            if (source !== target) {
-                if (graph.pathExist(target, source)) {
-                    answer[source].push(target)
-                }
-            }
-        }
+    for (let node: number = 0; node < n; node++) {
+        answer[node] = collectAncestors(transposedGraph, node, node, arrayOf(false, n))
+            .sort((a: number, b: number): number => a - b)
     }
 
     return answer
 }
 
-function buildGraph(edges: number[][]): DirectedUnweightedGraph {
+function collectAncestors(
+    graph: DirectedUnweightedGraph,
+    current: number,
+    original: number,
+    visited: boolean[]
+): number[] {
+    visited[current] = true
+
+    let ancestors: number[] = []
+    if (current !== original) {
+        ancestors.push(current)
+    }
+
+    for (const [neighbor] of graph.getNeighbours(current)) {
+        if (!visited[neighbor]) {
+            ancestors = ancestors.concat(collectAncestors(graph, neighbor, original, visited))
+        }
+    }
+
+    return ancestors
+}
+
+function buildTransposedGraph(edges: number[][]): DirectedUnweightedGraph {
     const graph: DirectedUnweightedGraph = new DirectedUnweightedGraph()
 
     for (const [from, to] of edges) {
-        graph.addEdge(from, to)
+        graph.addEdge(to, from)
     }
 
     return graph
