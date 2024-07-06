a85c14f4c79920c6a0c9547d985f62e23987b54d
Yaroslav Brahinets <y.brahinets@gmail.com>
2024-07-06 16:27:25 +0300

    #2582 Clean up

--- solutions/2582. Pass the Pillow/script.ts
+++ solutions/2582. Pass the Pillow/script.ts
@@ -4,8 +4,8 @@ function passThePillow(n: number, time: number): number {
     const passes: number = Math.floor(time / n)
 
     if (passes % 2 === 0) {
-        return (time) % n + 1
+        return time % n + 1
     } else {
-        return n - (time) % n - 1
+        return n - time % n - 1
     }
 }
