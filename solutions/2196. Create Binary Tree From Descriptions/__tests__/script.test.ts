95f2bfa9bb40b8c23b6dca3581561c7dff2c8dbe
Yaroslav Brahinets <y.brahinets@gmail.com>
2024-07-15 17:34:54 +0300

    #2196 Create Binary Tree From Descriptions

---
+++ solutions/2196. Create Binary Tree From Descriptions/__tests__/script.test.ts
@@ -0,0 +1,15 @@
+import {createBinaryTree} from '../script'
+import {arrayToTree} from "../../../common/tree-utils";
+
+describe('2196. Create Binary Tree From Descriptions', (): void => {
+
+    it('Almost full tree', (): void => {
+        expect(createBinaryTree([[20, 15, 1], [20, 17, 0], [50, 20, 1], [50, 80, 0], [80, 19, 1]]))
+            .toEqual(arrayToTree([50, 20, 80, 15, 17, 19]))
+    })
+
+    it('Lefty tree', (): void => {
+        expect(createBinaryTree([[1, 2, 1], [2, 3, 0], [3, 4, 1]]))
+            .toEqual(arrayToTree([1, 2, null, null, 3, 4]))
+    })
+})
