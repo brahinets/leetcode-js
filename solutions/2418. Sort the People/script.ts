627a7c02df1b878bf95f4045a3113bdf35200f33
Yaroslav Brahinets <y.brahinets@gmail.com>
2024-07-22 08:42:13 +0300

    #2418 Clean up

--- solutions/2418. Sort the People/script.ts
+++ solutions/2418. Sort the People/script.ts
@@ -1,18 +1,15 @@
 export {sortPeople}
 
 function sortPeople(names: string[], heights: number[]): string[] {
-    const people: Person[] = names
+    return names
         .map((name: string, index: number) => new Person(name, heights[index]))
+        .sort((a: Person, b: Person): number => {
+            if (a.height !== b.height) {
+                return b.height - a.height
+            }
 
-    people.sort((a: Person, b: Person): number => {
-        if (a.height !== b.height) {
-            return b.height - a.height
-        }
-
-        return a.name.localeCompare(b.name)
-    })
-
-    return people.map((person: Person): string => person.name)
+            return a.name.localeCompare(b.name)
+        }).map((person: Person): string => person.name)
 }
 
 class Person {
