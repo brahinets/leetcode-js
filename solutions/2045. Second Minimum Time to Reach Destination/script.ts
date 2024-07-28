1d29997d05d78a733bbe54941da255cbac74515a
Yaroslav Brahinets <y.brahinets@gmail.com>
2024-07-28 08:29:33 +0300

    #2045 Speed up by inserting in sorted manner

--- solutions/2045. Second Minimum Time to Reach Destination/script.ts
+++ solutions/2045. Second Minimum Time to Reach Destination/script.ts
@@ -1,5 +1,5 @@
 import {UndirectedUnweightedGraph} from "../../common/Graph"
-import {matrixOf} from "../../common/array-factories"
+import {matrixOf} from "../../common/array-factories";
 
 export {secondMinimum}
 
@@ -12,6 +12,9 @@ function secondMinimum(n: number, edges: number[][], time: number, change: numbe
 
     while (pq.length) {
         const [currentTime, u]: [number, number] = pq.shift()!
+        if (u === n && dist[n][1] !== Number.MAX_SAFE_INTEGER) {
+            return dist[n][1]
+        }
 
         for (const v of graph.getNeighbours(u).keys()) {
             let nextTime: number = currentTime
@@ -24,14 +27,12 @@ function secondMinimum(n: number, edges: number[][], time: number, change: numbe
             if (nextTime < dist[v][0]) {
                 dist[v][1] = dist[v][0]
                 dist[v][0] = nextTime
-                pq.push([nextTime, v])
+                insertSorted(pq, [nextTime, v])
             } else if (nextTime > dist[v][0] && nextTime < dist[v][1]) {
                 dist[v][1] = nextTime
-                pq.push([nextTime, v])
+                insertSorted(pq, [nextTime, v])
             }
         }
-
-        pq.sort((a: number[], b: number[]): number => a[0] - b[0])
     }
 
     return dist[n][1] === Number.MAX_SAFE_INTEGER ? -1 : dist[n][1]
@@ -45,4 +46,14 @@ function createGraph(edges: number[][]): UndirectedUnweightedGraph {
     }
 
     return graph
+}
+
+function insertSorted(arr: [number, number][], element: [number, number]): void {
+    const index: number = arr.findIndex(([time]: number[]): boolean => time > element[0])
+
+    if (index === -1) {
+        arr.push(element)
+    } else {
+        arr.splice(index, 0, element)
+    }
 }
