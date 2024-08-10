5a01f23069ff1f0bc84a8cdcaeb70d467e37510b
Yaroslav Brahinets <y.brahinets@gmail.com>
2024-08-10 15:50:40 +0300

    #959 Simplify

--- solutions/959. Regions Cut By Slashes/script.ts
+++ solutions/959. Regions Cut By Slashes/script.ts
@@ -4,56 +4,44 @@ function regionsBySlashes(grid: string[]): number {
     const size: number = grid.length * grid.length * 4
     const dsu: number[] = buildUnions(size, grid.length, grid)
 
-    let count: number = 0
-    for (let i: number = 0; i < size; i++) {
-        if (find(dsu, i) === i) {
-            count++
-        }
-    }
-
-    return count
+    return dsu.filter((_: number, i: number) => find(dsu, i) === i).length
 }
 
 function buildUnions(size: number, n: number, grid: string[]): number[] {
     const dsu: number[] = Array.from({length: size}, (_, i) => i)
 
-    for (let i: number = 0; i < n; i++) {
-        for (let j: number = 0; j < n; j++) {
-            const index: number = i * n + j
-            const start: number = index * 4
+    grid.forEach((row: string, i: number): void =>
+        row.split('').forEach((cell: string, j: number): void => {
+            const index: number = (i * n + j) * 4
 
-            if (grid[i][j] === ' ') {
-                union(dsu, start, start + 1)
-                union(dsu, start + 1, start + 2)
-                union(dsu, start + 2, start + 3)
-            } else if (grid[i][j] === '/') {
-                union(dsu, start, start + 3)
-                union(dsu, start + 1, start + 2)
+            if (cell === ' ') {
+                union(dsu, index, index + 1)
+                union(dsu, index + 1, index + 2)
+                union(dsu, index + 2, index + 3)
+            } else if (cell === '/') {
+                union(dsu, index, index + 3)
+                union(dsu, index + 1, index + 2)
             } else {
-                union(dsu, start, start + 1)
-                union(dsu, start + 2, start + 3)
+                union(dsu, index, index + 1)
+                union(dsu, index + 2, index + 3)
             }
 
             if (i > 0) {
-                union(dsu, start, (index - n) * 4 + 2)
+                union(dsu, index, ((i - 1) * n + j) * 4 + 2)
             }
-
             if (j > 0) {
-                union(dsu, start + 3, (index - 1) * 4 + 1)
+                union(dsu, index + 3, (i * n + j - 1) * 4 + 1)
             }
-        }
-    }
-    return dsu;
+        })
+    )
+
+    return dsu
 }
 
-function union(dsu: number[], x: number, y: number) {
+function union(dsu: number[], x: number, y: number): void {
     dsu[find(dsu, x)] = find(dsu, y)
 }
 
 function find(dsu: number[], val: number): number {
-    if (dsu[val] !== val) {
-        dsu[val] = find(dsu, dsu[val])
-    }
-
-    return dsu[val]
+    return dsu[val] === val ? val : (dsu[val] = find(dsu, dsu[val]))
 }
