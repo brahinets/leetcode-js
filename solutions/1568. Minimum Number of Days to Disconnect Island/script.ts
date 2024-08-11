81c02264aae1ed2240d9cab7597452d50be8a87d
Yaroslav Brahinets <y.brahinets@gmail.com>
2024-08-11 09:27:52 +0300

    #1568 Make less side effects

--- solutions/1568. Minimum Number of Days to Disconnect Island/script.ts
+++ solutions/1568. Minimum Number of Days to Disconnect Island/script.ts
@@ -26,32 +26,29 @@ function minDays(grid: number[][]): number {
 
 function countIslands(grid: number[][]): number {
     const visited: boolean[][] = matrixOf(false, grid.length, grid[0].length)
-    let islandCount: number = 0
+    const sizes: number[] = []
 
-    function dfs(x: number, y: number): void {
-        if (x < 0 || y < 0 || x >= grid.length || y >= grid[0].length || grid[x][y] === 0 || visited[x][y]) {
-            return
-        }
-
-        visited[x][y] = true
-
-        dfs(x + 1, y)
-        dfs(x - 1, y)
-        dfs(x, y + 1)
-        dfs(x, y - 1)
-    }
-
-    for (let i: number = 0; i < grid.length; i++) {
-        for (let j: number = 0; j < grid[0].length; j++) {
+    for (let i = 0; i < grid.length; i++) {
+        for (let j = 0; j < grid[0].length; j++) {
             if (grid[i][j] === 1 && !visited[i][j]) {
-                islandCount++
-
-                dfs(i, j)
+                sizes.push(dfs(i, j, grid, visited))
             }
         }
     }
 
-    return islandCount
+    return sizes.length
 }
 
+function dfs(x: number, y: number, grid: number[][], visited: boolean[][]): number {
+    if (x < 0 || y < 0 || x >= grid.length || y >= grid[0].length || grid[x][y] === 0 || visited[x][y]) {
+        return 0
+    }
 
+    visited[x][y] = true
+
+    return 1 +
+        dfs(x + 1, y, grid, visited) +
+        dfs(x - 1, y, grid, visited) +
+        dfs(x, y + 1, grid, visited) +
+        dfs(x, y - 1, grid, visited)
+}
