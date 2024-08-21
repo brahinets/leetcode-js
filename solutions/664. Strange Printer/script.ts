de09117965f51c0d95a4dc6a230255f8f1950c96
Yaroslav Brahinets <y.brahinets@gmail.com>
2024-08-21 10:09:57 +0300

    #664 Improve naming

--- solutions/664. Strange Printer/script.ts
+++ solutions/664. Strange Printer/script.ts
@@ -3,25 +3,25 @@ import {matrixOfZeros} from "../../common/array-factories"
 export {strangePrinter}
 
 function strangePrinter(s: string): number {
-    const dp: number[][] = matrixOfZeros(s.length, s.length)
+    const minTurns: number[][] = matrixOfZeros(s.length, s.length)
 
     for (let i: number = s.length - 1; i >= 0; i--) {
-        dp[i][i] = 1
+        minTurns[i][i] = 1
 
         for (let j: number = i + 1; j < s.length; j++) {
             if (s[i] === s[j]) {
-                dp[i][j] = dp[i][j - 1]
+                minTurns[i][j] = minTurns[i][j - 1]
             } else {
-                let min: number = Infinity
+                let minSplitTurns: number = Infinity
 
                 for (let k: number = i; k < j; k++) {
-                    min = Math.min(min, dp[i][k] + dp[k + 1][j])
+                    minSplitTurns = Math.min(minSplitTurns, minTurns[i][k] + minTurns[k + 1][j])
                 }
 
-                dp[i][j] = min
+                minTurns[i][j] = minSplitTurns
             }
         }
     }
 
-    return dp[0][s.length - 1]
+    return minTurns[0][s.length - 1]
 }
