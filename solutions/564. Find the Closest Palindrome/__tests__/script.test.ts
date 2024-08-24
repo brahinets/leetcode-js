61be27b3c18d4c26d2dccb96afbac4a9c4f3957a
Yaroslav Brahinets <y.brahinets@gmail.com>
2024-08-24 23:07:11 +0300

    #564 Use datatype safe for big values

--- solutions/564. Find the Closest Palindrome/__tests__/script.test.ts
+++ solutions/564. Find the Closest Palindrome/__tests__/script.test.ts
@@ -6,6 +6,11 @@ describe('564. Find the Closest Palindrome', (): void => {
             .toEqual("121")
     })
 
+    it('Return palindrome #2', (): void => {
+        expect(nearestPalindromic("807045053224792883"))
+            .toEqual("807045053350540708")
+    })
+
     it('Return smaller palindrome with different fraction', (): void => {
         expect(nearestPalindromic("11"))
             .toEqual("9")
--- solutions/564. Find the Closest Palindrome/script.ts
+++ solutions/564. Find the Closest Palindrome/script.ts
@@ -3,50 +3,54 @@ export {nearestPalindromic}
 function nearestPalindromic(n: string): string {
     const i: number = n.length % 2 === 0 ? (n.length / 2 - 1) : (n.length / 2)
     const half: number = Number(n.substring(0, i + 1))
-    const candidates: number[] = getCandidates(half, n.length)
+    const candidates: bigint[] = getCandidates(half, n.length)
 
-    let minDiff: number = Number.MAX_SAFE_INTEGER
-    let result: number = 0
-    const original: number = Number(n)
-    for (let candidate of   candidates) {
-        const diff: number = Math.abs(candidate - original)
+    let minDiff: bigint = BigInt(Number.MAX_SAFE_INTEGER)
+    let result: bigint = BigInt(0)
+    const original: bigint = BigInt(n)
+    for (let candidate of candidates) {
+        const diff: bigint = abs(candidate - original)
 
-        if (diff === 0) {
+        if (diff === BigInt(0)) {
             continue
         }
 
         if (diff < minDiff) {
             minDiff = diff
             result = candidate
-        } else if (diff === minDiff) {
-            result = Math.min(Number(result), Number(candidate))
+        } else if (diff === minDiff && candidate < result) {
+            result = candidate
         }
     }
 
     return result.toString()
 }
 
-function getCandidates(scope: number, length: number): number[] {
+function getCandidates(scope: number, length: number): bigint[] {
     return [
         halfToPalindrome(scope, length % 2 === 0),
         halfToPalindrome(scope + 1, length % 2 === 0),
         halfToPalindrome(scope - 1, length % 2 === 0),
-        10 ** (length - 1) - 1,
-        10 ** (length) + 1,
+        BigInt(10 ** (length - 1) - 1),
+        BigInt(10 ** (length) + 1),
     ]
 }
 
-function halfToPalindrome(half: number, isEven: boolean): number {
-    let result: number = half
+function halfToPalindrome(half: number, isEven: boolean): bigint {
+    let result: bigint = BigInt(half)
 
     if (!isEven) {
         half = Math.floor(half / 10)
     }
 
     while (half > 0) {
-        result = result * 10 + half % 10
+        result = result * 10n + BigInt(half % 10)
         half = Math.floor(half / 10)
     }
 
-    return result
+    return BigInt(result)
+}
+
+function abs(x: bigint): bigint {
+    return x < BigInt(0) ? -x : x;
 }
