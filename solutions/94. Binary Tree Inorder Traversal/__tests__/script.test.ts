723672acc23f81c7902c5255ed8404507117799a
Yaroslav Brahinets <y.brahinets@gmail.com>
2024-08-25 09:52:21 +0300

    #102 Reuse tree builder

--- solutions/94. Binary Tree Inorder Traversal/__tests__/script.test.ts
+++ solutions/94. Binary Tree Inorder Traversal/__tests__/script.test.ts
@@ -1,23 +1,19 @@
-import {inorderTraversal, TreeNode} from '../script'
+import {inorderTraversal} from '../script'
+import {arrayToTree} from "../../../common/tree-utils"
 
 describe('102. Binary Tree Level Order Traversal', (): void => {
     it('Empty tree', (): void => {
-        expect(inorderTraversal(null))
+        expect(inorderTraversal(arrayToTree([null])))
             .toEqual([])
     })
 
-
     it('Small tree', (): void => {
-        expect(inorderTraversal(new TreeNode(1)))
+        expect(inorderTraversal(arrayToTree([1])))
             .toEqual([1])
     })
 
     it('Big tree', (): void => {
-        expect(inorderTraversal(
-            new TreeNode(1,
-                null,
-                new TreeNode(2,
-                    new TreeNode(3)))))
+        expect(inorderTraversal(arrayToTree([1, null, 2, 3])))
             .toEqual([1, 3, 2])
     })
 })
