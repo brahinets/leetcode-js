88c8636f429ff8116ef04c724eb2f232c8a234a3
Yaroslav Brahinets <y.brahinets@gmail.com>
2024-08-27 08:11:14 +0300

    #1514 Use floating numbers precision assertion

--- solutions/1514. Path with Maximum Probability/__tests__/script.test.ts
+++ solutions/1514. Path with Maximum Probability/__tests__/script.test.ts
@@ -3,32 +3,32 @@ import {maxProbability} from '../script'
 describe('1514. Path with Maximum Probability', (): void => {
     it('Has one path', (): void => {
         expect(maxProbability(3, [[0, 1], [1, 2], [3, 2]], [0.5, 0.5, 0.2], 0, 2))
-            .toBe(0.25000)
+            .toBeCloseTo(0.25000, 5)
     })
 
     it('Has multiple paths, use longer', (): void => {
         expect(maxProbability(3, [[0, 1], [1, 2], [0, 2]], [0.5, 0.5, 0.2], 0, 2))
-            .toBe(0.25000)
+            .toBeCloseTo(0.25000, 5)
     })
 
     it('Has multiple paths, use shorter', (): void => {
         expect(maxProbability(3, [[0, 1], [1, 2], [0, 2]], [0.5, 0.5, 0.3], 0, 2))
-            .toBe(0.3000)
+            .toBeCloseTo(0.3000, 5)
     })
 
     it('Multiple paths with complex probability and reversed', (): void => {
         expect(maxProbability(5, [[1, 4], [2, 4], [0, 4], [0, 3], [0, 2], [2, 3]], [0.37, 0.17, 0.93, 0.23, 0.39, 0.04], 3, 4))
-            .toBe(0.21390)
+            .toBeCloseTo(0.21390, 5)
     })
 
     it('Has multiple paths, use reversed', (): void => {
         expect(maxProbability(3, [[0, 1], [0, 2], [0, 3], [1, 2], [3, 1]], [0.5, 1, 2, 0.5, 0.5], 0, 2))
-            .toBe(1)
+            .toBeCloseTo(1, 5)
     })
 
     it('No path available', (): void => {
         expect(maxProbability(3, [[0, 1]], [0.5], 0, 2))
-            .toBe(0.0)
+            .toBeCloseTo(0.0, 5)
     })
 
     // TODO #1514 Speed me up
