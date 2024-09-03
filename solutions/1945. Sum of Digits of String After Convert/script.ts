3e337161330dbb2d9d737713af076161a68f0c52
Yaroslav Brahinets <y.brahinets@gmail.com>
2024-09-03 10:46:47 +0300

    #1945 Improve readability

--- solutions/1945. Sum of Digits of String After Convert/script.ts
+++ solutions/1945. Sum of Digits of String After Convert/script.ts
@@ -5,8 +5,11 @@ function getLucky(s: string, k: number): number {
         .map(c => (c.charCodeAt(0) - 96).toString())
         .join('')
 
-    for (let i = 0; i < k; i++) {
-        codes = codes.split('').map(Number).reduce((digits, char) => digits + char, 0).toString()
+    for (let i: number = 0; i < k; i++) {
+        codes = codes.split('')
+            .map(Number)
+            .reduce((digits, char) => digits + char, 0)
+            .toString()
     }
 
     return Number(codes)
