947db8811a245db613e297740320d8364fdd5db2
Yaroslav Brahinets <y.brahinets@gmail.com>
2024-09-06 10:17:56 +0300

    #3217 Optimize number existence check

--- solutions/3217. Delete Nodes From Linked List Present in Array/script.ts
+++ solutions/3217. Delete Nodes From Linked List Present in Array/script.ts
@@ -3,11 +3,12 @@ import {ListNode} from "../../common/ListNode"
 export {modifiedList}
 
 function modifiedList(nums: number[], head: ListNode | null): ListNode | null {
+    const valuesToRemove: Set<number> = new Set<number>(nums)
     const dummy: ListNode = new ListNode(0, head)
     let current: ListNode = dummy
 
     while (current.next) {
-        if (nums.includes(current.next.val)) {
+        if (valuesToRemove.has(current.next.val)) {
             current.next = current.next.next
         } else {
             current = current.next
