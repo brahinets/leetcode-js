f1412308d0c4f72f8837c9748499cccf20f99904
Yaroslav Brahinets <y.brahinets@gmail.com>
2024-09-12 09:23:10 +0300

    #1684 Speed up

--- solutions/1684. Count the Number of Consistent Strings/script.ts
+++ solutions/1684. Count the Number of Consistent Strings/script.ts
@@ -1,10 +1,11 @@
 export {countConsistentStrings}
 
 function countConsistentStrings(allowed: string, words: string[]): number {
+    const allowedCharacters: Set<string> = new Set<string>(allowed.split(''))
     let count: number = 0
 
     for (const word of words) {
-        if (isConsistent(word, allowed)) {
+        if (isConsistent(word, allowedCharacters)) {
             count++
         }
     }
@@ -12,11 +13,11 @@ function countConsistentStrings(allowed: string, words: string[]): number {
     return count
 }
 
-function isConsistent(word: string, allowed: string) {
+function isConsistent(word: string, allowed: Set<string>): boolean {
     let isConsistent: boolean = true
 
     for (let char of word) {
-        if (!allowed.includes(char)) {
+        if (!allowed.has(char)) {
             isConsistent = false
             break
         }
