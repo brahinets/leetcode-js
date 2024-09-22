cd35f8dba99a209d12044d4cabb959e2e803bb39
Yaroslav Brahinets <y.brahinets@gmail.com>
2024-09-22 10:54:48 +0300

    #440 Speed up

--- solutions/440. K-th Smallest in Lexicographical Order/__tests__/script.test.ts
+++ solutions/440. K-th Smallest in Lexicographical Order/__tests__/script.test.ts
@@ -10,4 +10,9 @@ describe('440. K-th Smallest in Lexicographical Order', (): void => {
         expect(findKthNumber(13, 2))
             .toBe(10)
     })
+
+    it('Load test', (): void => {
+        expect(findKthNumber(804289384, 42641503))
+            .toBe(138377349)
+    })
 })
--- solutions/440. K-th Smallest in Lexicographical Order/script.ts
+++ solutions/440. K-th Smallest in Lexicographical Order/script.ts
@@ -1,7 +1,32 @@
-import {lexicalOrder} from "../386. Lexicographical Numbers/script"
-
 export {findKthNumber}
 
 function findKthNumber(n: number, k: number): number {
-    return lexicalOrder(n)[k - 1]
+    let current: number = 1
+
+    k--
+    while (k > 0) {
+        const steps: number = calculateSteps(n, current, current + 1)
+        if (steps <= k) {
+            current++
+            k -= steps
+        } else {
+            current *= 10
+            k--
+        }
+    }
+
+    return current
 }
+
+function calculateSteps(n: number, current: number, number: number): number {
+    let steps: number = 0
+
+    while (current <= n) {
+        steps += Math.min(n + 1, number) - current
+        current *= 10
+        number *= 10
+    }
+
+    return steps
+}
+
