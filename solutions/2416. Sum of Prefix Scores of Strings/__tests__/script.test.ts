acd7da35a026d8d4bfeac8602fff5800d25d6447
Yaroslav Brahinets <y.brahinets@gmail.com>
2024-09-25 21:37:02 +0300

    #2416 Fix logic

--- solutions/2416. Sum of Prefix Scores of Strings/__tests__/script.test.ts
+++ solutions/2416. Sum of Prefix Scores of Strings/__tests__/script.test.ts
@@ -11,4 +11,9 @@ describe('2416. Sum of Prefix Scores of Strings', (): void => {
         expect(sumPrefixScores(["abcd"]))
             .toEqual([4])
     })
+
+    it('Many prefixes', (): void => {
+        expect(sumPrefixScores(["qtcqcmwcin", "vkjotbrbzn", "eoorlyfche", "eoorlyhn", "eoorlyfcxk", "qfnmjilcom", "eoorlyfche", "qtcqcmwcnl", "qtcqcrpjr"]))
+            .toEqual([24, 10, 34, 26, 32, 13, 34, 24, 20])
+    })
 })
--- solutions/2416. Sum of Prefix Scores of Strings/script.ts
+++ solutions/2416. Sum of Prefix Scores of Strings/script.ts
@@ -4,15 +4,22 @@ export {sumPrefixScores}
 
 function sumPrefixScores(words: string[]): number[] {
     const trie: Trie = new Trie()
+    const prefixCount: Map<string, number> = new Map()
+
     for (const word of words) {
         trie.insert(word)
+
+        for (let i = 1; i <= word.length; i++) {
+            const prefix = word.slice(0, i)
+            prefixCount.set(prefix, (prefixCount.get(prefix) || 0) + 1)
+        }
     }
 
     return words.map(word => {
         let score: number = 0
         for (let i: number = 1; i <= word.length; i++) {
             const prefix: string = word.slice(0, i)
-            score += trie.startPrefix(prefix)
+            score += prefixCount.get(prefix) || 0
         }
 
         return score
