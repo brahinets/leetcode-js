ace66377651e81edcd2099cf892ee0c34d1a5114
Yaroslav Brahinets <y.brahinets@gmail.com>
2024-09-27 09:39:01 +0300

    #729 Improve code style

--- solutions/729. My Calendar I/__tests__/script.test.ts
+++ solutions/729. My Calendar I/__tests__/script.test.ts
@@ -4,13 +4,13 @@ describe('729. My Calendar I', (): void => {
     it('Test', (): void => {
         const myCalendar: MyCalendar = new MyCalendar()
 
-        let bookOnEmpty: boolean = myCalendar.book(10, 20)
+        const bookOnEmpty: boolean = myCalendar.book(10, 20)
         expect(bookOnEmpty).toBe(true)
 
-        let bookInTheMiddle: boolean = myCalendar.book(15, 25)
+        const bookInTheMiddle: boolean = myCalendar.book(15, 25)
         expect(bookInTheMiddle).toBe(false)
 
-        let bookInTheEnd: boolean = myCalendar.book(20, 30)
+        const bookInTheEnd: boolean = myCalendar.book(20, 30)
         expect(bookInTheEnd).toBe(true)
     })
 })
