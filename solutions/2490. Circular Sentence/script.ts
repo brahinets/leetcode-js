615db0494acac172eec6e375302d2e4e5d113eaa
Yaroslav Brahinets <y.brahinets@gmail.com>
2024-11-02 14:17:08 +0200

    #2490 Improve type-safety

--- solutions/2490. Circular Sentence/script.ts
+++ solutions/2490. Circular Sentence/script.ts
@@ -6,8 +6,8 @@ function isCircularSentence(sentence: string): boolean {
     const words: string[] = sentence.split(' ')
 
     for (let i: number = 0; i < words.length; i++) {
-        let current: string = words[i]
-        let previous: string = i > 0 ? words[i - 1] : last(words)!
+        const current: string = words[i]
+        const previous: string = i > 0 ? words[i - 1] : last(words)!
 
         if (current[0] !== previous[previous.length - 1]) {
             return false
