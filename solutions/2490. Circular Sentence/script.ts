154b4e8867d41a7a37209b5526cf6f143ff3ce28
Yaroslav Brahinets <y.brahinets@gmail.com>
2024-11-02 14:16:54 +0200

    #2490 Improve readability

--- solutions/2490. Circular Sentence/script.ts
+++ solutions/2490. Circular Sentence/script.ts
@@ -1,3 +1,5 @@
+import {last} from "../../common/array-utils"
+
 export {isCircularSentence}
 
 function isCircularSentence(sentence: string): boolean {
@@ -5,7 +7,7 @@ function isCircularSentence(sentence: string): boolean {
 
     for (let i: number = 0; i < words.length; i++) {
         let current: string = words[i]
-        let previous: string = i === 0 ? words[words.length - 1] : words[i - 1]
+        let previous: string = i > 0 ? words[i - 1] : last(words)!
 
         if (current[0] !== previous[previous.length - 1]) {
             return false
