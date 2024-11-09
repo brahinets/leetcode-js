c5401824718bc566e2f9e7a8f9142a2889c21416
Yaroslav Brahinets <y.brahinets@gmail.com>
2024-11-09 19:14:40 +0200

    #3133 Support big numbers

--- solutions/3133. Minimum Array End/__tests__/script.test.ts
+++ solutions/3133. Minimum Array End/__tests__/script.test.ts
@@ -10,4 +10,9 @@ describe('3133. Minimum Array End', (): void => {
         expect(minEnd(2, 7))
             .toBe(15)
     })
+
+    it('Big', (): void => {
+        expect(minEnd(6715154, 7193485))
+            .toBe(55012476815)
+    })
 })
--- solutions/3133. Minimum Array End/script.ts
+++ solutions/3133. Minimum Array End/script.ts
@@ -1,11 +1,17 @@
 export {minEnd}
 
 function minEnd(n: number, x: number): number {
-    let result: number = x
+    let result: bigint = BigInt(x)
 
-    while (--n > 0) {
-        result = (result + 1) | x
+    let remaining: bigint = BigInt(n - 1)
+    let position: bigint = 1n
+    while (remaining > 0n) {
+        if ((BigInt(x) & position) === 0n) {
+            result |= (remaining & 1n) * position
+            remaining >>= 1n
+        }
+        position <<= 1n
     }
 
-    return result
+    return Number(result)
 }
