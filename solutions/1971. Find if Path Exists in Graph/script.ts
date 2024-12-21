3ba4ac4f350867520d50f1a60f66308b7626cf0d
Yaroslav Brahinets <y.brahinets@gmail.com>
2024-12-21 14:11:14 +0200

    #1971 Reuse code

--- solutions/1971. Find if Path Exists in Graph/script.ts
+++ solutions/1971. Find if Path Exists in Graph/script.ts
@@ -1,52 +1,13 @@
+import {UndirectedUnweightedGraph} from "../../common/Graph"
+
 export {validPath}
 
 function validPath(n: number, edges: number[][], source: number, destination: number): boolean {
-    const graph: UndirectedGraph = new UndirectedGraph(n, edges)
+    const graph: UndirectedUnweightedGraph = new UndirectedUnweightedGraph()
+    for (const [a, b] of edges) {
+        graph.addEdge(a, b)
+    }
 
     return graph.pathExist(source, destination)
 }
 
-class UndirectedGraph {
-    private readonly size: number
-    private readonly nodes: Map<number, Map<number, number>>
-
-    constructor(n: number, edges: number[][]) {
-        this.size = n
-        this.nodes = new Map<number, Map<number, number>>()
-        edges.forEach((edge: number[]): void => this.addEdge(edge))
-    }
-
-    addEdge(edge: number[]): void {
-        const [from, to] = edge
-
-        const toNode: Map<number, number> = this.nodes.get(from) ?? new Map<number, number>()
-        toNode.set(to, 1)
-        this.nodes.set(from, toNode)
-
-        const otherNode: Map<number, number> = this.nodes.get(to) ?? new Map<number, number>()
-        otherNode.set(from, 1)
-        this.nodes.set(to, otherNode)
-    }
-
-    pathExist(from: number, to: number): boolean {
-        const visited: Set<number> = new Set<number>()
-
-        const dfs: (current: number) => boolean = (current: number): boolean => {
-            if (current === to) {
-                return true
-            }
-
-            visited.add(current)
-            const neighbors: Map<number, number> = this.nodes.get(current) ?? new Map<number, number>()
-            for (const neighbor of neighbors.keys()) {
-                if (!visited.has(neighbor) && dfs(neighbor)) {
-                    return true
-                }
-            }
-
-            return false
-        }
-
-        return dfs(from)
-    }
-}
