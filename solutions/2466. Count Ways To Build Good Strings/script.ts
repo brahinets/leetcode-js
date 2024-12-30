2cafbd7e6834a492bfd93223d920889ac3c15e9e
Yaroslav Brahinets <y.brahinets@gmail.com>
2024-12-30 08:34:34 +0200

    #2466 Improve readability

--- solutions/2466. Count Ways To Build Good Strings/script.ts
+++ solutions/2466. Count Ways To Build Good Strings/script.ts
@@ -2,22 +2,23 @@ import {arrayOfZeros} from "../../common/array-factories"
 
 export {countGoodStrings}
 
+const MOD: number = 10 ** 9 + 7
+
 function countGoodStrings(minLength: number, maxLength: number, zerosCount: number, onesCount: number): number {
-    const mod: number = 1e9 + 7
     const validCombinations: number[] = arrayOfZeros(maxLength + 1)
     validCombinations[0] = 1
 
     for (let length: number = 1; length <= maxLength; length++) {
         if (length >= zerosCount) {
-            validCombinations[length] += validCombinations[length - zerosCount] % mod
+            validCombinations[length] += validCombinations[length - zerosCount] % MOD
         }
 
         if (length >= onesCount) {
-            validCombinations[length] += validCombinations[length - onesCount] % mod
+            validCombinations[length] += validCombinations[length - onesCount] % MOD
         }
     }
 
     return validCombinations
         .slice(minLength)
-        .reduce((prev: number, sum: number): number => (prev + sum) % mod, 0)
+        .reduce((sum: number, combinations: number): number => (sum + combinations) % MOD, 0)
 }
