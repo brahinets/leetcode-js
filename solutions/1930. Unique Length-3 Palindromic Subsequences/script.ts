7d79e657665c09903628471bfee23849724af02f
Yaroslav Brahinets <y.brahinets@gmail.com>
2025-01-04 10:08:56 +0200

    #1930 Improve type-safety

--- solutions/1930. Unique Length-3 Palindromic Subsequences/script.ts
+++ solutions/1930. Unique Length-3 Palindromic Subsequences/script.ts
@@ -7,14 +7,14 @@ function countPalindromicSubsequence(s: string): number {
 
     const bounds: Set<string> = new Set<string>([...count(s.split(''))]
         .filter(([, count]: [string, number]): boolean => count > 1)
-        .map(([char,]: [string, number]) => char))
+        .map(([char,]: [string, number]): string => char))
 
     for (const char of bounds) {
         const first: number = s.indexOf(char)
         const last: number = s.lastIndexOf(char)
 
         if (first !== last) {
-            palindromicSubsequenceCount += new Set(s.slice(first + 1, last)).size
+            palindromicSubsequenceCount += new Set<string>(s.slice(first + 1, last)).size
         }
     }
 
