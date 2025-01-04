e3653dba8b0939dc8f9065265931094ea6becac5
Yaroslav Brahinets <y.brahinets@gmail.com>
2025-01-04 10:14:42 +0200

    #1930 Optimize

--- solutions/1930. Unique Length-3 Palindromic Subsequences/script.ts
+++ solutions/1930. Unique Length-3 Palindromic Subsequences/script.ts
@@ -1,22 +1,37 @@
-import {count} from "../../common/array-utils"
+import {arrayOf} from "../../common/array-factories"
 
 export {countPalindromicSubsequence}
 
+const ALPHABET_LENGTH: number = 26
+
 function countPalindromicSubsequence(s: string): number {
+    const [firstOccurrence, lastOccurrence]: [number[], number[]] = findEdgeOccurrences(s)
+
     let palindromicSubsequenceCount: number = 0
-
-    const bounds: Set<string> = new Set<string>([...count(s.split(''))]
-        .filter(([, count]: [string, number]): boolean => count > 1)
-        .map(([char,]: [string, number]): string => char))
-
-    for (const char of bounds) {
-        const first: number = s.indexOf(char)
-        const last: number = s.lastIndexOf(char)
-
-        if (first !== last) {
-            palindromicSubsequenceCount += new Set<string>(s.slice(first + 1, last)).size
+    for (let i: number = 0; i < ALPHABET_LENGTH; i++) {
+        if (firstOccurrence[i] !== -1 && firstOccurrence[i] !== lastOccurrence[i]) {
+            const uniqueChars: Set<string> = new Set<string>(s.slice(firstOccurrence[i] + 1, lastOccurrence[i]))
+            palindromicSubsequenceCount += uniqueChars.size
         }
     }
 
     return palindromicSubsequenceCount
+}
+
+function findEdgeOccurrences(s: string): [number[], number[]] {
+    const firstOccurrence: number[] = arrayOf(-1, ALPHABET_LENGTH)
+    const lastOccurrence: number[] = arrayOf(-1, ALPHABET_LENGTH)
+    const aCharCode: number = 'a'.charCodeAt(0)
+
+    for (let i: number = 0; i < s.length; i++) {
+        const charIndex: number = s.charCodeAt(i) - aCharCode
+
+        if (firstOccurrence[charIndex] === -1) {
+            firstOccurrence[charIndex] = i
+        }
+
+        lastOccurrence[charIndex] = i
+    }
+
+    return [firstOccurrence, lastOccurrence]
 }
