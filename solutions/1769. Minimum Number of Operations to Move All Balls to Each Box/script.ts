7a2a31f569946db6b25e46b64ff71c2a93118a60
Yaroslav Brahinets <y.brahinets@gmail.com>
2025-01-06 17:36:52 +0200

    #1769 Optimise solution

--- solutions/1769. Minimum Number of Operations to Move All Balls to Each Box/script.ts
+++ solutions/1769. Minimum Number of Operations to Move All Balls to Each Box/script.ts
@@ -3,14 +3,22 @@ import {arrayOfZeros} from "../../common/array-factories"
 export {minOperations}
 
 function minOperations(boxes: string): number[] {
-    let answer: number[] = arrayOfZeros(boxes.length)
+    const answer: number[] = arrayOfZeros(boxes.length)
 
-    for (let currentBox: number = 0; currentBox < boxes.length; currentBox++) {
-        if (boxes.charAt(currentBox) == '1') {
-            for (let newPosition = 0; newPosition < boxes.length; newPosition++) {
-                answer[newPosition] += Math.abs(newPosition - currentBox)
-            }
-        }
+    let ballsToLeft: number = 0
+    let movesToLeft: number = 0
+    let ballsToRight: number = 0
+    let movesToRight: number = 0
+
+    for (let i: number = 0; i < boxes.length; i++) {
+        answer[i] += movesToLeft
+        ballsToLeft += Number(boxes.charAt(i))
+        movesToLeft += ballsToLeft
+
+        const j: number = boxes.length - 1 - i
+        answer[j] += movesToRight
+        ballsToRight += Number(boxes.charAt(j))
+        movesToRight += ballsToRight
     }
 
     return answer
