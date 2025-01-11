2d751efba9d10cf332810e9eebdbb4b44fc765fa
Yaroslav Brahinets <y.brahinets@gmail.com>
2025-01-11 10:23:40 +0200

    #2251 Optimize solution

--- solutions/2251. Number of Flowers in Full Bloom/script.ts
+++ solutions/2251. Number of Flowers in Full Bloom/script.ts
@@ -1,20 +1,38 @@
-import {arrayOfZeros} from "../../common/array-factories"
-
 export {fullBloomFlowers}
 
 function fullBloomFlowers(flowers: number[][], people: number[]): number[] {
-    const bloom: number[] = arrayOfZeros(365)
-
+    const starts: number[] = []
+    const ends: number[] = []
     for (const [start, end] of flowers) {
-        for (let i: number = start; i <= end; i++) {
-            bloom[i]++
+        starts.push(start)
+        ends.push(end + 1)
+    }
+    starts.sort((a: number, b: number): number => a - b)
+    ends.sort((a: number, b: number): number => a - b)
+
+    const answer: number[] = []
+    for (const person of people) {
+        const i: number = binarySearch(starts, person)
+        const j: number = binarySearch(ends, person)
+        answer.push(i - j)
+    }
+
+    return answer
+}
+
+function binarySearch(arr: number[], target: number): number {
+    let left: number = 0
+    let right: number = arr.length
+
+    while (left < right) {
+        const mid: number = Math.floor((left + right) / 2)
+
+        if (target < arr[mid]) {
+            right = mid
+        } else {
+            left = mid + 1
         }
     }
 
-    const result: number[] = []
-    for (const day of people) {
-        result.push(bloom[day])
-    }
-
-    return result
+    return left
 }
