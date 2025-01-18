b10724a4506e12c65594df068a0ac9aac6bcc911
Yaroslav Brahinets <y.brahinets@gmail.com>
2025-01-18 11:08:24 +0200

    #1368 Improve code style

--- solutions/1368. Minimum Cost to Make at Least One Valid Path in a Grid/__tests__/script.test.ts
+++ solutions/1368. Minimum Cost to Make at Least One Valid Path in a Grid/__tests__/script.test.ts
@@ -14,14 +14,14 @@ describe('1368. Minimum Cost to Make at Least One Valid Path in a Grid', (): voi
         expect(minCost([
             [1, 1, 3],
             [3, 2, 2],
-            [1, 1, 4
-        ]])).toBe(0)
+            [1, 1, 4]
+        ])).toBe(0)
     })
 
     it('Three', (): void => {
         expect(minCost([
             [1, 2],
-            [4, 3
-        ]])).toBe(1)
+            [4, 3]
+        ])).toBe(1)
     })
 })
