594e5ab764d86c73371eac912ce2ec79229d677a
Yaroslav Brahinets <y.brahinets@gmail.com>
2025-01-20 10:51:19 +0200

    #2661 Improve naming

--- solutions/2661. First Completely Painted Row or Column/script.ts
+++ solutions/2661. First Completely Painted Row or Column/script.ts
@@ -1,16 +1,14 @@
 export {firstCompleteIndex}
 
 function firstCompleteIndex(arr: number[], mat: number[][]): number {
-    const numToPos: Map<number, Pair<number, number>> = buildPositions(mat)
+    const numToPos: Map<number, Position<number, number>> = buildPositions(mat)
 
     for (let i = 0; i < arr.length; i++) {
         const num: number = arr[i]
-        const position: Pair<number, number> = numToPos.get(num)!
-        const row: number = position.key
-        const col: number = position.value
-        mat[row][col] = -mat[row][col]
+        const position: Position<number, number> = numToPos.get(num)!
+        mat[position.row][position.column] = -mat[position.row][position.column]
 
-        if (checkRow(row, mat) || checkColumn(col, mat)) {
+        if (checkRow(position.row, mat) || checkColumn(position.column, mat)) {
             return i
         }
     }
@@ -18,13 +16,13 @@ function firstCompleteIndex(arr: number[], mat: number[][]): number {
     return -1
 }
 
-function buildPositions(mat: number[][]): Map<number, Pair<number, number>> {
-    const numToPos: Map<number, Pair<number, number>> = new Map<number, Pair<number, number>>()
+function buildPositions(mat: number[][]): Map<number, Position<number, number>> {
+    const numToPos: Map<number, Position<number, number>> = new Map<number, Position<number, number>>()
 
     for (let row: number = 0; row < mat.length; row++) {
         for (let col: number = 0; col < mat[0].length; col++) {
             const value: number = mat[row][col]
-            numToPos.set(value, new Pair(row, col))
+            numToPos.set(value, new Position(row, col))
         }
     }
 
@@ -51,7 +49,7 @@ function checkColumn(col: number, mat: number[][]): boolean {
     return true
 }
 
-class Pair<T, U> {
-    constructor(readonly key: T, readonly value: U) {
+class Position<T, U> {
+    constructor(readonly row: T, readonly column: U) {
     }
 }
