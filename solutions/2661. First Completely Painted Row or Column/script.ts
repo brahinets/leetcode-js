fc5631d273c46fd788224c83e77410f344be4643
Yaroslav Brahinets <y.brahinets@gmail.com>
2025-01-20 10:58:35 +0200

    #2661 Optimise solution

--- solutions/2661. First Completely Painted Row or Column/script.ts
+++ solutions/2661. First Completely Painted Row or Column/script.ts
@@ -1,14 +1,21 @@
+import {arrayOfZeros} from "../../common/array-factories"
+
 export {firstCompleteIndex}
 
 function firstCompleteIndex(arr: number[], mat: number[][]): number {
-    const numToPos: Map<number, Position<number, number>> = buildPositions(mat)
+    const rowCount: number[] = arrayOfZeros(mat.length)
+    const colCount: number[] = arrayOfZeros(mat[0].length)
+    const numToPosition: Map<number, number[]> = buildPositions(mat)
 
-    for (let i = 0; i < arr.length; i++) {
+    for (let i: number = 0; i < arr.length; i++) {
         const num: number = arr[i]
-        const position: Position<number, number> = numToPos.get(num)!
-        mat[position.row][position.column] = -mat[position.row][position.column]
+        const position: number[] = numToPosition.get(num)!
+        const [row, col]: number[] = position
 
-        if (checkRow(position.row, mat) || checkColumn(position.column, mat)) {
+        rowCount[row]++
+        colCount[col]++
+
+        if (rowCount[row] == mat[0].length || colCount[col] == mat.length) {
             return i
         }
     }
@@ -16,40 +23,15 @@ function firstCompleteIndex(arr: number[], mat: number[][]): number {
     return -1
 }
 
-function buildPositions(mat: number[][]): Map<number, Position<number, number>> {
-    const numToPos: Map<number, Position<number, number>> = new Map<number, Position<number, number>>()
+function buildPositions(mat: number[][]): Map<number, number[]> {
+    const numToPosition: Map<number, number[]> = new Map<number, number[]>
 
     for (let row: number = 0; row < mat.length; row++) {
         for (let col: number = 0; col < mat[0].length; col++) {
             const value: number = mat[row][col]
-            numToPos.set(value, new Position(row, col))
+            numToPosition.set(value, [row, col])
         }
     }
 
-    return numToPos
-}
-
-function checkRow(row: number, mat: number[][]): boolean {
-    for (let col: number = 0; col < mat[0].length; col++) {
-        if (mat[row][col] > 0) {
-            return false
-        }
-    }
-
-    return true
-}
-
-function checkColumn(col: number, mat: number[][]): boolean {
-    for (let row: number = 0; row < mat.length; row++) {
-        if (mat[row][col] > 0) {
-            return false
-        }
-    }
-
-    return true
-}
-
-class Position<T, U> {
-    constructor(readonly row: T, readonly column: U) {
-    }
+    return numToPosition
 }
