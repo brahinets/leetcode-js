c0173a15e6c336a00dd165f66dbc1bd588635809
Yaroslav Brahinets <y.brahinets@gmail.com>
2025-02-05 15:11:42 +0200

    #1790 Check if One String Swap Can Make Strings Equal

---
+++ solutions/1790. Check if One String Swap Can Make Strings Equal/script.ts
@@ -0,0 +1,20 @@
+export {areAlmostEqual}
+
+function areAlmostEqual(s1: string, s2: string): boolean {
+    if (s1 === s2) {
+        return true
+    }
+
+    const delta: number[] = []
+    for (let i = 0; i < s1.length; i++) {
+        if (s1[i] !== s2[i]) {
+            delta.push(i)
+        }
+    }
+
+    if (delta.length !== 2) {
+        return false
+    }
+
+    return s1[delta[0]] === s2[delta[1]] && s1[delta[1]] === s2[delta[0]]
+}
