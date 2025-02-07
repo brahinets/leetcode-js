af36eefb5b17e4851f9784baeca9af5012620a28
Yaroslav Brahinets <y.brahinets@gmail.com>
2025-02-07 16:38:19 +0200

    #3160 Optimise solution

--- solutions/3160. Find the Number of Distinct Colors Among the Balls/script.ts
+++ solutions/3160. Find the Number of Distinct Colors Among the Balls/script.ts
@@ -1,29 +1,27 @@
-import {arrayOfZeros} from "../../common/array-factories"
-
 export {queryResults}
 
 function queryResults(limit: number, queries: number[][]): number[] {
+    let result = []
     const colorMap: Map<number, number> = new Map<number, number>()
-
-    let result: number[] = []
-    let ballArray: number[] = arrayOfZeros(limit + 1)
+    const ballMap: Map<number, number> = new Map<number, number>()
 
     for (let i: number = 0; i < queries.length; i++) {
         let ball: number = queries[i][0]
         let color: number = queries[i][1]
 
-        if (ballArray[ball] != 0) {
-            let prevColor: number = ballArray[ball]
+        if (ballMap.has(ball)) {
+            let prevColor: number = ballMap.get(ball)!
             colorMap.set(prevColor, colorMap.get(prevColor)! - 1)
 
-            if (colorMap.get(prevColor) == 0) {
+            if (colorMap.get(prevColor)! == 0) {
                 colorMap.delete(prevColor)
             }
         }
-        ballArray[ball] = color
-
+        ballMap.set(ball, color)
         colorMap.set(color, (colorMap.get(color) ?? 0) + 1)
+
         result[i] = colorMap.size
     }
+
     return result
 }
