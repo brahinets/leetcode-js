c39f348b72871217fbbd8fd92379e18c0bfc4ff0
Yaroslav Brahinets <y.brahinets@gmail.com>
2025-02-08 19:40:59 +0200

    #2349 Optimize solution using PriorityQueue

--- solutions/2349. Design a Number Container System/script.ts
+++ solutions/2349. Design a Number Container System/script.ts
@@ -1,81 +1,43 @@
+import {PriorityQueue} from "../../common/PriorityQueue"
+
 export {NumberContainers}
 
+const ascendingNumbersSorting: (a: number, b: number) => number = (a: number, b: number): number => a - b
+
 class NumberContainers {
-    private indexToNumber: Map<number, number>
-    private numberToIndices: Map<number, MinHeap>
+    private readonly numberToIndices: Map<number, PriorityQueue<number>>
+    private readonly indexToNumbers: Map<number, number>
 
     constructor() {
-        this.indexToNumber = new Map<number, number>()
-        this.numberToIndices = new Map<number, MinHeap>()
+        this.numberToIndices = new Map<number, PriorityQueue<number>>()
+        this.indexToNumbers = new Map<number, number>()
     }
 
-    change(index: number, number: number): void {
-        if (this.indexToNumber.has(index)) {
-            const oldNumber: number = this.indexToNumber.get(index)!
+    public change(index: number, number: number): void {
+        this.indexToNumbers.set(index, number)
 
-            if (this.numberToIndices.has(oldNumber)) {
-                this.numberToIndices.get(oldNumber)!.remove(index)
-            }
-        }
+        const numberPriorityQueue: PriorityQueue<number> = this.numberToIndices.get(number) ?? new PriorityQueue<number>(ascendingNumbersSorting)
+        numberPriorityQueue.enqueue(index)
 
-        this.indexToNumber.set(index, number)
+        this.numberToIndices.set(number, numberPriorityQueue)
+    }
+
+    public find(number: number): number {
         if (!this.numberToIndices.has(number)) {
-            this.numberToIndices.set(number, new MinHeap())
-        }
-        this.numberToIndices.get(number)!.insert(index)
-    }
-
-    find(number: number): number {
-        if (!this.numberToIndices.has(number) || this.numberToIndices.get(number)!.isEmpty()) {
             return -1
         }
 
-        return this.numberToIndices.get(number)!.top()
+        const minHeap: PriorityQueue<number> = this.numberToIndices.get(number)!
+        while (!minHeap.isEmpty()) {
+            const index: number = minHeap.peek()!
+
+            if (this.indexToNumbers.get(index) == number) {
+                return index
+            }
+
+            minHeap.dequeue()
+        }
+
+        return -1
     }
 }
-
-class MinHeap {
-    private heap: number[]
-    private indexSet: Set<number>
-
-    constructor() {
-        this.heap = []
-        this.indexSet = new Set<number>()
-    }
-
-    insert(value: number): void {
-        if (!this.indexSet.has(value)) {
-            this.indexSet.add(value)
-            this.heap.push(value)
-            this.heapifyUp()
-        }
-    }
-
-    remove(value: number): void {
-        if (this.indexSet.has(value)) {
-            this.indexSet.delete(value)
-            this.heap = this.heap.filter((v: number): boolean => this.indexSet.has(v))
-            this.heapifyDown()
-        }
-    }
-
-    top(): number {
-        while (this.heap.length > 0 && !this.indexSet.has(this.heap[0])) {
-            this.heap.shift()
-        }
-        return this.heap.length > 0 ? this.heap[0] : -1
-    }
-
-    isEmpty(): boolean {
-        return this.heap.length === 0 || this.indexSet.size === 0
-    }
-
-    private heapifyUp(): void {
-        this.heap.sort((a: number, b: number): number => a - b)
-    }
-
-    private heapifyDown(): void {
-        this.heap.sort((a: number, b: number): number => a - b)
-    }
-}
-
