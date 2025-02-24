898106211cc1a95801cc5e423e2fddf268231e6c
Yaroslav Brahinets <y.brahinets@gmail.com>
2025-02-24 16:26:42 +0200

    #2467 Simplify and optimize solution

--- solutions/2467. Most Profitable Path in a Tree/script.ts
+++ solutions/2467. Most Profitable Path in a Tree/script.ts
@@ -1,38 +1,38 @@
-import {UndirectedUnweightedGraph} from "../../common/Graph"
 import {arrayOf} from "../../common/array-factories"
 
 export {mostProfitablePath}
 
 function mostProfitablePath(edges: number[][], bob: number, amount: number[]): number {
-    const graph: UndirectedUnweightedGraph = new UndirectedUnweightedGraph()
+    const tree: number[][] = arrayOf([], amount.length)
     const bobPath: Map<number, number> = new Map<number, number>()
     const visited: boolean[] = arrayOf(false, amount.length)
     let maxIncome: number = Number.MIN_SAFE_INTEGER
-    const queue: [number, number, number][] = [[0, 0, 0]]
-
+    const queue: [number, number, number][] = [[0, 0, 0]];
     for (const [u, v] of edges) {
-        graph.addEdge(u, v)
+        tree[u].push(v)
+        tree[v].push(u)
     }
 
-    findBobPath(graph, bob, 0, bobPath, visited)
+    findBobPath(tree, bob, 0, bobPath, visited)
 
     visited.fill(false)
 
-    while (queue.length > 0) {
+    while (queue.length) {
         const [node, time, income]: [number, number, number] = queue.shift()!
         let newIncome: number = income
+        const bobTime: number | undefined = bobPath.get(node)
 
-        if (!bobPath.has(node) || time < bobPath.get(node)!) {
-            newIncome += amount[node]
-        } else if (time === bobPath.get(node)!) {
-            newIncome += Math.floor(amount[node] / 2)
+        if (bobTime === undefined || time < bobTime) {
+            newIncome += amount[node];
+        } else if (time === bobTime) {
+            newIncome += Math.floor(amount[node] / 2);
         }
 
-        if (graph.getNeighbours(node).size === 1 && node !== 0) {
+        if (tree[node].length === 1 && node !== 0) {
             maxIncome = Math.max(maxIncome, newIncome)
         }
 
-        for (const neighbor of graph.getNeighbours(node).keys()) {
+        for (const neighbor of tree[node]) {
             if (!visited[neighbor]) {
                 queue.push([neighbor, time + 1, newIncome])
             }
@@ -45,7 +45,7 @@ function mostProfitablePath(edges: number[][], bob: number, amount: number[]): n
 }
 
 function findBobPath(
-    graph: UndirectedUnweightedGraph,
+    tree: number[][],
     node: number,
     time: number,
     bobPath: Map<number, number>,
@@ -58,8 +58,8 @@ function findBobPath(
         return true
     }
 
-    for (const neighbor of graph.getNeighbours(node).keys()) {
-        if (!visited[neighbor] && findBobPath(graph, neighbor, time + 1, bobPath, visited)) {
+    for (const neighbor of tree[node]) {
+        if (!visited[neighbor] && findBobPath(tree, neighbor, time + 1, bobPath, visited)) {
             return true
         }
     }
