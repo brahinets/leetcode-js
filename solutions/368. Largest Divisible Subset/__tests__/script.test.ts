802a2d341b6d98e0331f09d8a773d5c7d3c62791
Yaroslav Brahinets <y.brahinets@gmail.com>
2025-04-06 16:15:39 +0300

    #368 Improve test case naming

--- solutions/368. Largest Divisible Subset/__tests__/script.test.ts
+++ solutions/368. Largest Divisible Subset/__tests__/script.test.ts
@@ -1,13 +1,13 @@
 import {largestDivisibleSubset} from '../script'
 
 describe('368. Largest Divisible Subset', (): void => {
-    it('One', (): void => {
-        expect(largestDivisibleSubset( [1,2,3]))
-            .toEqual([1,2])
+    it('Subset', (): void => {
+        expect(largestDivisibleSubset([1, 2, 3]))
+            .toEqual([1, 2])
     })
 
-    it('Two', (): void => {
-        expect(largestDivisibleSubset([1,2,4,8]))
+    it('Full set', (): void => {
+        expect(largestDivisibleSubset([1, 2, 4, 8]))
             .toEqual([1, 2, 4, 8])
     })
 })
