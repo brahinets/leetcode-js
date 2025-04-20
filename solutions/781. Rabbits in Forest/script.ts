04962de7ee13ce8fe5af78b204bf379066093deb
Yaroslav Brahinets <y.brahinets@gmail.com>
2025-04-20 18:57:42 +0300

    #781 Reuse code

--- solutions/781. Rabbits in Forest/script.ts
+++ solutions/781. Rabbits in Forest/script.ts
@@ -1,16 +1,12 @@
+import {count} from "../../common/array-utils"
+
 export {numRabbits}
 
 function numRabbits(answers: number[]): number {
-    const map: Map<number, number> = new Map<number, number>()
-    for (const answer of answers) {
-        if (!map.has(answer)) {
-            map.set(answer, 0)
-        }
-        map.set(answer, map.get(answer)! + 1)
-    }
+    const counts: Map<number, number> = count(answers)
 
     let result: number = 0
-    for (const [key, value] of map.entries()) {
+    for (const [key, value] of counts.entries()) {
         result += Math.ceil(value / (key + 1)) * (key + 1)
     }
 
