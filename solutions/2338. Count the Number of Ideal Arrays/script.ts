247102762289e4927769a12a17f75e20705e3159
Yaroslav Brahinets <y.brahinets@gmail.com>
2025-04-22 09:31:05 +0300

    #2338 Optimize

--- solutions/2338. Count the Number of Ideal Arrays/script.ts
+++ solutions/2338. Count the Number of Ideal Arrays/script.ts
@@ -27,37 +27,43 @@ function buildCombinations(n: number): number[][] {
 }
 
 function buildDivisibleSequences(n: number, maxValue: number): number[][] {
-    const sequencesOfLength: number[][] = matrixOfZeros(maxValue + 1, n + 1)
+    const sequences: number[][] = matrixOfZeros(maxValue + 1, n + 1)
 
     for (let value: number = 1; value <= maxValue; value++) {
-        sequencesOfLength[value][1] = 1
+        sequences[value][1] = 1
     }
 
-    for (let length = 2; length <= n; length++) {
+    for (let length: number = 2; length <= n; length++) {
         for (let previous: number = 1; previous <= maxValue; previous++) {
-            const count: number = sequencesOfLength[previous][length - 1]
+            const count: number = sequences[previous][length - 1]
 
-            if (!count) {
+            if (count === 0) {
                 continue
             }
 
-            for (let next: number = previous * 2; next <= maxValue; next += previous) {
-                sequencesOfLength[next][length] = (sequencesOfLength[next][length] + count) % mod
+            for (let multiple: number = previous * 2; multiple <= maxValue; multiple += previous) {
+                sequences[multiple][length] = (sequences[multiple][length] + count) % mod
             }
         }
     }
 
-    return sequencesOfLength
+    return sequences
 }
 
-function calculateTotal(dp: number[][], combinations: number[][], n: number): number {
+function calculateTotal(sequences: number[][], combinations: number[][], n: number): number {
     let total: number = 0
 
-    for (let value: number = 1; value < dp.length; value++) {
+    for (let value: number = 1; value < sequences.length; value++) {
         for (let length: number = 1; length <= n; length++) {
-            const count: number = dp[value][length]
+            const count: number = sequences[value][length]
 
-            total = (total + count * combinations[n - 1][length - 1]) % mod
+            if (count === 0) {
+                continue
+            }
+
+            const waysToPlace: number = combinations[n - 1][length - 1]
+
+            total = (total + count * waysToPlace) % mod
         }
     }
 
