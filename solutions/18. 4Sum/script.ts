d0b3d864c99b72de4cd39070115170b91954b507
Yaroslav Brahinets <y.brahinets@gmail.com>
2025-04-29 10:40:27 +0300

    #18 Decrease cognitive load

--- solutions/18. 4Sum/script.ts
+++ solutions/18. 4Sum/script.ts
@@ -2,7 +2,6 @@ export {fourSum}
 
 function fourSum(nums: number[], target: number): number[][] {
     const result: number[][] = []
-
     const sortedNums: number[] = nums.sort((a: number, b: number): number => a - b)
 
     for (let i: number = 0; i < sortedNums.length - 3; i++) {
@@ -15,32 +14,48 @@ function fourSum(nums: number[], target: number): number[][] {
                 continue
             }
 
-            let left: number = j + 1
-            let right: number = sortedNums.length - 1
-
-            while (left < right) {
-                const sum: number = sortedNums[i] + sortedNums[j] + sortedNums[left] + sortedNums[right]
-
-                if (sum === target) {
-                    result.push([sortedNums[i], sortedNums[j], sortedNums[left], sortedNums[right]])
-
-                    while (left < right && sortedNums[left] === sortedNums[left + 1]) {
-                        left++
-                    }
-                    while (left < right && sortedNums[right] === sortedNums[right - 1]) {
-                        right--
-                    }
-
-                    left++
-                    right--
-                } else if (sum < target) {
-                    left++
-                } else {
-                    right--
-                }
-            }
+            result.push(...findTwoSum(sortedNums, i, j, target))
         }
     }
 
     return result
+}
+
+function findTwoSum(nums: number[], i: number, j: number, target: number): number[][] {
+    let result: number[][] = []
+    let left: number = j + 1
+    let right: number = nums.length - 1
+
+    while (left < right) {
+        const sum: number = nums[i] + nums[j] + nums[left] + nums[right]
+
+        if (sum === target) {
+            result.push([nums[i], nums[j], nums[left], nums[right]])
+
+            left = skipDuplicates(nums, left, right, true)
+            right = skipDuplicates(nums, left, right, false)
+        } else if (sum < target) {
+            left++
+        } else {
+            right--
+        }
+    }
+
+    return result
+}
+
+function skipDuplicates(nums: number[], left: number, right: number, isLeft: boolean): number {
+    if (isLeft) {
+        while (left < right && nums[left] === nums[left + 1]) {
+            left++
+        }
+
+        return left + 1
+    } else {
+        while (left < right && nums[right] === nums[right - 1]) {
+            right--
+        }
+
+        return right - 1
+    }
 }
