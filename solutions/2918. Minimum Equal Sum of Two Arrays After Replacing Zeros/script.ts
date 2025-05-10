283c4b21b3d89029520d1b03372dd4994457b4b0
Yaroslav Brahinets <y.brahinets@gmail.com>
2025-05-10 08:16:34 +0300

    #2918 Improve type safety

--- solutions/2918. Minimum Equal Sum of Two Arrays After Replacing Zeros/script.ts
+++ solutions/2918. Minimum Equal Sum of Two Arrays After Replacing Zeros/script.ts
@@ -1,11 +1,11 @@
 export {minSum}
 
 function minSum(nums1: number[], nums2: number[]): number {
-    const zerosInFirst = nums1.filter((i: number): boolean => i === 0).length
-    const zerosInSecond = nums2.filter((i: number): boolean => i === 0).length
+    const zerosInFirst: number = nums1.filter((i: number): boolean => i === 0).length
+    const zerosInSecond: number = nums2.filter((i: number): boolean => i === 0).length
 
-    const sumOfFirst = nums1.reduce((sum: number, val: number): number => sum + val, 0) + zerosInFirst
-    const sumOfSecond = nums2.reduce((sum: number, val: number): number => sum + val, 0) + zerosInSecond
+    const sumOfFirst: number = nums1.reduce((sum: number, val: number): number => sum + val, 0) + zerosInFirst
+    const sumOfSecond: number = nums2.reduce((sum: number, val: number): number => sum + val, 0) + zerosInSecond
 
     if ((zerosInFirst === 0 && sumOfSecond > sumOfFirst) || (zerosInSecond === 0 && sumOfFirst > sumOfSecond)) {
         return -1
