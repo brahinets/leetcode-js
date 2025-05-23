ee78f807b1f55db057c60725a3694350141176c7
Yaroslav Brahinets <y.brahinets@gmail.com>
2025-05-23 14:49:51 +0300

    #3068 Improve type-safety

--- solutions/3068. Find the Maximum Sum of Node Values/script.ts
+++ solutions/3068. Find the Maximum Sum of Node Values/script.ts
@@ -4,7 +4,7 @@ function maximumValueSum(nums: number[], k: number, _: number[][]): number {
     const addedValues: number[] = collectAddedValues(nums, k)
 
     let sum: number = nums.reduce((sum: number, val: number): number => sum + val, 0)
-    for (let i = 0; i < Math.floor(nums.length / 2); i++) {
+    for (let i: number = 0; i < Math.floor(nums.length / 2); i++) {
         const pair: number = addedValues[i * 2] + addedValues[i * 2 + 1]
 
         if (pair > 0) {
@@ -18,8 +18,8 @@ function maximumValueSum(nums: number[], k: number, _: number[][]): number {
 function collectAddedValues(nums: number[], k: number): number[] {
     const addedValue: number[] = []
 
-    for (let i = 0; i < nums.length; i++) {
-        addedValue.push((nums[i] ^ k) - nums[i])
+    for (const num of nums) {
+        addedValue.push((num ^ k) - num)
     }
 
     addedValue.sort((a: number, b: number): number => b - a)
