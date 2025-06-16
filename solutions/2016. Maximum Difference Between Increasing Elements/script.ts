7fd50f8f3f4f93dad2a0ceda50b7ebeb6e22ec1d
Yaroslav Brahinets <y.brahinets@gmail.com>
2025-06-16 10:56:25 +0300

    #2016 Use closes neighbours when two same diffs exist

--- solutions/2016. Maximum Difference Between Increasing Elements/script.ts
+++ solutions/2016. Maximum Difference Between Increasing Elements/script.ts
@@ -5,7 +5,7 @@ function maximumDifference(nums: number[]): number {
     let minElement: number = nums[0]
 
     for (let i: number = 1; i < nums.length; i++) {
-        if (nums[i] < minElement) {
+        if (nums[i] <= minElement) {
             minElement = nums[i]
         } else {
             maxDiff = Math.max(maxDiff, nums[i] - minElement)
