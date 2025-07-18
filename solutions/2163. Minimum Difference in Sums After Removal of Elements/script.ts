e2d5a3e84ef0c8b7c40f0294dd97485decd4bf68
Yaroslav Brahinets <y.brahinets@gmail.com>
2025-07-18 11:39:43 +0300

    #2163 Improve code readability

--- solutions/2163. Minimum Difference in Sums After Removal of Elements/script.ts
+++ solutions/2163. Minimum Difference in Sums After Removal of Elements/script.ts
@@ -1,15 +1,27 @@
 import {PriorityQueue} from '../../common/PriorityQueue'
+import {arrayOfZeros} from "../../common/array-factories"
 
 export {minimumDifference}
 
 function minimumDifference(nums: number[]): number {
-    const totalLength: number = nums.length
-    const partitionSize: number = Math.floor(totalLength / 3)
+    const partitionSize = Math.floor(nums.length / 3)
 
-    const leftSums: number[] = new Array(partitionSize + 1).fill(0)
+    const leftSums = calculateLeftSums(nums, partitionSize)
+    const rightSums = calculateRightSums(nums, partitionSize)
+
+    let minDifference: number = Infinity
+    for (let i: number = 0; i <= partitionSize; i++) {
+        minDifference = Math.min(minDifference, leftSums[i] - rightSums[i])
+    }
+
+    return minDifference
+}
+
+function calculateLeftSums(nums: number[], partitionSize: number): number[] {
+    const leftSums: number[] = arrayOfZeros(partitionSize + 1)
+
     let leftSum: number = 0
-
-    const maxHeap: PriorityQueue<number> = new PriorityQueue<number>((a: number, b: number): number => b - a)
+    const maxHeap = new PriorityQueue<number>((a: number, b: number): number => b - a)
     for (let i: number = 0; i < partitionSize; i++) {
         leftSum += nums[i]
         maxHeap.enqueue(nums[i])
@@ -23,20 +35,26 @@ function minimumDifference(nums: number[]): number {
         leftSums[i - (partitionSize - 1)] = leftSum
     }
 
+    return leftSums
+}
+
+function calculateRightSums(nums: number[], partitionSize: number): number[] {
+    const rightSums: number[] = arrayOfZeros(partitionSize + 1)
+
     let rightSum: number = 0
-    const minHeap: PriorityQueue<number> = new PriorityQueue<number>((a: number, b: number): number => a - b)
-    for (let i: number = totalLength - 1; i >= partitionSize * 2; i--) {
+    const minHeap = new PriorityQueue<number>((a: number, b: number): number => a - b)
+    for (let i = nums.length - 1; i >= partitionSize * 2; i--) {
         rightSum += nums[i]
         minHeap.enqueue(nums[i])
     }
 
-    let minDifference: number = leftSums[partitionSize] - rightSum
+    rightSums[partitionSize] = rightSum
     for (let i: number = partitionSize * 2 - 1; i >= partitionSize; i--) {
         rightSum += nums[i]
         minHeap.enqueue(nums[i])
         rightSum -= minHeap.dequeue()!
-        minDifference = Math.min(minDifference, leftSums[i - partitionSize] - rightSum)
+        rightSums[i - partitionSize] = rightSum
     }
 
-    return minDifference
+    return rightSums
 }
