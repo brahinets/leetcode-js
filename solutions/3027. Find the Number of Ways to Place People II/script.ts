9d7cfc52ff4a61276d86927b947e18cdb58c1ac5
Yaroslav Brahinets <y.brahinets@gmail.com>
2025-09-03 21:20:04 +0300

    #3027 Optimize

--- solutions/3027. Find the Number of Ways to Place People II/script.ts
+++ solutions/3027. Find the Number of Ways to Place People II/script.ts
@@ -1,54 +1,25 @@
 export {numberOfPairs}
 
 function numberOfPairs(points: number[][]): number {
-    let validPairCount: number = 0
+    points.sort((a: number[], b: number[]): number => a[0] - b[0] || b[1] - a[1])
 
-    for (let i: number = 0; i < points.length; i++) {
-        for (let j: number = 0; j < points.length; j++) {
-            if (i === j) {
-                continue
-            }
+    let result: number = 0
+    for (let i: number = 0; i < points.length - 1; i++) {
+        const pointA: number[] = points[i]
+        const xRange: number[] = [pointA[0] - 1, Infinity]
+        const yRange: number[] = [-Infinity, pointA[1] + 1]
 
-            const upperLeft: [number, number] = [points[i][0], points[i][1]]
-            const lowerRight: [number, number] = [points[j][0], points[j][1]]
+        for (let j = i + 1; j < points.length; j++) {
+            const pointB = points[j]
 
-            if (isValidUpperLeftToLowerRightPair(upperLeft, lowerRight)) {
-                if (hasNoPointsInRectangle(points, upperLeft, lowerRight, i, j)) {
-                    validPairCount++
-                }
+            if (pointB[0] > xRange[0] && pointB[0] < xRange[1] && pointB[1] > yRange[0] && pointB[1] < yRange[1]) {
+                result++
+
+                xRange[0] = pointB[0]
+                yRange[0] = pointB[1]
             }
         }
     }
 
-    return validPairCount
-}
-
-function isValidUpperLeftToLowerRightPair(upperLeft: [number, number], lowerRight: [number, number]): boolean {
-    return upperLeft[0] <= lowerRight[0] && upperLeft[1] >= lowerRight[1]
-}
-
-function isPointInRectangle(currentPoint: [number, number], upperLeft: [number, number], lowerRight: [number, number]): boolean {
-    const [x, y] = currentPoint
-    const [leftX, topY] = upperLeft
-    const [rightX, bottomY] = lowerRight
-
-    const isWithinXBounds: boolean = x >= leftX && x <= rightX
-    const isWithinYBounds: boolean = y <= topY && y >= bottomY
-
-    return isWithinXBounds && isWithinYBounds
-}
-
-function hasNoPointsInRectangle(points: number[][], upperLeft: [number, number], lowerRight: [number, number], i: number, j: number): boolean {
-    for (let k: number = 0; k < points.length; k++) {
-        if (k === i || k === j) {
-            continue
-        }
-
-        const currentPoint: [number, number] = [points[k][0], points[k][1]]
-        if (isPointInRectangle(currentPoint, upperLeft, lowerRight)) {
-            return false
-        }
-    }
-
-    return true
+    return result
 }
