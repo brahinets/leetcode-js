e3f2d5ec21957733b7cdef64663d502973c5d1ee
Yaroslav Brahinets <y.brahinets@gmail.com>
2025-09-17 12:56:27 +0300

    #2353 Optimize using priority queue

--- solutions/2353. Design a Food Rating System/script.ts
+++ solutions/2353. Design a Food Rating System/script.ts
@@ -1,9 +1,16 @@
+import {PriorityQueue} from "../../common/PriorityQueue"
+
 export {FoodRatings}
+
+interface FoodInfo {
+    name: string
+    rating: number
+}
 
 class FoodRatings {
     private foodToCuisine: Map<string, string> = new Map<string, string>()
     private foodToRating: Map<string, number> = new Map<string, number>()
-    private cuisineFoods: Map<string, Set<string>> = new Map<string, Set<string>>()
+    private cuisineToFoods: Map<string, PriorityQueue<FoodInfo>> = new Map<string, PriorityQueue<FoodInfo>>()
 
     constructor(foods: string[], cuisines: string[], ratings: number[]) {
         for (let i: number = 0; i < foods.length; i++) {
@@ -12,33 +19,43 @@ class FoodRatings {
         }
 
         for (let i: number = 0; i < foods.length; i++) {
-            let cuisine: string = cuisines[i]
+            const cuisine: string = cuisines[i]
 
-            if (!this.cuisineFoods.has(cuisine)) {
-                this.cuisineFoods.set(cuisine, new Set<string>())
+            if (!this.cuisineToFoods.has(cuisine)) {
+                this.cuisineToFoods.set(
+                    cuisine,
+                    new PriorityQueue<FoodInfo>((a: FoodInfo, b: FoodInfo): number => {
+                        if (a.rating !== b.rating) return b.rating - a.rating
+                        return a.name.localeCompare(b.name)
+                    })
+                )
             }
 
-            this.cuisineFoods.get(cuisine)!.add(foods[i])
+            this.cuisineToFoods.get(cuisine)!.enqueue({name: foods[i], rating: ratings[i]})
         }
     }
 
     changeRating(food: string, newRating: number): void {
         this.foodToRating.set(food, newRating)
+
+        const cuisine: string = this.foodToCuisine.get(food)!
+
+        this.cuisineToFoods.get(cuisine)!.enqueue({name: food, rating: newRating})
     }
 
     highestRated(cuisine: string): string {
-        let bestFood: string = ""
-        let bestRating: number = -1
-
-        for (let food of this.cuisineFoods.get(cuisine)!) {
-            let rating: number = this.foodToRating.get(food)!
-
-            if (rating > bestRating || (rating === bestRating && food < bestFood)) {
-                bestRating = rating
-                bestFood = food
-            }
+        const pq: PriorityQueue<FoodInfo> | undefined = this.cuisineToFoods.get(cuisine)
+        if (!pq) {
+            throw new Error('There is no food with this cuisine')
         }
 
-        return bestFood
+        while (true) {
+            const top: FoodInfo = pq.peek()!
+            if (this.foodToRating.get(top.name) === top.rating) {
+                return top.name
+            }
+
+            pq.dequeue()
+        }
     }
 }
