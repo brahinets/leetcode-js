36c596fcab3d6f4f6417490f567dec5fc6d6cca8
Yaroslav Brahinets <y.brahinets@gmail.com>
2025-09-20 19:43:14 +0300

    #3508 Optimize using queue

--- solutions/3508. Implement Router/script.ts
+++ solutions/3508. Implement Router/script.ts
@@ -1,3 +1,5 @@
+import {PriorityQueue} from "../../common/PriorityQueue"
+
 export {Router}
 
 interface Packet {
@@ -6,15 +8,24 @@ interface Packet {
     timestamp: number
 }
 
+interface Timestamps {
+    startIndex: number
+    timestamps: number[]
+}
+
 class Router {
+    private static readonly THERE_ARE_NO_PACKETS: number[] = []
+    private static readonly PACKETS_NOT_FOUND_FOR_GIVEN_TIMESTAMP_RANGE: number = 0
     private readonly memoryLimit: number
-    private readonly packets: Packet[]
+    private readonly packets: PriorityQueue<Packet>
     private readonly packetSet: Set<string>
+    private readonly destinationToTimestamps: Map<number, Timestamps>
 
     constructor(memoryLimit: number) {
         this.memoryLimit = memoryLimit
-        this.packets = []
+        this.packets = new PriorityQueue<Packet>((a: Packet, b: Packet): number => a.timestamp - b.timestamp)
         this.packetSet = new Set<string>()
+        this.destinationToTimestamps = new Map<number, Timestamps>()
     }
 
     private static packetKey(source: number, destination: number, timestamp: number): string {
@@ -22,27 +33,37 @@ class Router {
     }
 
     addPacket(source: number, destination: number, timestamp: number): boolean {
-        const key: string = Router.packetKey(source, destination, timestamp)
+        const packet: Packet = {source, destination, timestamp}
+        const key: string = Router.packetKey(packet.source, packet.destination, packet.timestamp)
+
         if (this.packetSet.has(key)) {
             return false
         }
 
-        if (this.packets.length >= this.memoryLimit) {
-            this.forwardPacket()
+        if (this.packets.size() === this.memoryLimit) {
+            const toRemove = this.packets.dequeue()!
+            const toRemoveAsString = Router.packetKey(toRemove.source, toRemove.destination, toRemove.timestamp)
+            this.packetSet.delete(toRemoveAsString)
         }
 
-        this.packets.push({source, destination, timestamp})
+        this.packets.enqueue(packet)
         this.packetSet.add(key)
+
+        if (!this.destinationToTimestamps.has(destination)) {
+            this.destinationToTimestamps.set(destination, {startIndex: 0, timestamps: []})
+        }
+
+        this.destinationToTimestamps.get(destination)!.timestamps.push(timestamp)
 
         return true
     }
 
     forwardPacket(): number[] {
-        if (this.packets.length === 0) {
-            return []
+        if (this.packets.isEmpty()) {
+            return Router.THERE_ARE_NO_PACKETS
         }
 
-        const packet: Packet = this.packets.shift()!
+        const packet: Packet = this.packets.dequeue()!
         const key: string = Router.packetKey(packet.source, packet.destination, packet.timestamp)
 
         this.packetSet.delete(key)
@@ -51,14 +72,26 @@ class Router {
     }
 
     getCount(destination: number, startTime: number, endTime: number): number {
-        let count: number = 0
-
-        for (const packet of this.packets) {
-            if (packet.destination === destination && packet.timestamp >= startTime && packet.timestamp <= endTime) {
-                count++
-            }
+        if (!this.destinationToTimestamps.has(destination)) {
+            return Router.PACKETS_NOT_FOUND_FOR_GIVEN_TIMESTAMP_RANGE
         }
 
-        return count
+        const timestamps: number[] = this.destinationToTimestamps.get(destination)!.timestamps
+        let startIndex: number = this.destinationToTimestamps.get(destination)!.startIndex
+        let endIndex: number = timestamps.length - 1
+
+        if (startIndex > endIndex || startIndex === timestamps.length || timestamps[startIndex] > endTime || timestamps[endIndex] < startTime) {
+            return Router.PACKETS_NOT_FOUND_FOR_GIVEN_TIMESTAMP_RANGE
+        }
+
+        while (startIndex < endIndex && timestamps[startIndex] < startTime) {
+            ++startIndex
+        }
+
+        while (endIndex > 0 && timestamps[endIndex] > endTime) {
+            --endIndex
+        }
+
+        return endIndex - startIndex + 1
     }
 }
