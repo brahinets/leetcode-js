cdfb1224840eab54d7086860d6f98f5708f99292
Yaroslav Brahinets <y.brahinets@gmail.com>
2025-09-26 20:55:04 +0300

    #611 Optimize

--- solutions/611. Valid Triangle Number/script.ts
+++ solutions/611. Valid Triangle Number/script.ts
@@ -1,15 +1,19 @@
 export {triangleNumber}
 
 function triangleNumber(nums: number[]): number {
+    nums.sort((a: number, b: number): number => a - b)
+
     let count: number = 0
 
     for (let i: number = 0; i < nums.length - 2; i++) {
-        for (let j: number = i + 1; j < nums.length - 1; j++) {
-            for (let k: number = j + 1; k < nums.length; k++) {
-                if (nums[i] + nums[j] > nums[k] && nums[i] + nums[k] > nums[j] && nums[j] + nums[k] > nums[i]) {
-                    count++
-                }
+        let k: number = i + 2
+
+        for (let j: number = i + 1; j < nums.length - 1 && nums[i] != 0; j++) {
+            while (k < nums.length && nums[i] + nums[j] > nums[k]) {
+                k++
             }
+
+            count += k - j - 1
         }
     }
 
