b345b7d40f724d06cdf1e3bdec1d8de74d6009a6
Yaroslav Brahinets <y.brahinets@gmail.com>
2025-09-29 11:43:08 +0300

    #1039 Improve test case naming

--- solutions/1039. Minimum Score Triangulation of Polygon/__tests__/script.test.ts
+++ solutions/1039. Minimum Score Triangulation of Polygon/__tests__/script.test.ts
@@ -1,17 +1,17 @@
 import {minScoreTriangulation} from '../script'
 
 describe('1039. Minimum Score Triangulation of Polygon', (): void => {
-    it('One', (): void => {
+    it('The polygon is already triangulated', (): void => {
         expect(minScoreTriangulation([1, 2, 3]))
             .toBe(6)
     })
 
-    it('Two', (): void => {
+    it('There are two triangulations', (): void => {
         expect(minScoreTriangulation([3, 7, 4, 5]))
             .toBe(144)
     })
 
-    it('Three', (): void => {
+    it('There are many triangulations', (): void => {
         expect(minScoreTriangulation([1, 3, 1, 4, 1, 5]))
             .toBe(13)
     })
