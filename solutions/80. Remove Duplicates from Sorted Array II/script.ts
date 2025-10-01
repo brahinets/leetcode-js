203de8bebebe51371ca62188a738236e0a0faf68
Yaroslav Brahinets <y.brahinets@gmail.com>
2025-10-01 13:44:12 +0300

    #80 Improve readability

--- solutions/80. Remove Duplicates from Sorted Array II/script.ts
+++ solutions/80. Remove Duplicates from Sorted Array II/script.ts
@@ -1,13 +1,13 @@
 export {removeDuplicates}
 
 function removeDuplicates(nums: number[]): number {
-    let j: number = 2
+    let numsLeft: number = 2
 
     for (let i: number = 2; i < nums.length; i++) {
-        if (nums[i] !== nums[j - 2]) {
-            nums[j++] = nums[i]
+        if (nums[i] !== nums[numsLeft - 2]) {
+            nums[numsLeft++] = nums[i]
         }
     }
 
-    return j
+    return numsLeft
 }
