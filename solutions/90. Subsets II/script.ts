1305f4c2279f6dd333a8e8b8bbb7ab82fdbfeb7a
Yaroslav Brahinets <y.brahinets@gmail.com>
2025-11-03 09:55:46 +0200

    #90 Avoid inner functions

--- solutions/90. Subsets II/script.ts
+++ solutions/90. Subsets II/script.ts
@@ -4,25 +4,29 @@ function subsetsWithDup(nums: number[]): number[][] {
     nums.sort((a: number, b: number): number => a - b)
 
     const result: number[][] = []
-    const subset: number[] = []
 
-    function backtrack(start: number): void {
-        result.push([...subset])
-
-        for (let i: number = start; i < nums.length; i++) {
-            if (i > start && nums[i] === nums[i - 1]) {
-                continue
-
-            }
-            subset.push(nums[i])
-
-            backtrack(i + 1)
-
-            subset.pop()
-        }
-    }
-
-    backtrack(0)
+    generateSubsets(nums, 0, [], result)
 
     return result
+}
+
+function generateSubsets(
+    nums: number[],
+    startIndex: number,
+    currentSubset: number[],
+    result: number[][]
+): void {
+    result.push([...currentSubset])
+
+    for (let i: number = startIndex; i < nums.length; i++) {
+        if (i > startIndex && nums[i] === nums[i - 1]) {
+            continue
+        }
+
+        currentSubset.push(nums[i])
+
+        generateSubsets(nums, i + 1, currentSubset, result)
+
+        currentSubset.pop()
+    }
 }
