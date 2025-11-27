ff0abe6074dc181541157c24f7c06cfa02ffc8de
Yaroslav Brahinets <y.brahinets@gmail.com>
2025-11-27 15:46:20 +0200

    #3381 Optimize solution

--- solutions/3381. Maximum Subarray Sum With Length Divisible by K/script.ts
+++ solutions/3381. Maximum Subarray Sum With Length Divisible by K/script.ts
@@ -1,20 +1,21 @@
-import {arrayOfZeros} from "../../common/array-factories"
+import {arrayOf} from "../../common/array-factories"
 
 export {maxSubarraySum}
 
 function maxSubarraySum(nums: number[], k: number): number {
-    const prefixSums: number[] = arrayOfZeros(nums.length + 1)
-    for (let i: number = 0; i < nums.length; i++) {
-        prefixSums[i + 1] = prefixSums[i] + nums[i]
+    const length: number = nums.length
+    let currentPrefixSum: number = 0
+    let maximumSubarraySum: number = -Number.MAX_SAFE_INTEGER
+    const minPrefixSumByRemainder: number[] = arrayOf(Number.MAX_SAFE_INTEGER / 2, k)
+    minPrefixSumByRemainder[k - 1] = 0
+
+    for (let index: number = 0; index < length; index++) {
+        currentPrefixSum += nums[index]
+
+        const remainder: number = index % k
+        maximumSubarraySum = Math.max(maximumSubarraySum, currentPrefixSum - minPrefixSumByRemainder[remainder])
+        minPrefixSumByRemainder[remainder] = Math.min(minPrefixSumByRemainder[remainder], currentPrefixSum)
     }
 
-    let maxSum: number = -Infinity
-    for (let start: number = 0; start < nums.length; start++) {
-        for (let end: number = start + k; end <= nums.length; end += k) {
-            const currentSum: number = prefixSums[end] - prefixSums[start]
-            maxSum = Math.max(maxSum, currentSum)
-        }
-    }
-
-    return maxSum
+    return maximumSubarraySum
 }
