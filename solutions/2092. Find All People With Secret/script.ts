1d385393fc85eee780729b5cb7c12e5e556774ac
Yaroslav Brahinets <y.brahinets@gmail.com>
2025-12-19 11:24:07 +0200

    #2092 Optimize

--- solutions/2092. Find All People With Secret/script.ts
+++ solutions/2092. Find All People With Secret/script.ts
@@ -1,51 +1,62 @@
 export {findAllPeople}
 
 function findAllPeople(n: number, meetings: number[][], firstPerson: number): number[] {
-    const graph: Graph = new Graph(meetings)
+    const timeMap: Map<number, [number, number][]> = groupMeetingsByTime(meetings)
+    const known: Set<number> = new Set<number>([0, firstPerson])
+    const times: number[] = Array.from(timeMap.keys()).sort((a: number, b: number): number => a - b)
 
-    const awareSince: Map<number, number> = new Map<number, number>()
-    awareSince.set(0, 0)
-    awareSince.set(firstPerson, 0)
+    for (const t of times) {
+        const edges: [number, number][] = timeMap.get(t)!
+        const group: Set<number> = spreadSecretAtTime(edges, known)
+        for (const person of group) {
+            known.add(person)
+        }
+    }
 
-    const leakQueue: number[][] = [[0, 0], [firstPerson, 0]]
-    while (leakQueue.length > 0) {
-        const [leaker, leakerSince] = leakQueue.pop()!
+    return Array.from(known).sort((a: number, b: number): number => a - b)
+}
 
-        for (const [coMeeter, coMeeterAt] of graph.getNeighbours(leaker)) {
-            if (coMeeterAt >= leakerSince && (awareSince.get(coMeeter) === undefined || awareSince.get(coMeeter)! > coMeeterAt)) {
-                awareSince.set(coMeeter, coMeeterAt)
-                leakQueue.push([coMeeter, coMeeterAt])
+function groupMeetingsByTime(meetings: number[][]): Map<number, [number, number][]> {
+    const timeMap: Map<number, [number, number][]> = new Map<number, [number, number][]>()
+    for (const [a, b, t] of meetings) {
+        if (!timeMap.has(t)) {
+            timeMap.set(t, [])
+        }
+
+        timeMap.get(t)!.push([a, b])
+    }
+
+    return timeMap
+}
+
+function spreadSecretAtTime(edges: [number, number][], known: Set<number>): Set<number> {
+    const group: Set<number> = new Set<number>()
+
+    for (const [a, b] of edges) {
+        if (known.has(a) || known.has(b)) {
+            group.add(a)
+            group.add(b)
+        }
+    }
+
+    let changed: boolean = true
+    while (changed) {
+        changed = false
+
+        for (const [a, b] of edges) {
+            if (group.has(a) || group.has(b)) {
+                if (!group.has(a)) {
+                    group.add(a)
+                    changed = true
+                }
+
+                if (!group.has(b)) {
+                    group.add(b)
+                    changed = true
+                }
             }
         }
     }
 
-    return [...awareSince.keys()]
-        .sort((a: number, b: number): number => a - b)
-}
-
-class Graph {
-    private readonly nodes: Map<number, number[][]>
-
-    constructor(edges: number[][]) {
-        this.nodes = new Map<number, number[][]>()
-
-        edges.forEach((edge: number[]): void => {
-            this.addEdge([edge[0], edge[1], edge[2]])
-            this.addEdge([edge[1], edge[0], edge[2]])
-        })
-    }
-
-    addEdge(edge: number[]): void {
-        const [from, to, distance] = edge
-
-        if (!this.nodes.has(from)) {
-            this.nodes.set(from, [])
-        }
-
-        this.nodes.get(from)!.push([to, distance])
-    }
-
-    getNeighbours(from: number): number[][] {
-        return this.nodes.get(from) ?? []
-    }
+    return group
 }
