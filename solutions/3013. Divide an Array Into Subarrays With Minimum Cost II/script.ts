225558e39eb065eb39fca802449a3cd8d0cf42e8
Yaroslav Brahinets <y.brahinets@gmail.com>
2026-02-02 10:46:23 +0200

    #3013 Optimize using priority queue

--- solutions/3013. Divide an Array Into Subarrays With Minimum Cost II/script.ts
+++ solutions/3013. Divide an Array Into Subarrays With Minimum Cost II/script.ts
@@ -1,99 +1,126 @@
 export { minimumCost }
 
+import { PriorityQueue } from '../../common/PriorityQueue'
+import { arrayOf } from '../../common/array-factories'
+
 type ValueWithIndex = [number, number]
 
 function minimumCost(nums: number[], k: number, dist: number): number {
     const n: number = nums.length
     const elementsToSelect: number = k - 1
 
-    const smallestElements: ValueWithIndex[] = []
-    const remainingElements: ValueWithIndex[] = []
+    const smallestMaxHeap: PriorityQueue<ValueWithIndex> = new PriorityQueue<ValueWithIndex>((a: ValueWithIndex, b: ValueWithIndex): number => b[0] - a[0] || b[1] - a[1])
+    const remainingMinHeap: PriorityQueue<ValueWithIndex> = new PriorityQueue<ValueWithIndex>((a: ValueWithIndex, b: ValueWithIndex): number => a[0] - b[0] || a[1] - b[1])
+
+    const inSmallest: boolean[] = arrayOf(false, n)
+    const removed: boolean[] = arrayOf(false, n)
+    let smallestSize: number = 0
+
     let smallestElementsSum: number = 0
 
     const initialWindowEnd: number = Math.min(1 + dist, n - 1)
     for (let i: number = 1; i <= initialWindowEnd; i++) {
-        const element: ValueWithIndex = [nums[i], i]
-
-        if (smallestElements.length < elementsToSelect) {
-            insertIntoSortedArray(smallestElements, element)
-            smallestElementsSum += element[0]
-        } else if (element[0] < smallestElements[smallestElements.length - 1][0]) {
-            const evictedElement: ValueWithIndex = smallestElements.pop()!
-
-            smallestElementsSum -= evictedElement[0]
-            insertIntoSortedArray(remainingElements, evictedElement)
-
-            insertIntoSortedArray(smallestElements, element)
-            smallestElementsSum += element[0]
-        } else {
-            insertIntoSortedArray(remainingElements, element)
-        }
+        addElementToWindow(nums[i], i)
     }
 
     let minimumSubarrayCost: number = smallestElementsSum
 
     for (let windowRight: number = 2 + dist; windowRight < n; windowRight++) {
         const exitingIndex: number = windowRight - dist - 1
-        const exitingValue: number = nums[exitingIndex]
 
-        if (removeByOriginalIndex(smallestElements, exitingIndex)) {
-            smallestElementsSum -= exitingValue
+        removeElementFromWindow(nums[exitingIndex], exitingIndex)
+        addElementToWindow(nums[windowRight], windowRight)
 
-            if (remainingElements.length > 0) {
-                const promotedElement: ValueWithIndex = remainingElements.shift()!
-
-                insertIntoSortedArray(smallestElements, promotedElement)
-
-                smallestElementsSum += promotedElement[0]
-            }
-        } else {
-            removeByOriginalIndex(remainingElements, exitingIndex)
-        }
-
-        const enteringElement: ValueWithIndex = [nums[windowRight], windowRight]
-        if (smallestElements.length < elementsToSelect) {
-            insertIntoSortedArray(smallestElements, enteringElement)
-
-            smallestElementsSum += enteringElement[0]
-        } else if (enteringElement[0] < smallestElements[smallestElements.length - 1][0]) {
-            const evictedElement: ValueWithIndex = smallestElements.pop()!
-
-            smallestElementsSum -= evictedElement[0]
-
-            insertIntoSortedArray(remainingElements, evictedElement)
-            insertIntoSortedArray(smallestElements, enteringElement)
-
-            smallestElementsSum += enteringElement[0]
-        } else {
-            insertIntoSortedArray(remainingElements, enteringElement)
-        }
-
-        if (smallestElements.length === elementsToSelect) {
+        if (smallestSize === elementsToSelect) {
             minimumSubarrayCost = Math.min(minimumSubarrayCost, smallestElementsSum)
         }
     }
 
     return nums[0] + minimumSubarrayCost
-}
 
-function insertIntoSortedArray(array: ValueWithIndex[], element: ValueWithIndex): void {
-    let position: number = 0
+    function addElementToWindow(value: number, index: number): void {
+        pruneSmallestHeap()
 
-    while (position < array.length && (array[position][0] < element[0] || (array[position][0] === element[0] && array[position][1] < element[1]))) {
-        position++
-    }
+        if (smallestSize < elementsToSelect) {
+            smallestMaxHeap.enqueue([value, index])
+            inSmallest[index] = true
+            smallestSize += 1
+            smallestElementsSum += value
+        } else {
+            const largestOfSmallest: ValueWithIndex = smallestMaxHeap.peek()!
 
-    array.splice(position, 0, element)
-}
+            if (value < largestOfSmallest[0]) {
+                smallestMaxHeap.dequeue()
+                if (inSmallest[largestOfSmallest[1]]) {
+                    inSmallest[largestOfSmallest[1]] = false
+                    smallestSize -= 1
+                }
+                smallestElementsSum -= largestOfSmallest[0]
+                remainingMinHeap.enqueue(largestOfSmallest)
 
-function removeByOriginalIndex(array: ValueWithIndex[], originalIndex: number): boolean {
-    for (let i: number = 0; i < array.length; i++) {
-        if (array[i][1] === originalIndex) {
-            array.splice(i, 1)
-
-            return true
+                smallestMaxHeap.enqueue([value, index])
+                inSmallest[index] = true
+                smallestSize += 1
+                smallestElementsSum += value
+            } else {
+                remainingMinHeap.enqueue([value, index])
+            }
         }
     }
 
-    return false
+    function removeElementFromWindow(value: number, index: number): void {
+        removed[index] = true
+
+        if (inSmallest[index]) {
+            inSmallest[index] = false
+            smallestSize -= 1
+            smallestElementsSum -= value
+        }
+
+        rebalanceHeaps()
+    }
+
+    function rebalanceHeaps(): void {
+        pruneSmallestHeap()
+        pruneRemainingHeap()
+
+        while (smallestSize < elementsToSelect && !remainingMinHeap.isEmpty()) {
+            pruneRemainingHeap()
+
+            if (remainingMinHeap.isEmpty()) {
+                break
+            }
+
+            const promoted: ValueWithIndex = remainingMinHeap.dequeue()!
+
+            smallestMaxHeap.enqueue(promoted)
+            inSmallest[promoted[1]] = true
+            smallestSize += 1
+            smallestElementsSum += promoted[0]
+        }
+    }
+
+    function pruneSmallestHeap(): void {
+        while (!smallestMaxHeap.isEmpty()) {
+            const top: ValueWithIndex = smallestMaxHeap.peek()!
+
+            if (removed[top[1]]) {
+                smallestMaxHeap.dequeue()
+            } else {
+                break
+            }
+        }
+    }
+
+    function pruneRemainingHeap(): void {
+        while (!remainingMinHeap.isEmpty()) {
+            const top: ValueWithIndex = remainingMinHeap.peek()!
+
+            if (removed[top[1]]) {
+                remainingMinHeap.dequeue()
+            } else {
+                break
+            }
+        }
+    }
 }
