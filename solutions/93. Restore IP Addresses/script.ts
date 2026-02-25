c5e623f50f8214d45030355bf8c3a45d7fd668dd
Yaroslav Brahinets <y.brahinets@gmail.com>
2026-02-26 00:16:47 +0200

    #93 Simplify solution

--- solutions/93. Restore IP Addresses/script.ts
+++ solutions/93. Restore IP Addresses/script.ts
@@ -1,37 +1,32 @@
-export {restoreIpAddresses}
+export { restoreIpAddresses }
 
 function restoreIpAddresses(s: string): string[] {
     const result: string[] = []
+    const n: number = s.length
 
-    backtrack(result, 0, [], s)
+    for (let i: number = 1; i <= 3 && i < n; i++) {
+        for (let j: number = i + 1; j <= i + 3 && j < n; j++) {
+            for (let k: number = j + 1; k <= j + 3 && k < n; k++) {
+                const s1: string = s.substring(0, i)
+                const s2: string = s.substring(i, j)
+                const s3: string = s.substring(j, k)
+                const s4: string = s.substring(k)
+
+                if (s4.length <= 3 &&
+                    isValidSegment(s1) &&
+                    isValidSegment(s2) &&
+                    isValidSegment(s3) &&
+                    isValidSegment(s4)
+                ) {
+                    result.push(`${s1}.${s2}.${s3}.${s4}`)
+                }
+            }
+        }
+    }
 
     return result
 }
 
-function backtrack(result: string[], startIndex: number, segments: string[], s: string): void {
-    if (segments.length === 4 && startIndex === s.length) {
-        result.push(segments.join('.'))
-        return
-    }
-
-    if (segments.length >= 4) {
-        return
-    }
-
-    for (let length: number = 1; length <= 3; length++) {
-        if (startIndex + length > s.length) {
-            break
-        }
-
-        const segment: string = s.substring(startIndex, startIndex + length)
-        if ((segment.startsWith('0') && segment.length > 1) || (length === 3 && Number(segment) > 255)) {
-            continue
-        }
-
-        segments.push(segment)
-
-        backtrack(result, startIndex + length, segments, s)
-
-        segments.pop()
-    }
+function isValidSegment(segment: string): boolean {
+    return !(segment.startsWith('0') && segment.length > 1) && Number(segment) <= 255
 }
