3327cf8fc77dce4e5e44c461be317b776daa2e1b
Yaroslav Brahinets <y.brahinets@gmail.com>
2026-03-17 12:41:08 +0200

    #176 Add SQL solution

---
+++ solutions/176. Second Highest Salary/solution.sql
@@ -0,0 +1,8 @@
+SELECT
+    COALESCE(
+        (SELECT DISTINCT salary
+         FROM Employee
+         ORDER BY salary DESC
+         LIMIT 1 OFFSET 1),
+        NULL
+    ) AS SecondHighestSalary;
