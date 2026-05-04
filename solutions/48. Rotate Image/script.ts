1d42c41f1e78a4a4041dfad2a2424cf345b12612
Yaroslav Brahinets <y.brahinets@gmail.com>
2026-05-04 12:00:47 +0200

    #48 Improve formatting

--- solutions/48. Rotate Image/script.ts
+++ solutions/48. Rotate Image/script.ts
@@ -1,4 +1,4 @@
-export {rotate}
+export { rotate }
 
 function rotate(matrix: number[][]): void {
     const n: number = matrix.length
@@ -6,6 +6,7 @@ function rotate(matrix: number[][]): void {
     for (let i: number = 0; i < n / 2; i++) {
         for (let j: number = i; j < n - i - 1; j++) {
             const temp: number = matrix[i][j]
+
             matrix[i][j] = matrix[n - j - 1][i]
             matrix[n - j - 1][i] = matrix[n - i - 1][n - j - 1]
             matrix[n - i - 1][n - j - 1] = matrix[j][n - i - 1]
